1d66ebdf364c914bfbc3ca66ac5a9f9a4bcdc77d
Mike Lewis <mikelikespie@gmail.com>
2012-10-10 13:07:10 -0700

    Compiler warning cleanup

--- SRWebSocketTests/SRTAutobahnTests.m
+++ SRWebSocketTests/SRTAutobahnTests.m
@@ -139,7 +139,7 @@
         return;
     }
     [aRun start];
-    for (int i = 1; i <= aRun.test.testCaseCount; i++) {
+    for (NSUInteger i = 1; i <= aRun.test.testCaseCount; i++) {
         SEL sel = @selector(performTestWithNumber:);
         NSInvocation *invocation = [NSInvocation invocationWithMethodSignature:[[self class] instanceMethodSignatureForSelector:sel]];
         
