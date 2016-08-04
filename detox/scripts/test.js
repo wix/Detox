b26912961a63cb1bd2e0ed18ecbcf8ef673dd26a
Daniel Zlotin <danielzl@wix.com>
2016-08-04 17:34:00 +0300

    babel fixed to use react-native-stage-0 to avoid react-native bugs

--- detox/scripts/test.js
+++ detox/scripts/test.js
@@ -6,7 +6,7 @@ shell.cd('test');
 
 shell.echo('\n#################################################################');
 shell.echo('# npm run e2e');
-if (shell.exec('npm run e2e').code !== 0) {
+if (shell.exec('npm run e2e', {stdio: ['pipe', 'pipe', 'pipe']}).code !== 0) {
   shell.echo('error: npm run e2e');
   process.exit(1);
 }
