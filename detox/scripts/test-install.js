932792eb56eab492ede629ce5eaecde643884672
talkol <talkol@wix.com>
2016-08-07 16:32:55 +0300

    fix weird permissions issue that happens only on my computer

--- detox/scripts/test-install.js
+++ detox/scripts/test-install.js
@@ -21,7 +21,7 @@ if (shell.exec('npm install').code !== 0) {
 shell.echo('\n#################################################################');
 if (!shell.exec('netstat -atp tcp | grep -i "listen" | grep ".8099"', {silent: true}).stdout) {
   shell.echo('# detox-server is not running, run it');
-  if (shell.exec('../node_modules/.bin/ttab -w ./node_modules/.bin/detox-server').code !== 0) {
+  if (shell.exec('../node_modules/.bin/ttab -w node ./node_modules/.bin/detox-server').code !== 0) {
     shell.echo('# error: cannot run detox-server in a new tab');
     process.exit(1);
   }
