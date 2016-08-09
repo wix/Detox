8487efaf390d3d267ac98fb8ae82bb40e4e88d4d
talkol <talkol@wix.com>
2016-08-09 20:53:28 +0300

    improve test script

--- detox/scripts/test-install.js
+++ detox/scripts/test-install.js
@@ -19,7 +19,7 @@ if (shell.exec('npm install').code !== 0) {
 }
 
 shell.echo('\n#################################################################');
-if (!shell.exec('netstat -atp tcp | grep -i "listen" | grep ".8099"', {silent: true}).stdout) {
+if (!shell.exec('netstat -n -atp tcp | grep -i "listen" | grep ".8099"', {silent: true}).stdout) {
   shell.echo('# detox-server is not running, run it');
   if (shell.exec('../node_modules/.bin/ttab -w node ./node_modules/.bin/detox-server').code !== 0) {
     shell.echo('# error: cannot run detox-server in a new tab');
--- detox/scripts/test.js
+++ detox/scripts/test.js
@@ -5,6 +5,18 @@ shell.echo('# cd test');
 shell.cd('test');
 
 shell.echo('\n#################################################################');
+if (!shell.exec('netstat -n -atp tcp | grep -i "listen" | grep ".8099"', {silent: true}).stdout) {
+  shell.echo('# detox-server is not running, run it');
+  if (shell.exec('../node_modules/.bin/ttab -w node ./node_modules/.bin/detox-server').code !== 0) {
+    shell.echo('# error: cannot run detox-server in a new tab');
+    process.exit(1);
+  }
+  shell.exec('sleep 2');
+} else {
+  shell.echo('# detox-server is already running');
+}
+
+shell.echo('\n#################################################################');
 shell.echo('# npm run e2e (debug)');
 if (shell.exec('npm run e2e').code !== 0) {
   shell.echo('error: npm run e2e');
