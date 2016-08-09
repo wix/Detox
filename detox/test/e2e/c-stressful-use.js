2a675c55b4a8c45192f1086c079e2b2e500dc251
EtgarSH <etgar90@gmail.com>
2016-08-09 18:35:00 +0300

    Removed unnecessary tests in Multi Test

--- detox/test/e2e/c-stressful-use.js
+++ detox/test/e2e/c-stressful-use.js
@@ -30,20 +30,9 @@ describe.only('Stressful Use', function() {
           simulator.reloadReactNativeApp(done)
         })
 
-        it('should have welcome screen', function () {
-          expect(element(by.label('Welcome'))).toBeVisible();
-          expect(element(by.label('Say Hello'))).toBeVisible();
-          expect(element(by.label('Say World'))).toBeVisible();
-        });
-
         it('should show hello screen after tap', function () {
           element(by.label('Say Hello')).tap();
           expect(element(by.label('Hello!!!'))).toBeVisible();
-        });
-
-        it('should show world screen after tap', function () {
-          element(by.label('Say World')).tap();
-          expect(element(by.label('World!!!'))).toBeVisible();
         });
       })
     }
