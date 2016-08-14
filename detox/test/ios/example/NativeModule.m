071758d875b64e118addd45a962c5493aa495f77
talkol <talkol@wix.com>
2016-08-14 18:52:06 +0300

    improved constants for stress tests

--- detox/test/ios/example/NativeModule.m
+++ detox/test/ios/example/NativeModule.m
@@ -1,20 +1,24 @@
 #import "NativeModule.h"
 
+static int CALL_COUNTER = 0;
+
 @implementation NativeModule
 
 RCT_EXPORT_MODULE();
 
 RCT_EXPORT_METHOD(echoWithoutResponse:(NSString *)str)
 {
-  NSLog(@"NativeModule echoWithoutResponse called");
+  // NSLog(@"NativeModule echoWithoutResponse called");
+  CALL_COUNTER++;
 }
 
 RCT_EXPORT_METHOD(echoWithResponse:(NSString *)str
                           resolver:(RCTPromiseResolveBlock)resolve
                           rejecter:(RCTPromiseRejectBlock)reject)
 {
+  CALL_COUNTER++;
   resolve(str);
-  NSLog(@"NativeModule echoWithResponse called");
+  // NSLog(@"NativeModule echoWithResponse called");
 }
 
 @end
