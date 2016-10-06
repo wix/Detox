6dd0ea84cd1522d824f5eed9ff1d47cc0ec285be
talkol <talkol@wix.com>
2016-10-06 15:57:50 +0300

    fix strange crash on reload due to idling resource for shadow view queue being installed twice

--- detox/ios/Detox/ReactNativeUIManagerIdlingResource.m
+++ detox/ios/Detox/ReactNativeUIManagerIdlingResource.m
@@ -22,6 +22,19 @@
     id<RN_RCTBridge> _bridge;
 }
 
++ (dispatch_queue_t)getUIQueueFromBridge:(id)bridge
+{
+    dispatch_queue_t queue = [[bridge uiManager] methodQueue];
+    return queue;
+}
+
+// this is a hack since we don't have a public API to check this and we crash over an assertion if we install twice
++ (GREYDispatchQueueIdlingResource*)checkIfDispatchQueueIdlingResourceAlreadyInstalled:(id)bridge
+{
+    dispatch_queue_t queue = [self getUIQueueFromBridge:bridge];
+    return [[GREYDispatchQueueIdlingResource class] performSelector:@selector(grey_resourceForCurrentlyTrackedDispatchQueue:) withObject:queue];
+}
+
 + (instancetype)idlingResourceForBridge:(id)bridge name:(NSString *)name
 {
     if (bridge == nil)
@@ -32,6 +45,16 @@
     }
     
     if (bridge == nil) return nil;
+    
+    // we have an issue that calling unregister on the idling resource (sometimes?)
+    GREYDispatchQueueIdlingResource *existing = [self checkIfDispatchQueueIdlingResourceAlreadyInstalled:bridge];
+    if (existing != nil)
+    {
+        // I suspect that we have a race condition and deregister doesn't free the idling resource immediately, need to investigate further
+        NSLog(@"Detox Warning: ReactNativeUIManagerIdlingResource already installed and had to be recycled");
+        return (ReactNativeUIManagerIdlingResource*)existing;
+    }
+    
     ReactNativeUIManagerIdlingResource *res = [[ReactNativeUIManagerIdlingResource alloc] initWithBridge:bridge name:name];
     [[GREYUIThreadExecutor sharedInstance] registerIdlingResource:res];
     return res;
@@ -44,7 +67,7 @@
 
 - (instancetype)initWithBridge:(id<RN_RCTBridge>)bridge name:(NSString *)name
 {
-    dispatch_queue_t queue = [[bridge uiManager] methodQueue];
+    dispatch_queue_t queue = [ReactNativeUIManagerIdlingResource getUIQueueFromBridge:bridge];
     self = [super initWithDispatchQueue:queue name:name];
     if (self)
     {
