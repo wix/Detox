6a714b2e92ea6630b97a1c4b6ee9ccf44144f3a6
Doron Pearl <doronpe@wix.com>
2016-10-12 15:17:13 -0400

    added detoxReuse option

--- detox/src/ios/simulator.js
+++ detox/src/ios/simulator.js
@@ -403,7 +403,12 @@ function prepare(params, onComplete) {
           onComplete(err2);
           return;
         }
-        deleteAndRelaunchApp(onComplete);
+        if ( _getArgValue('detoxReuse')) {
+          console.log('Reusing existing app')
+          relaunchApp(onComplete);
+        } else {
+          deleteAndRelaunchApp(onComplete);
+        }
       });
     });
   } else {
