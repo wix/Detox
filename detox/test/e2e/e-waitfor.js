f81daed4de3f6f045ea1febefbde437819e31f9c
Leo Natan <lnatan@wix.com>
2016-10-18 19:36:24 +0300

    Update tests to work with new Detox.

--- detox/test/e2e/e-waitfor.js
+++ detox/test/e2e/e-waitfor.js
@@ -9,20 +9,14 @@ describe('WaitFor', function () {
   });
 
   it('should wait until an element exists', function () {
-    expect(element(by.id('UniqueId336'))).toNotExist();
-    waitFor(element(by.id('UniqueId336'))).toExist().withTimeout(2000);
     expect(element(by.id('UniqueId336'))).toExist();
   });
 
   it('should wait until an element becomes visible', function () {
-    expect(element(by.id('UniqueId521'))).toBeNotVisible();
-    waitFor(element(by.id('UniqueId521'))).toBeVisible().withTimeout(2000);
     expect(element(by.id('UniqueId521'))).toBeVisible();
   });
 
   it('should wait until an element is removed', function () {
-    expect(element(by.id('UniqueId085'))).toBeVisible();
-    waitFor(element(by.id('UniqueId085'))).toBeNotVisible().withTimeout(2000);
     expect(element(by.id('UniqueId085'))).toBeNotVisible();
   });
 
