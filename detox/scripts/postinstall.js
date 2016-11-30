c41678adf3407a500343dc6de5a148ca79cf0223
Leo Natan <leo.natan@outlook.com>
2016-11-30 15:32:51 +0200

    Update postinstall.js

--- detox/scripts/postinstall.js
+++ detox/scripts/postinstall.js
@@ -9,18 +9,24 @@ if(shell.exec('scripts/fixup-softlinks.sh').code != 0) {
   process.exit(1);
 }
 
-
-shell.echo('\n#################################################################');
-shell.echo('# installing fbsimctl');
-if(shell.exec('brew help', {silent: true}).code !== 0) {
-  shell.echo('error: Brew is not installed. Please install brew and run again.');
-  process.exit(1);
+if(shell.exec('brew list fbsimctl', {silent: true}).code != 0)
+{
+  shell.echo('\n#################################################################');
+  if(shell.exec('brew help', {silent: true}).code !== 0) {
+    shell.echo('error: Brew is not installed. Please install brew and run again.');
+    process.exit(1);
+  }
+  if(shell.exec('brew tap facebook/fb').code != 0) {
+    shell.echo('error: Facebook Tap install failed.');
+    process.exit(1);
+  }
+  shell.echo('# installing fbsimctl');
+  if(shell.exec('brew install fbsimctl').code != 0) {
+    shell.echo('error: fbsimctl install failed.');
+    process.exit(1);
+  }
 }
-if(shell.exec('brew tap facebook/fb').code != 0) {
-  shell.echo('error: Facebook Tap install failed.');
-  process.exit(1);
-}
-if(shell.exec('brew install fbsimctl --HEAD').code != 0) {
-  shell.echo('error: fbsimctl install failed.');
-  process.exit(1);
+else
+{
+  shell.echo('# fbsimctl already installed.');
 }
