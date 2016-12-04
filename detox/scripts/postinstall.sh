31135e56061255fff9266967eb6ff774a085f776
Leo Natan <leo.natan@outlook.com>
2016-12-04 15:20:59 +0200

    Limit iOS postinstall to Darwin

--- detox/scripts/postinstall.sh
+++ detox/scripts/postinstall.sh
@@ -1,32 +1,5 @@
 #!/bin/bash
 
-printf "\n#################################################################\n"
-
-if [ -f Detox.framework.tar ]; then
-  tar -xf Detox.framework.tar
-  rm -f Detox.framework.tar
-fi
-
-brew list fbsimctl &> /dev/null
-if [ $? != 0 ]; then
-  printf "\n#################################################################\n"
-  brew help &> /dev/null
-  if [ $? != 0 ]; then
-    echo "error: could not fix SocketRocket soft links."
-    exit 1
-  fi
-
-  brew tap facebook/fb
-  if [ $? != 0 ]; then
-    echo "error: Facebook Tap install failed."
-    exit 1
-  fi
-
-  brew install fbsimctl --HEAD
-  if [ $? != 0 ]; then
-    echo "error: fbsimctl install failed."
-    exit 1
-  fi
-else
-echo "# fbsimctl already installed."
+if [ `uname` == "Darwin" ]; then
+  source postinstall.ios.sh
 fi
