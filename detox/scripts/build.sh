03b0988b51871322c872878062475560800db97f
Leo Natan <leo.natan@outlook.com>
2017-01-17 13:01:25 +0200

    Print Detox.framework build stage, remove `clean` directive to make iterative builds faster.

--- detox/scripts/build.sh
+++ detox/scripts/build.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 BABEL_ENV=test babel src -d lib
-xcodebuild clean build -project ios/Detox.xcodeproj -scheme DetoxFramework -configuration Release -derivedDataPath DetoxBuild > /dev/null
+echo -e "\nBuilding Detox.framework"
+xcodebuild build -project ios/Detox.xcodeproj -scheme DetoxFramework -configuration Release -derivedDataPath DetoxBuild > /dev/null
 cp -r DetoxBuild/Build/Products/Release-universal/Detox.framework .
 rm -fr DetoxBuild
 tar -cf Detox.framework.tar Detox.framework
