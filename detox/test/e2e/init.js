b3f9e1a5a781fa17e8b3c7978fd1519260daf803
Rotem M <rotemm@wix.com>
2017-01-17 10:23:03 +0200

    stop overriding the timeout config in init.js

--- detox/test/e2e/init.js
+++ detox/test/e2e/init.js
@@ -2,7 +2,6 @@ var detox = require('../../src/index');
 var config = require('../package.json').detox;
 
 before(function (done) {
-  this.timeout(240000);
   detox.config(config);
   detox.start(done);
 });
