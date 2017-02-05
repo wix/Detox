437ab7d996b54423c997fd3c8d5fd5fdcebc51ce
Rotem M <rotemm@wix.com>
2017-02-05 20:10:30 +0200

    retry test - 100%

--- detox/src/utils/retry.test.js
+++ detox/src/utils/retry.test.js
@@ -21,15 +21,15 @@ describe('retry', () => {
     expect(mockFn).toHaveBeenCalledTimes(2);
   });
 
-  it(`a promise that rejects two times and then resolves`, async () => {
+  it(`a promise that rejects two times, with two retries`, async () => {
     const mockFn = jest.fn()
                        .mockReturnValueOnce(Promise.reject('a thing'))
-                       .mockReturnValueOnce(Promise.resolve());
+                       .mockReturnValueOnce(Promise.reject('a thing'));
     try {
-      await retry({retries: 1, interval: 1}, mockFn);
+      await retry({retries: 2, interval: 1}, mockFn);
       fail('expected retry to fail to throw');
     } catch (object) {
-      expect(mockFn).toHaveBeenCalledTimes(1);
+      expect(mockFn).toHaveBeenCalledTimes(2);
       expect(object).toBeDefined();
     }
   });
