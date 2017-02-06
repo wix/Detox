0c6e53e735e6106f0a3955dcf4a09d53c763fa61
Rotem M <rotemm@wix.com>
2017-02-06 22:58:17 +0200

    tidy up the build script log

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -e
 
-npm install -g lerna@2.0.0-beta.34
-npm install -g react-native-cli
-gem install xcpretty
+npm install -g lerna@2.0.0-beta.34 >/dev/null 2>&1
+npm install -g react-native-cli >/dev/null 2>&1
+gem install xcpretty >/dev/null 2>&1
 
 lerna bootstrap
 
@@ -11,7 +11,7 @@ npm run unit
 npm run build
 
 cd test
-(export RCT_NO_LAUNCH_PACKAGER=true && xcodebuild -project ios/example.xcodeproj -scheme example -configuration Release -sdk iphonesimulator -derivedDataPath ios/build) | xcpretty
-npm run detox-server &
+(export RCT_NO_LAUNCH_PACKAGER=true && xcodebuild -project ios/example.xcodeproj -scheme example -configuration Release -sdk iphonesimulator -derivedDataPath ios/build) >/dev/null 2>&1
+npm run detox-server >/dev/null 2>&1 &
 npm run e2e
 pkill -f "detox-server" || true
