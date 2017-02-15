ee6f4bb30f1529e6c2ef125ffdabd94a77ec9b5a
Rotem M <rotemm@wix.com>
2017-02-15 11:58:21 +0200

    Publish
     - detox@4.2.0

---
+++ detox/test/e2e/k-user-notifications.js
@@ -0,0 +1,13 @@
+describe('User Notifications', () => {
+  beforeEach((done) => {
+    simulator.reloadReactNativeApp(done);
+  });
+
+  beforeEach(() => {
+    element(by.label('Sanity')).tap();
+  });
+
+  it('receive local notification', () => {
+
+  });
+});
