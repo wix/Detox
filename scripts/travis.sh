61444296b5913879e60008d546f8db465a19e3de
Rotem M <rotemm@wix.com>
2017-02-15 12:41:12 +0200

    updated iOS build scripts

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -9,10 +9,10 @@ lerna bootstrap
 cd detox
 npm run unit
 npm run build -- noframework
-xcodebuild -project ios/Detox.xcodeproj -scheme Detox -configuration Debug test -destination 'platform=iOS Simulator,name=iPhone 7 Plus' | XCPretty
+xcodebuild -project ios/Detox.xcodeproj -scheme Detox -configuration Debug test -destination 'platform=iOS Simulator,name=iPhone 7 Plus' | xcpretty && exit ${PIPESTATUS[0]}
 
 cd test
-(export RCT_NO_LAUNCH_PACKAGER=true && xcodebuild -project ios/example.xcodeproj -scheme example -configuration Release -sdk iphonesimulator -derivedDataPath ios/build) | xcpretty
+export RCT_NO_LAUNCH_PACKAGER=true && xcodebuild -project ios/example.xcodeproj -scheme example -configuration Release -sdk iphonesimulator -derivedDataPath ios/build | xcpretty && exit ${PIPESTATUS[0]}
 npm run detox-server &
 npm run e2e
 pkill -f "detox-server" || true
