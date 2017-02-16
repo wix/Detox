8ba615ac1018a265f27a568c8f7d12c17a99e4d0
Rotem M <rotemm@wix.com>
2017-02-16 10:05:58 +0200

    Revert "fixed “use Xcode 8.2 in travis environment [3a0673e]”"
    This reverts commit 436628394d4c42dc25cd5ae4a99c1471f6b42962.

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -9,7 +9,7 @@ lerna bootstrap
 cd detox
 npm run unit
 npm run build -- noframework
-set -o pipefail && xcodebuild -project ios/Detox.xcodeproj -scheme Detox -configuration Debug test -destination 'platform=iOS Simulator,name=iPhone 7 Plus,OS=10.2' | xcpretty
+set -o pipefail && xcodebuild -project ios/Detox.xcodeproj -scheme Detox -configuration Debug test -destination 'platform=iOS Simulator,name=iPhone 7 Plus' | xcpretty
 
 cd test
 set -o pipefail && export RCT_NO_LAUNCH_PACKAGER=true && xcodebuild -project ios/example.xcodeproj -scheme example -configuration Release -sdk iphonesimulator -derivedDataPath ios/build | xcpretty
