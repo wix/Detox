55ec8a54dc57ee95e52b6ab4387aef5d1eba7461
Rotem M <rotemm@wix.com>
2017-03-05 11:24:32 +0200

    added uninstall() + install() + relaunch() test to simulator

--- detox/test/e2e/f-simulator.js
+++ detox/test/e2e/f-simulator.js
@@ -19,4 +19,13 @@ describe('Simulator', () => {
     await element(by.label('Say Hello')).tap();
     await expect(element(by.label('Hello!!!'))).toBeVisible();
   });
+
+  it('uninstall() + install() + relaunch() - should tap successfully', async () => {
+    await device.uninstallApp();
+    await device.installApp();
+    await device.relaunchApp();
+    await element(by.label('Sanity')).tap();
+    await element(by.label('Say Hello')).tap();
+    await expect(element(by.label('Hello!!!'))).toBeVisible();
+  });
 });
