c55788490421123f3c49de692a547a31f9b3e3dc
Rotem M <rotemm@wix.com>
2017-03-14 01:32:11 +0200

    updated lerna and build scripts to support new api changes.

--- detox/scripts/test-exec.js
+++
@@ -1,9 +0,0 @@
-require('shelljs/global');
-require('./logger');
-set('-e');
-
-cd('test');
-
-console.step('npm run e2e (debug)');
-let argv = process.argv.slice(2).map(str => str.toLowerCase());
-exec('npm run e2e -- --___detoxargs___:::"' + argv.join(' ') + '"');
--- scripts/travis.sh
+++ scripts/travis.sh
@@ -1,13 +1,12 @@
 #!/bin/bash -e
 
 lerna bootstrap
+lerna run build
+lerna run test
 
 cd detox
-npm run unit
-npm run build -- noframework
-set -o pipefail && xcodebuild -project ios/Detox.xcodeproj -scheme Detox -configuration Debug test -destination 'platform=iOS Simulator,name=iPhone 7 Plus' | xcpretty
 
 cd test
 set -o pipefail && export RCT_NO_LAUNCH_PACKAGER=true && xcodebuild -project ios/example.xcodeproj -scheme example -configuration Release -sdk iphonesimulator -derivedDataPath ios/build | xcpretty
+
 npm run e2e
-pkill -f "detox-server" || true
