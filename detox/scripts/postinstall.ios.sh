c1a54adfe8193100a9bf584ab1d16744f7f23bb2
Rotem M <rotemm@wix.com>
2017-03-14 01:02:57 +0200

    removed `brew install fbsimctl` from post install script. users will now need to install it manually.

--- detox/scripts/postinstall.ios.sh
+++ detox/scripts/postinstall.ios.sh
@@ -1,32 +1,10 @@
 #!/bin/bash
 
-printf "\n#################################################################\n"
+echo "###############################"
+echo "Extracting Detox.framework..."
 
 if [ -f Detox.framework.tar ]; then
   tar -xjf Detox.framework.tbz
   rm -f Detox.framework.tbz
 fi
-
-brew list fbsimctl &> /dev/null
-if [ $? != 0 ]; then
-  printf "\n#################################################################\n"
-  brew help &> /dev/null
-  if [ $? != 0 ]; then
-    echo "error: Brew is not installed. Visit https://brew.sh/ for more information."
-    exit 1
-  fi
-
-  brew tap facebook/fb
-  if [ $? != 0 ]; then
-    echo "error: Facebook Tap install failed."
-    exit 1
-  fi
-
-  brew install fbsimctl --HEAD
-  if [ $? != 0 ]; then
-    echo "error: fbsimctl install failed."
-    exit 1
-  fi
-else
-echo "# fbsimctl already installed."
-fi
+echo "###############################"
