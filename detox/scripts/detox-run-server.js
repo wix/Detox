1fea3a59b039de25dfe8fd7db9eec41fb04b939a
Rotem M <rotemm@wix.com>
2017-03-13 20:44:27 +0200

    detox-cli: fixed run-server to work under shell

--- detox/scripts/detox-run-server.js
+++ detox/scripts/detox-run-server.js
@@ -1,8 +1,7 @@
 #!/usr/bin/env node
 
 const program = require('commander');
-const child_process = require('child_process');
-program
-  .parse(process.argv);
+const cp = require('child_process');
+program.parse(process.argv);
 
-child_process.execSync('detox-server', {stdio: 'inherit'});
+cp.execSync('node_modules/.bin/detox-server', {stdio: 'inherit'});
