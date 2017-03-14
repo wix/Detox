bf63e8f12ff02cf3fc1d6dad8f1121ad7e0b2ed4
Rotem M <rotemm@wix.com>
2017-03-14 12:12:07 +0200

    start using local scripts in build process

--- detox/scripts/detox-test.js
+++ detox/scripts/detox-test.js
@@ -4,9 +4,10 @@ const program = require('commander');
 const path = require('path');
 const cp = require('child_process');
 program
-  .option('-r, --runner [runner]', 'test runner', 'mocha')
-  .option('-c, --runner-config [config]', 'test runner config file', 'mocha.opts')
+  .option('-r, --runner [runner]', 'Test runner (currently supports mocha)', 'mocha')
+  .option('-c, --runner-config [config]', 'Test runner config file', 'mocha.opts')
   .option('-l, --loglevel [value]', 'info, debug, verbose, silly')
+  .option('-d, --device [device name]', 'Run test on this device')
   .parse(process.argv);
 
 const config = require(path.join(process.cwd(), 'package.json')).detox;
@@ -14,13 +15,16 @@ const testFolder = config.specs || 'e2e';
 
 console.log('runner', program.runner);
 
+const loglevel = program.loglevel ? `--loglevel ${program.loglevel}` : '';
+const device = program.device ? `--device ${program.device}` : '';
+
 let command;
 switch (program.runner) {
   case 'mocha':
-    command = `node_modules/.bin/${program.runner} ${testFolder} --opts ${testFolder}/${program.runnerConfig} --loglevel ${program.loglevel}`;
+    command = `node_modules/.bin/${program.runner} ${testFolder} --opts ${testFolder}/${program.runnerConfig} ${device} ${loglevel}`;
     break;
   default:
-    throw new Error(`${program.runner} is not supported in detox cli tools. You can still runb your tests with the runner's own cli tool`);
+    throw new Error(`${program.runner} is not supported in detox cli tools. You can still run your tests with the runner's own cli tool`);
 }
 
 console.log(command);
--- scripts/travis.sh
+++ scripts/travis.sh
@@ -1,12 +1,12 @@
 #!/bin/bash -e
 
 lerna bootstrap
-lerna run build
-lerna run test
+lerna run --ignore detox-demo*  build
+lerna run --ignore detox-demo*  test
 
-cd detox
+#cd detox
 
 cd test
-set -o pipefail && export RCT_NO_LAUNCH_PACKAGER=true && xcodebuild -project ios/example.xcodeproj -scheme example -configuration Release -sdk iphonesimulator -derivedDataPath ios/build | xcpretty
+#set -o pipefail && export RCT_NO_LAUNCH_PACKAGER=true && xcodebuild -project ios/example.xcodeproj -scheme example -configuration Release -sdk iphonesimulator -derivedDataPath ios/build | xcpretty
 
 npm run e2e
