eb4c5c40e415a3fb42563901acc8822c94f99025
Rotem M <rotemm@wix.com>
2017-03-14 13:11:03 +0200

    fixed path when trying access the test project

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -4,9 +4,5 @@ lerna bootstrap
 lerna run --ignore detox-demo*  build
 lerna run --ignore detox-demo*  test
 
-#cd detox
-
-cd test
-#set -o pipefail && export RCT_NO_LAUNCH_PACKAGER=true && xcodebuild -project ios/example.xcodeproj -scheme example -configuration Release -sdk iphonesimulator -derivedDataPath ios/build | xcpretty
-
+cd detox/test
 npm run e2e
