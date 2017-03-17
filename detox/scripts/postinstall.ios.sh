c909ceb242b4c27c1f7fee6f9ae23e902b2eaa83
Rotem M <rotemm@wix.com>
2017-03-17 23:54:22 +0200

    iOS postinstall script was not looking for the right flie to decompress.

--- detox/scripts/postinstall.ios.sh
+++ detox/scripts/postinstall.ios.sh
@@ -3,7 +3,7 @@
 echo "###############################"
 echo "Extracting Detox.framework..."
 
-if [ -f Detox.framework.tar ]; then
+if [ -f Detox.framework.tbz ]; then
   tar -xjf Detox.framework.tbz
   rm -f Detox.framework.tbz
 fi
