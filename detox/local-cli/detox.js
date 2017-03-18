f07cc6846b19d4fff4e1fa239f23d7bc583107f2
Rotem M <rotemm@wix.com>
2017-03-18 21:47:55 +0200

    updated inline documentation in local-cli

--- detox/local-cli/detox.js
+++ detox/local-cli/detox.js
@@ -5,6 +5,6 @@ const program = require('commander');
 program
   .arguments('<process>')
   .command('test', 'Initiating your test suite')
-  .command('build', `[convince method] Run the command defined in 'device.build'`)
+  .command('build', `[convince method] Run the command defined in 'configuration.build'`)
   .command('run-server', 'Starts a standalone detox server')
   .parse(process.argv);
