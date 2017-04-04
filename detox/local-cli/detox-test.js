0924ed61845675b0ef746d24fa55385f45f51ec7
Doron Pearl <doronpe@wix.com>
2017-04-04 16:09:54 +0300

    added `reuse` option to CLI

--- detox/local-cli/detox-test.js
+++ detox/local-cli/detox-test.js
@@ -9,6 +9,7 @@ program
   .option('-l, --loglevel [value]', 'info, debug, verbose, silly, wss')
   .option('-c, --configuration [device configuration]', 'Select a device configuration from your defined configurations,'
                                                         + 'if not supplied, and there\'s only one configuration, detox will default to it')
+  .option('-r, --reuse', 'Reuse existing installed app (do not delete and re-install) for a faster run.', false)
   .option('-u, --cleanup', 'shutdown simulator when test is over, useful for CI scripts, to make sure detox exists cleanly with no residue', false)
   .parse(process.argv);
 
@@ -18,11 +19,12 @@ const testFolder = config.specs || 'e2e';
 const loglevel = program.loglevel ? `--loglevel ${program.loglevel}` : '';
 const configuration = program.configuration ? `--configuration ${program.configuration}` : '';
 const cleanup = program.cleanup ? `--cleanup` : '';
+const reuse = program.reuse ? `--reuse` : '';
 
 let command;
 switch (program.runner) {
   case 'mocha':
-    command = `node_modules/.bin/${program.runner} ${testFolder} --opts ${testFolder}/${program.runnerConfig} ${configuration} ${loglevel} ${cleanup}`;
+    command = `node_modules/.bin/${program.runner} ${testFolder} --opts ${testFolder}/${program.runnerConfig} ${configuration} ${loglevel} ${cleanup} ${reuse}`;
     break;
   default:
     throw new Error(`${program.runner} is not supported in detox cli tools. You can still run your tests with the runner's own cli tool`);
