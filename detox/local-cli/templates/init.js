d199c7c31819a7aecc160ca35750b0a7b7c50088
bogobogo <eladbo@wix.com>
2017-04-20 22:24:52 +0300

    Create init.js

---
+++ detox/local-cli/templates/init.js
@@ -0,0 +1,13 @@
+const initjsContent = `require('babel-polyfill');
+const detox = require('detox');
+const config = require('../package.json').detox;
+
+before(async () => {
+  await detox.init(config);
+});
+
+after(async () => {
+  await detox.cleanup();
+});`
+
+exports.initjs = initjsContent
