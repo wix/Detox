7b9c6e3ec2a582ad8cad06d31d4126c11a6b3adc
bogobogo <eladbo@wix.com>
2017-04-20 22:20:21 +0300

    created templates in local-cli and added mocha

---
+++ detox/local-cli/templates/mocha.js
@@ -0,0 +1,23 @@
+const mochaOptsContent= '--recursive --timeout 120000 --bail'
+const firstTestContent = `describe('Example', () => {
+  beforeEach(async () => {
+    await device.reloadReactNative();
+  });
+  
+  it('should have welcome screen', async () => {
+    await expect(element(by.id('welcome'))).toBeVisible();
+  });
+  
+  it('should show hello screen after tap', async () => {
+    await element(by.id('hello_button')).tap();
+    await expect(element(by.label('Hello!!!'))).toBeVisible();
+  });
+  
+  it('should show world screen after tap', async () => {
+    await element(by.id('world_button')).tap();
+    await expect(element(by.label('World!!!'))).toBeVisible();
+  });
+})`
+
+exports.firstTest = firstTestContent
+exports.runnerConfig = mochaOptsContent
