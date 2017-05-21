1cfc199a1ee51d1ab9016efb687df8da61255cde
Daniel Schmidt <danielmschmidt92@gmail.com>
2017-05-21 08:56:29 +0200

    add orientation change e2e test

--- detox/test/e2e/f-simulator.js
+++ detox/test/e2e/f-simulator.js
@@ -28,4 +28,28 @@ describe('Simulator', () => {
     await element(by.label('Say Hello')).tap();
     await expect(element(by.label('Hello!!!'))).toBeVisible();
   });
+
+  describe('device orientation', () => {
+    beforeEach(async() => {
+      await device.reloadReactNative();
+      await element(by.label('Orientation')).tap();
+
+      // Check if the element whichs input we will test actually exists
+      await expect(element(by.id('currentOrientation'))).toExist();
+    });
+
+    it('OrientationLandscape', async () => {
+      await device.setOrientation('landscape');
+
+      await expect(element(by.id('currentOrientation'))).toHaveText('Landscape');
+    });
+
+    it('OrientationPortrait', async() => {
+      // As default is portrait we need to set it otherwise
+      await device.setOrientation('landscape');
+      await device.setOrientation('portrait');
+
+      await expect(element(by.id('currentOrientation'))).toHaveText('Portrait');
+    });
+  });
 });
