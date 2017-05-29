9362ad0a89e123670f9305d317dad891f33bc82b
Rotem M <rotemm@wix.com>
2017-05-30 00:20:26 +0300

    fixed #141: `detox run-server` now works with both yarn and npm

--- detox/local-cli/detox-run-server.js
+++ detox/local-cli/detox-run-server.js
@@ -2,6 +2,14 @@
 
 const program = require('commander');
 const cp = require('child_process');
+const fs = require('fs');
+const path = require('path');
+
 program.parse(process.argv);
 
-cp.execSync('node_modules/.bin/detox-server', {stdio: 'inherit'});
+if (fs.existsSync(path.join(__dirname, 'node_modules/.bin/detox-server'))) {
+  cp.execSync('node_modules/.bin/detox-server', {stdio: 'inherit'});
+} else {
+  cp.execSync('node_modules/detox/node_modules/.bin/detox-server', {stdio: 'inherit'});
+}
+
