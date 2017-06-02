1bf8d949d726e4cc403540100950c837dc653395
Leo Natan <leo.natan@outlook.com>
2017-06-02 17:26:50 +0300

    Break the build and test phase into two steps to prevent timeouts. Move xcode build and test into .sh script to prevent lerna buggy output.

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -4,5 +4,8 @@ lerna bootstrap
 lerna run --ignore detox-demo*  build
 lerna run --ignore detox-demo*  test
 
+set -o pipefail && xcodebuild -project detox/ios/Detox.xcodeproj -scheme Detox -configuration Debug -sdk iphonesimulator build-for-testing | xcpretty
+set -o pipefail && xcodebuild -project detox/ios/Detox.xcodeproj -scheme Detox -configuration Debug -sdk iphonesimulator test-without-building -destination 'platform=iOS Simulator,name=iPhone 7 Plus' | xcpretty
+
 npm run e2e
 #npm run release
