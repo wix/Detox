f16449b61b99db8cc89da5af7fd08262fb87d1fd
dassir <dassir@wix.com>
2017-06-04 14:21:17 +0300

    added wrapper for applesimutils

---
+++ detox/src/devices/AppleSimUtils.js
@@ -0,0 +1,27 @@
+const _ = require('lodash');
+const exec = require('../utils/exec');
+
+class AppleSimUtils {
+
+
+    async setPermissions(udid, bundleId, permissionsObj) {
+        const statusLogs = {
+            trying: `Trying to set permissions...`,
+            successful: 'Permissions sre set'
+        };
+        let permissions = [];
+        _.forEach(permissionsObj, function (shouldAllow, permission) {
+            permissions.push(permission + '=' + shouldAllow);
+        });
+        const options = {args: `--simulator ${udid} --bundle ${bundleId} --setPermissions ${_.join(permissions, ',')}`};
+        await this._execAppleSimUtilsCommand(options, statusLogs, 1);
+
+    }
+
+    async _execAppleSimUtilsCommand(options, statusLogs, retries, interval) {
+        const bin = `applesimutils`;
+        return await exec.execWithRetriesAndLogs(bin, options, statusLogs, retries, interval);
+    }
+}
+
+module.exports = AppleSimUtils;
