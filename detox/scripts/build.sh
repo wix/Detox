10d344170ad785d3b4b364264bdb41ffafa626eb
Leo Natan <leo.natan@outlook.com>
2017-06-05 17:53:57 +0300

    Possible fix for signing issues + improve build script

--- detox/scripts/build.sh
+++ detox/scripts/build.sh
@@ -3,8 +3,9 @@
 BABEL_ENV=test babel src -d lib
 if [ "$1" != "noframework" ]; then
 echo -e "\nBuilding Detox.framework"
+	rm -fr Detox.framework
 	xcodebuild build -project ios/Detox.xcodeproj -scheme DetoxFramework -configuration Release -derivedDataPath DetoxBuild > /dev/null
-	cp -r DetoxBuild/Build/Products/Release-universal/Detox.framework .
+	cp -fr DetoxBuild/Build/Products/Release-universal/Detox.framework .
 	rm -fr DetoxBuild
 	tar -cjf Detox.framework.tbz Detox.framework
 fi
