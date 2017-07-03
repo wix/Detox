e0f6c7e7a81395e5fffd5c66f26b6fe4532f3ae2
Rotem M <rotemm@wix.com>
2017-07-04 00:29:47 +0300

    install applesimutils

--- detox/src/devices/AppleSimUtils.js
+++ detox/src/devices/AppleSimUtils.js
@@ -3,25 +3,24 @@ const exec = require('../utils/exec');
 
 class AppleSimUtils {
 
+  async setPermissions(udid, bundleId, permissionsObj) {
+    const statusLogs = {
+      trying: `Trying to set permissions...`,
+      successful: 'Permissions sre set'
+    };
+    let permissions = [];
+    _.forEach(permissionsObj, function(shouldAllow, permission) {
+      permissions.push(permission + '=' + shouldAllow);
+    });
+    const options = {args: `--simulator ${udid} --bundle ${bundleId} --setPermissions ${_.join(permissions, ',')}`};
+    await this._execAppleSimUtilsCommand(options, statusLogs, 1);
 
-    async setPermissions(udid, bundleId, permissionsObj) {
-        const statusLogs = {
-            trying: `Trying to set permissions...`,
-            successful: 'Permissions sre set'
-        };
-        let permissions = [];
-        _.forEach(permissionsObj, function (shouldAllow, permission) {
-            permissions.push(permission + '=' + shouldAllow);
-        });
-        const options = {args: `--simulator ${udid} --bundle ${bundleId} --setPermissions ${_.join(permissions, ',')}`};
-        await this._execAppleSimUtilsCommand(options, statusLogs, 1);
+  }
 
-    }
-
-    async _execAppleSimUtilsCommand(options, statusLogs, retries, interval) {
-        const bin = `applesimutils`;
-        return await exec.execWithRetriesAndLogs(bin, options, statusLogs, retries, interval);
-    }
+  async _execAppleSimUtilsCommand(options, statusLogs, retries, interval) {
+    const bin = `applesimutils`;
+    return await exec.execWithRetriesAndLogs(bin, options, statusLogs, retries, interval);
+  }
 }
 
 module.exports = AppleSimUtils;
