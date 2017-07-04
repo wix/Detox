ac82a499bc68bf9f0de0f937d88fb03bf290ebe4
Rotem M <rotemm@wix.com>
2017-07-04 17:36:15 +0300

    Deep links test cleanup

--- detox/test/e2e/n-deep-links.js
+++ detox/test/e2e/n-deep-links.js
@@ -1,21 +1,15 @@
-describe.only('Deep Links', () => {
+describe('Deep Links', () => {
 
-  it('relaucnhApp({url: url}) should pass to app', async () => {
+  it('relaucnhApp({url: url}) should launch app and trigger handling of deep links in app', async () => {
     const url = 'detoxtesturlscheme://such-string';
     await device.relaunchApp({url: url});
     await expect(element(by.label(url))).toBeVisible();
   });
 
-  it('device.openURL({url: url}) should', async () => {
+  it('device.openURL({url: url}) should trigger handling of deep links in app when app is in foreground', async () => {
     const url = 'detoxtesturlscheme://such-string';
     await device.relaunchApp();
-
     await device.openURL({url: url});
     await expect(element(by.label(url))).toBeVisible();
   });
 });
-
-
-async function timeout(ms) {
-  return new Promise((resolve) => setTimeout(resolve, ms));
-}
