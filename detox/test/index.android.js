b94a473220dc11ec97975ed762fdc0c2b8eeec58
Rotem M <rotemm@wix.com>
2017-07-24 00:44:09 +0300

    Updated test app to support both iOS and Android

--- detox/test/index.android.js
+++ detox/test/index.android.js
@@ -1,49 +1,11 @@
-import React, { Component } from 'react';
+import example from './src/app';
+
 import {
   AppRegistry,
-  StyleSheet,
-  Text,
-  View,
-  TouchableOpacity
 } from 'react-native';
 
-export default class example extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      greeting: undefined
-    };
-  }
-  render() {
-    if (this.state.greeting) return this.renderAfterButton();
-    return (
-      <View testID='welcome' style={{flex: 1, paddingTop: 20, justifyContent: 'center', alignItems: 'center'}}>
-        <Text style={{fontSize: 25, marginBottom: 30}}>
-          Welcome
-        </Text>
-        <TouchableOpacity testID='hello_button' onPress={this.onButtonPress.bind(this, 'Hello')}>
-          <Text style={{color: 'blue', marginBottom: 20}}>Say Hello</Text>
-        </TouchableOpacity>
-        <TouchableOpacity testID='world_button' onPress={this.onButtonPress.bind(this, 'World')}>
-          <Text style={{color: 'blue', marginBottom: 20}}>Say World</Text>
-        </TouchableOpacity>
-      </View>
-    );
-  }
-  renderAfterButton() {
-    return (
-      <View style={{flex: 1, paddingTop: 20, justifyContent: 'center', alignItems: 'center'}}>
-        <Text style={{fontSize: 25}}>
-          {this.state.greeting}!!!
-        </Text>
-      </View>
-    );
-  }
-  onButtonPress(greeting) {
-    this.setState({
-      greeting: greeting
-    });
-  }
+class exampleAndroid extends example {
+
 }
 
-AppRegistry.registerComponent('example', () => example);
+AppRegistry.registerComponent('example', () => exampleAndroid);
---
+++ detox/test/src/app.js
@@ -0,0 +1,98 @@
+import React, {Component} from 'react';
+import {
+  Text,
+  View,
+  TouchableOpacity,
+  Linking,
+} from 'react-native';
+
+import * as Screens from './Screens';
+
+class example extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      screen: undefined,
+      url: undefined,
+      notification: undefined
+    };
+  }
+
+  renderScreenButton(title, component) {
+    return (
+      <TouchableOpacity onPress={() => {
+        this.setState({screen: component});
+      }}>
+        <Text style={{color: 'blue', marginBottom: 20}}>{title}</Text>
+      </TouchableOpacity>
+    );
+  }
+
+  renderText(text) {
+    return (
+      <View style={{flex: 1, paddingTop: 20, justifyContent: 'center', alignItems: 'center'}}>
+        <Text style={{fontSize: 25}}>
+          {text}
+        </Text>
+      </View>
+    );
+  }
+
+  async componentDidMount() {
+    const url = await Linking.getInitialURL();
+    if (url) {
+      this.setState({url: url});
+    }
+  }
+
+  componentWillMount() {
+    Linking.addEventListener('url', (params) => this._handleOpenURL(params));
+  }
+
+  render() {
+    if (this.state.notification) {
+      return this.renderText(this.state.notification);
+    }
+
+    else if (this.state.url) {
+      return this.renderText(this.state.url);
+    }
+
+    if (!this.state.screen) {
+      return (
+        <View style={{flex: 1, paddingTop: 20, justifyContent: 'center', alignItems: 'center'}}>
+          <Text style={{fontSize: 20, marginBottom: 30}}>
+            Choose a test
+          </Text>
+          {this.renderScreenButton('Sanity', Screens.SanityScreen)}
+          {this.renderScreenButton('Matchers', Screens.MatchersScreen)}
+          {this.renderScreenButton('Actions', Screens.ActionsScreen)}
+          {this.renderScreenButton('Assertions', Screens.AssertionsScreen)}
+          {this.renderScreenButton('WaitFor', Screens.WaitForScreen)}
+          {this.renderScreenButton('Stress', Screens.StressScreen)}
+          {this.renderScreenButton('Switch Root', Screens.SwitchRootScreen)}
+          {this.renderScreenButton('Timeouts', Screens.TimeoutsScreen)}
+          {this.renderScreenButton('Orientation', Screens.Orientation)}
+          {this.renderScreenButton('Permissions', Screens.Permissions)}
+          {this.renderScreenButton('Network', Screens.NetworkScreen)}
+          {this.renderScreenButton('Animations', Screens.AnimationsScreen)}
+        </View>
+      );
+    }
+    const Screen = this.state.screen;
+    return (
+      <Screen />
+    );
+  }
+
+  _onNotification(notification) {
+    this.setState({notification: notification.getAlert()});
+  }
+
+  _handleOpenURL(params) {
+    this.setState({url: params.url});
+  }
+}
+
+module.exports = example;
