325980f5a7a7c11e67ee272edb720615bf47ca4c
Rotem M <rotemm@wix.com>
2017-07-25 00:50:14 +0300

    EmulatorDriver still crude, but it works. Can now start test on the first attached adb device.

--- detox/src/devices/EmulatorDriver.js
+++ detox/src/devices/EmulatorDriver.js
@@ -1,27 +1,44 @@
 const exec = require('./../utils/exec').execWithRetriesAndLogs;
+const spawn = require('child-process-promise').spawn;
 const _ = require('lodash');
-const adb = require('adbkit');
 const InvocationManager = require('../invoke').InvocationManager;
+
 const DeviceDriverBase = require('./DeviceDriverBase');
+
+//ANDROID_SDK_ROOT
+const ANDROID_HOME = process.env.ANDROID_HOME;
 
 class EmulatorDriver extends DeviceDriverBase {
 
   constructor(client) {
     super(client);
-    this._adb = adb.createClient();
     const expect = require('../android/expect');
     expect.exportGlobals();
     expect.setInvocationManager(new InvocationManager(client));
   }
 
-  async acquireFreeDevice(name) {
-    const list = await this._adb.listDevices();
-    if (!list) {
-      throw new Error('There are no Android devices attached to ADB, please start an emulator or connect a device');
-    }
+  //async boot(deviceId) {
+  //  await exec(`${ANDROID_HOME}/tools/emulator -no-boot-anim -gpu host -no-audio @${deviceId}`);
+  //}
 
-    console.log(list);
-    return list[0].id;
+  async createDevice() {
+    //`android create avd -n <name> -t <targetID>`
+  }
+
+  async acquireFreeDevice(name) {
+    return (await exec(`adb devices | awk 'NR>1 {print $1}'`, undefined, undefined, 1)).stdout.trim();
+    //return '192.168.60.101:5555';
+    //const output = await exec(`$ANDROID_HOME/tools/emulator --list-avds`);
+    //const output = await exec(`$ANDROID_HOME/tools/bin/avdmanager list avd -c`);
+    //const list = _.split(output.stdout.trim(), '\n');
+    //
+    ////const list = await this._adb.listDevices();
+    ////if (_.isEmpty(list)) {
+    ////  throw new Error('There are no Android devices attached to ADB, please start an emulator or connect a device');
+    ////}
+    //
+    //console.log(list);
+    //return list[0].id;
   }
 
   async getBundleIdFromBinary(appPath) {
@@ -36,8 +53,17 @@ class EmulatorDriver extends DeviceDriverBase {
   }
 
   async uninstallApp(deviceId, bundleId) {
-    await this.adbCmd(deviceId, `uninstall ${bundleId}`);
-    await this.adbCmd(deviceId, `uninstall ${bundleId}.test`);
+    try {
+      await this.adbCmd(deviceId, `uninstall ${bundleId}`);
+    } catch (ex) {
+      //this is fine
+    }
+
+    try {
+      await this.adbCmd(deviceId, `uninstall ${bundleId}.test`);
+    } catch (ex) {
+      //this is fine
+    }
   }
 
   async launch(deviceId, bundleId, launchArgs) {
@@ -46,8 +72,25 @@ class EmulatorDriver extends DeviceDriverBase {
       args.push(`${key} ${value}`);
     });
 
-    this.adbCmd(deviceId, `shell am instrument -w -r ${args.join(' ')} -e debug false ${bundleId}.test/android.support.test.runner.AndroidJUnitRunner`);
+    const instrument = spawn(`adb`, [`-s`, `${deviceId}`, `shell` ,`am`, `instrument`, `-w` ,`-r`, `${args.join(' ')}`,`-e`, `debug`, `false` ,`${bundleId}.test/android.support.test.runner.AndroidJUnitRunner`]);
+    const childProcess = instrument.childProcess;
+
+    console.log('[spawn] childProcess.pid: ', childProcess.pid);
+    childProcess.stdout.on('data', function (data) {
+      console.log('[spawn] stdout: ', data.toString());
+    });
+    childProcess.stderr.on('data', function (data) {
+      console.log('[spawn] stderr: ', data.toString());
+    });
+
+    instrument.then(function () {
+      console.log('[spawn] done!');
+    }).catch(function (err) {
+      console.error('[spawn] ERROR: ', err);
+      throw err;
+    });
   }
+
 
   async terminate(deviceId, bundleId) {
     await this.adbCmd(deviceId, `shell am force-stop ${bundleId}`);
@@ -59,9 +102,10 @@ class EmulatorDriver extends DeviceDriverBase {
   }
 
   async adbCmd(deviceId, params) {
-    await exec(`adb wait-for-device`);
-    const cmd = `adb ${deviceId ? `-s ${deviceId}` : ''} ${params}`;
-    await exec(cmd);
+    const serial = `${deviceId ? `-s ${deviceId}` : ''}`;
+    await exec(`adb ${serial} wait-for-device`, undefined, undefined, 1);
+    const cmd = `adb ${serial} ${params}`;
+    await exec(cmd, undefined, undefined, 1);
   }
 }
 
