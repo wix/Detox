804c85bcc489f6c6c4f2ddc02634457cdb011f43
Daniel Zlotin <danielzl@wix.com>
2017-09-25 13:33:28 +0300

    only run location tests if fbsimctl installed

--- detox/test/e2e/o-location.js
+++ detox/test/e2e/o-location.js
@@ -1,13 +1,34 @@
+const exec = require('child-process-promise').exec;
+
+async function isFbsimctlInstalled() {
+  try {
+    await exec(`which fbsimctl`);
+    return true;
+  } catch (e) {
+    return false;
+  }
+}
+
 describe('location', () => {
   it('Location should be unavabilable', async () => {
-    await device.relaunchApp({permissions: {location: 'never'}});
+    const fbsimclInstalled = await isFbsimctlInstalled();
+    if (!fbsimclInstalled) {
+      console.log(`setLocation only works through fbsimcl currently`);
+      return;
+    }
+    await device.relaunchApp({ permissions: { location: 'never' } });
     await element(by.label('Location')).tap();
     await element(by.id('getLocationButton')).tap();
     await expect(element(by.id('error'))).toBeVisible();
   });
 
   it('Should receive location (20,20)', async () => {
-    await device.relaunchApp({permissions: {location: 'always'}});
+    const fbsimclInstalled = await isFbsimctlInstalled();
+    if (!fbsimclInstalled) {
+      console.log(`setLocation only works through fbsimcl currently`);
+      return;
+    }
+    await device.relaunchApp({ permissions: { location: 'always' } });
     await device.setLocation(20, 20);
     await element(by.label('Location')).tap();
     await element(by.id('getLocationButton')).tap();
@@ -17,3 +38,4 @@ describe('location', () => {
     await expect(element(by.text('Longitude: 20'))).toBeVisible();
   });
 });
+
