dd32eb3de5c0f18c0a9118cfffe0c47cde0c1ee3
Daniel Schmidt <danielmschmidt92@gmail.com>
2017-10-04 11:56:37 +0200

    Move matchers to generated code (#306)
    * generation: add tests for helper methods
    
    * generation: remove unused code part
    
    * generation: add transformed grey matchers
    
    * generation: remove prompt if there is no filtered method
    
    * generation: replace generated matchers
    
    * generation: generate GREYMatcher adapter code
    
    * generation: refactor hand written adapters to generated ones
    
    * generation: add support for Matcher ids
    
    This allows us to transform the rest of the matchers and it is a
    potential solution for other transformations regarding ids as well

--- generation/earl-grey/index.js
+++ generation/earl-grey/index.js
@@ -1,4 +1,5 @@
 const t = require("babel-types");
+const template = require("babel-template");
 const objectiveCParser = require("objective-c-parser");
 const generate = require("babel-generator").default;
 const fs = require("fs");
@@ -19,6 +20,19 @@ const isPoint = [
   generateTypeCheck("number", { selector: "y" })
 ];
 const isOneOf = generateIsOneOfCheck;
+const isGreyMatcher = ({ name }) => template(`
+  if (
+    typeof ARG !== "object" || 
+    ARG.type !== "Invocation" ||
+    typeof ARG.value !== "object" || 
+    typeof ARG.value.target !== "object" ||
+    ARG.value.target.value !== "GREYMatchers"
+  ) {
+    throw new Error('${name} should be a GREYMatcher, but got ' + JSON.stringify(ARG));
+  }
+`)({
+    ARG: t.identifier(name)
+  })
 
 // Constants
 const SUPPORTED_TYPES = [
@@ -30,6 +44,7 @@ const SUPPORTED_TYPES = [
   "NSString *",
   "NSString",
   "NSUInteger",
+  "id<GREYMatcher>"
 ];
 
 /**
@@ -172,8 +187,13 @@ function addArgumentTypeSanitizer(json) {
   return json.type;
 }
 
+// These types need no wrapping with {type: ..., value: }
+const plainArgumentTypes = ["id<GREYMatcher>"];
+function shouldBeWrapped({ type }) {
+  return !plainArgumentTypes.includes(type);
+}
 function createReturnStatement(className, json) {
-  const args = json.args.map(arg =>
+  const args = json.args.map(arg => shouldBeWrapped(arg) ?
     t.objectExpression([
       t.objectProperty(
         t.identifier("type"),
@@ -183,7 +203,7 @@ function createReturnStatement(className, json) {
         t.identifier("value"),
         addArgumentContentSanitizerCall(arg)
       )
-    ])
+    ]) : addArgumentContentSanitizerCall(arg)
   );
 
   return t.returnStatement(
@@ -214,16 +234,12 @@ function createTypeCheck(json) {
     "NSDate *": isNumber,
     GREYDirection: isOneOf(["left", "right", "up", "down"]),
     GREYContentEdge: isOneOf(["left", "right", "top", "bottom"]),
-    GREYPinchDirection: isOneOf(["outward", "inward"])
+    GREYPinchDirection: isOneOf(["outward", "inward"]),
+    "id<GREYMatcher>": isGreyMatcher,
   };
 
   const typeCheckCreator = typeInterfaces[json.type];
   const isListOfChecks = typeCheckCreator instanceof Array;
-
-  if (typeof typeCheckCreator !== "function" && !isListOfChecks) {
-    console.info("Could not find ", json);
-    return;
-  }
 
   return isListOfChecks
     ? typeCheckCreator.map(singleCheck => singleCheck(json))
@@ -248,10 +264,13 @@ module.exports = function(files) {
     fs.writeFileSync(outputFile, code, "utf8");
 
     // Output methods that were not created due to missing argument support
-    console.log(`Could not generate the following methods for ${json.name}`);
-    const unsupportedMethods = json.methods.filter(x => !filterMethodsWithUnsupportedParams(x)).forEach(method => {
-      const methodArgs = method.args.filter(methodArg => !SUPPORTED_TYPES.includes(methodArg.type)).map(methodArg => methodArg.type);
-      console.log(`\t ${method.name} misses ${methodArgs}`);
-    });
+    const unsupportedMethods = json.methods.filter(x => !filterMethodsWithUnsupportedParams(x));
+    if (unsupportedMethods.length) {
+      console.log(`Could not generate the following methods for ${json.name}`);
+      unsupportedMethods.forEach(method => {
+        const methodArgs = method.args.filter(methodArg => !SUPPORTED_TYPES.includes(methodArg.type)).map(methodArg => methodArg.type);
+        console.log(`\t ${method.name} misses ${methodArgs}`);
+      });
+    }
   });
 };
