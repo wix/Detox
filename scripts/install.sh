92dba07e6ebb41f436c21e0786361674ef50ce7c
Rotem M <rotemm@wix.com>
2017-10-07 00:14:53 +0300

    CIDD

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -ex
 
-source ~/.nvm/nvm.sh
+#curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.1/install.sh | bash
+export NVM_DIR="$HOME/.nvm" && [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
 nvm install stable
-node --version
 
 npm install -g lerna@2.1.2 >/dev/null 2>&1
 npm install -g react-native-cli >/dev/null 2>&1
