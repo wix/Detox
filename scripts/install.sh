ca69cbcd4b681e300b8e90525e659c4c6667818e
Rotem M <rotemm@wix.com>
2017-10-07 00:22:32 +0300

    CIDD

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,6 +1,6 @@
-#!/bin/bash -ex
+#!/bin/bash -e
 
-#curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.1/install.sh | bash
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.5/install.sh | bash
 export NVM_DIR="$HOME/.nvm" && [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
 nvm install stable
 
