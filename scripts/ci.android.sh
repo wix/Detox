a9e8ef0ba6adba9ea92b039c40f9732c9dc6fe8e
Rotem M <rotemm@wix.com>
2017-10-14 01:58:26 +0300

    create android emulator pre-test

--- scripts/ci.android.sh
+++ scripts/ci.android.sh
@@ -2,6 +2,7 @@
 
 $(dirname "$0")/ci.sh
 
+echo no | "$ANDROID_HOME"/tools/bin/avdmanager create avd --force --name Nexus_5X_API_26 -t android-26 --abi armeabi-v7a --device "Nexus 5X"
 pushd detox/test
 npm run build:android
 npm run e2e:android
