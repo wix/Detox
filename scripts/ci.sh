3192050eb531111863e6675db05f158539b5ebbc
Rotem M <rotemm@wix.com>
2017-10-14 01:14:57 +0300

    cont...

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -1,5 +1,12 @@
 #!/bin/bash -e
 
+case "$REACT_NATIVE_VERSION" in
+     0.44.2)
+          echo "Applying git patch to convert React Native version in test project to $REACT_NATIVE_VERSION"
+          git apply scripts/testProjRN49to44.diff
+          ;;
+esac
+
 lerna bootstrap
 lerna run --ignore detox-demo* build
 lerna run --ignore detox-demo* test
