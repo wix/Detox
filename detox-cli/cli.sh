f28d8ab3c96cf95a7f604b663a257a9fa254b23d
Rotem M <rotemm@wix.com>
2017-10-17 17:10:22 +0300

    fix an issue where paths with spaces break detox-cli

--- detox-cli/cli.sh
+++ detox-cli/cli.sh
@@ -3,10 +3,10 @@
 DETOX_PATH="${PWD}/node_modules/detox";
 DETOX_PACKAGE_JSON_PATH="${DETOX_PATH}/package.json";
 
-if [ -f $DETOX_PACKAGE_JSON_PATH ]; then
+if [ -f "${DETOX_PACKAGE_JSON_PATH}" ]; then
   "${PWD}/node_modules/.bin/detox" $@
 else
-  echo $DETOX_PACKAGE_JSON_PATH
+  echo "${DETOX_PACKAGE_JSON_PATH}"
   echo "detox is not installed in this directory"
   exit 1
 fi
