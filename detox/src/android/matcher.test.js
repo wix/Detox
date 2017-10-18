676278bf35374ac23964d1141c55303e1a921b93
Daniel Schmidt <danielmschmidt92@gmail.com>
2017-10-18 19:33:04 +0200

    Add snapshot tests for matchers
    This enables us to refactor matchers more easily

---
+++ detox/src/android/matcher.test.js
@@ -0,0 +1,26 @@
+describe('android matcher', async () => {
+  let m;
+
+  beforeEach(() => {
+    m = require('./matcher');
+  });
+
+  // These snapshots preserve the output of the matchers against unwanted change
+  it("snapshots", () => {
+    const textBasedMatchers = ['LabelMatcher', 'IdMatcher', 'TypeMatcher', 'TextMatcher', 'ValueMatcher'];
+    const chainableMatchers = ['withAncestor', 'withDescendant', 'and', 'or'];
+    const noArgsMatchers = ['VisibleMatcher', 'NotVisibleMatcher', 'ExistsMatcher', 'NotExistsMatcher'];
+
+    textBasedMatchers.forEach(matcher => {
+      expect(new m[matcher]("mySelector")._call()).toMatchSnapshot();
+    });
+
+    chainableMatchers.forEach(matcher => {
+      expect(new m.LabelMatcher("mySelector")[matcher](new m.LabelMatcher("myOtherSelector"))._call()).toMatchSnapshot()
+    });
+
+    noArgsMatchers.forEach(matcher => {
+      expect(new m[matcher]()._call()).toMatchSnapshot();
+    });
+  });
+});
