8e670415202c155d3fc723ea0e9aa41d0f566a8a
vasyas <vasyl@stashuk.com>
2017-11-13 14:35:24 +0200

    extracted common code from emulator & attached drivers

--- detox/src/devices/AndroidDriver.js
+++ detox/src/devices/AndroidDriver.js
@@ -110,6 +110,27 @@ class AndroidDriver extends DeviceDriverBase {
     return 'android';
   }
 
+  async findDeviceId(filter) {
+    const adbDevices = await this.adb.devices();
+    const filteredDevices = _.filter(adbDevices, filter);
+
+    let adbName;
+    switch (filteredDevices.length) {
+      case 1:
+        const adbDevice = filteredDevices[0];
+        adbName = adbDevice.adbName;
+        break;
+      case 0:
+        throw new Error(`Could not find '${name}' on the currently ADB attached devices, 
+      try restarting adb 'adb kill-server && adb start-server'`);
+        break;
+      default:
+        throw new Error(`Got more than one device corresponding to the name: ${name}`);
+    }
+
+    return adbName;
+  }
+
   async setURLBlacklist(urlList) {
     const call = invoke.call(invoke.Android.Class(EspressoDetox), 'setURLBlacklist', urlList);
     await this.invocationManager.execute(call);
