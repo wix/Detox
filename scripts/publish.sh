af4151769876a626ea8e1868d91430a98bb0a3f9
Leo Natan <lnatan@wix.com>
2017-11-21 21:16:45 +0200

    Support new labels

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -7,7 +7,7 @@ fi
 
 lerna publish --cd-version "$VERSION_TYPE" --yes --skip-git
 VERSION=`node -p "require('./detox/package.json').version"`
-github_changelog_generator --future-release "$VERSION" --bugs-label "**Fixed Bugs**" --enhancement-label "**Enhancements**" --issues-label "**Closed Issues**" --pr-label "**Merged Pull Requests**" --no-verbose
+github_changelog_generator --future-release "$VERSION" --no-verbose
 git add -A
 git commit -m "[skip ci] Publish $VERSION"
 git tag "$VERSION"
