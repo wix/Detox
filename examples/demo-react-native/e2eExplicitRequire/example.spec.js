c091a9caebbb19f058c59c337443864375f64d0a
Rotem M <rotemm@wix.com>
2017-12-11 12:37:26 +0200

    - Added support for custom specs with cli tool - Updated e2e tests for demo-react-native

--- examples/demo-react-native/e2eExplicitRequire/example.spec.js
+++ examples/demo-react-native/e2eExplicitRequire/example.spec.js
@@ -6,7 +6,6 @@ describe('Example', () => {
   });
   
   it('should have welcome screen', async () => {
-    await waitFor(element(by.id('welcome'))).toExist().withTimeout(2000);
     await expect(element(by.id('welcome'))).toBeVisible();
   });
   
@@ -18,5 +17,10 @@ describe('Example', () => {
   it('should show world screen after tap', async () => {
     await element(by.id('world_button')).tap();
     await expect(element(by.text('World!!!'))).toBeVisible();
+  });
+
+  it('waitFor should be exported', async () => {
+    await waitFor(element(by.id('welcome'))).toExist().withTimeout(2000);
+    await expect(element(by.id('welcome'))).toExist();
   });
 });
