a69ce4100b9f38ba61d1139a1360ade93275875d
Daniel Schmidt <danielmschmidt92@gmail.com>
2018-01-09 15:13:20 +0100

    add detox matcher to generated code (#492)

---
+++ detox/src/android/espressoapi/DetoxMatcher.js
@@ -0,0 +1,67 @@
+/**
+
+	This code is generated.
+	For more information see generation/README.md.
+*/
+
+
+
+class DetoxMatcher {
+  static matcherForSufficientlyVisible() {
+    return {
+      target: {
+        type: "Class",
+        value: "com.wix.detox.espresso.DetoxMatcher"
+      },
+      method: "matcherForSufficientlyVisible",
+      args: []
+    };
+  }
+
+  static matcherForNotVisible() {
+    return {
+      target: {
+        type: "Class",
+        value: "com.wix.detox.espresso.DetoxMatcher"
+      },
+      method: "matcherForNotVisible",
+      args: []
+    };
+  }
+
+  static matcherForNotNull() {
+    return {
+      target: {
+        type: "Class",
+        value: "com.wix.detox.espresso.DetoxMatcher"
+      },
+      method: "matcherForNotNull",
+      args: []
+    };
+  }
+
+  static matcherForNull() {
+    return {
+      target: {
+        type: "Class",
+        value: "com.wix.detox.espresso.DetoxMatcher"
+      },
+      method: "matcherForNull",
+      args: []
+    };
+  }
+
+  static matcherForAnything() {
+    return {
+      target: {
+        type: "Class",
+        value: "com.wix.detox.espresso.DetoxMatcher"
+      },
+      method: "matcherForAnything",
+      args: []
+    };
+  }
+
+}
+
+module.exports = DetoxMatcher;
