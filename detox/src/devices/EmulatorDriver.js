ca67b24a9fde6211c78bdcc65cc351846de0ad6c
Donatas Petrauskas <donatasp@wix.com>
2018-02-02 14:12:24 +0200

    Move new methods to AndroidDriver.
    Now it's possible to record videos and screenshots when running on device.

--- detox/src/devices/EmulatorDriver.js
+++ detox/src/devices/EmulatorDriver.js
@@ -8,7 +8,6 @@ const AndroidDriver = require('./AndroidDriver');
 const ini = require('ini');
 const fs = require('fs');
 const os = require('os');
-const AndroidArtifact = require('./android/AndroidArtifact');
 
 class EmulatorDriver extends AndroidDriver {
   constructor(client) {
@@ -83,84 +82,6 @@ class EmulatorDriver extends AndroidDriver {
     const telnet = new EmulatorTelnet();
     await telnet.connect(port);
     await telnet.kill();
-  }
-
-  async takeScreenshot(deviceId) {
-    const screenshotPath = this._getScreenshotPath(this._nextId());
-    await this.adb.screencap(deviceId, screenshotPath);
-    return new AndroidArtifact(screenshotPath, this.adb, deviceId);
-  }
-
-  async startVideo(deviceId) {
-    const adb = this.adb;
-    const videoPath = this._getVideoPath(this._nextId());
-    let {width, height} = await adb.getScreenSize();
-    let promise = spawnRecording();
-    promise.catch(handleRecordingTermination);
-
-    // XXX: ugly loop to make sure we continue only if recording has begun.
-    let recording = false;
-    let size = 0;
-    while (!recording) {
-      size = 0;
-      recording = true;
-      try {
-        size = await adb.getFileSize(deviceId, videoPath);
-        if (size < 1) {
-          recording = false;
-        }
-      } catch (e) {
-        recording = false;
-      }
-    }
-
-    this._recordings[deviceId] = {
-      process: promise.childProcess,
-      promise,
-      videoPath
-    };
- 
-    function handleRecordingTermination(result) {
-      const proc = result.childProcess;
-      // XXX: error code -38 (= 218) means that encoder was not able to create
-      // video of current size, let's try smaller resolution.
-      if (proc.exitCode === 218) {
-        width >>= 1;
-        height >>= 1;
-        promise = spawnRecording();
-        promise.catch(handleRecordingTermination);
-      }
-    }
-
-    function spawnRecording() {
-      return adb.screenrecord(deviceId, videoPath, width, height);
-    }
-  }
-
-  stopVideo(deviceId) {
-    if (this._recordings[deviceId]) {
-      const {process, promise, videoPath} = this._recordings[deviceId];
-      delete this._recordings[deviceId];
-      return new Promise((resolve) => {
-        promise.catch(() => resolve(
-          new AndroidArtifact(videoPath, this.adb, deviceId)
-        ));
-        process.kill(2);
-      });
-    }
-    return Promise.resolve(null);
-  }
-
-  _nextId() {
-    return this._uniqueId++;
-  }
-
-  _getVideoPath(id) {
-    return `/sdcard/recording-${id}.mp4`;
-  }
-
-  _getScreenshotPath(id) {
-    return `/sdcard/screenshot-${id}.png`;
   }
 }
 
