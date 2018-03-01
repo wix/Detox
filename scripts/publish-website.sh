878d38278c90c3a346fdcc819f1c757bf78691fd
Daniel Schmidt <danielmschmidt92@gmail.com>
2018-03-01 08:28:53 +0100

    abort publish on PRs
    It seems like $TRAVIS_BRANCH describes the branch to be merged in
    on Pull Requests, therefore we need to check if it is a PR
    individually

--- scripts/publish-website.sh
+++ scripts/publish-website.sh
@@ -2,14 +2,16 @@
 
 set -e
 
-if [[ "$TRAVIS_BRANCH" != "master" ]]; then 
-  echo 'Not deploying the website'; 
+BRANCH=$(if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then echo $TRAVIS_BRANCH; else echo $TRAVIS_PULL_REQUEST_BRANCH; fi)
+
+if [[ "$BRANCH" != "master" ]]; then 
+  echo 'Not deploying the website because we are not on master'; 
   exit 0; 
-else
-  git config --global user.email "$GIT_USER@users.noreply.github.com";
-  git config --global user.name "Wix";
-  echo "machine github.com login $GIT_USER password $GIT_TOKEN" > ~/.netrc;
-  cd website;
-  npm install;
-  GIT_USER=$GIT_USER CURRENT_BRANCH=master npm run publish-gh-pages;
 fi
+
+git config --global user.email "$GIT_USER@users.noreply.github.com";
+git config --global user.name "Wix";
+echo "machine github.com login $GIT_USER password $GIT_TOKEN" > ~/.netrc;
+cd website;
+npm install;
+GIT_USER=$GIT_USER CURRENT_BRANCH=master npm run publish-gh-pages;
