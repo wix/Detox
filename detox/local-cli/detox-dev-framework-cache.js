048924d9e84a5dd3101def6c717cb453e0138ab1
Rotem M <rotemm@wix.com>
2018-03-14 19:08:40 +0200

    Use the same script for both dev and release modes (if a user has ios folder, it will use it, else, will extract zip and build from it ios_src)

--- detox/local-cli/detox-dev-framework-cache.js
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env node
-
-const path = require('path');
-const cp = require('child_process');
-
-cp.execSync(path.join(__dirname, '../scripts/build_framework_dev.ios.sh'), { stdio: 'inherit' });
