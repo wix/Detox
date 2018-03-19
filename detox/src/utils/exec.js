598239689411d205fc215f2bb478815426fe3eb4
Simon Buchan <simon.buchan@skilitics.com>
2018-03-19 13:52:10 +1300

    Replace CRLFs in exec.js, fixes running the emulator on windows.

--- detox/src/utils/exec.js
+++ detox/src/utils/exec.js
@@ -47,6 +47,15 @@ async function execWithRetriesAndLogs(bin, options, statusLogs, retries = 10, in
   //  log.error(`${_operationCounter}: stderr:`, result.stderr);
   //}
 
+  if (process.platform === 'win32') {
+    if (result.stdout) {
+      result.stdout = result.stdout.replace(/\r\n/g, '\n');
+    }
+    if (result.stderr) {
+      result.stderr = result.stderr.replace(/\r\n/g, '\n');
+    }
+  }
+
   return result;
 }
 
