a6169b225f1ba1342637fb138719216b068b4a49
Simon Buchan <simon.buchan@skilitics.com>
2018-03-19 16:21:48 +1300

    Make eslint pass on JS scripts.

--- detox/scripts/build.js
+++ detox/scripts/build.js
@@ -2,46 +2,29 @@
 
 // Javascript for windows support.
 
-const fs = require('fs-extra');
-const childProcess = require('child_process');
+if (process.argv[2] === "android" || process.argv[3] === "android") {
+	console.log("\nBuilding Detox aars");
+	const aars = [
+		"detox-minReactNative44-debug.aar",
+		"detox-minReactNative46-debug.aar",
+		"detox-minReactNative44-release.aar",
+		"detox-minReactNative46-release.aar"
+	];
+	aars.forEach(aar => {
+		fs.removeSync(aar);
+	});
 
-if (process.platform === 'darwin') {
-    console.log("\nPackaging Detox iOS sources");
+	childProcess.execFileSync(
+		"android/gradlew",
+		["assembleDebug", "assembleRelease"],
+		{
+			cwd: "android",
+			stdio: "inherit",
+			shell: true
+		}
+	);
 
-    fs.removeSync('Detox-ios-src.tbz');
-    // Prepare Earl Grey without building
-    childProcess.execFileSync('ios/EarlGrey/Scripts/setup-earlgrey.sh', {
-        stdio: ['ignore', 'ignore', 'inherit'],
-    });
-    childProcess.execSync('find ./ios -name Build -type d -exec rm -rf {} \\;', {
-        stdio: ['ignore', 'ignore', 'inherit'],
-    });
-
-    childProcess.execSync('tar -cjf ../Detox-ios-src.tbz .', {
-        cwd: 'ios',
-        stdio: ['ignore', 'inherit', 'inherit'],
-    });
-}
-
-if (process.argv[2] === 'android' || process.argv[3] === 'android') {
-    console.log("\nBuilding Detox aars");
-    const aars = [
-        'detox-minReactNative44-debug.aar',
-        'detox-minReactNative46-debug.aar',
-        'detox-minReactNative44-release.aar',
-        'detox-minReactNative46-release.aar',
-    ];
-    aars.forEach(aar => {
-        fs.removeSync(aar);
-    });
-
-    childProcess.execFileSync('android/gradlew', ['assembleDebug', 'assembleRelease'], {
-        cwd: 'android',
-        stdio: 'inherit',
-        shell: true,
-    });
-
-    aars.forEach(aar => {
-        fs.copySync(`android/detox/build/outputs/aar/${aar}`, aar);
-    });
+	aars.forEach(aar => {
+		fs.copySync(`android/detox/build/outputs/aar/${aar}`, aar);
+	});
 }
--- detox/scripts/postinstall.js
+++ detox/scripts/postinstall.js
@@ -1,8 +1,8 @@
 #!/usr/bin/env node
 
 // Javascript for windows support.
-if (process.platform === 'darwin') {
-    require('child_process').execSync(`${__dirname}/build_framework.ios.sh`, {
-        stdio: 'inherit',
-    });
+if (process.platform === "darwin") {
+	require("child_process").execSync(`${__dirname}/build_framework.ios.sh`, {
+		stdio: "inherit"
+	});
 }
