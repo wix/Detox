ae1d48e86a97147abf28e4dfc15b59010b5a0233
Leo Natan <LeoNatan@users.noreply.github.com>
2018-03-20 15:58:48 +0200

    Update s-datePicker.js

--- detox/test/e2e/s-datePicker.js
+++ detox/test/e2e/s-datePicker.js
@@ -7,7 +7,7 @@ describe('DatePicker', () => {
       await element(by.text('DatePicker')).tap();
     });
     
-    it('datePicker should be scroll and change time', async () => {
+    it('datePicker should trigger change handler correctly', async () => {
       await element(by.type('UIPickerView')).setColumnToValue(1,"6");
       await element(by.type('UIPickerView')).setColumnToValue(2,"34");
       await expect(element(by.id("timeLabel"))).toHaveText('choosenTime is 6:34');
