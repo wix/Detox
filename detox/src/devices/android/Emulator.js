36449c937aa16dd3324caa04b84b58e2cbbdf5e1
Shalom Yerushalmy <yershalom@gmail.com>
2018-04-24 12:56:43 +0300

    Fix line for command

--- detox/src/devices/android/Emulator.js
+++ detox/src/devices/android/Emulator.js
@@ -23,14 +23,14 @@ class Emulator {
   }
 
   async boot(emulatorName) {
-    // const cmd = `-verbose -gpu host -no-audio @${emulatorName}`;
-    const cmd = `echo "hello"`;
+    const cmd = `-verbose -gpu host -no-audio @${emulatorName}`;
     log.verbose(this.emulatorBin, cmd);
     const tempLog = `./${emulatorName}.log`;
     const stdout = fs.openSync(tempLog, 'a');
     const stderr = fs.openSync(tempLog, 'a');
     const tail = new Tail(tempLog);
-    const promise = spawn(this.emulatorBin, _.split(cmd, ' '), {detached: true, stdio: ['ignore', stdout, stderr]});
+    // const promise = spawn(this.emulatorBin, _.split(cmd, ' '), {detached: true, stdio: ['ignore', stdout, stderr]});
+    const promise = spawn('echo "hello"', {detached: true, stdio: ['ignore', stdout, stderr]});
 
     const childProcess = promise.childProcess;
     childProcess.unref();
