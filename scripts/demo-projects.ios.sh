2daaf6827245473aa119b536bac5bca5a153c10d
Shalom Yerushalmy <yershalom@gmail.com>
2018-04-26 12:48:24 +0300

    Remove lerna bootstrap from demo project script

--- scripts/demo-projects.ios.sh
+++ scripts/demo-projects.ios.sh
@@ -2,8 +2,6 @@
 
 source $(dirname "$0")/travis_logger.sh
 
-run_f "lerna bootstrap"
-
 pushd examples/demo-react-native
 run_f "detox build -c ios.sim.release"
 run_f "detox test -c ios.sim.release"
