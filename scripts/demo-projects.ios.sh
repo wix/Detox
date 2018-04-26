c683c05ac23e7d341cec2c8d04b1c3bc9cf57476
Shalom Yerushalmy <yershalom@gmail.com>
2018-04-26 13:08:33 +0300

    Add node debug to demo project

--- scripts/demo-projects.ios.sh
+++ scripts/demo-projects.ios.sh
@@ -1,8 +1,11 @@
 #!/bin/bash -e
 
+
+node -v
 source $(dirname "$0")/travis_logger.sh
 
 pushd examples/demo-react-native
+node -v
 run_f "detox build -c ios.sim.release"
 run_f "detox test -c ios.sim.release"
 run_f "detox test -c ios.sim.release --specs e2eExplicitRequire --runner-config e2eExplicitRequire/mocha.opts"
