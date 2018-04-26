2ef56c8b1e0e230ba6458113f9423c41850b380e
Shalom Yerushalmy <yershalom@gmail.com>
2018-04-26 13:14:39 +0300

    Revert "Add node debug to demo project"
    This reverts commit c683c05ac23e7d341cec2c8d04b1c3bc9cf57476.

--- scripts/demo-projects.ios.sh
+++ scripts/demo-projects.ios.sh
@@ -1,11 +1,8 @@
 #!/bin/bash -e
 
-
-node -v
 source $(dirname "$0")/travis_logger.sh
 
 pushd examples/demo-react-native
-node -v
 run_f "detox build -c ios.sim.release"
 run_f "detox test -c ios.sim.release"
 run_f "detox test -c ios.sim.release --specs e2eExplicitRequire --runner-config e2eExplicitRequire/mocha.opts"
