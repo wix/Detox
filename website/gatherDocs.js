056027fb61ed1c8427276ba12e8c930e5012c8cf
Daniel Schmidt <dschmidt@mesosphere.com>
2018-04-28 08:26:27 +0200

    refactor gathering documents functionality into smaller chunks

--- website/gatherDocs.js
+++ website/gatherDocs.js
@@ -1,19 +1,12 @@
 #!/usr/bin/env node
 const fs = require("fs-extra");
 const git = require("nodegit");
-const execSync = require('child_process').execSync;
+const execSync = require("child_process").execSync;
 const { major } = require("semver");
-const REPO_URL = "https://github.com/wix/detox.git"
-
-// From https://gist.github.com/joerx/3296d972735adc5b4ec1
-function clearRequireCache() {
-  Object.keys(require.cache).forEach(function (key) {
-    delete require.cache[key];
-  });
-}
+const REPO_URL = "https://github.com/wix/detox.git";
 
 async function getVersions() {
-  const tmp = fs.mkdtempSync('detox-versions');
+  const tmp = fs.mkdtempSync("detox-versions");
   const repo = await git.Clone(REPO_URL, tmp);
   const tags = await git.Tag.list(repo);
 
@@ -28,44 +21,73 @@ async function getVersions() {
 
 const sidebars = [];
 
-(async function () {
-  const versions = await getVersions();
+async function cleanupExistingVersions() {
   console.log("Cleanup versioned docs");
-  await fs.remove("./versions.json")
+  await fs.remove("./versions.json");
   await fs.emptyDir("./versioned_docs");
   await fs.emptyDir("./versioned_sidebars");
+}
+
+async function checkoutVersion(version) {
+  const tempDir = fs.mkdtempSync(`detox-${version}`);
+  const repo = await git.Clone(REPO_URL, tempDir);
+
+  console.log("Checking out version", version);
+  await repo.checkoutBranch(version);
+  return tempDir;
+}
+
+function applyTemporalFix(tempDir) {
+  // We need to do this as we forgot the header in this one file, but added git tags with it included
+  console.log("Temporary fix for Guide.DebuggingInXcode");
+  const header =
+    "---\nid: Guide.DebuggingInXcode\ntitle: Debugging in Xcode During Detox Tests\n---";
+  execSync(
+    `echo "${header}" | cat - Guide.DebuggingInXcode.md > /tmp/out && mv /tmp/out Guide.DebuggingInXcode.md`,
+    {
+      cwd: tempDir + "/docs"
+    }
+  );
+}
+
+function generateAndCopyDocusaurusVersion(tempDir, version) {
+  console.log("Generating versioned doc for", version);
+  execSync(
+    `npm install && rm versions.json && rm -rf {versioned_docs,versioned_sidebars} && npm run version ${version}`,
+    { cwd: tempDir + "/website" }
+  );
+
+  console.log("Copy versioned doc");
+  fs.copySync(
+    `${tempDir}/website/versioned_docs/version-${version}`,
+    `./versioned_docs/version-${version}`
+  );
+
+  console.log("Copy sidebar into versioned_sidebars");
+  fs.copyFileSync(
+    `${tempDir}/website/versioned_sidebars/version-${version}-sidebars.json`,
+    `./versioned_sidebars/version-${version}-sidebars.json`
+  );
+}
+
+async function cleanUp(tempDir) {
+  console.log("Cleanup temporary clone");
+  await fs.remove(tempDir);
+}
+
+(async function() {
+  const versions = await getVersions();
+  await cleanupExistingVersions();
 
   fs.writeFileSync("./versions.json", JSON.stringify(versions), "utf8");
+
   for (let version of versions) {
-    const tempDir = fs.mkdtempSync(`detox-${version}`);
     console.log("Clone repository into tmp directory");
-    const repo = await git.Clone(REPO_URL, tempDir);
+    const tempDir = await checkoutVersion(version);
+    applyTemporalFix(tempDir);
+    generateAndCopyDocusaurusVersion(tempDir, version);
 
-    console.log("Checking out version", version);
-    await repo.checkoutBranch(version);
-
-    // We need to do this as we forgot the header in this one file, but added git tags with it included
-    console.log("Temporary fix for Guide.DebuggingInXcode")
-    const header = '---\nid: Guide.DebuggingInXcode\ntitle: Debugging in Xcode During Detox Tests\n---';
-    execSync(
-      `echo "${header}" | cat - Guide.DebuggingInXcode.md > /tmp/out && mv /tmp/out Guide.DebuggingInXcode.md`,
-      {
-        cwd: tempDir + "/docs"
-      }
-    );
-
-    console.log("Generating versioned doc for", version)
-    execSync(`npm install && rm versions.json && rm -rf {versioned_docs,versioned_sidebars} && npm run version ${version}`, { cwd: tempDir + '/website' })
-
-    console.log("Copy versioned doc");
-    fs.copySync(`${tempDir}/website/versioned_docs/version-${version}`, `./versioned_docs/version-${version}`);
-
-    console.log("Copy sidebar into versioned_sidebars");
-    fs.copyFileSync(`${tempDir}/website/versioned_sidebars/version-${version}-sidebars.json`, `./versioned_sidebars/version-${version}-sidebars.json`)
-
-    console.log("Cleanup temporary clone");
-    await fs.remove(tempDir);
+    await cleanUp(tempDir);
     console.log(`Done with ${version}\n\n`);
   }
 })();
-
