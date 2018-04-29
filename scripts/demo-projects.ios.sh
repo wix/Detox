664728b315a167f9f4c588de983635132361fbe1
Shalom Yerushalmy <yershalom@gmail.com>
2018-04-29 13:56:03 +0300

    Comment one example project

--- scripts/demo-projects.ios.sh
+++ scripts/demo-projects.ios.sh
@@ -6,11 +6,11 @@ if [ ! -z ${REACT_NATIVE_VERSION} ]; then
   node scripts/change_react_native_version.js "examples/demo-react-native" ${REACT_NATIVE_VERSION}
 fi
 
-pushd examples/demo-react-native
-run_f "detox build -c ios.sim.release"
-run_f "detox test -c ios.sim.release"
-run_f "detox test -c ios.sim.release --specs e2eExplicitRequire --runner-config e2eExplicitRequire/mocha.opts"
-popd
+#pushd examples/demo-react-native
+#run_f "detox build -c ios.sim.release"
+#run_f "detox test -c ios.sim.release"
+#run_f "detox test -c ios.sim.release --specs e2eExplicitRequire --runner-config e2eExplicitRequire/mocha.opts"
+#popd
 
 if [ ! -z ${REACT_NATIVE_VERSION} ]; then
   node scripts/change_react_native_version.js "examples/demo-react-native-jest" ${REACT_NATIVE_VERSION}
