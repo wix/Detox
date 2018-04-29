2779eccd1b76ef2c29c796cc1500f193ce6e137e
Shalom Yerushalmy <yershalom@gmail.com>
2018-04-29 10:23:11 +0300

    Add change react-native version from env var

--- scripts/ci.android.sh
+++ scripts/ci.android.sh
@@ -8,7 +8,7 @@ popd
 #echo no | "$ANDROID_HOME"/tools/bin/avdmanager create avd --force --name Nexus_5X_API_26  --abi armeabi-v7a --device "Nexus 5X" -k system-images;android-26;default;armeabi-v7a
 
 pushd detox/test
-# Workaround until react android issue will be fixed
+# Workaround until react android issue will be fixed - react-native: 0.55
 mv node_modules/react-native/ReactAndroid/release.gradle node_modules/react-native/ReactAndroid/release.gradle.bak
 cp extras/release.gradle node_modules/react-native/ReactAndroid/
 
