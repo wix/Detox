c5022488a483de90a1526c2f8471cb4c98877d88
Shalom Yerushalmy <yershalom@gmail.com>
2018-04-30 08:51:15 +0300

    Add unit iOS to ci.ios.sh script

--- scripts/ci.ios.sh
+++ scripts/ci.ios.sh
@@ -1,10 +1,10 @@
 #!/bin/bash -e
 
-source $(dirname "$0")/travis_logger.sh
 source $(dirname "$0")/ci.sh
+
+run_f "$(dirname "$0")/unit.ios.sh"
 
 pushd detox/test
 run_f "npm run build:ios"
 run_f "npm run e2e:ios"
 popd
-#npm run release
