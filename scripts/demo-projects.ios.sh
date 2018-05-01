14a89d3e6ed3b998315ca26c19c5eb06ab28f38a
Shalom Yerushalmy <yershalom@gmail.com>
2018-05-01 10:49:37 +0300

    Remove one of the bootstrap from demo projects script

--- scripts/demo-projects.ios.sh
+++ scripts/demo-projects.ios.sh
@@ -4,6 +4,7 @@ source $(dirname "$0")/travis_logger.sh
 
 if [ ! -z ${REACT_NATIVE_VERSION} ]; then
   node scripts/change_react_native_version.js "examples/demo-react-native" ${REACT_NATIVE_VERSION}
+  node scripts/change_react_native_version.js "examples/demo-react-native-jest" ${REACT_NATIVE_VERSION}
 fi
 
 lerna bootstrap
@@ -13,12 +14,6 @@ run_f "detox build -c ios.sim.release"
 run_f "detox test -c ios.sim.release"
 run_f "detox test -c ios.sim.release --specs e2eExplicitRequire --runner-config e2eExplicitRequire/mocha.opts"
 popd
-
-if [ ! -z ${REACT_NATIVE_VERSION} ]; then
-  node scripts/change_react_native_version.js "examples/demo-react-native-jest" ${REACT_NATIVE_VERSION}
-fi
-
-lerna bootstrap
 
 pushd examples/demo-react-native-jest
 run_f "detox build -c ios.sim.release"
