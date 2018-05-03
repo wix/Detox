6349d91ec1232d0d537f6b21842749081971f202
Dmytro Ponomarenko <dmytropo@wix.com>
2018-05-03 11:01:14 +0300

    cover negative tests

--- detox/src/ios/PasteboardInfo.test.js
+++ detox/src/ios/PasteboardInfo.test.js
@@ -6,11 +6,20 @@ describe('PasteboardInfo', async () => {
         e = require('./expect');
     })
 
-    it('PasteboardInfo test', async() => {
+    it('PasteboardInfo positive test', async() => {
         e.expect(new PasteboardInfo({pbString : 'test'})).toHaveString('test');
         e.expect(new PasteboardInfo({pbImage : new Image})).toHaveImage();
         e.expect(new PasteboardInfo({pbColor : 'red'})).toHaveColor();
         e.expect(new PasteboardInfo({pbString : 'www.wix.com'})).toHaveURL('www.wix.com');
-    })
+    });
+
+    it('PasteboardInfo negative test', async() => {
+        e.expect(new PasteboardInfo({pbString : 'test'})).toHaveString('negative-test');
+        e.expect(new PasteboardInfo({pbString : undefined })).toHaveString('negative-test');
+        e.expect(new PasteboardInfo({pbImage : undefined})).toHaveImage();
+        e.expect(new PasteboardInfo({pbColor : undefined})).toHaveColor();
+        e.expect(new PasteboardInfo({pbString : 'www.negativeTest.com'})).toHaveURL('www.wix.com');
+        e.expect(new PasteboardInfo({pbString : undefined})).toHaveURL('www.wix.com');
+    });
 
 });  
