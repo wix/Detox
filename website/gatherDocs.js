a0d2cd3ab98ce0f35feec9a7ad8991fbd81e322a
Daniel Schmidt <danielmschmidt92@gmail.com>
2018-05-04 07:43:41 +0200

    fix checkout process

--- website/gatherDocs.js
+++ website/gatherDocs.js
@@ -28,9 +28,30 @@ async function cleanupExistingVersions() {
   await fs.emptyDir("./versioned_sidebars");
 }
 
+// https://stackoverflow.com/questions/29569913/switch-branch-tag-with-nodegit?utm_medium=organic&utm_source=google_rich_qa&utm_campaign=google_rich_qa
+function checkOutTag(repo, tag) {
+  return git.Reference
+    .dwim(repo, "refs/tags/" + tag)
+    .then(function (ref) {
+      return ref.peel(git.Object.TYPE.COMMIT);
+    })
+    .then(function (ref) {
+      return repo.getCommit(ref);
+    })
+    .then(function (commit) {
+      return git.Checkout
+        .tree(repo, commit, { checkoutStrategy: git.Checkout.STRATEGY.FORCE })
+        .then(function () {
+          return repo.setHeadDetached(commit, repo.defaultSignature,
+            "Checkout: HEAD " + commit.id());
+        })
+    });
+}
+
 async function checkoutVersion(repo, version) {
   console.log("Checking out version", version);
-  await repo.checkoutBranch(version);
+  repo.cleanup();
+  await checkOutTag(repo, version);
 }
 
 function applyTemporalFix(tempDir) {
