4de26a465ed766cdb9de602d02671d7651562e9a
Dmytro Ponomarenko <dmytropo@wix.com>
2018-05-07 12:58:17 +0300

    fix unit tests

--- detox/src/ios/PasteboardInfo.js
+++ detox/src/ios/PasteboardInfo.js
@@ -1,37 +1,47 @@
 const _ = require('lodash');
 
+const errors = {
+  EMPTY_STRING: `pasteboard doesn't have string value`,
+  STRING_NOT_EQUAL: `value is not equal to pasteboard string value`,
+  EMPTY_IMAGE : `pasteboard have not image`,
+  EMPTY_COLOR : `pasteboard have not color`,
+  EMPTY_URL : `pasteboard have not URL value`,
+  URL_NOT_EQUAL : `URL is not equal to pasteboard URL value`
+}
+
 class PasteboardInfo {
   constructor(data) {
     this._currentData = data;
   }
   async toHaveString(value) {
     if (_.isEmpty(this._currentData.pbString)) {
-      throw new Error(`pasteboard doesn't have string value`);
+      throw new Error(errors.EMPTY_STRING);
     }
     if (!_.isEqual(this._currentData.pbString, value)) {
-      throw new Error(`value is not equal to pasteboard string value`);
+      throw new Error(errors.STRING_NOT_EQUAL);
     }  
   }
   async toHaveImage() {
     if (_.isEmpty(this._currentData.pbImage)) {
-      throw new Error(`pasteboard have not image`);
+      throw new Error(errors.EMPTY_IMAGE);
     }
   }
   async toHaveColor() {
     if (_.isEmpty(this._currentData.pbColor)) {
-      throw new Error(`pasteboard have not color`);
+      throw new Error(errors.EMPTY_COLOR);
     }
   }
   async toHaveURL(value) {
     if (_.isEmpty(this._currentData.pbString)) {
-      throw new Error(`pasteboard have not URL value`);
+      throw new Error(errors.EMPTY_URL);
     }
     if (!_.isEqual(this._currentData.pbString, value)) {
-      throw new Error(`URL is not equal to pasteboard URL value`);
+      throw new Error(errors.URL_NOT_EQUAL);
     } 
   }
 }
 
 module.exports = {
     PasteboardInfo,
+    errors
 };
--- detox/src/ios/PasteboardInfo.test.js
+++ detox/src/ios/PasteboardInfo.test.js
@@ -1,4 +1,4 @@
-const {PasteboardInfo} = require('./PasteboardInfo')
+const {PasteboardInfo, errors} = require('./PasteboardInfo')
 describe('PasteboardInfo', async () => {
     let pb;
     let e;
@@ -7,19 +7,26 @@ describe('PasteboardInfo', async () => {
     })
 
     it('PasteboardInfo positive test', async() => {
-        e.expect(new PasteboardInfo({pbString : 'test'})).toHaveString('test');
-        e.expect(new PasteboardInfo({pbImage : new Image})).toHaveImage();
-        e.expect(new PasteboardInfo({pbColor : 'red'})).toHaveColor();
-        e.expect(new PasteboardInfo({pbString : 'www.wix.com'})).toHaveURL('www.wix.com');
+        await e.expect(new PasteboardInfo({pbString : 'test'})).toHaveString('test');
+        await e.expect(new PasteboardInfo({pbImage : 'new Image'})).toHaveImage();
+        await e.expect(new PasteboardInfo({pbColor : 'red'})).toHaveColor();
+        await e.expect(new PasteboardInfo({pbString : 'www.wix.com'})).toHaveURL('www.wix.com');
     });
 
     it('PasteboardInfo negative test', async() => {
-        e.expect(new PasteboardInfo({pbString : 'test'})).toHaveString('negative-test');
-        e.expect(new PasteboardInfo({pbString : undefined })).toHaveString('negative-test');
-        e.expect(new PasteboardInfo({pbImage : undefined})).toHaveImage();
-        e.expect(new PasteboardInfo({pbColor : undefined})).toHaveColor();
-        e.expect(new PasteboardInfo({pbString : 'www.negativeTest.com'})).toHaveURL('www.wix.com');
-        e.expect(new PasteboardInfo({pbString : undefined})).toHaveURL('www.wix.com');
+        await expectToThrow(() => e.expect(new PasteboardInfo({pbString : ''})).toHaveString('negative-test'), errors.EMPTY_STRING);
+        await expectToThrow(() => e.expect(new PasteboardInfo({pbString : 'test'})).toHaveString('negative-test'), errors.STRING_NOT_EQUAL);
+        await expectToThrow(() => e.expect(new PasteboardInfo({pbImage : undefined})).toHaveImage(), errors.EMPTY_IMAGE);
+        await expectToThrow(() => e.expect(new PasteboardInfo({pbColor : undefined})).toHaveColor(), errors.EMPTY_COLOR);
+        await expectToThrow(() => e.expect(new PasteboardInfo({pbString : 'www.negativeTest.com'})).toHaveURL('www.wix.com'), errors.URL_NOT_EQUAL);
+        await expectToThrow(() => e.expect(new PasteboardInfo({pbString : undefined})).toHaveURL('www.wix.com'), errors.EMPTY_URL);
     });
-
 });  
+
+async function expectToThrow(func, errorMessage) {
+    try {
+      await func();
+    } catch (ex) {
+      expect(ex).toEqual(new Error(errorMessage));
+    }
+  }
