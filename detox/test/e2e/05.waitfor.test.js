1f21ca482ce10a0e43642de9c615f033927a225c
Rotem M <rotemm@wix.com>
2018-05-08 18:35:50 +0300

    waitFor timeout increased

--- detox/test/e2e/05.waitfor.test.js
+++ detox/test/e2e/05.waitfor.test.js
@@ -6,19 +6,19 @@ describe('WaitFor', () => {
 
   it('should wait until an element is created and exists in layout', async () => {
     await expect(element(by.id('createdAndVisibleText'))).toNotExist();
-    await waitFor(element(by.id('createdAndVisibleText'))).toExist().withTimeout(2000);
+    await waitFor(element(by.id('createdAndVisibleText'))).toExist().withTimeout(20000);
     await expect(element(by.id('createdAndVisibleText'))).toExist();
   });
 
   it('should wait until an invisible element becomes visible', async() => {
     await expect(element(by.id('invisibleBecomingVisibleText'))).toBeNotVisible();
-    await waitFor(element(by.id('invisibleBecomingVisibleText'))).toBeVisible().withTimeout(2000);
+    await waitFor(element(by.id('invisibleBecomingVisibleText'))).toBeVisible().withTimeout(20000);
     await expect(element(by.id('invisibleBecomingVisibleText'))).toBeVisible();
   });
 
   it('should wait until an element is removed', async() => {
     await expect(element(by.id('deletedFromHierarchyText'))).toBeVisible();
-    await waitFor(element(by.id('deletedFromHierarchyText'))).toBeNotVisible().withTimeout(2000);
+    await waitFor(element(by.id('deletedFromHierarchyText'))).toBeNotVisible().withTimeout(20000);
     await expect(element(by.id('deletedFromHierarchyText'))).toBeNotVisible();
   });
 
