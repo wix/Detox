a27e2f2d417ce155e8295d86807a2e324edd228d
Daniel Schmidt <dschmidt@mesosphere.com>
2018-05-12 14:53:15 +0200

    apply prettier styles to code generation

--- generation/adapters/android.js
+++ generation/adapters/android.js
@@ -1,53 +1,48 @@
-const t = require("babel-types");
-const generator = require("../core/generator");
-const { callGlobal } = require("../helpers");
+const t = require('babel-types');
+const generator = require('../core/generator');
+const { callGlobal } = require('../helpers');
 
-const {
-	isNumber,
-	isString,
-	isBoolean,
-	isOfClass
-} = require("../core/type-checks");
+const { isNumber, isString, isBoolean, isOfClass } = require('../core/type-checks');
 
 const typeCheckInterfaces = {
 	Integer: isNumber,
 	Double: isNumber,
 	String: isString,
 	boolean: isBoolean,
-	"Matcher<View>": isOfClass("Matcher")
+	'Matcher<View>': isOfClass('Matcher')
 };
 
 const contentSanitizersForFunction = {
 	scrollInDirection: {
-		argumentName: "direction",
-		newType: "String",
-		name: "sanitize_android_direction",
-		value: callGlobal("sanitize_android_direction")
+		argumentName: 'direction',
+		newType: 'String',
+		name: 'sanitize_android_direction',
+		value: callGlobal('sanitize_android_direction')
 	},
 	swipeInDirection: {
-		argumentName: "direction",
-		newType: "String",
-		name: "sanitize_android_direction",
-		value: callGlobal("sanitize_android_direction")
+		argumentName: 'direction',
+		newType: 'String',
+		name: 'sanitize_android_direction',
+		value: callGlobal('sanitize_android_direction')
 	},
 	scrollToEdge: {
-		argumentName: "edge",
-		newType: "String",
-		name: "sanitize_android_edge",
-		value: callGlobal("sanitize_android_edge")
+		argumentName: 'edge',
+		newType: 'String',
+		name: 'sanitize_android_edge',
+		value: callGlobal('sanitize_android_edge')
 	},
-	"Matcher<View>": {
-		type: "String",
-		name: "sanitize_matcher",
-		value: callGlobal("sanitize_matcher")
+	'Matcher<View>': {
+		type: 'String',
+		name: 'sanitize_matcher',
+		value: callGlobal('sanitize_matcher')
 	}
 };
 
 const contentSanitizersForType = {
-	"Matcher<View>": {
-		type: "Invocation",
-		name: "sanitize_matcher",
-		value: callGlobal("sanitize_matcher")
+	'Matcher<View>': {
+		type: 'Invocation',
+		name: 'sanitize_matcher',
+		value: callGlobal('sanitize_matcher')
 	}
 };
 
@@ -55,18 +50,10 @@ module.exports = generator({
 	typeCheckInterfaces,
 	contentSanitizersForFunction,
 	contentSanitizersForType,
-	supportedTypes: [
-		"Integer",
-		"int",
-		"double",
-		"Double",
-		"boolean",
-		"String",
-		"Matcher<View>"
-	],
+	supportedTypes: ['Integer', 'int', 'double', 'Double', 'boolean', 'String', 'Matcher<View>'],
 	renameTypesMap: {
-		int: "Integer", // TODO: add test
-		double: "Double"
+		int: 'Integer', // TODO: add test
+		double: 'Double'
 	},
 	classValue: ({ package: pkg, name }) => `${pkg}.${name}`
 });
