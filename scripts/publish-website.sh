fd0fb8b80f95b51fd06f709f18ea22489b662e54
Shalom Yerushalmy <yershalom@gmail.com>
2018-05-15 15:55:44 +0300

    Add publish website to jenkins also

--- scripts/publish-website.sh
+++ scripts/publish-website.sh
@@ -1,15 +1,27 @@
 #!/bin/bash -e
 
-
-if [ "$TRAVIS_PULL_REQUEST" = "false" ] && [ "$TRAVIS_BRANCH" = "master" ]; then
-  git config user.email "$GIT_USER@users.noreply.github.com"
-  git config user.name "Wix"
-  echo "machine github.com login $GIT_USER password $GIT_TOKEN" > ~/.netrc
-
-  cd website
-  npm install
-  GIT_USER=$GIT_USER CURRENT_BRANCH=master npm run publish-gh-pages
+if [ $JENKINS_CI ]; then
+    if [ $JENKINS_MASTER ]; then
+        git config user.email "$GIT_USER@users.noreply.github.com"
+        git config user.name "Wix"
+        echo "machine github.com login $GIT_USER password $GIT_TOKEN" > ~/.netrc
+        cd website
+        npm install
+      GIT_USER=$GIT_USER CURRENT_BRANCH=master npm run publish-gh-pages
+    else
+        echo 'Not deploying the website'
+        exit 0;
+    fi
 else
-  echo 'Not deploying the website'
-  exit 0;
+    if [ "$TRAVIS_PULL_REQUEST" = "false" ] && [ "$TRAVIS_BRANCH" = "master" ]; then
+      git config user.email "$GIT_USER@users.noreply.github.com"
+      git config user.name "Wix"
+      echo "machine github.com login $GIT_USER password $GIT_TOKEN" > ~/.netrc
+      cd website
+      npm install
+      GIT_USER=$GIT_USER CURRENT_BRANCH=master npm run publish-gh-pages
+    else
+      echo 'Not deploying the website'
+      exit 0;
+    fi
 fi
