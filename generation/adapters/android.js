4e7a88e16baee6caa0a068c87989014b0c0bcaf2
Daniel Schmidt <danielmschmidt92@gmail.com>
2018-05-17 22:18:07 +0200

    apply prettier styles

--- generation/adapters/android.js
+++ generation/adapters/android.js
@@ -5,55 +5,55 @@ const { callGlobal } = require('../helpers');
 const { isNumber, isString, isBoolean, isOfClass } = require('../core/type-checks');
 
 const typeCheckInterfaces = {
-	Integer: isNumber,
-	Double: isNumber,
-	String: isString,
-	boolean: isBoolean,
-	'Matcher<View>': isOfClass('Matcher')
+  Integer: isNumber,
+  Double: isNumber,
+  String: isString,
+  boolean: isBoolean,
+  'Matcher<View>': isOfClass('Matcher')
 };
 
 const contentSanitizersForFunction = {
-	scrollInDirection: {
-		argumentName: 'direction',
-		newType: 'String',
-		name: 'sanitize_android_direction',
-		value: callGlobal('sanitize_android_direction')
-	},
-	swipeInDirection: {
-		argumentName: 'direction',
-		newType: 'String',
-		name: 'sanitize_android_direction',
-		value: callGlobal('sanitize_android_direction')
-	},
-	scrollToEdge: {
-		argumentName: 'edge',
-		newType: 'String',
-		name: 'sanitize_android_edge',
-		value: callGlobal('sanitize_android_edge')
-	},
-	'Matcher<View>': {
-		type: 'String',
-		name: 'sanitize_matcher',
-		value: callGlobal('sanitize_matcher')
-	}
+  scrollInDirection: {
+    argumentName: 'direction',
+    newType: 'String',
+    name: 'sanitize_android_direction',
+    value: callGlobal('sanitize_android_direction')
+  },
+  swipeInDirection: {
+    argumentName: 'direction',
+    newType: 'String',
+    name: 'sanitize_android_direction',
+    value: callGlobal('sanitize_android_direction')
+  },
+  scrollToEdge: {
+    argumentName: 'edge',
+    newType: 'String',
+    name: 'sanitize_android_edge',
+    value: callGlobal('sanitize_android_edge')
+  },
+  'Matcher<View>': {
+    type: 'String',
+    name: 'sanitize_matcher',
+    value: callGlobal('sanitize_matcher')
+  }
 };
 
 const contentSanitizersForType = {
-	'Matcher<View>': {
-		type: 'Invocation',
-		name: 'sanitize_matcher',
-		value: callGlobal('sanitize_matcher')
-	}
+  'Matcher<View>': {
+    type: 'Invocation',
+    name: 'sanitize_matcher',
+    value: callGlobal('sanitize_matcher')
+  }
 };
 
 module.exports = generator({
-	typeCheckInterfaces,
-	contentSanitizersForFunction,
-	contentSanitizersForType,
-	supportedTypes: ['Integer', 'int', 'double', 'Double', 'boolean', 'String', 'Matcher<View>'],
-	renameTypesMap: {
-		int: 'Integer', // TODO: add test
-		double: 'Double'
-	},
-	classValue: ({ package: pkg, name }) => `${pkg}.${name}`
+  typeCheckInterfaces,
+  contentSanitizersForFunction,
+  contentSanitizersForType,
+  supportedTypes: ['Integer', 'int', 'double', 'Double', 'boolean', 'String', 'Matcher<View>'],
+  renameTypesMap: {
+    int: 'Integer', // TODO: add test
+    double: 'Double'
+  },
+  classValue: ({ package: pkg, name }) => `${pkg}.${name}`
 });
--- generation/utils/downloadEspresso.js
+++ generation/utils/downloadEspresso.js
@@ -4,14 +4,14 @@ const downloadFileSync = require('download-file-sync');
 const fs = require('fs');
 
 module.exports = function downloadEspresso(fullyQualifiedClass) {
-	const tmpDir = os.tmpdir();
-	const path = fullyQualifiedClass.replace(/\./g, '/');
-	const fileContent = downloadFileSync(
-		`http://android.googlesource.com/platform/frameworks/testing/+/android-support-test/espresso/core/src/main/java/${path}.java?format=TEXT`
-	);
+  const tmpDir = os.tmpdir();
+  const path = fullyQualifiedClass.replace(/\./g, '/');
+  const fileContent = downloadFileSync(
+    `http://android.googlesource.com/platform/frameworks/testing/+/android-support-test/espresso/core/src/main/java/${path}.java?format=TEXT`
+  );
 
-	const result = Buffer.from(fileContent, 'base64').toString('ascii');
-	const filePath = tmpDir + '/download.java';
-	fs.writeFileSync(filePath, result);
-	return filePath;
+  const result = Buffer.from(fileContent, 'base64').toString('ascii');
+  const filePath = tmpDir + '/download.java';
+  fs.writeFileSync(filePath, result);
+  return filePath;
 };
