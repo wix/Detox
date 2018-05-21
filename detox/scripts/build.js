7f03f0b50505e83a8d4e1dbd71b9c075ca13e3ba
Simon Buchan <simon.buchan@skilitics.com>
2018-05-22 10:29:53 +1200

    Oops, missed building Detox-ios-src.tbz

--- detox/scripts/build.js
+++ detox/scripts/build.js
@@ -1,5 +1,26 @@
 #!/usr/bin/env node
 
+const childProcess = require('child_process');
+const fs = require('fs-extra');
+
+// Just make the usage a little prettier
+function sh(cmdline, opts) {
+  const args = cmdline.split(' ');
+  const cmd = args.shift();
+  return childProcess.execFileSync(cmd, args, opts);
+}
+
+if (process.platform === 'darwin') {
+  console.log("\nPackaging Detox iOS sources");
+
+  fs.removeSync('Detox-ios-src.tbz');
+  // Prepare Earl Grey without building
+  sh("ios/EarlGrey/Scripts/setup-earlgrey.sh");
+  sh("find ./ios -name Build -type d -exec rm -rf {} ;");
+
+  sh("tar -cjf ../Detox-ios-src.tbz .", { cwd: "ios" });
+}
+
 if (process.argv[2] === "android" || process.argv[3] === "android") {
 	console.log("\nBuilding Detox aars");
 	const aars = [
@@ -12,15 +33,11 @@ if (process.argv[2] === "android" || process.argv[3] === "android") {
 		fs.removeSync(aar);
 	});
 
-	childProcess.execFileSync(
-		"android/gradlew",
-		["assembleDebug", "assembleRelease"],
-		{
-			cwd: "android",
-			stdio: "inherit",
-			shell: true
-		}
-	);
+	sh("./gradlew assembleDebug assembleRelease", {
+		cwd: "android",
+		stdio: "inherit",
+		shell: true
+	});
 
 	aars.forEach(aar => {
 		fs.copySync(`android/detox/build/outputs/aar/${aar}`, aar);
