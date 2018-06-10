e03cc6e7d513e221ac25b26edf24fcba0f7c7af9
Daniel Schmidt <danielmschmidt92@gmail.com>
2018-06-10 20:49:46 +0200

    Android: Move matchers to generated code (#745)
    * move android matchers to generated code
    
    Closes #725
    
    * apply prettier styles to code generation

--- generation/utils/downloadFile.js
+++ generation/utils/downloadFile.js
@@ -4,11 +4,11 @@ const uuidv4 = require('uuid/v4');
 const downloadFileSync = require('download-file-sync');
 
 module.exports = function downloadJava(url) {
-    const tmpDir = os.tmpdir();
-    const fileContent = downloadFileSync(url);
-  
-    const result = Buffer.from(fileContent, 'base64').toString('ascii');
-    const filePath = tmpDir + `/${uuidv4()}.java`;
-    fs.writeFileSync(filePath, result);
-    return filePath;
-}
+  const tmpDir = os.tmpdir();
+  const fileContent = downloadFileSync(url);
+
+  const result = Buffer.from(fileContent, 'base64').toString('ascii');
+  const filePath = tmpDir + `/${uuidv4()}.java`;
+  fs.writeFileSync(filePath, result);
+  return filePath;
+};
