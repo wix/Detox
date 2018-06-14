62472f77ca2b9db413cc9808f1e9e04985c96f53
Yaroslav Serhieiev <noomorph@gmail.com>
2018-06-14 16:52:59 +0300

    fix: snapshot calculation in artifacts verification aftertest

--- detox/test/scripts/verify_artifacts_are_not_missing.js
+++ detox/test/scripts/verify_artifacts_are_not_missing.js
@@ -4,13 +4,14 @@ const fs = require('fs');
 const path = require('path');
 
 module.exports = function verify() {
+  let index = 0;
   const subdir = _.last(fs.readdirSync('artifacts'));
   const artifacts = cp.execSync('find .', {
     cwd: path.join('artifacts', subdir),
     encoding: 'utf8'
-  }).split('\n').map((filename, index) => {
+  }).split('\n').map((filename) => {
     if (filename.endsWith('.startup.log')) {
-      return `${index}.startup.log`;
+      return `${++index}.startup.log`;
     }
 
     return filename;
