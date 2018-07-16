a2943306dbb7a5b6f4283259e7a487177978bfaf
louismarie <louismarie.soulard@gmail.com>
2018-07-16 22:20:48 +0200

    fix device test

--- detox/test/e2e/06.device.test.js
+++ detox/test/e2e/06.device.test.js
@@ -55,31 +55,7 @@ describe('Device', () => {
     await expect(element(by.text('Shaken, not stirred'))).toBeVisible();
   });
 
-  describe('device orientation', () => {
-    beforeEach(async() => {
-      await device.reloadReactNative();
-      await element(by.text('Orientation')).tap();
-
-      // Check if the element which input we will test actually exists
-      await expect(element(by.id('currentOrientation'))).toExist();
-    });
-
-    it('OrientationLandscape', async () => {
-      await device.setOrientation('landscape');
-
-      await expect(element(by.id('currentOrientation'))).toHaveText('Landscape');
-    });
-
-    it('OrientationPortrait', async() => {
-      // As default is portrait we need to set it otherwise
-      await device.setOrientation('landscape');
-      await device.setOrientation('portrait');
-
-      await expect(element(by.id('currentOrientation'))).toHaveText('Portrait');
-    });
-  });
-
-  describe('device button', () => {
+  describe(':android: device back button', () => {
     beforeEach(async() => {
       await device.reloadReactNative();
       await element(by.text('Actions')).tap();
