3ca08de41c63280e1e05f668ead3f499a3cc5dc0
Yaroslav Serhieiev <noomorph@gmail.com>
2018-08-06 20:25:50 +0300

    fix: for MacOS

--- detox/src/artifacts/log/ios/SimulatorLogPlugin.sh
+++ detox/src/artifacts/log/ios/SimulatorLogPlugin.sh
@@ -5,6 +5,7 @@ _kill_tails() {
 }
 
 trap _kill_tails SIGTERM SIGINT
-tail -F $1 | sed>>$3 -u -e "s/^/stdout: /" &
-tail -F $2 | sed>>$3 -u -e "s/^/stderr: /" &
+
+tail -F $1 | awk>>$3 '{print "stdout:",$0;fflush(stdout)}' &
+tail -F $2 | awk>>$3 '{print "stderr:",$0;fflush(stdout)}' &
 wait
