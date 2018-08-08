1c708ab2085ad46a47b22b91b29ac8bebb0a6226
Yaroslav Serhieiev <yaroslavs@wix.com>
2018-08-08 17:13:14 +0300

    fix: added Linux implementation for SimulatorLogPlugin.sh

--- detox/src/artifacts/log/ios/SimulatorLogPlugin.sh
+++ detox/src/artifacts/log/ios/SimulatorLogPlugin.sh
@@ -6,6 +6,15 @@ _kill_tails() {
 
 trap _kill_tails SIGTERM SIGINT
 
-tail -F $1 | awk>>$3 '{print "stdout:",$0;fflush(stdout)}' &
-tail -F $2 | awk>>$3 '{print "stderr:",$0;fflush(stdout)}' &
-wait
+case "$(uname -s)" in
+   Darwin)
+     tail -F $1 | awk>>$3 '{print "stdout:",$0;fflush(stdout)}' &
+     tail -F $2 | awk>>$3 '{print "stderr:",$0;fflush(stdout)}' &
+     wait
+     ;;
+   *)
+     tail -F $1 | sed>>$3 -u -e "s/^/stdout: /" &
+     tail -F $2 | sed>>$3 -u -e "s/^/stderr: /" &
+     wait
+     ;;
+esac
