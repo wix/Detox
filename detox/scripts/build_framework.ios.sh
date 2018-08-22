a49005deb12b499378278c04fff6ff79777827a3
Leo Natan <lnatan@wix.com>
2018-08-22 11:37:15 +0300

    Ensure Xcode is installed or print a warning message and return.
    Closes #897

--- detox/scripts/build_framework.ios.sh
+++ detox/scripts/build_framework.ios.sh
@@ -1,5 +1,8 @@
 #!/bin/bash -e -x
 
+# Ensure Xcode is installed or print a warning message and return.
+xcodebuild -version &>/dev/null || (echo "WARNING: Xcode is not installed on this machine. Skipping iOS framework build phase" && return 0)
+
 detoxRootPath="$(dirname $(dirname ${0}))"
 detoxVersion=`node -p "require('${detoxRootPath}/package.json').version"`
 
