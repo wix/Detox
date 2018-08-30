d5a0272996acdc41b4362f82126f9db696f0cd6a
Rotem M <rotemm@wix.com>
2018-08-31 00:02:09 +0300

    oops

--- scripts/demo-projects.ios.sh
+++ scripts/demo-projects.ios.sh
@@ -3,12 +3,12 @@
 source $(dirname "$0")/demo-projects.sh
 
 pushd examples/demo-react-native
-run_f "detox build -c android.emu.release"
-run_f "detox test -c android.emu.release"
-run_f "detox test -c android.emu.release --specs e2eExplicitRequire --runner-config e2eExplicitRequire/mocha.opts"
+run_f "detox build -c ios.sim.release"
+run_f "detox test -c ios.sim.release"
+run_f "detox test -c ios.sim.release --specs e2eExplicitRequire --runner-config e2eExplicitRequire/mocha.opts"
 popd
 
 pushd examples/demo-react-native-jest
-run_f "detox build -c android.emu.release"
-run_f "detox test -c android.emu.release"
+run_f "detox build -c ios.sim.release"
+run_f "detox test -c ios.sim.release"
 popd
