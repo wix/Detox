8c648b53e95b9726217a5a59d1c45ee154a0ee80
Luis Naranjo <jonaranj@microsoft.com>
2018-09-09 12:43:50 -0700

    add language UI to language screen

--- detox/test/src/Screens/LanguageScreen.js
+++ detox/test/src/Screens/LanguageScreen.js
@@ -4,7 +4,8 @@ import {
   View,
   TouchableOpacity
 } from 'react-native';
-import DeviceInfo from 'react-native-device-info';
+import { NativeModules } from 'react-native'
+import _ from "lodash";
 
 export default class LanguageScreen extends Component {
 
@@ -29,10 +30,10 @@ export default class LanguageScreen extends Component {
     return (
       <View style={{flex: 1, paddingTop: 20, justifyContent: 'center', alignItems: 'center'}}>
         <Text style={{fontSize: 25, marginBottom: 30}}>
-          Current language: {DeviceInfo.getDeviceLocale()}
+          Current locale: {NativeModules.SettingsManager.settings.AppleLocale}
         </Text>
         <Text style={{fontSize: 25, marginBottom: 30}}>
-          Current country: {DeviceInfo.getDeviceCountry()}
+          Current language: {_.take(NativeModules.SettingsManager.settings.AppleLanguages, 1)}
         </Text>
         {this.renderTestButton('Say Hello', this.onButtonPress.bind(this, 'Hello'))}
         {this.renderTestButton('Say World', this.onButtonPress.bind(this, 'World'))}
