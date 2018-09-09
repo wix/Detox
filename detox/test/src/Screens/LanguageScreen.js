3f6bb3023e602c21b6670c9928bbb2ef7102fcb4
Luis Naranjo <jonaranj@microsoft.com>
2018-09-09 13:01:57 -0700

    clean up language screen

--- detox/test/src/Screens/LanguageScreen.js
+++ detox/test/src/Screens/LanguageScreen.js
@@ -4,16 +4,7 @@ import { NativeModules } from 'react-native';
 import _ from 'lodash';
 
 export default class LanguageScreen extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      greeting: undefined
-    };
-    console.log('LanguageScreen react component constructed (console.log test)');
-  }
-
   render() {
-    if (this.state.greeting) return this.renderAfterButton();
     return (
       <View style={{ flex: 1, paddingTop: 20, justifyContent: 'center', alignItems: 'center' }}>
         <Text style={{ fontSize: 25, marginBottom: 30 }}>Current locale: {NativeModules.SettingsManager.settings.AppleLocale}</Text>
