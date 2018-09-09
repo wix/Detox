6b1b4462588eecdf5cc813cb2285fd1e2c3f7b5d
Luis Naranjo <jonaranj@microsoft.com>
2018-09-09 14:00:00 -0700

    add language support to test app for android

--- detox/test/src/Screens/LanguageScreen.js
+++ detox/test/src/Screens/LanguageScreen.js
@@ -1,15 +1,25 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, Platform } from 'react-native';
 import { NativeModules } from 'react-native';
 import _ from 'lodash';
 
 export default class LanguageScreen extends Component {
   render() {
+    const locale = Platform.select({
+        ios: () => NativeModules.SettingsManager.settings.AppleLocale,
+        android: () => NativeModules.I18nManager.localeIdentifier
+    })();
+
+    const language = Platform.select({
+        ios: () => _.take(NativeModules.SettingsManager.settings.AppleLanguages, 1),
+        android: () => 'N/A'
+    })();
+
     return (
       <View style={{ flex: 1, paddingTop: 20, justifyContent: 'center', alignItems: 'center' }}>
-        <Text style={{ fontSize: 25, marginBottom: 30 }}>Current locale: {NativeModules.SettingsManager.settings.AppleLocale}</Text>
+        <Text style={{ fontSize: 25, marginBottom: 30 }}>Current locale: {locale}</Text>
         <Text style={{ fontSize: 25, marginBottom: 30 }}>
-          Current language: {_.take(NativeModules.SettingsManager.settings.AppleLanguages, 1)}
+          Current language: {language}
         </Text>
       </View>
     );
