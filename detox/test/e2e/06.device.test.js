4691118c5a5ed1c13be995bc9e2e6d073684f397
Luis Naranjo <jonaranj@microsoft.com>
2018-09-09 15:52:29 -0700

    uncomment some tests

--- detox/test/e2e/06.device.test.js
+++ detox/test/e2e/06.device.test.js
@@ -1,43 +1,43 @@
 describe('Device', () => {
-  // it('reloadReactNative - should tap successfully', async () => {
-  //   await device.reloadReactNative();
-  //   await element(by.text('Sanity')).tap();
-  //   await element(by.text('Say Hello')).tap();
-  //   await expect(element(by.text('Hello!!!'))).toBeVisible();
-  // });
+  it('reloadReactNative - should tap successfully', async () => {
+    await device.reloadReactNative();
+    await element(by.text('Sanity')).tap();
+    await element(by.text('Say Hello')).tap();
+    await expect(element(by.text('Hello!!!'))).toBeVisible();
+  });
 
-  // it('relaunchApp - should tap successfully', async () => {
-  //   await device.relaunchApp();
-  //   await element(by.text('Sanity')).tap();
-  //   await element(by.text('Say Hello')).tap();
-  //   await expect(element(by.text('Hello!!!'))).toBeVisible();
-  // });
+  it('relaunchApp - should tap successfully', async () => {
+    await device.relaunchApp();
+    await element(by.text('Sanity')).tap();
+    await element(by.text('Say Hello')).tap();
+    await expect(element(by.text('Hello!!!'))).toBeVisible();
+  });
 
-  // it('relaunchApp({delete: true}) - should tap successfully', async () => {
-  //   await device.relaunchApp({delete: true});
-  //   await element(by.text('Sanity')).tap();
-  //   await element(by.text('Say Hello')).tap();
-  //   await expect(element(by.text('Hello!!!'))).toBeVisible();
-  // });
+  it('relaunchApp({delete: true}) - should tap successfully', async () => {
+    await device.relaunchApp({delete: true});
+    await element(by.text('Sanity')).tap();
+    await element(by.text('Say Hello')).tap();
+    await expect(element(by.text('Hello!!!'))).toBeVisible();
+  });
 
-  // it('uninstall() + install() + relaunch() - should tap successfully', async () => {
-  //   await device.uninstallApp();
-  //   await device.installApp();
-  //   await device.relaunchApp();
-  //   await element(by.text('Sanity')).tap();
-  //   await element(by.text('Say Hello')).tap();
-  //   await expect(element(by.text('Hello!!!'))).toBeVisible();
-  // });
+  it('uninstall() + install() + relaunch() - should tap successfully', async () => {
+    await device.uninstallApp();
+    await device.installApp();
+    await device.relaunchApp();
+    await element(by.text('Sanity')).tap();
+    await element(by.text('Say Hello')).tap();
+    await expect(element(by.text('Hello!!!'))).toBeVisible();
+  });
 
-  // it('launchApp({newInstance: true}) + sendToHome() + launchApp() - should bring up previous instance', async () => {
-  //   await device.launchApp({newInstance: true});
-  //   await element(by.text('Sanity')).tap();
-  //   await element(by.text('Say Hello')).tap();
-  //   await device.sendToHome();
-  //   await device.launchApp();
+  it('launchApp({newInstance: true}) + sendToHome() + launchApp() - should bring up previous instance', async () => {
+    await device.launchApp({newInstance: true});
+    await element(by.text('Sanity')).tap();
+    await element(by.text('Say Hello')).tap();
+    await device.sendToHome();
+    await device.launchApp();
 
-  //   await expect(element(by.text('Hello!!!'))).toBeVisible();
-  // });
+    await expect(element(by.text('Hello!!!'))).toBeVisible();
+  });
 
   it('launchApp in a different language', async () => {
     let languageAndLocale = {
@@ -61,32 +61,32 @@ describe('Device', () => {
     await expect(element(by.text(`Current language: ${languageAndLocale.language}`))).toBeVisible();
   });
 
-  // it('resetContentAndSettings() + install() + relaunch() - should tap successfully', async () => {
-  //   await device.resetContentAndSettings();
-  //   await device.installApp();
-  //   await device.launchApp({ newInstance: true });
-  //   await element(by.text('Sanity')).tap();
-  //   await element(by.text('Say Hello')).tap();
-  //   await expect(element(by.text('Hello!!!'))).toBeVisible();
-  // });
+  it('resetContentAndSettings() + install() + relaunch() - should tap successfully', async () => {
+    await device.resetContentAndSettings();
+    await device.installApp();
+    await device.launchApp({ newInstance: true });
+    await element(by.text('Sanity')).tap();
+    await element(by.text('Say Hello')).tap();
+    await expect(element(by.text('Hello!!!'))).toBeVisible();
+  });
 
-  // it(':ios: shake() should shake screen', async () => {
-  //   await device.reloadReactNative();
-  //   await element(by.text('Shake')).tap();
-  //   await device.shake();
-  //   await expect(element(by.text('Shaken, not stirred'))).toBeVisible();
-  // });
+  it(':ios: shake() should shake screen', async () => {
+    await device.reloadReactNative();
+    await element(by.text('Shake')).tap();
+    await device.shake();
+    await expect(element(by.text('Shaken, not stirred'))).toBeVisible();
+  });
 
-  // describe(':android: device back button', () => {
-  //   beforeEach(async() => {
-  //     await device.reloadReactNative();
-  //     await element(by.text('Actions')).tap();
-  //   });
+  describe(':android: device back button', () => {
+    beforeEach(async() => {
+      await device.reloadReactNative();
+      await element(by.text('Actions')).tap();
+    });
 
-  //   it(':android: should show popup back pressed when back button is pressed', async () => {
-  //     await device.pressBack();
-  //     await expect(element(by.text('Back pressed !'))).toBeVisible();
-  //   });
-  // });
+    it(':android: should show popup back pressed when back button is pressed', async () => {
+      await device.pressBack();
+      await expect(element(by.text('Back pressed !'))).toBeVisible();
+    });
+  });
 
 });
