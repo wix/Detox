756ced3e41562f71f4408b3215cbc3f9bc9ee41f
Yaroslav Serhieiev <yaroslavs@wix.com>
2018-10-02 09:46:31 +0300

    test: fix floating point represenation issue

--- detox/test/e2e/16.location.test.js
+++ detox/test/e2e/16.location.test.js
@@ -22,18 +22,18 @@ describe('location', () => {
     await expect(element(by.id('error'))).toBeVisible();
   });
 
-  it(':ios: Should receive location (20,20)', async () => {
+  it(':ios: Should receive location (20.1,20.25)', async () => {
     if (!await isFbsimctlInstalled()) {
       return;
     }
     await device.relaunchApp({ permissions: { location: 'always' } });
-    await device.setLocation(20.1, 20.2);
+    await device.setLocation(20.1, 20.25);
     await element(by.text('Location')).tap();
     await element(by.id('getLocationButton')).tap();
     await waitFor(element(by.text('Latitude: 20.1'))).toBeVisible().withTimeout(3000);
 
     await expect(element(by.text('Latitude: 20.1'))).toBeVisible();
-    await expect(element(by.text('Longitude: 20.2'))).toBeVisible();
+    await expect(element(by.text('Longitude: 20.25'))).toBeVisible();
   });
 
   it(':android: Should receive location (20,20)', async () => {
