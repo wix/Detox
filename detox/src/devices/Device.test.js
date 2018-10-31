ed5d4c6a4995c1a127bd570b47c81b8bcda4223d
Marius Reimer <reime005@gmail.com>
2018-10-31 12:09:53 +0100

    added testBinaryPath tests

--- detox/src/devices/Device.test.js
+++ detox/src/devices/Device.test.js
@@ -276,12 +276,21 @@ describe('Device', () => {
     expect(device.deviceDriver.installApp).toHaveBeenCalledWith(device._deviceId, 'newAppPath');
   });
 
+  it(`installApp() with a custom test app path should use custom test app path`, async () => {
+    device = validDevice();
+    fs.existsSync.mockReturnValue(true);
+
+    await device.installApp('newAppPath', 'newTestAppPath');
+
+    expect(device.deviceDriver.installApp).toHaveBeenCalledWith(device._deviceId, 'newAppPath', 'newTestAppPath');
+  });
+
   it(`installApp() with no params should use the default path given in configuration`, async () => {
     device = validDevice();
 
     await device.installApp();
 
-    expect(device.deviceDriver.installApp).toHaveBeenCalledWith(device._deviceId, device._binaryPath);
+    expect(device.deviceDriver.installApp).toHaveBeenCalledWith(device._deviceId, device._binaryPath, device._testBinaryPath);
   });
 
   it(`uninstallApp() with a custom app path should use custom app path`, async () => {
