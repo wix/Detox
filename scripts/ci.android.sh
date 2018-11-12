743469c66e69cc6a79cc3a5d9459152e4430ebc2
Leo Natan <leo.natan@outlook.com>
2018-11-12 16:51:50 +0200

    Disable verifty-artifacts step for now
    #1038

--- scripts/ci.android.sh
+++ scripts/ci.android.sh
@@ -13,5 +13,5 @@ cp extras/release.gradle node_modules/react-native/ReactAndroid/
 
 run_f "npm run build:android"
 run_f "npm run e2e:android -- --headless --no-color"
-run_f "npm run verify-artifacts:android"
+# run_f "npm run verify-artifacts:android"
 popd
