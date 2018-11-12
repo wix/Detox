c9573fa40b3ed56fe8dd2d4334f5b6eb76167e68
Leo Natan <leo.natan@outlook.com>
2018-11-12 16:55:37 +0200

    Run transitions test only on iOS

--- detox/test/e2e/20.background-foreground.transitions.js
+++ detox/test/e2e/20.background-foreground.transitions.js
@@ -1,4 +1,4 @@
-describe("Background-Foreground Transitions", () => {
+describe(":ios: Background-Foreground Transitions", () => {
 	it("Backgrounding and foregrounding an app should wait for transition to finish", async () => {
 		await device.launchApp({newInstance: true});
 		await device.sendToHome();
