47aef93cc0f36423be19d74802cd109376f9b7ba
Daniel Schmidt <dschmidt@mesosphere.com>
2018-11-25 10:42:58 +0100

    remove unused code as we use submodules now

--- generation/utils/downloadEspresso.js
+++
@@ -1,9 +0,0 @@
-// Gets a fully qualified class path of espresso and downloads it in a temp dir
-const downloadFile = require('./downloadFile');
-
-module.exports = function downloadEspresso(fullyQualifiedClass) {
-  const path = fullyQualifiedClass.replace(/\./g, '/');
-  return downloadFile(
-    `http://android.googlesource.com/platform/frameworks/testing/+/android-support-test/espresso/core/src/main/java/${path}.java?format=TEXT`
-  );
-};
