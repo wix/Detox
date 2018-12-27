b3aac24559406ec0dd448d3beb5d1a1dec19c189
Daniel Schmidt <dschmidt@mesosphere.com>
2018-12-27 16:21:46 +0100

    enable tests for local cli

--- detox/cli.js
+++ detox/cli.js
@@ -3,7 +3,15 @@
 const yargs = require('yargs');
 yargs
   .scriptName('detox')
-  .commandDir('local-cli')
+  .commandDir('local-cli', {
+    exclude: function(path) {
+      // This is a test file
+      if (/\.test\.js$/.test(path)) {
+        return true;
+      }
+      return false;
+    }
+  })
   .demandCommand()
   .recommendCommands()
   .help()
