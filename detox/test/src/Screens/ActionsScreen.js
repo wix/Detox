2596eea0ebd739740a34a5540a45e5ed77eb235b
Maxime Sraïki <maxime.sraiki@gmail.com>
2019-01-06 20:21:12 +0100

    Fix ios artifacts

--- detox/test/src/Screens/ActionsScreen.js
+++ detox/test/src/Screens/ActionsScreen.js
@@ -94,7 +94,7 @@ export default class ActionsScreen extends Component {
           }>
           </ScrollView>
         </View>
-        <View style={{}}>
+        <View>
           <ScrollView testID='PinchableScrollView' minimumZoomScale={1} maximumZoomScale={10}>
             <View>
               <View testID='UniqueId007' style={{ height: 30, width: 30, backgroundColor:'red' }} />
