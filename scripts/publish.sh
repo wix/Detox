c047047df89139cbe90e9b598e54bc8448b553b6
Amit Davidi <amit.d4vidi@gmail.com>
2019-01-28 16:55:07 +0200

    Enforce a github token for change-log generator before publishing

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,5 +1,10 @@
 #!/bin/bash -e
 
+if [ -z "$CHANGELOG_GITHUB_TOKEN" ]; then
+	echo "Please provide a github token for the change-log generator (i.e. set CHANGELOG_GITHUB_TOKEN). See https://developer.github.com/v3/#rate-limiting for more details."
+	exit 1
+fi
+
 VERSION_TYPE="$1"
 if [ -z "$VERSION_TYPE" ]; then
 	VERSION_TYPE="patch"
