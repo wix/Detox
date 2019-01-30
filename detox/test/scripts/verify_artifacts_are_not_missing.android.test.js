86cfa2cef4ec85af34c9931e3e8a4bbfd1c9a5da
Maxime Sraïki <maxime.sraiki@gmail.com>
2019-01-30 08:36:21 +0100

    Remove useless artifacts tests

--- detox/test/scripts/verify_artifacts_are_not_missing.android.test.js
+++
@@ -1,5 +0,0 @@
-const verify = require('./verify_artifacts_are_not_missing');
-
-describe('artifacts health check', () => {
-  it('should have all the Android artifacts in ./artifacts folder', verify('android.emu.release'));
-});
