789ded54e0a15d95e98b9dff1019b96013baebfa
Amit Davidi <amitd.d4vidi@gmail.com>
2019-02-18 10:24:22 +0200

    Remove unneeded require (dummy commit) [ci skip]

--- detox/src/Detox.js
+++ detox/src/Detox.js
@@ -14,7 +14,6 @@ const Client = require('./client/Client');
 const DetoxServer = require('./server/DetoxServer');
 const URL = require('url').URL;
 const ArtifactsManager = require('./artifacts/ArtifactsManager');
-const AsyncEmitter = require('./utils/AsyncEmitter');
 
 const DEVICE_CLASSES = {
   'ios.simulator': SimulatorDriver,
