b92271b8a89ba6070f624bde033cf3e5dcb09fc8
Amit Davidi <amit.d4vidi@gmail.com>
2019-02-27 21:32:11 +0200

    Android: Stabilize animations short-timer e2e (minor change in timers IR)

--- detox/android/detox/src/test/java/com/wix/detox/espresso/ReactNativeTimersIdlingResourceTest.kt
+++ detox/android/detox/src/test/java/com/wix/detox/espresso/ReactNativeTimersIdlingResourceTest.kt
@@ -105,9 +105,16 @@ class ReactNativeTimersIdlingResourceTest {
         assertThat(uut().isIdleNow).isFalse()
     }
 
-    @Test fun `should be idle if the only timer is overdue (due in the past)`() {
+    /**
+     * Note: Reversed logic due to this issue: https://github.com/wix/Detox/issues/1171 !!!
+     *
+     * Apparently at times (rare) this caused Espresso to think we're idle too soon, rendering
+     * it never to query any idling resource again even after the timer effectively expires...
+     */
+    @Test fun `should be *busy* even if all timers are overdue`() {
         givenTimer(anOverdueTimer())
-        assertThat(uut().isIdleNow).isTrue()
+        givenTimer(anOverdueTimer())
+        assertThat(uut().isIdleNow).isFalse()
     }
 
     @Test fun `should be busy if has a meaningful pending timer set beyond an overdue timer`() {
