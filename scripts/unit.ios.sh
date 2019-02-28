443d657980ee8033d40f3f305e4f94fca769de3d
Shalom Yerushalmy <yershalom@gmail.com>
2019-02-28 16:33:41 +0200

    Remove xcpretty

--- scripts/unit.ios.sh
+++ scripts/unit.ios.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -e
 
-set -o pipefail && xcodebuild -project detox/ios/Detox.xcodeproj -scheme Detox -configuration Debug -sdk iphonesimulator build-for-testing -UseModernBuildSystem=NO | xcpretty
+set -o pipefail && xcodebuild -project detox/ios/Detox.xcodeproj -scheme Detox -configuration Debug -sdk iphonesimulator build-for-testing -UseModernBuildSystem=NO
 set -o pipefail && xcodebuild -project detox/ios/Detox.xcodeproj -scheme Detox -configuration Debug -sdk iphonesimulator test-without-building -destination 'platform=iOS Simulator,name=iPhone 7 Plus' | xcpretty
