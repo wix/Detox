104f2bff57b54c7cc468d8a55b1d5417d6a6a1da
Andrew Jones <ximenean@hotmail.com>
2019-03-13 11:44:30 +0000

    add tabBar trait

--- detox/src/ios/earlgreyapi/GREYMatchers.js
+++ detox/src/ios/earlgreyapi/GREYMatchers.js
@@ -4,7 +4,6 @@
 	For more information see generation/README.md.
 */
 
-
 function sanitize_uiAccessibilityTraits(value) {
   let traits = 0;
   for (let i = 0; i < value.length; i++) {
@@ -54,6 +53,9 @@ function sanitize_uiAccessibilityTraits(value) {
       case 'pageTurn':
         traits |= 16384;
         break;
+      case 'tabBar':
+        traits |= 32768;
+        break;
       case 'header':
         traits |= 65536;
         break;
@@ -65,7 +67,7 @@ function sanitize_uiAccessibilityTraits(value) {
   }
 
   return traits;
-} 
+}
 function sanitize_greyContentEdge(action) {
   switch (action) {
     case 'left':
@@ -80,7 +82,7 @@ function sanitize_greyContentEdge(action) {
     default:
       throw new Error(`GREYAction.GREYContentEdge must be a 'left'/'right'/'top'/'bottom', got ${action}`);
   }
-} 
+}
 class GREYMatchers {
   /*Matcher for application's key window.
   
