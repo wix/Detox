2731c66ec3573e0a896d07dc23a83c404b81869c
Yaroslav Serhieiev <yaroslavs@wix.com>
2019-04-03 18:41:04 +0300

    Revert "temporary print xcode version"
    This reverts commit 05557b0f22463e79b193267c419b55d4e98aa711.

--- scripts/unit.ios.sh
+++ scripts/unit.ios.sh
@@ -1,5 +1,4 @@
 #!/bin/bash -e
 
-xcodebuild -version
 set -o pipefail && xcodebuild -project detox/ios/Detox.xcodeproj -scheme Detox -configuration Debug -sdk iphonesimulator build-for-testing | xcpretty
 set -o pipefail && xcodebuild -project detox/ios/Detox.xcodeproj -scheme Detox -configuration Debug -sdk iphonesimulator test-without-building -destination 'platform=iOS Simulator,name=iPhone 7 Plus' | xcpretty
