28a3b3c893fc437352f2bec3127b2b68b651f115
Yaroslav Serhieiev <yaroslavs@wix.com>
2019-04-03 18:26:39 +0300

    Revert "temporary disable xcpretty"

--- scripts/unit.ios.sh
+++ scripts/unit.ios.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -e
 
 xcodebuild -version
-set -o pipefail && xcodebuild -project detox/ios/Detox.xcodeproj -scheme Detox -configuration Debug -sdk iphonesimulator build-for-testing
-set -o pipefail && xcodebuild -project detox/ios/Detox.xcodeproj -scheme Detox -configuration Debug -sdk iphonesimulator test-without-building -destination 'platform=iOS Simulator,name=iPhone 7 Plus'
+set -o pipefail && xcodebuild -project detox/ios/Detox.xcodeproj -scheme Detox -configuration Debug -sdk iphonesimulator build-for-testing | xcpretty
+set -o pipefail && xcodebuild -project detox/ios/Detox.xcodeproj -scheme Detox -configuration Debug -sdk iphonesimulator test-without-building -destination 'platform=iOS Simulator,name=iPhone 7 Plus' | xcpretty
