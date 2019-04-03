05557b0f22463e79b193267c419b55d4e98aa711
Yaroslav Serhieiev <yaroslavs@wix.com>
2019-04-03 16:59:53 +0300

    temporary print xcode version

--- scripts/unit.ios.sh
+++ scripts/unit.ios.sh
@@ -1,4 +1,5 @@
 #!/bin/bash -e
 
+xcodebuild -version
 set -o pipefail && xcodebuild -project detox/ios/Detox.xcodeproj -scheme Detox -configuration Debug -sdk iphonesimulator build-for-testing
 set -o pipefail && xcodebuild -project detox/ios/Detox.xcodeproj -scheme Detox -configuration Debug -sdk iphonesimulator test-without-building -destination 'platform=iOS Simulator,name=iPhone 7 Plus'
