ea2850f8c64a88376e2a251967fab98230ba01cb
Amit Davidi <amit.d4vidi@gmail.com>
2019-04-22 21:33:44 +0300

    Restore example project tests in CI (context: issue #1323)

--- examples/demo-react-native-jest/e2e/init.js
+++ examples/demo-react-native-jest/e2e/init.js
@@ -1,9 +1,10 @@
+
 const detox = require('detox');
 const config = require('../package.json').detox;
 const adapter = require('detox/runners/jest/adapter');
 
 // Set the default timeout
-jest.setTimeout(120000);
+jest.setTimeout(300000);
 jasmine.getEnv().addReporter(adapter);
 
 beforeAll(async () => {
