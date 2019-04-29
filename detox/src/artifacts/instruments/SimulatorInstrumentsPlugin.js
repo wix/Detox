2c91d61cf70618845b61feb49c788c84978e9b6f
Yaroslav Serhieiev <yaroslavs@wix.com>
2019-04-29 17:24:12 +0300

    feat: support for DETOX_INSTRUMENTS_PATH environment variable

--- detox/src/artifacts/instruments/SimulatorInstrumentsPlugin.js
+++ detox/src/artifacts/instruments/SimulatorInstrumentsPlugin.js
@@ -10,8 +10,7 @@ class SimulatorInstrumentsPlugin extends WholeTestRecorderPlugin {
     super(config);
 
     this.client = config.client;
-    this.enabled = false;
-    this.shouldRecord = argparse.getArgValue('record-performance') === 'all';
+    this.enabled = argparse.getArgValue('record-performance') === 'all';
   }
 
   async onBeforeUninstallApp(event) {
@@ -38,14 +37,13 @@ class SimulatorInstrumentsPlugin extends WholeTestRecorderPlugin {
   async onBeforeLaunchApp(event) {
     await super.onBeforeLaunchApp(event);
 
-    if (!this.enabled && this.shouldRecord) {
-      const isInstalled = await this._assertDetoxInstrumentsInstalled(event.launchArgs['-instrumentsPath']);
-      this.enabled = this.shouldRecord = isInstalled;
-    }
-
     const isInsideRunningTest = !!this.context.testSummary;
     if (this.enabled && isInsideRunningTest && !this.testRecording) {
       this.testRecording = this.createTrackedTestRecording();
+    }
+
+    if (process.env.DETOX_INSTRUMENTS_PATH) {
+      event.launchArgs['-detoxInstrumentsPath'] = event.launchArgs['-detoxInstrumentsPath'] || process.env.DETOX_INSTRUMENTS_PATH;
     }
 
     if (this.testRecording) {
@@ -59,22 +57,6 @@ class SimulatorInstrumentsPlugin extends WholeTestRecorderPlugin {
     if (this.testRecording) {
       await this.testRecording.start({ dry: true }); // start nominally, to set a correct recording state
     }
-  }
-
-  async _assertDetoxInstrumentsInstalled(customInstrumentsPath) {
-    const instrumentsPath = customInstrumentsPath || SimulatorInstrumentsPlugin.DEFAULT_INSTRUMENTS_PATH;
-    if (await fs.exists(instrumentsPath)) {
-      return true;
-    }
-
-    const hint = customInstrumentsPath
-      ? `Please verify that -instrumentsPath argument points to the existing Detox Instruments installation.`
-      : `To enable recording performance profiles, please follow: https://github.com/wix/DetoxInstruments#installation`;
-
-    log.warn({ event: 'INSTRUMENTS_NOT_FOUND' },
-      `Failed to find Detox Instruments app at path: ${instrumentsPath}\n${hint}`);
-
-    return false;
   }
 
   createTestRecording() {
