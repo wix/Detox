caf9d6ed806c6cb784770c762787c9b7ba06744b
Yaroslav Serhieiev <yaroslavs@wix.com>
2019-04-30 17:05:02 +0300

    ci: collect code coverage from e2e (#1317)

---
+++ scripts/aggregate_coverage.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+if [ -d aggregated-coverage ]; then
+  npm install --no-save --no-package-lock "lcov-result-merger@3.x.x" "coveralls@3.x.x"
+
+  rm -f "aggregated-coverage/merged.lcov"
+  {
+    for lcov in aggregated-coverage/**/*.lcov; do
+      echo "Forcing relative paths in: $lcov"
+      sed -i 's/^SF:.*\(detox-ios-51-pr\|detox-ios-56-pr\|detox-android-56-pr\)\//SF:/g' $lcov
+    done
+
+    echo "Merging LCOV files"
+    node_modules/.bin/lcov-result-merger 'aggregated-coverage/**/*.lcov' "aggregated-coverage/merged.lcov"
+  } && {
+    echo "Uploading to coveralls"
+    cat "aggregated-coverage/merged.lcov" | node_modules/.bin/coveralls
+  } || {
+    echo "Failed to aggregate the test coverage, skipping."
+  }
+else
+  echo "Aggregated coverage directory was not found, skipping."
+fi
