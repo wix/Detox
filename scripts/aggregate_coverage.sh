b141e8540f9da57a452c58cd5317d9341cf33c8e
Yaroslav Serhieiev <yaroslavs@wix.com>
2019-04-30 09:47:36 +0300

    ci: aggregate coverage script

---
+++ scripts/aggregate_coverage.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+if [ -d aggregated-coverage ]; then
+  npm install --no-save --no-package-lock "lcov-result-merger@3.x.x" "coveralls@3.x.x"
+
+  rm -f "aggregated-coverage/merged.lcov"
+  {
+    node_modules/.bin/lcov-result-merger 'aggregated-coverage/**/lcov.info' "aggregated-coverage/merged.lcov"
+  } && {
+    cat "aggregated-coverage/merged.lcov" | node_modules/.bin/coveralls
+  } || {
+    echo "Failed to aggregate the test coverage, skipping."
+  }
+else
+  echo "Aggregated coverage directory was not found, skipping."
+fi
