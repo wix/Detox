acddd9cede663bdee4b105dae9624208a003e33d
Amit Davidi <amit.d4vidi@gmail.com>
2019-05-06 13:01:26 +0300

    Make spec-logging adaptive, introduce --jest-report-spec cli switch

--- detox/runners/jest/specReporter.js
+++ detox/runners/jest/specReporter.js
@@ -1,2 +1,8 @@
-const Reporter = require('./JasmineSpecReporter');
-module.exports = new Reporter();
+const argparse = require('../../src/utils/argparse');
+
+if (argparse.getArgValue('reportSpecs') === 'true') {
+  const Reporter = require('./JasmineSpecReporter');
+  module.exports = new Reporter();
+} else {
+  module.exports = {};
+}
