99fdfa58bdf471b51344c65114e13c6c76db3dc1
Amit Davidi <amit.d4vidi@gmail.com>
2019-05-28 10:32:53 +0300

    Introduce a work-around for tests currently failing on new RN versions

---
+++ detox/test/e2e/03.fs-scroll-actions.test.js
@@ -0,0 +1,53 @@
+/**
+ * A mini suite providing an alternative to tests failing due to issues found in RN 58+ on Android (see
+ * https://github.com/facebook/react-native/issues/23870).
+ * It basically runs similar use cases -- all of which involve visibility and scrolling, but in a
+ * setup where they _can_ pass, so as to assert that the core Detox functionality (waitFor(), scroll())
+ * is valid nevertheless.
+ */
+
+describe('Fullscreen scrolling Actions', () => {
+  beforeEach(async () => {
+    await device.reloadReactNative();
+    await element(by.text('Scroll-Actions')).tap();
+  });
+
+  it('should scroll for a small amount in direction', async () => {
+    await expect(scrollViewDriver.element()).toBeVisible();
+    await expect(scrollViewDriver.firstItem()).toBeVisible();
+    await expect(scrollViewDriver.lastItem()).toBeNotVisible();
+
+    await scrollViewDriver.scrollBy(60);
+    await expect(scrollViewDriver.firstItem()).toBeNotVisible();
+    await expect(scrollViewDriver.secondItem()).toBeVisible();
+    await scrollViewDriver.scrollBy(-60);
+    await expect(scrollViewDriver.firstItem()).toBeVisible();
+    await expect(scrollViewDriver.lastItem()).toBeNotVisible();
+  });
+
+  it('should scroll for a large amount in direction', async () => {
+    await expect(scrollViewDriver.element()).toBeVisible();
+    await expect(scrollViewDriver.firstItem()).toBeVisible();
+    await expect(scrollViewDriver.lastItem()).toBeNotVisible();
+
+    await scrollViewDriver.scrollBy(1000);
+    await expect(scrollViewDriver.firstItem()).toBeNotVisible();
+    await expect(scrollViewDriver.lastItem()).toBeVisible();
+  });
+
+  it('should find element by scrolling until it is visible', async () => {
+    await expect(scrollViewDriver.lastItem()).toBeNotVisible();
+    await waitFor(scrollViewDriver.lastItem()).toBeVisible().whileElement(scrollViewDriver.viewFilter()).scroll(200, 'down');
+    await expect(scrollViewDriver.lastItem()).toBeVisible();
+  });
+});
+
+const scrollViewDriver = {
+  viewFilter: () => by.id('FSScrollActions.scrollView'),
+  element: () => element(scrollViewDriver.viewFilter()),
+  listItem: (index) => element(by.text(`Text${index}`)),
+  firstItem: () => scrollViewDriver.listItem(1),
+  secondItem: () => scrollViewDriver.listItem(2),
+  lastItem: () => scrollViewDriver.listItem(20),
+  scrollBy: (amount) => scrollViewDriver.element().scroll(Math.abs(amount), (amount > 0 ? 'down' : 'up')),
+};
--- detox/test/e2e/05.waitfor.test.js
+++ detox/test/e2e/05.waitfor.test.js
@@ -1,3 +1,5 @@
+const custom = require('./utils/custom-it');
+
 describe('WaitFor', () => {
   beforeEach(async() => {
     await device.reloadReactNative();
@@ -11,18 +13,17 @@ describe('WaitFor', () => {
     await expect(element(by.id('createdAndVisibleText'))).toExist();
   });
 
-  it('should wait until an element is removed', async() => {
+  it('should wait until an element is removed', async () => {
     await expect(element(by.id('deletedFromHierarchyText'))).toBeVisible();
     await element(by.id('GoButton')).tap();
     await waitFor(element(by.id('deletedFromHierarchyText'))).toBeNotVisible().withTimeout(20000);
     await expect(element(by.id('deletedFromHierarchyText'))).toBeNotVisible();
   });
 
-  it('should find element by scrolling until it is visible', async() => {
+  custom.it.withFailureIf.android.rn58OrNewer('should find element by scrolling until it is visible', async () => {
     await expect(element(by.text('Text5'))).toBeNotVisible();
     await element(by.id('GoButton')).tap();
     await waitFor(element(by.text('Text5'))).toBeVisible().whileElement(by.id('ScrollView')).scroll(50, 'down');
     await expect(element(by.text('Text5'))).toBeVisible();
   });
-
 });
