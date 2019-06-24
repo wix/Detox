005ad3306133a8323eb40d561d955dcd3ccdb848
Yaroslav Serhieiev <yaroslavs@wix.com>
2019-06-24 14:05:09 +0300

    feat: allow disabling of post install with env var (#1347)
    Resolves #1338

--- detox/scripts/postinstall.js
+++ detox/scripts/postinstall.js
@@ -1,4 +1,4 @@
-if (process.platform === "darwin") {
+if (process.platform === "darwin" && !process.env.DETOX_DISABLE_POSTINSTALL) {
 	require("child_process").execFileSync(`${__dirname}/build_framework.ios.sh`, {
 		stdio: "inherit"
 	});
