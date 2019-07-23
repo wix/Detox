c4ca6e86cd60b108ff97c7d117f1983bbb0827f0
Amit Davidi <amit.d4vidi@gmail.com>
2019-07-23 14:29:11 +0300

    Introduce more integrative scroll e2e tests

--- detox/test/e2e/03.integ-actions.test.js
+++ detox/test/e2e/03.integ-actions.test.js
@@ -1,4 +1,5 @@
 const {scrollViewDriver} = require('./drivers/fs-scroll-driver');
+const {scrollingTextInputsDriver, scrollingTextsDriver} = require('./drivers/integ-actions-drivers');
 
 describe(':android: Integrative actions', () => {
   beforeEach(async () => {
@@ -18,4 +19,33 @@ describe(':android: Integrative actions', () => {
     await expect(element(by.text(expectedAlertText))).toBeVisible();
   });
 
+  it('should scroll various distances accurately, and tap scroll targets', async () => {
+    const iterations = 10;
+    const textHeight = 40;
+    const baseMargin = 20;
+
+    await element(by.text('Integrative Actions')).tap();
+    for (let i = 1; i <= iterations; i++) {
+      await scrollingTextsDriver.tapOnText(i);
+      await scrollingTextsDriver.assertTextTappedOnce(i);
+      await scrollingTextsDriver.scrollDown(textHeight + (baseMargin * i));
+    }
+  });
+
+  /**
+   * This use case refers to this issue: https://github.com/wix/Detox/issues/1495
+   */
+  it('should scroll various distances accurately, and type text into text fields', async () => {
+    const iterations = 10;
+    const inputHeight = 40;
+    const baseMargin = 20;
+
+    await element(by.text('Integrative Actions')).tap();
+
+    for (let i = 1; i <= iterations; i++) {
+      await scrollingTextInputsDriver.typeInField(i);
+      await scrollingTextInputsDriver.assertFieldText(i);
+      await scrollingTextInputsDriver.scrollDown(inputHeight + (baseMargin * i));
+    }
+  });
 });
