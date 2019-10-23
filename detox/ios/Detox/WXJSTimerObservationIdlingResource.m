bc0c108d77e964a5daf0cae2be2536b3e36e3bd0
Rodrigo Salazar <rodrigos@fb.com>
2019-10-23 12:14:04 -0700

    Handle createTimer API change

--- detox/ios/Detox/WXJSTimerObservationIdlingResource.m
+++ detox/ios/Detox/WXJSTimerObservationIdlingResource.m
@@ -118,38 +118,56 @@ DTX_CREATE_LOG(WXJSTimerObservationIdlingResource)
 		Class cls = NSClassFromString(@"RCTTiming");
 		SEL createTimerSel = NSSelectorFromString(@"createTimer:duration:jsSchedulingTime:repeats:");
 		Method m = class_getInstanceMethod(cls, createTimerSel);
-		
-		void (*orig_createTimer)(id, SEL, NSNumber*, NSTimeInterval, NSDate*, BOOL) = (void(*)(id, SEL, NSNumber*, NSTimeInterval, NSDate*, BOOL))method_getImplementation(m);
-		method_setImplementation(m, imp_implementationWithBlock(^(id _self, NSNumber* timerID, NSTimeInterval duration, NSDate* jsDate, BOOL repeats) {
-			__strong __typeof(weakSelf) strongSelf = weakSelf;
-			
-			dispatch_sync(_timersObservationQueue, ^{
-				_WXJSTimingObservationWrapper* _observationWrapper = [strongSelf->_observations objectForKey:_self];
-				
-				if(_observationWrapper == nil)
-				{
-					_observationWrapper = [[_WXJSTimingObservationWrapper alloc] initWithTimers:[_self valueForKey:@"_timers"]];
-					[_self setValue:_observationWrapper forKey:@"_timers"];
-					[strongSelf->_observations setObject:_observationWrapper forKey:_self];
-				}
-				
-				
-				if(duration > 0 && duration <= _durationThreshold && repeats == NO)
-				{
-					dtx_log_info(@"Observing timer: %@ d: %@ r: %@", timerID, @(duration), @(repeats));
-					
-					[_observationWrapper addObservedTimer:timerID];
-				}
-				else
-				{
-					dtx_log_info(@"Ignoring timer: %@ failure reason: \"%@\"", timerID, [strongSelf failuireReasonForDuration:duration repeats:repeats]);
-				}
-			});
-			
-			orig_createTimer(_self, createTimerSel, timerID, duration, jsDate, repeats);
-		}));
+
+		// Check if the createTimer interface is using doubles or NSObjects.
+		// Earlier versions of react native use NSObjects for the timer and
+		// date params, while later versions use doubles for these.
+		char timerArgType[10];
+		method_getArgumentType(m, 2, timerArgType, 10);
+		if (strcmp(timerArgType, "d") == 0) {
+			void (*orig_createTimer)(id, SEL, double, NSTimeInterval, double, BOOL) = (void(*)(id, SEL, double, NSTimeInterval, double, BOOL))method_getImplementation(m);
+			method_setImplementation(m, imp_implementationWithBlock(^(id _self, double timerID, NSTimeInterval duration, double jsDate, BOOL repeats) {
+				__strong __typeof(weakSelf) strongSelf = weakSelf;
+				[strongSelf attachObservation:_self timerID:[NSNumber numberWithDouble:timerID] duration:duration repeats:repeats];
+				orig_createTimer(_self, createTimerSel, timerID, duration, jsDate, repeats);
+			}));
+		} else {
+			void (*orig_createTimer)(id, SEL, NSNumber*, NSTimeInterval, NSDate*, BOOL) = (void(*)(id, SEL, NSNumber*, NSTimeInterval, NSDate*, BOOL))method_getImplementation(m);
+			method_setImplementation(m, imp_implementationWithBlock(^(id _self, NSNumber* timerID, NSTimeInterval duration, NSDate* jsDate, BOOL repeats) {
+				__strong __typeof(weakSelf) strongSelf = weakSelf;
+				[strongSelf attachObservation:_self timerID:timerID duration:duration repeats:repeats];
+				orig_createTimer(_self, createTimerSel, timerID, duration, jsDate, repeats);
+			}));
+		}
 	}
 	return self;
+}
+
+- (void)attachObservation:(id)_self
+                  timerID:(NSNumber *)timerID
+                 duration:(NSTimeInterval)duration
+                  repeats:(BOOL)repeats
+{
+	dispatch_sync(_timersObservationQueue, ^{
+		_WXJSTimingObservationWrapper* _observationWrapper = [self->_observations objectForKey:_self];
+
+		if(_observationWrapper == nil)
+		{
+			_observationWrapper = [[_WXJSTimingObservationWrapper alloc] initWithTimers:[_self valueForKey:@"_timers"]];
+			[_self setValue:_observationWrapper forKey:@"_timers"];
+			[self->_observations setObject:_observationWrapper forKey:_self];
+		}
+
+		if(duration > 0 && duration <= _durationThreshold && repeats == NO)
+		{
+			dtx_log_info(@"Observing timer: %@ d: %@ r: %@", timerID, @(duration), @(repeats));
+			[_observationWrapper addObservedTimer:timerID];
+		}
+		else
+		{
+			dtx_log_info(@"Ignoring timer: %@ failure reason: \"%@\"", timerID, [self failuireReasonForDuration:duration repeats:repeats]);
+		}
+	});
 }
 
 - (BOOL)isIdleNow
