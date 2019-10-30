c3e79b4b4398ffae63decf2879e4ffdd3b5aacdb
Amit Davidi <amit.d4vidi@gmail.com>
2019-10-30 11:22:00 +0200

    Migrate missing pieces to androidx using Jetifier

--- detox/android/detox/src/main/java/com/wix/detox/ReactNativeSupport.java
+++ detox/android/detox/src/main/java/com/wix/detox/ReactNativeSupport.java
@@ -2,7 +2,7 @@ package com.wix.detox;
 
 import android.content.Context;
 import android.os.Looper;
-import android.support.annotation.NonNull;
+import androidx.annotation.NonNull;
 import android.util.Log;
 
 import com.facebook.react.ReactApplication;
