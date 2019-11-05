8d4fe61ba436724c6978992dcc93aafe5db27fbf
Amit Davidi <amit.d4vidi@gmail.com>
2019-11-05 18:56:01 +0200

    Introduce compliance with RN 0.61.x

--- detox/android/detox/src/main/java/com/wix/detox/ReactNativeCompat.java
+++ detox/android/detox/src/main/java/com/wix/detox/ReactNativeCompat.java
@@ -1,14 +1,12 @@
 package com.wix.detox;
 
-import android.content.Context;
-
 import org.joor.Reflect;
 
 import java.util.HashMap;
 import java.util.Map;
 
 public class ReactNativeCompat {
-    static Map<String, Object> VERSION;
+    private static Map<String, Object> VERSION;
 
     static {
         try {
@@ -26,19 +24,5 @@ public class ReactNativeCompat {
 
     public static int getMinor() {
         return (Integer) VERSION.get("minor");
-    }
-
-    public static void waitForReactNativeLoad(Context reactNativeHostHolder) {
-        if (getMinor() >= 50) {
-            ReactNativeSupport.waitForReactNativeLoad(reactNativeHostHolder);
-            try {
-                //TODO- Temp hack to make Detox usable for RN>=50 till we find a better sync solution.
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        } else {
-            ReactNativeSupport.waitForReactNativeLoad(reactNativeHostHolder);
-        }
     }
 }
