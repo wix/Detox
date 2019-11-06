79f56ff38b4b90433475dcee020e8f7e0586b6d6
Leo Natan <LeoNatan@users.noreply.github.com>
2019-11-06 15:21:01 +0200

    Update install.ios.sh

--- scripts/install.ios.sh
+++ scripts/install.ios.sh
@@ -3,8 +3,8 @@
 $(dirname "$0")/install.sh
 
 gem install xcpretty >/dev/null 2>&1
-export GEM_HOME=$HOME/.gem
-export PATH=$GEM_HOME/bin:$PATH
+#export GEM_HOME=$HOME/.gem
+#export PATH=$GEM_HOME/bin:$PATH
 HOMEBREW_NO_INSTALL_CLEANUP=1 HOMEBREW_NO_AUTO_UPDATE=1 brew install cocoapods
 
 export CODE_SIGNING_REQUIRED=NO
