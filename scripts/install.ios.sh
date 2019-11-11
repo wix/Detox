6a7cc9d686b81d4e1e879708709b65112fb52abb
Leo Natan <leo.natan@outlook.com>
2019-11-12 01:58:36 +0200

    Remove cocoapods installation from script

--- scripts/install.ios.sh
+++ scripts/install.ios.sh
@@ -3,9 +3,6 @@
 $(dirname "$0")/install.sh
 
 gem install xcpretty >/dev/null 2>&1
-#export GEM_HOME=$HOME/.gem
-#export PATH=$GEM_HOME/bin:$PATH
-HOMEBREW_NO_INSTALL_CLEANUP=1 HOMEBREW_NO_AUTO_UPDATE=1 brew install cocoapods
 
 export CODE_SIGNING_REQUIRED=NO
 HOMEBREW_NO_INSTALL_CLEANUP=1 HOMEBREW_NO_AUTO_UPDATE=1 brew tap wix/brew
