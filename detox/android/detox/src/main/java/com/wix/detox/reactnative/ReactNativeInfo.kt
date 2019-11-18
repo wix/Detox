786b612450b82f87bf9b2c2d1699566c7d33a738
Amit Davidi <amit.d4vidi@gmail.com>
2019-11-18 17:52:53 +0200

    Fix wix-app crash introduced due to recent Detox RN-code refactoring

--- detox/android/detox/src/main/java/com/wix/detox/reactnative/ReactNativeInfo.kt
+++ detox/android/detox/src/main/java/com/wix/detox/reactnative/ReactNativeInfo.kt
@@ -1,6 +1,7 @@
 package com.wix.detox.reactnative
 
 import org.joor.Reflect
+import java.util.Map
 
 data class RNVersion(val major: Int, val minor: Int, val patch: Int)
 
@@ -9,7 +10,7 @@ object ReactNativeInfo {
             try {
                 val versionClass = Class.forName("com.facebook.react.modules.systeminfo.ReactNativeVersion")
                 val versionMap: Map<String, Int> = Reflect.on(versionClass).field("VERSION").get()
-                RNVersion(versionMap.getValue("major"), versionMap.getValue("minor"), versionMap.getValue("patch"))
+                RNVersion(versionMap.get("major")!!, versionMap.get("minor")!!, versionMap.get("patch")!!)
             } catch (e: ClassNotFoundException) {
                 // ReactNativeVersion was introduced in RN50, default to latest previous version.
                 RNVersion(0, 49, 0)
