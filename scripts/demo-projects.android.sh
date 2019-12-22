e1dab34e8e56fbef2cf966bc7ac57b0b2c7f57a4
Amit Davidi <amit.d4vidi@gmail.com>
2019-12-22 14:54:59 +0200

    (Android) Example-project: Always run headless on CI

--- scripts/demo-projects.android.sh
+++ scripts/demo-projects.android.sh
@@ -11,11 +11,11 @@ popd
 
 pushd examples/demo-react-native
 run_f "npm run build:android-release"
-run_f "npm run test:android-release -- --device-launch-args=\"-read-only\" --headless"
-run_f "npm run test:android-explicit-require"
+run_f "npm run test:android-release-ci -- --device-launch-args=\"-read-only\""
+run_f "npm run test:android-explicit-require-ci"
 popd
 
 pushd examples/demo-react-native-jest
-run_f "npm run test:android-release-ci -- --headless"
-run_f "npm run test:jest-circus:android-release-ci -- --headless"
+run_f "npm run test:android-release-ci"
+run_f "npm run test:jest-circus:android-release-ci"
 popd
