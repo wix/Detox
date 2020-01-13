4a4d67c5d879fb3fa7ed54e0093d72a23b3190d9
Amit Davidi <amit.d4vidi@gmail.com>
2020-01-13 12:59:30 +0200

    (Android) Apply existing tap improvements and adjustments to tapAtPoint()

--- detox/android/detox/src/main/java/com/wix/detox/espresso/action/RNClickAction.java
+++ detox/android/detox/src/main/java/com/wix/detox/espresso/action/RNClickAction.java
@@ -10,6 +10,7 @@ import org.hamcrest.Matcher;
 
 import androidx.test.espresso.UiController;
 import androidx.test.espresso.ViewAction;
+import androidx.test.espresso.action.CoordinatesProvider;
 import androidx.test.espresso.action.GeneralClickAction;
 import androidx.test.espresso.action.GeneralLocation;
 import androidx.test.espresso.action.Press;
@@ -17,13 +18,25 @@ import androidx.test.espresso.action.Press;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayingAtLeast;
 
 public class RNClickAction implements ViewAction {
-    private final GeneralClickAction clickAction =
-            new GeneralClickAction(
-                new DetoxSingleTap(),
-                GeneralLocation.VISIBLE_CENTER,
-                Press.FINGER,
-                InputDevice.SOURCE_UNKNOWN,
-                MotionEvent.BUTTON_PRIMARY);
+    private final GeneralClickAction clickAction;
+
+    public RNClickAction() {
+        clickAction = new GeneralClickAction(
+                        new DetoxSingleTap(),
+                        GeneralLocation.VISIBLE_CENTER,
+                        Press.FINGER,
+                        InputDevice.SOURCE_UNKNOWN,
+                        MotionEvent.BUTTON_PRIMARY);
+    }
+
+    public RNClickAction(CoordinatesProvider coordinatesProvider) {
+        clickAction = new GeneralClickAction(
+                        new DetoxSingleTap(),
+                        coordinatesProvider,
+                        Press.FINGER,
+                        InputDevice.SOURCE_UNKNOWN,
+                        MotionEvent.BUTTON_PRIMARY);
+    }
 
     @Override
     public Matcher<View> getConstraints() {
