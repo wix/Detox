f307442b15e21b441f33e3fc9829b5b1a7f7ec03
Rotem Mizrachi-Meidan <rotemm@wix.com>
2020-01-20 12:10:29 +0200

    Initial work on InvocationNext

---
+++ detox/src/invoke/InvokeTwo.js
@@ -0,0 +1,364 @@
+class Expect {
+  constructor(element) {
+    this.element = element;
+  }
+
+  toBeVisible() {
+    return this.expect('toBeVisible');
+  }
+
+  toBeNotVisible() {
+    return this.expect('toBeNotVisible');
+  }
+
+  toExist() {
+    return this.expect('toExist');
+  }
+
+  toNotExist() {
+    return this.expect('toNotExist');
+  }
+
+  toHaveText(text) {
+    return this.expect('toHaveText', text);
+  }
+
+  toHaveLabel(label) {
+    return this.expect('toHaveLabel', label);
+  }
+
+  toHaveId(id) {
+    return this.expect('toHaveId', id);
+  }
+
+  toHaveValue(value) {
+    return this.expect('toHaveValue', value);
+  }
+
+  expect(expectation, ...params) {
+    return {
+      type: 'expectation',
+      predicate: this.element.matcher.predicate,
+      expectation,
+      ...(params.length !== 0 && { params })
+    };
+  }
+}
+
+class Element {
+  constructor(matcher) {
+    this.matcher = matcher;
+  }
+
+  tap() {
+    return this.withAction('tap');
+  }
+
+  longPress(duration) {
+    return this.withAction('longPress', duration);
+  }
+
+  multiTap(times) {
+    return this.withAction('multiTap', times);
+  }
+
+  tapAtPoint(point) {
+    return this.withAction('tapAtPoint', point);
+  }
+
+  tapBackspaceKey() {
+    return this.withAction('tapBackspaceKey');
+  }
+
+  tapReturnKey() {
+    return this.withAction('tapReturnKey');
+  }
+
+  typeText(text) {
+    return this.withAction('typeText', text);
+  }
+
+  replaceText(text) {
+    return this.withAction('replaceText', text);
+  }
+
+  clearText() {
+    return this.withAction('clearText');
+  }
+
+  scroll(pixels, direction, startPositionX, startPositionY) {
+    return this.withAction('scroll', pixels, direction, startPositionX, startPositionY);
+  }
+
+  scrollTo(edge) {
+    return this.withAction('scrollTo', edge);
+  }
+
+  swipe(direction, speed, percentage) {
+    return this.withAction('swipe', direction, speed, percentage);
+  }
+
+  setColumnToValue(column, value) {
+    return this.withAction('setColumnToValue', column, value);
+  }
+
+  setDatePickerDate(dateString, dateFormat) {
+    return this.withAction('setColumnToValue', dateString, dateFormat);
+  }
+
+  pinchWithAngle(direction, speed, angle) {
+    return this.withAction('pinchWithAngle', direction, speed, angle);
+  }
+
+  withAction(action, ...params) {
+
+    params = params.filter(function(el) {
+      return el != null;
+    });
+
+    return {
+      type: 'action',
+      action,
+      ...(params.length !== 0 && { params }),
+      predicate: this.matcher.predicate
+    };
+  }
+}
+
+class By {
+  id(byId) {
+    return new Matcher().id(byId);
+  }
+
+  text(text) {
+    return new Matcher().text(text);
+  }
+}
+
+class Matcher {
+
+  accessibilityLabel(byLabel) {
+    return this.label(byLabel);
+  }
+
+  label(byLabel) {
+    this.predicate = { type: 'label', value: byLabel };
+    return this;
+  }
+
+  id(byId) {
+    this.predicate = { type: 'id', value: byId };
+    return this;
+  }
+
+  //TODO - no type matcher!!
+
+  traits(byTraits) {
+    this.predicate = { type: 'traits', value: byTraits };
+    return this;
+  }
+
+  value(byValue) {
+    this.predicate = { type: 'value', value: byValue };
+    return this;
+  }
+
+  text(byText) {
+    this.predicate = { type: 'text', value: byText };
+    return this;
+  }
+
+  withAncestor(matcher) {
+    this.and({ predicate: { type: 'ancestor', value: matcher.predicate } });
+    return this;
+  }
+
+  withDescendant(matcher) {
+    this.and({ predicate: { type: 'descendant', value: matcher.predicate } });
+    return this;
+  }
+
+  and(matcher) {
+    if (this.predicate.type === 'and') {
+      this.predicate.predicates.push(matcher);
+    } else {
+      this.predicate = { type: 'and', predicates: [this.predicate, matcher.predicate] };
+    }
+    return this;
+  }
+}
+
+class WaitFor {
+  constructor(element) {
+    this.element = element;
+  }
+
+  toBeVisible() {
+    this.expectation = expect(this.element).toBeVisible();
+    return this;
+  }
+
+  toBeNotVisible() {
+    this.expectation = expect(this.element).toBeNotVisible();
+    return this;
+  }
+
+  toExist() {
+    this.expectation = expect(this.element).toExist();
+    return this;
+  }
+
+  toNotExist() {
+    this.expectation = expect(this.element).toNotExist();
+    return this;
+  }
+
+  toHaveText(text) {
+    this.expectation = expect(this.element).toHaveText(text);
+    return this;
+  }
+
+  toHaveLabel(label) {
+    this.expectation = expect(this.element).toHaveLabel(label);
+    return this;
+  }
+
+  toHaveId(id) {
+    this.expectation = expect(this.element).toHaveId(id);
+    return this;
+  }
+
+  toHaveValue(value) {
+    this.expectation = expect(this.element).toHaveValue(value);
+    return this;
+  }
+
+  withTimeout(timeout) {
+    this.timeout = timeout;
+    return this.waitForWithTimeout();
+  }
+
+  whileElement(matcher) {
+    this.actionableElement = element(matcher);
+    return this;
+  }
+
+  tap() {
+    this.action = this.actionableElement.tap();
+    return this.waitForWithAction();
+  }
+
+  longPress(duration) {
+    this.action = this.actionableElement.longPress(duration);
+    return this.waitForWithAction();
+  }
+
+  multiTap(times) {
+    this.action = this.actionableElement.multiTap(times);
+    return this.waitForWithAction();
+  }
+
+  tapAtPoint(point) {
+    this.action = this.actionableElement.tapAtPoint(point);
+    return this.waitForWithAction();
+  }
+
+  tapBackspaceKey() {
+    this.action = this.actionableElement.tapBackspaceKey();
+    return this.waitForWithAction();
+  }
+
+  tapReturnKey() {
+    this.action = this.actionableElement.tapReturnKey();
+    return this.waitForWithAction();
+  }
+
+  typeText(text) {
+    this.action = this.actionableElement.typeText(text);
+    return this.waitForWithAction();
+  }
+
+  replaceText(text) {
+    this.action = this.actionableElement.replaceText(text);
+    return this.waitForWithAction();
+  }
+
+  clearText() {
+    this.action = this.actionableElement.clearText();
+    return this.waitForWithAction();
+  }
+
+  scroll(pixels, direction, startPositionX, startPositionY) {
+    this.action = this.actionableElement.scroll(pixels, direction, startPositionX, startPositionY);
+    return this.waitForWithAction();
+  }
+
+  scrollTo(edge) {
+    this.action = this.actionableElement.scrollTo(edge);
+    return this.waitForWithAction();
+  }
+
+  swipe(direction, speed, percentage) {
+    this.action = this.actionableElement.swipe(direction, speed, percentage);
+    return this.waitForWithAction();
+  }
+
+  setColumnToValue(column, value) {
+    this.action = this.actionableElement.setColumnToValue(column, value);
+    return this.waitForWithAction();
+  }
+
+  setDatePickerDate(dateString, dateFormat) {
+    this.action = this.actionableElement.setDatePickerDate(dateString, dateFormat);
+    return this.waitForWithAction();
+  }
+
+  pinchWithAngle(direction, speed, angle) {
+    this.action = this.actionableElement.pinchWithAngle(direction, speed, angle);
+    return this.waitForWithAction();
+  }
+
+  waitForWithAction() {
+    const expectation = this.expectation;
+    const action = this.action;
+
+    return {
+      ...action,
+      while: {
+        ...expectation
+      }
+    };
+  }
+
+  waitForWithTimeout() {
+    const expectation = this.expectation;
+    const action = this.action;
+    const timeout = this.timeout;
+    expectation.type = "waitFor";
+
+    return {
+      ...action,
+      ...expectation,
+      timeout
+      }
+  }
+}
+
+function element(matcher) {
+  return new Element(matcher);
+}
+
+function expect(element) {
+  return new Expect(element);
+}
+
+function waitFor(element) {
+  return new WaitFor(element);
+}
+
+module.exports = {
+  expect,
+  element,
+  waitFor,
+  by: new By()
+};
---
+++ detox/src/invoke/InvokeTwo.test.js
@@ -0,0 +1,205 @@
+const _ = require('lodash');
+const dexpect = require('./InvokeTwo').expect;
+const element = require('./InvokeTwo').element;
+const by = require('./InvokeTwo').by;
+const waitFor = require('./InvokeTwo').waitFor;
+
+describe('InvokeTwo', () => {
+  it(`element(by.text('tapMe')).tap()`, () => {
+    const testCall = element(by.text('tapMe')).tap();
+    const jsonOutput = {
+      type: 'action',
+      action: 'tap',
+      predicate: {
+        type: 'text',
+        value: 'tapMe'
+      }
+    };
+
+    expect(testCall).deepEquals(jsonOutput);
+  });
+
+  it(`element(by.id('uniqueId').and(by.text('some text'))).tap()`, () => {
+    const testCall = element(by.id('uniqueId').and(by.text('some text'))).tap();
+    const jsonOutput = {
+      type: 'action',
+      action: 'tap',
+      predicate: {
+        type: 'and',
+        predicates: [
+          {
+            type: 'id',
+            value: 'uniqueId'
+          },
+          {
+            type: 'text',
+            value: 'some text'
+          }
+        ]
+      }
+    };
+
+    expect(testCall).deepEquals(jsonOutput);
+  });
+
+  it(`element(by.id('child').withAncestor(by.id('parent'))).tap()`, () => {
+    const testCall = element(by.id('child').withAncestor(by.id('parent'))).tap();
+    const jsonOutput = {
+      type: 'action',
+      action: 'tap',
+      predicate: {
+        type: 'and',
+        predicates: [
+          {
+            type: 'id',
+            value: 'child'
+          },
+          {
+            type: 'ancestor',
+            value: {
+              type: 'id',
+              value: 'parent'
+            }
+          }
+        ]
+      }
+    };
+
+    expect(testCall).deepEquals(jsonOutput);
+  });
+
+  it(`element(by.id('child').withAncestor(by.id('parent').and(by.text('text')))).tap()`, () => {
+    const testCall = element(by.id('child').withAncestor(by.id('parent').and(by.text('text')))).tap();
+    const jsonOutput = {
+      type: 'action',
+      action: 'tap',
+      predicate: {
+        type: 'and',
+        predicates: [
+          {
+            type: 'id',
+            value: 'child'
+          },
+          {
+            type: 'ancestor',
+            value: {
+              type: 'and',
+              predicates: [
+                {
+                  type: 'id',
+                  value: 'parent'
+                },
+                {
+                  type: 'text',
+                  value: 'text'
+                }
+              ]
+            }
+          }
+        ]
+      }
+    };
+
+    expect(testCall).deepEquals(jsonOutput);
+  });
+
+  it(`element(by.id('tappable')).tapAtPoint({x:5, y:10})`, () => {
+    const testCall = element(by.id('tappable')).tapAtPoint({ x: 5, y: 10 });
+    const jsonOutput = {
+      type: 'action',
+      action: 'tapAtPoint',
+      params: [
+        {
+          x: 5,
+          y: 10
+        }
+      ],
+      predicate: {
+        type: 'id',
+        value: 'tappable'
+      }
+    };
+
+    expect(testCall).deepEquals(jsonOutput);
+  });
+
+  it(`expect(element(by.text('Tap Working!!!'))).toBeVisible()`, () => {
+    const testCall = dexpect(element(by.text('Tap Working!!!'))).toBeVisible();
+    const jsonOutput = {
+      type: 'expectation',
+      predicate: {
+        type: 'text',
+        value: 'Tap Working!!!'
+      },
+      expectation: 'toBeVisible'
+    };
+
+    expect(testCall).deepEquals(jsonOutput);
+  });
+
+  it(`expect(element(by.id('UniqueId204'))).toHaveText('I contain some text')`, () => {
+    const testCall = dexpect(element(by.id('UniqueId204'))).toHaveText('I contain some text');
+    const jsonOutput = {
+      type: 'expectation',
+      predicate: {
+        type: 'id',
+        value: 'UniqueId204'
+      },
+      expectation: 'toHaveText',
+      params: ['I contain some text']
+    };
+
+    expect(testCall).deepEquals(jsonOutput);
+  });
+
+  it(`waitFor(element(by.text('Text5'))).toBeVisible().whileElement(by.id('ScrollView630')).scroll(50, 'down')`, () => {
+    const testCall = waitFor(element(by.text('Text5'))).toBeVisible().whileElement(by.id('ScrollView630')).scroll(50, 'down');
+    const jsonOutput = {
+      type: 'action',
+      action: 'scroll',
+      params: [50, 'down'],
+      predicate: {
+        type: 'id',
+        value: 'ScrollView630'
+      },
+      while: {
+        type: 'expectation',
+        predicate: {
+          type: 'text',
+          value: 'Text5'
+        },
+        expectation: 'toBeVisible'
+      }
+    };
+
+    expect(testCall).deepEquals(jsonOutput);
+  });
+});
+
+it(`waitFor(element(by.id('uniqueId'))).toHaveValue('Some value').withTimeout(2000)`, () => {
+  const testCall = waitFor(element(by.id('uniqueId'))).toHaveValue('Some value').withTimeout(2000);
+  const jsonOutput = {
+    type: 'waitFor',
+    timeout: 2000,
+    predicate: {
+      type: 'id',
+      value: 'uniqueId'
+    },
+    expectation: 'toHaveValue',
+    params: ['Some value']
+  };
+
+  expect(testCall).deepEquals(jsonOutput);
+});
+
+
+expect.extend({
+  deepEquals(a, b) {
+    const pass = _.isEqual(a, b);
+    return {
+      pass,
+      message: () => `${JSON.stringify(a)} does not match 
+       ${JSON.stringify(b)}`
+    };
+  }
+});
