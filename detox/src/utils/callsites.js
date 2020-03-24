a0e2c106726b752909d66e3b17c719685534451c
Yaroslav Serhieiev <yaroslavs@wix.com>
2020-03-24 18:34:06 +0200

    fix(windows): unescaped regexp issue, unit tests (#1966)

--- detox/src/utils/callsites.js
+++ detox/src/utils/callsites.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const {escapeRegExp} = require('lodash');
 const cwd = process.cwd() + path.sep;
 
 // Taken from https://github.com/sindresorhus/callsites
@@ -15,5 +16,5 @@ module.exports.stackdump = (endFrame = 0) => {
     .split('\n')
     .slice(2 + endFrame) // 2 = 1 for 'Error:' prefix and 1 for this function's frame
     .join('\n')
-    .replace(new RegExp(cwd, 'g'), '');
+    .replace(new RegExp(escapeRegExp(cwd), 'g'), '');
 };
