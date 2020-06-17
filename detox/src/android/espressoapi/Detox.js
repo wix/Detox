047a41f2d5a182962c2a1c9e90b51bf673974947
Amit Davidi <amit.d4vidi@gmail.com>
2020-06-17 09:33:18 +0300

    Add missing generated api's

--- detox/src/android/espressoapi/Detox.js
+++ detox/src/android/espressoapi/Detox.js
@@ -57,44 +57,6 @@ class Detox {
     };
   }
 
-  static cleanIntent() {
-    return {
-      target: {
-        type: "Class",
-        value: "com.wix.detox.Detox"
-      },
-      method: "cleanIntent",
-      args: []
-    };
-  }
-
-  static intentWithUrl(url, initialLaunch) {
-    if (typeof url !== "string") throw new Error("url should be a string, but got " + (url + (" (" + (typeof url + ")"))));
-    if (typeof initialLaunch !== "boolean") throw new Error("initialLaunch should be a boolean, but got " + (initialLaunch + (" (" + (typeof initialLaunch + ")"))));
-    return {
-      target: {
-        type: "Class",
-        value: "com.wix.detox.Detox"
-      },
-      method: "intentWithUrl",
-      args: [url, {
-        type: "boolean",
-        value: initialLaunch
-      }]
-    };
-  }
-
-  static readLaunchArgs() {
-    return {
-      target: {
-        type: "Class",
-        value: "com.wix.detox.Detox"
-      },
-      method: "readLaunchArgs",
-      args: []
-    };
-  }
-
 }
 
 module.exports = Detox;
