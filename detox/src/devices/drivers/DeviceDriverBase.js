d3a90729f50d5920371029d9e9debe85577f1047
Amit Davidi <amit.d4vidi@gmail.com>
2020-06-17 22:18:03 +0300

    Remove explicit require of fs in driver-base

--- detox/src/devices/drivers/DeviceDriverBase.js
+++ detox/src/devices/drivers/DeviceDriverBase.js
@@ -108,7 +108,6 @@ class DeviceDriverBase {
   }
 
   createPayloadFile(notification) {
-    const fs = require('fs');
     const notificationFilePath = path.join(this.createRandomDirectory(), `payload.json`);
     fs.writeFileSync(notificationFilePath, JSON.stringify(notification, null, 2));
     return notificationFilePath;
