eb15d3dc0a8941de27adb6881462c394572a10b1
Yaroslav Serhieiev <noomorph@gmail.com>
2020-06-22 17:25:42 +0300

    ci: nvm install 14

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -2,6 +2,8 @@
 
 source $(dirname "$0")/logger.sh
 
+nvm install 14
+
 if [ ! -z ${REACT_NATIVE_VERSION} ]; then
   node scripts/change_react_native_version.js "detox/test" ${REACT_NATIVE_VERSION}
 fi
