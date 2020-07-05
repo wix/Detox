700677609d01f0e5854e53ea7398fffc69bdb937
Leo Natan <leo.natan@outlook.com>
2020-07-05 18:54:13 +0300

    Add support for Detox Recorder in Detox CLI

--- detox-cli/cli.js
+++ detox-cli/cli.js
@@ -15,14 +15,29 @@ if (fs.existsSync(detoxPackageJsonPath)) {
       { stdio: 'inherit' });
     process.exit(result.status);
   } else {
-    const result = cp.spawnSync(
-      path.join(process.cwd(), 'node_modules/.bin/detox'),
-      process.argv.slice(2),
-      { stdio: 'inherit' });
-    process.exit(result.status);
+    const cliArgs = process.argv.slice(2);
+    if(cliArgs.length == 0 || cliArgs[0] !== "recorder") {
+      //Detox
+      const result = cp.spawnSync(
+        path.join(process.cwd(), 'node_modules/.bin/detox'),
+        cliArgs,
+        { stdio: 'inherit' });
+      process.exit(result.status);
+    } else {
+      //Detox Recorder path
+      const detoxRecorderPath = path.join(process.cwd(), 'node_modules/detox-recorder');
+      const detoxRecorderCLIPath = path.join(detoxRecorderPath, "DetoxRecorderCLI");
+      
+      if (fs.existsSync(detoxRecorderCLIPath)) {
+        const result = cp.spawnSync(detoxRecorderCLIPath, cliArgs, { stdio: 'inherit' });
+        process.exit(result.status);
+      } else {
+        console.log(`Detox Recorder is not installed in this directory: ${detoxRecorderPath}`);
+        process.exit(1);
+      }
+    }
   }
 } else {
-  console.log(detoxPackageJsonPath);
-  console.log("detox is not installed in this directory");
+  console.log(`Detox is not installed in this directory: ${detoxPath}`);
   process.exit(1);
 }
