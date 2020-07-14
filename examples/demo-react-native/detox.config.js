b5b9508f3dd5d6b7491160d3cb8919e80ebf8ad5
Dmytro Kostiuk <dmytro.kostiuk@mendix.com>
2020-07-14 11:33:32 +0200

    Mention regex-based 'adbName' property in docs and examples

--- examples/demo-react-native/detox.config.js
+++ examples/demo-react-native/detox.config.js
@@ -51,6 +51,14 @@ module.exports = {
       "device": {
         "avdName": "Pixel_API_28"
       }
+    },
+    "android.att.release": {
+      "binaryPath": "android/app/build/outputs/apk/release/app-release.apk",
+      "build": "cd android ; ./gradlew assembleRelease assembleAndroidTest -DtestBuildType=release ; cd -",
+      "type": "android.attached",
+      "device": {
+        "adbName": "emulator-.*"
+      }
     }
   }
 };
