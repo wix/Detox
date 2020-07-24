90772e8217180ed5a1a19e335d9d402ddf3f489b
Dmytro Kostiuk <dmytro.kostiuk@mendix.com>
2020-07-24 11:17:25 +0200

    Review: duck test parrent matching iso mocking RegExp

--- detox/src/devices/drivers/android/attached/FreeAdbDeviceFinder.test.js
+++ detox/src/devices/drivers/android/attached/FreeAdbDeviceFinder.test.js
@@ -2,16 +2,10 @@ const FreeAdbDeviceFinder = require('./FreeAdbDeviceFinder');
 
 describe('FreeAdbDeviceFinder', () => {
   it("should test candidate's adbName against a regular expression", async () => {
-    const mockRegExpTest = jest.fn();
-    const mockRegExpConstructor = jest.fn(() => ({ test: mockRegExpTest }));
-    global.RegExp = mockRegExpConstructor;
+    const finder = new FreeAdbDeviceFinder(undefined, undefined);
+    const onlyDigits = '^\\d+$';
 
-    const freeAdbDeviceFinder = new FreeAdbDeviceFinder(undefined, undefined);
-    const candidate = { adbName: 'mockAdbName' };
-    const adbNamePattern = 'mockAdbNamePattern';
-
-    await freeAdbDeviceFinder.isDeviceMatching(candidate, adbNamePattern);
-    expect(mockRegExpConstructor).toHaveBeenCalledWith(adbNamePattern);
-    expect(mockRegExpTest).toHaveBeenCalledWith(candidate.adbName);
+    expect(await finder.isDeviceMatching({ adbName: '5555' }, onlyDigits)).toBe(true);
+    expect(await finder.isDeviceMatching({ adbName: 'emulator-5555' }, onlyDigits)).toBe(false);
   });
 });
