2450864937bd15ccde630711a334ad7429464da9
Yaroslav Serhieiev <yaroslavs@wix.com>
2020-09-08 11:47:58 +0300

    fix: use relative paths

--- detox/runners/jest/FailingTestsReporter.js
+++ detox/runners/jest/FailingTestsReporter.js
@@ -1,10 +1,12 @@
+const path = require('path');
 const { saveLastFailedTests } = require('../../src/utils/lastFailedTests');
 
 class FailingTestsReporter {
   async onRunComplete(_contexts, { testResults }) {
+    const cwd = process.cwd();
     const failedFiles = testResults
       .filter(result => result.numFailingTests > 0)
-      .map(result => result.testFilePath);
+      .map(result => path.relative(cwd, result.testFilePath));
 
     await saveLastFailedTests(failedFiles);
   }
