136df022d06000d8b83f3795e21a71c2ff79ec62
Yaroslav Serhieiev <yaroslavs@wix.com>
2020-09-08 11:55:51 +0300

    code: remove remnants of attempt to support Mocha

--- detox/runners/mocha/DetoxMochaAdapter.js
+++ detox/runners/mocha/DetoxMochaAdapter.js
@@ -1,7 +1,6 @@
 class DetoxMochaAdapter {
   constructor(detox) {
     this.detox = detox;
-    this._invocations = 1 + Number(process.env.DETOX_RERUN_INDEX || '0');
   }
 
   async beforeEach(context) {
@@ -9,7 +8,6 @@ class DetoxMochaAdapter {
       title: context.currentTest.title,
       fullName: context.currentTest.fullTitle(),
       status: this._mapStatus(context, false),
-      invocations: this._invocations,
     });
   }
 
@@ -18,7 +16,6 @@ class DetoxMochaAdapter {
       title: context.currentTest.title,
       fullName: context.currentTest.fullTitle(),
       status: this._mapStatus(context, true),
-      invocations: this._invocations,
       timedOut: context.currentTest.timedOut,
     });
   }
