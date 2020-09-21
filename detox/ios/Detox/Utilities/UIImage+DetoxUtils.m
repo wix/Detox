31fcb9cdc0b2d2dcc019a9962d14809be8e55064
Leo Natan <leo.natan@outlook.com>
2020-09-22 00:51:33 +0300

    New visibility algorithm, taking into account any possible occlusion from window or view

--- detox/ios/Detox/Utilities/UIImage+DetoxUtils.m
+++ detox/ios/Detox/Utilities/UIImage+DetoxUtils.m
@@ -10,7 +10,14 @@
 
 @implementation UIImage (DetoxUtils)
 
-- (NSUInteger)dtx_numberOfVisiblePixelsWithThreshold:(CGFloat)threshold totalPixels:(NSUInteger*)totalPixels
+- (UIImage *)dtx_imageByCroppingInRect:(CGRect)rect
+{
+	rect = CGRectMake(rect.origin.x * self.scale, rect.origin.y * self.scale, rect.size.width * self.scale, rect.size.height * self.scale);
+	
+	return [UIImage imageWithCGImage:CGImageCreateWithImageInRect(self.CGImage, rect) scale:1 orientation:self.imageOrientation];
+}
+
+- (NSUInteger)dtx_numberOfVisiblePixelsWithAlphaThreshold:(CGFloat)threshold totalPixels:(NSUInteger*)totalPixels
 {
 	CGImageRef cgImage = self.CGImage;
 	
@@ -18,23 +25,65 @@
 	dtx_defer {
 		CFRelease(pixelData);
 	};
-	const UInt8* data = CFDataGetBytePtr(pixelData);
+	const uint8_t* bytes = CFDataGetBytePtr(pixelData);
+	
+	size_t width  = CGImageGetWidth(cgImage);
+	size_t height = CGImageGetHeight(cgImage);
+	
+	size_t bpr = CGImageGetBytesPerRow(cgImage);
+	size_t bpp = CGImageGetBitsPerPixel(cgImage);
+	size_t bpc = CGImageGetBitsPerComponent(cgImage);
+	size_t bytes_per_pixel = bpp / bpc;
+	
+	CGImageAlphaInfo alphaInfo = CGImageGetAlphaInfo(cgImage);
+	CGBitmapInfo bitmapInfo = CGImageGetBitmapInfo(cgImage);
+	
+	uint8_t alphaOffset;
+	if(alphaInfo == kCGImageAlphaPremultipliedFirst)
+	{
+		if((bitmapInfo & kCGBitmapByteOrderMask) == kCGBitmapByteOrder32Little)
+		{
+			alphaOffset = 3;
+		}
+		else
+		{
+			alphaOffset = 0;
+		}
+	}
+	else
+	{
+		if((bitmapInfo & kCGBitmapByteOrderMask) == kCGBitmapByteOrder32Little)
+		{
+			alphaOffset = 0;
+		}
+		else
+		{
+			alphaOffset = 3;
+		}
+	}
 	
 	NSUInteger visible = 0;
 	NSUInteger total = 0;
 	
-	for (NSUInteger y = 0; y < self.size.height; y++) {
-		for (NSUInteger x = 0; x < self.size.width; x++) {
+	for(size_t row = 0; row < height; row++)
+	{
+		for(size_t col = 0; col < width; col++)
+		{
 			total++;
-			uint8_t alpha255 = data[((NSUInteger)self.size.width * y + x) * 4 + 3];
+			
+			const uint8_t* pixel =
+			&bytes[row * bpr + col * bytes_per_pixel];
+			
+			uint8_t alpha255 = pixel[alphaOffset];
 			CGFloat alpha = alpha255 / 255.0;
+			
 			if(alpha < threshold)
 			{
 				visible++;
 			}
 		}
 	}
-	
+
 	if(totalPixels != NULL)
 	{
 		*totalPixels = total;
---
+++ detox/ios/Detox/Utilities/UIView+Drawing.m
@@ -0,0 +1,110 @@
+//
+//  UIView+Drawing.m
+//  Detox
+//
+//  Created by Leo Natan on 9/17/20.
+//  Copyright © 2020 Wix. All rights reserved.
+//
+
+#import "UIView+Drawing.h"
+@import ObjectiveC;
+
+@interface CALayer (BackdropDrawingUtils) @end
+@implementation CALayer (BackdropDrawingUtils)
+
++ (void)load
+{
+	@autoreleasepool
+	{
+		Method m = class_getInstanceMethod(CALayer.class, @selector(renderInContext:));
+		class_addMethod(NSClassFromString(@"CABackdropLayer"), method_getName(m), (IMP)_dtx_backdrop_renderInContext, method_getTypeEncoding(m));
+	}
+}
+
+void _dtx_backdrop_renderInContext(CALayer* self, SEL _sel, CGContextRef ctx)
+{
+	CGContextSaveGState(ctx);
+	CGContextBeginTransparencyLayer(ctx, nil);
+	CGContextSetAlpha(ctx, self.opacity);
+	if (@available(iOS 13.0, *)) {
+		[UIColor.systemBackgroundColor setFill];
+	} else {
+		[UIColor.whiteColor setFill];
+	}
+	CGContextFillRect(ctx, self.bounds);
+	CGContextEndTransparencyLayer(ctx);
+	CGContextRestoreGState(ctx);
+}
+
+@end
+
+@implementation UIView (Drawing)
+
+- (CGRect)dtx_recursiveBounds
+{
+	if(self.clipsToBounds == YES)
+	{
+		return self.bounds;
+	}
+	
+	CGRect rv = self.bounds;
+	for (UIView* subview in self.subviews) {
+		rv = CGRectUnion(rv, subview.dtx_recursiveBounds);
+	}
+	return rv;
+}
+
+- (void)dtx_drawViewHierarchyUpToSubview:(UIView*)subview inRect:(CGRect)rect afterScreenUpdates:(BOOL)afterUpdates
+{
+	return [self _dtx_drawViewHierarchyUpToSubview:subview rootView:self inRect:rect afterScreenUpdates:afterUpdates suspectDescendant:YES];
+}
+
+CALayer* _DTXLayerForView(UIView* view, BOOL afterUpdates)
+{
+	return afterUpdates ? view.layer : view.layer.presentationLayer;
+}
+
+- (void)_dtx_drawViewHierarchyUpToSubview:(UIView*)subview rootView:(UIView*)rootView inRect:(CGRect)rect afterScreenUpdates:(BOOL)afterUpdates suspectDescendant:(BOOL)suspectDescendant
+{
+	CGContextRef ctx = UIGraphicsGetCurrentContext();
+	
+	if(subview == self)
+	{
+		return;
+	}
+	
+	if(suspectDescendant == NO)
+	{
+		[_DTXLayerForView(self, afterUpdates) renderInContext:ctx];
+		
+		return;
+	}
+	
+	if([subview isDescendantOfView:self] == NO)
+	{
+		[_DTXLayerForView(self, afterUpdates) renderInContext:ctx];
+		
+		return;
+	}
+	
+	for (UIView* obj in self.subviews.reverseObjectEnumerator) {
+		if([subview isDescendantOfView:obj] == NO)
+		{
+			CGContextSaveGState(ctx);
+			CGRect bounds = obj.bounds;
+			CGRect boundsInRootViewCoords = [rootView convertRect:bounds fromView:obj];
+			CGContextTranslateCTM(ctx, boundsInRootViewCoords.origin.x, boundsInRootViewCoords.origin.y);
+			[_DTXLayerForView(obj, afterUpdates) renderInContext:ctx];
+			CGContextRestoreGState(ctx);
+		}
+		else
+		{
+			[obj _dtx_drawViewHierarchyUpToSubview:subview rootView:rootView inRect:obj.bounds afterScreenUpdates:afterUpdates suspectDescendant:YES];
+			
+			//Everything else is now under the view we are searching for.
+			return;
+		}
+	}
+}
+
+@end
