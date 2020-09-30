db4aad2a07714e2d34a2116ca0861e1951cb36d6
Yaroslav Serhieiev <yaroslavs@wix.com>
2020-09-30 12:32:13 +0300

    test(e2e): unite iOS and Android test scenarios

--- detox/test/e2e/28.visibility.test.js
+++ detox/test/e2e/28.visibility.test.js
@@ -13,28 +13,7 @@ describe('Visibility Cases', () => {
       .scroll(200, 'right');
   });
 
-  it(':ios: should be able to tap on the main ScrollView in a blind way, on the center, when the on-screen keyboard is enabled', async () => {
-    await element(by.id('inputExample')).tap();
-
-    const { elementSafeBounds: bounds } = await element(by.id('screenScroll')).getAttributes();
-
-    await element(by.id('screenScroll')).tap({
-      x: Math.floor(0.5 * bounds.width),
-      y: Math.floor(0.5 * bounds.height),
-    });
-  });
-
-  it(':ios: should be able to dismiss the keyboard by scrolling down in the ScrollView', async () => {
-    await expect(element(by.id('screenScroll'))).toBeVisible();
-
-    await element(by.id('inputExample')).tap();
-    await expect(element(by.id('screenScroll'))).not.toBeVisible();
-
-    await element(by.id('screenScroll')).scroll(50, 'down');
-    await expect(element(by.id('screenScroll'))).toBeVisible();
-  });
-
-  it(':android: should be able to tap on the main ScrollView regardless of the on-screen keyboard', async () => {
+  it('should dismiss the on-screen keyboard upon a tap on the main ScrollView', async () => {
     await element(by.id('inputExample')).tap();
     await element(by.id('screenScroll')).tap();
   });
