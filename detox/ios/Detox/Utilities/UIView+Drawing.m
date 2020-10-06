b0540d91ab0698a0f9d416308d1ffa7239a9608e
Leo Natan <leo.natan@outlook.com>
2020-10-06 22:08:06 +0300

    Fix crashes caused by MKMapView render bugs
    Closes #2384

--- detox/ios/Detox/Utilities/UIView+Drawing.m
+++ detox/ios/Detox/Utilities/UIView+Drawing.m
@@ -12,6 +12,30 @@
 @interface CALayer (BackdropDrawingUtils) @end
 @implementation CALayer (BackdropDrawingUtils)
 
+
+static void (*__orig_VKMapView_renderInContext)(id self, SEL _cmd, CGContextRef ctx);
++ (void)_dtx_applyDrawingFixes
+{
+	Class cls = NSClassFromString(@"VKMapView");
+	if(cls != nil)
+	{
+		Method m = class_getInstanceMethod(cls, @selector(renderInContext:));
+		__orig_VKMapView_renderInContext = (void*)method_getImplementation(m);
+		method_setImplementation(m, (void*)_dtx_map_renderInContext);
+	}
+}
+
++ (void)_dtx_restoreDrawingDefaults
+{
+	Class cls = NSClassFromString(@"VKMapView");
+	if(cls != nil)
+	{
+		Method m = class_getInstanceMethod(cls, @selector(renderInContext:));
+		__orig_VKMapView_renderInContext = (void*)method_getImplementation(m);
+		method_setImplementation(m, (void*)__orig_VKMapView_renderInContext);
+	}
+}
+
 + (void)load
 {
 	@autoreleasepool
@@ -19,6 +43,30 @@
 		Method m = class_getInstanceMethod(CALayer.class, @selector(renderInContext:));
 		class_addMethod(NSClassFromString(@"CABackdropLayer"), method_getName(m), (IMP)_dtx_backdrop_renderInContext, method_getTypeEncoding(m));
 	}
+}
+
+static void _dtx_map_renderInContext(CALayer* self, SEL _sel, CGContextRef ctx)
+{
+	CGContextSaveGState(ctx);
+	CGContextBeginTransparencyLayer(ctx, nil);
+	CGContextSetAlpha(ctx, self.opacity);
+	if (@available(iOS 13.0, *)) {
+		[[UIColor colorWithDynamicProvider:^UIColor * _Nonnull(UITraitCollection * _Nonnull traitCollection) {
+			if(traitCollection.userInterfaceStyle == UIUserInterfaceStyleDark)
+			{
+				return [UIColor colorWithRed:44.0 / 255.0 green:45.0 / 255.0 blue:47.0 / 255.0 alpha:1.0];
+			}
+			else
+			{
+				return [UIColor colorWithRed:250.0 / 255.0 green:245.0 / 255.0 blue:237.0 / 255.0 alpha:1.0];
+			}
+		}] setFill];
+	} else {
+		[UIColor.whiteColor setFill];
+	}
+	CGContextFillRect(ctx, self.bounds);
+	CGContextEndTransparencyLayer(ctx);
+	CGContextRestoreGState(ctx);
 }
 
 static void _dtx_backdrop_renderInContext(CALayer* self, SEL _sel, CGContextRef ctx)
@@ -43,7 +91,11 @@ DTX_DIRECT_MEMBERS
 
 - (void)dtx_drawViewHierarchyUpToSubview:(UIView*)subview inRect:(CGRect)rect afterScreenUpdates:(BOOL)afterUpdates
 {
-	return [self _dtx_drawViewHierarchyUpToSubview:subview rootView:self inRect:rect afterScreenUpdates:afterUpdates];
+	[CALayer _dtx_applyDrawingFixes];
+	
+	[self _dtx_drawViewHierarchyUpToSubview:subview rootView:self inRect:rect afterScreenUpdates:afterUpdates];
+	
+	[CALayer _dtx_restoreDrawingDefaults];
 }
 
 CALayer* _DTXLayerForView(UIView* view, BOOL afterUpdates)
