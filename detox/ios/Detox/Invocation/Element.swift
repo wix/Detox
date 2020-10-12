d59130e3cfe7d33b8c4979ba4647e71b1abbde37
Leo Natan <leo.natan@outlook.com>
2020-10-12 18:45:43 +0300

    Xcode 12.2 🤦‍♂️

--- detox/ios/Detox/Invocation/Element.swift
+++ detox/ios/Detox/Invocation/Element.swift
@@ -66,7 +66,7 @@ class Element : NSObject {
 		let element : UIView
 		if let index = index {
 			guard index < array.count else {
-				dtx_fatalError("Index \(index) beyond bounds [0 .. \(array.count - 1)] for “\(self.description)”", viewDescription: failDebugAttributes)
+				dtx_fatalError("Index \(index) beyond bounds \(array.count > 0 ? "[0 .. \(array.count - 1)] " : " ")for “\(self.description)”", viewDescription: failDebugAttributes)
 			}
 			element = array[index]
 		} else {
