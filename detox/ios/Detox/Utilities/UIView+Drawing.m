d9df3938edbb795d8487fff9184ef16797ea02e1
Leo Natan <leo.natan@outlook.com>
2020-10-15 22:17:56 +0300

    Support any subclass that might implement its own drawing logic

--- detox/ios/Detox/Utilities/UIView+Drawing.m
+++ detox/ios/Detox/Utilities/UIView+Drawing.m
@@ -14,120 +14,24 @@
 - (void)_renderSublayersInContext:(struct CGContext *)arg1;
 - (void)_renderForegroundInContext:(struct CGContext *)arg1;
 - (void)_renderBackgroundInContext:(struct CGContext *)arg1;
+- (void)_renderBorderInContext:(struct CGContext *)arg1;
 
 @end
 
 @interface CALayer (BackdropDrawingUtils) @end
 @implementation CALayer (BackdropDrawingUtils)
 
-static BOOL _hack = NO;
-static BOOL _found = NO;
-static UIView* _lookingFor = nil;
-
-static NSArray* (*__orig_CALayer_sublayers)(id self, SEL _cmd);
-static NSArray* _dtx_layer_sublayers(id self, SEL _cmd)
-{
-	if(_hack == NO)
-	{
-		return __orig_CALayer_sublayers(self, _cmd);
-	}
-	
-	NSArray* zSorted = [__orig_CALayer_sublayers(self, _cmd) sortedArrayWithOptions:NSSortStable usingComparator:^NSComparisonResult(CALayer* _Nonnull obj1, CALayer* _Nonnull obj2) {
-		return [@(obj1.zPosition) compare:@(obj2.zPosition)];
-	}];
-	
-	return zSorted;
-}
-
-static void (*__orig_CALayer_renderForegroundInContext)(id self, SEL _cmd, CGContextRef ctx);
-static void _dtx_layer_renderForegroundInContext(CALayer* self, SEL _cmd, CGContextRef ctx)
-{
-	if(_hack == NO)
-	{
-		__orig_CALayer_renderForegroundInContext(self, _cmd, ctx);
-		return;
-	}
-	
-	UIView* delegate = (self.delegate != nil && [self.delegate isKindOfClass:UIView.class]) ? (id)self.delegate : nil;
-	
-	if(_found == NO && (delegate == nil || [_lookingFor isDescendantOfView:delegate] == NO))
-	{
-		//This layer tree is under the subview we are looking for.
-		return;
-	}
-	
-	if([_lookingFor isDescendantOfView:delegate] == YES)
-	{
-		return;
-	}
-	
-	__orig_CALayer_renderForegroundInContext(self, _cmd, ctx);
-}
-
-static void (*__orig_CALayer_renderBackgroundInContext)(id self, SEL _cmd, CGContextRef ctx);
-static void _dtx_layer_renderBackgroundInContext(CALayer* self, SEL _cmd, CGContextRef ctx)
-{
-	if(_hack == NO)
-	{
-		__orig_CALayer_renderBackgroundInContext(self, _cmd, ctx);
-		return;
-	}
-	
-	UIView* delegate = (self.delegate != nil && [self.delegate isKindOfClass:UIView.class]) ? (id)self.delegate : nil;
-	
-	if(_found == NO && (delegate == nil || [_lookingFor isDescendantOfView:delegate] == NO))
-	{
-		//This layer tree is under the subview we are looking for.
-		return;
-	}
-	
-	if([_lookingFor isDescendantOfView:delegate] == YES)
-	{
-		return;
-	}
-	
-	__orig_CALayer_renderBackgroundInContext(self, _cmd, ctx);
-}
-
-static void (*__orig_CALayer_renderInContext)(id self, SEL _cmd, CGContextRef ctx);
-static void _dtx_layer_renderInContext(CALayer* self, SEL _cmd, CGContextRef ctx)
-{
-	if(_hack == NO)
-	{
-		__orig_CALayer_renderInContext(self, _cmd, ctx);
-		return;
-	}
-	
-	UIView* delegate = (self.delegate != nil && [self.delegate isKindOfClass:UIView.class]) ? (id)self.delegate : nil;
-	
-	if(_found == NO && (delegate == nil || [_lookingFor isDescendantOfView:delegate] == NO))
-	{
-		//This layer tree is under the subview we are looking for.
-		return;
-	}
-	
-	if(delegate == _lookingFor)
-	{
-		//This layer tree is the one we are looking for, mark as found and return.
-		_found = YES;
-		return;
-	}
-	
-	if(_found == YES)
-	{
-		__orig_CALayer_renderInContext(self, _cmd, ctx);
-		return;
-	}
-	
-//	[self _renderSublayersInContext:ctx];
-	__orig_CALayer_renderInContext(self, _cmd, ctx);
-}
+static BOOL __currentlyDrawing = NO;
+static BOOL __subviewFound = NO;
+static UIView* __subview = nil;
 
 static void (*__orig_VKMapView_renderInContext)(id self, SEL _cmd, CGContextRef ctx);
 + (void)_dtx_applyDrawingFixes
 {
-	_hack = YES;
-	_found = NO;
+	[CALayer _fixupRenderingOnce];
+	
+	__currentlyDrawing = YES;
+	__subviewFound = NO;
 	
 	Class cls = NSClassFromString(@"VKMapView");
 	if(cls != nil)
@@ -148,7 +52,7 @@ static void (*__orig_VKMapView_renderInContext)(id self, SEL _cmd, CGContextRef 
 		method_setImplementation(m, (void*)__orig_VKMapView_renderInContext);
 	}
 	
-	_hack = NO;
+	__currentlyDrawing = NO;
 }
 
 + (void)load
@@ -157,23 +61,132 @@ static void (*__orig_VKMapView_renderInContext)(id self, SEL _cmd, CGContextRef 
 	{
 		Method m = class_getInstanceMethod(CALayer.class, @selector(renderInContext:));
 		class_addMethod(NSClassFromString(@"CABackdropLayer"), method_getName(m), (IMP)_dtx_backdrop_renderInContext, method_getTypeEncoding(m));
-		
-		m = class_getInstanceMethod(CALayer.class, @selector(renderInContext:));
-		__orig_CALayer_renderInContext = (void*)method_getImplementation(m);
-		method_setImplementation(m, (void*)_dtx_layer_renderInContext);
-		
-		m = class_getInstanceMethod(CALayer.class, @selector(_renderForegroundInContext:));
-		__orig_CALayer_renderForegroundInContext = (void*)method_getImplementation(m);
-		method_setImplementation(m, (void*)_dtx_layer_renderForegroundInContext);
-		
-		m = class_getInstanceMethod(CALayer.class, @selector(_renderBackgroundInContext:));
-		__orig_CALayer_renderBackgroundInContext = (void*)method_getImplementation(m);
-		method_setImplementation(m, (void*)_dtx_layer_renderBackgroundInContext);
-		
-		m = class_getInstanceMethod(CALayer.class, @selector(sublayers));
-		__orig_CALayer_sublayers = (void*)method_getImplementation(m);
-		method_setImplementation(m, (void*)_dtx_layer_sublayers);
 	}
+}
+
++ (void)_fixupRenderingOnce
+{
+	static dispatch_once_t onceToken;
+	dispatch_once(&onceToken, ^{
+		//Do all this to support custom layer classes thay may implement their own renderInContext: and co.
+		
+		Class* classes = NULL;
+		int numClasses = objc_getClassList(NULL, 0);
+		if (numClasses > 0)
+		{
+			classes = (Class*)malloc(sizeof(Class) * numClasses);
+			numClasses = objc_getClassList(classes, numClasses);
+
+			for (int i = 0; i < numClasses; i++)
+			{
+				Class superClass = classes[i];
+				while(superClass && superClass != CALayer.class)
+				{
+					superClass = class_getSuperclass(superClass);
+				}
+
+				if (superClass == nil)
+				{
+					continue;
+				}
+
+				unsigned int numMethods = 0;
+				Method* methods = class_copyMethodList(classes[i], &numMethods);
+				
+				for(unsigned int j = 0; j < numMethods; j++)
+				{
+					Method m = methods[j];
+					if(sel_isEqual(method_getName(m), @selector(renderInContext:)))
+					{
+						void (*__orig_CALayer_renderInContext)(id self, SEL _cmd, CGContextRef ctx) = (void*)method_getImplementation(m);
+						method_setImplementation(m, imp_implementationWithBlock(^(CALayer* self, CGContextRef ctx) {
+							SEL _cmd = @selector(renderInContext:);
+							
+							if(__currentlyDrawing == NO)
+							{
+								__orig_CALayer_renderInContext(self, _cmd, ctx);
+								return;
+							}
+							
+							UIView* delegate = (self.delegate != nil && [self.delegate isKindOfClass:UIView.class]) ? (id)self.delegate : nil;
+							
+							if(__subviewFound == NO && (delegate == nil || [__subview isDescendantOfView:delegate] == NO))
+							{
+								//This layer tree is under the subview we are looking for.
+								return;
+							}
+							
+							if(delegate == __subview)
+							{
+								//This layer tree is the one we are looking for, mark as found and return.
+								__subviewFound = YES;
+								return;
+							}
+							
+							if(__subviewFound == YES)
+							{
+								__orig_CALayer_renderInContext(self, _cmd, ctx);
+								return;
+							}
+							
+							//	[self _renderSublayersInContext:ctx];
+							__orig_CALayer_renderInContext(self, _cmd, ctx);
+						}));
+					}
+					else if(sel_isEqual(method_getName(m), @selector(_renderForegroundInContext:)) || sel_isEqual(method_getName(m), @selector(_renderBackgroundInContext:)) || sel_isEqual(method_getName(m), @selector(_renderBorderInContext:)))
+					{
+						SEL _cmd = method_getName(m);
+						
+						void (*__orig_CALayer_something)(id self, SEL _cmd, CGContextRef ctx) = (void*)method_getImplementation(m);
+						method_setImplementation(m, imp_implementationWithBlock(^(CALayer* self, CGContextRef ctx) {
+							if(__currentlyDrawing == NO)
+							{
+								__orig_CALayer_something(self, _cmd, ctx);
+								return;
+							}
+							
+							UIView* delegate = (self.delegate != nil && [self.delegate isKindOfClass:UIView.class]) ? (id)self.delegate : nil;
+							
+							if(__subviewFound == NO && (delegate == nil || [__subview isDescendantOfView:delegate] == NO))
+							{
+								//This layer tree is under the subview we are looking for.
+								return;
+							}
+							
+							if([__subview isDescendantOfView:delegate] == YES)
+							{
+								return;
+							}
+							
+							__orig_CALayer_something(self, _cmd, ctx);
+						}));
+					}
+					else if(sel_isEqual(method_getName(m), @selector(sublayers)))
+					{
+						NSArray* (*__orig_CALayer_sublayers)(id self, SEL _cmd) = (void*)method_getImplementation(m);
+						method_setImplementation(m, imp_implementationWithBlock(^(CALayer* self, CGContextRef ctx) {
+							SEL _cmd = @selector(sublayers);
+							
+							if(__currentlyDrawing == NO)
+							{
+								return __orig_CALayer_sublayers(self, _cmd);
+							}
+							
+							NSArray* zSorted = [__orig_CALayer_sublayers(self, _cmd) sortedArrayWithOptions:NSSortStable usingComparator:^NSComparisonResult(CALayer* _Nonnull obj1, CALayer* _Nonnull obj2) {
+								return [@(obj1.zPosition) compare:@(obj2.zPosition)];
+							}];
+							
+							return zSorted;
+						}));
+					}
+				}
+				
+				free(methods);
+			}
+
+			free(classes);
+		}
+	});
 }
 
 static void _dtx_map_renderInContext(CALayer* self, SEL _cmd, CGContextRef ctx)
@@ -228,7 +241,7 @@ CALayer* _DTXLayerForView(UIView* view, BOOL afterUpdates)
 - (void)dtx_drawViewHierarchyUpToSubview:(UIView*)subview inRect:(CGRect)rect afterScreenUpdates:(BOOL)afterUpdates
 {
 	[CALayer _dtx_applyDrawingFixes];
-	_lookingFor = subview;
+	__subview = subview;
 	
 	CGContextRef ctx = UIGraphicsGetCurrentContext();
 	CGContextSaveGState(ctx);
