wix/Detox

commit 9dd9635f0ad01fea8c306dd1090bf6612c88c081

Introduce tap-time verification through ui-controller swizzling (#2423)

detox/android/detox/src/main/java/com/wix/detox/espresso/UiAutomatorHelper.java
@@ -7,6 +7,8 @@ import android.util.Log;
 import android.view.Choreographer;
 import android.view.View;
 
+import com.wix.detox.espresso.common.utils.UiControllerUtils;
+
 import org.hamcrest.core.IsAnything;
 import org.joor.Reflect;
 import org.joor.ReflectException;
@@ -25,10 +27,7 @@ import androidx.test.platform.app.InstrumentationRegistry;
 public class UiAutomatorHelper {
     private static final String LOG_TAG = "detox";
 
-    private static final String FIELD_UI_CONTROLLER = "uiController";
-
     private static final String METHOD_LOOP_UNTIL_IDLE = "loopMainThreadUntilIdle";
-    private static final String METHOD_LOOP_AT_LEAST = "loopMainThreadForAtLeast";
 
     /**
      * This triggers a full Espresso sync. It's intended use is to sync UIAutomator calls.
@@ -37,12 +36,11 @@ public class UiAutomatorHelper {
         // I want to invoke Espresso's sync mechanism manually.
         // This turned out to be amazingly difficult. This below is the
         // nicest solution I could come up with.
-        final ViewInteraction interaction = Espresso.onView(new IsAnything<View>());
         InstrumentationRegistry.getInstrumentation().runOnMainSync(new Runnable() {
             @Override
             public void run() {
                 try {
-                    Reflect.on(interaction).field(FIELD_UI_CONTROLLER).call(METHOD_LOOP_UNTIL_IDLE);
+                    Reflect.on(UiControllerUtils.getUiController()).call(METHOD_LOOP_UNTIL_IDLE);
                 } catch (ReflectException e) {
                     Log.e(LOG_TAG, "Failed to sync Espresso manually.", e.getCause());
                 }
@@ -59,12 +57,11 @@ public class UiAutomatorHelper {
         // I want to invoke Espresso's sync mechanism manually.
         // This turned out to be amazingly difficult. This below is the
         // nicest solution I could come up with.
-        final ViewInteraction interaction = Espresso.onView(new IsAnything<View>());
         InstrumentationRegistry.getInstrumentation().runOnMainSync(new Runnable() {
             @Override
             public void run() {
                 try {
-                    Reflect.on(interaction).field(FIELD_UI_CONTROLLER).call(METHOD_LOOP_AT_LEAST, millis);
+                    Reflect.on(UiControllerUtils.getUiController()).call(METHOD_LOOP_UNTIL_IDLE, millis);
                 } catch (ReflectException e) {
                     Log.e(LOG_TAG, "Failed to sync Espresso manually.", e.getCause());
                 }

detox/android/detox/src/main/java/com/wix/detox/Detox.java
@@ -9,6 +9,7 @@ import android.os.Looper;
 import android.util.Log;
 
 import com.wix.detox.config.DetoxConfig;
+import com.wix.detox.espresso.UiControllerSpy;
 
 import java.util.concurrent.TimeUnit;
 
@@ -190,9 +191,11 @@ public final class Detox {
      */
     public static void runTests(ActivityTestRule activityTestRule, @NonNull final Context context, DetoxConfig detoxConfig) {
         DetoxConfig.CONFIG = detoxConfig;
-        detoxConfig.apply();
+        DetoxConfig.CONFIG.apply();
 
         sActivityTestRule = activityTestRule;
+
+        UiControllerSpy.attachThroughProxy();
 
         Intent intent = extractInitialIntent();
         sActivityTestRule.launchActivity(intent);

detox/android/detox/src/main/java/com/wix/detox/common/DetoxErrors.java
@@ -20,4 +20,10 @@ public interface DetoxErrors {
             super(cause);
         }
     }
+
+    class DetoxIllegalStateException extends DetoxRuntimeException {
+        public DetoxIllegalStateException(String message) {
+            super(message);
+        }
+    }
 }

detox/android/detox/src/main/java/com/wix/detox/common/collect/PairsIterator.kt
@@ -1,0 +1,18 @@
+package com.wix.detox.common.collect
+
+import java.lang.IllegalStateException
+
+class PairsIterator<T>(private val delegate: Iterator<T>): Iterator<Pair<T, T>> {
+    constructor(iterable: Iterable<T>): this(iterable.iterator())
+
+    override fun hasNext(): Boolean = delegate.hasNext()
+    override fun next(): Pair<T, T> {
+        val next = delegate.next()
+        if (!delegate.hasNext()) {
+            throw IllegalStateException("Uneven iterator content!")
+        }
+
+        val nextNext = delegate.next()
+        return Pair(next, nextNext)
+    }
+}

detox/android/detox/src/main/java/com/wix/detox/common/proxy/MethodsSpy.kt
@@ -1,0 +1,76 @@
+package com.wix.detox.common.proxy
+
+import android.util.Log
+import java.util.*
+
+data class CallInfo(var inTime: Long? = null, var outTime: Long? = null) {
+    fun open(inTime: Long = System.currentTimeMillis()): CallInfo {
+        this.inTime = inTime
+        this.outTime = null
+        return this
+    }
+
+    fun close(outTime: Long = System.currentTimeMillis()): CallInfo {
+        this.outTime = outTime
+        return this
+    }
+
+    operator fun minus(other: CallInfo): Long? {
+        val inTime = other.inTime
+        val outTime = this.outTime
+        if (outTime != null && inTime != null) {
+            return outTime - inTime
+        }
+        return null
+    }
+
+    override fun toString() = "$inTime -> $outTime"
+}
+
+open class MethodsSpy(private val entityName: String) {
+    private val methodHistories = mutableMapOf<String, LinkedList<CallInfo>>()
+    private var enabled = false
+
+    fun start() {
+        reset()
+        enabled = true
+    }
+
+    fun stop() {
+        enabled = false
+    }
+
+    fun onBeforeCall(methodName: String) {
+        if (enabled) {
+            historyOf(methodName).addFirst(CallInfo().open())
+        }
+    }
+
+    fun onAfterCall(methodName: String) {
+        if (enabled) {
+            historyOf(methodName).peekFirst()?.close()
+        }
+    }
+
+    fun getHistory(methodName: String): Queue<CallInfo>?
+        = methodHistories[methodName]
+
+    fun dumpToLog() {
+        for ((methodName, history) in methodHistories.entries) {
+            Log.d(LOG_TAG, "[$entityName] method $methodName: $history")
+        }
+    }
+
+    internal fun historyOf(key: String) =
+        methodHistories.getOrPut(key) {
+            LinkedList()
+        }
+
+    private fun reset() {
+        methodHistories.clear()
+    }
+
+    companion object {
+        private const val LOG_TAG = "MethodsSpy"
+    }
+}

detox/android/detox/src/main/java/com/wix/detox/common/proxy/SpyingInvocationHandler.kt
@@ -1,0 +1,30 @@
+package com.wix.detox.common.proxy
+
+import java.lang.reflect.InvocationHandler
+import java.lang.reflect.InvocationTargetException
+import java.lang.reflect.Method
+import java.lang.reflect.Proxy
+
+class SpyingInvocationHandler(private val target: Any, private val spy: MethodsSpy) : InvocationHandler {
+    override fun invoke(proxy: Any, m: Method, args: Array<Any>): Any {
+        val result: Any
+        try {
+            spy.onBeforeCall(m.name)
+            result = m.invoke(target, *args)
+        } catch (e: InvocationTargetException) {
+            throw e.targetException
+        } finally {
+            spy.onAfterCall(m.name)
+        }
+        return result
+    }
+
+    companion object {
+        @JvmStatic
+        fun newInstance(obj: Any, journal: MethodsSpy) =
+            Proxy.newProxyInstance(
+                    obj.javaClass.classLoader,
+                    obj.javaClass.interfaces,
+                    SpyingInvocationHandler(obj, journal))
+    }
+}

detox/android/detox/src/main/java/com/wix/detox/espresso/UiControllerSpy.kt
@@ -1,0 +1,38 @@
+package com.wix.detox.espresso
+
+import androidx.test.espresso.UiController
+import com.wix.detox.common.proxy.CallInfo
+import com.wix.detox.common.proxy.SpyingInvocationHandler
+import com.wix.detox.common.proxy.MethodsSpy
+import com.wix.detox.espresso.common.utils.getUiController
+import org.joor.Reflect
+
+class UiControllerSpy: MethodsSpy("uiController") {
+    fun eventInjectionsIterator(): Iterator<CallInfo?> = historyOf("injectMotionEvent").iterator()
+
+    companion object {
+        @JvmStatic
+        val instance = UiControllerSpy()
+
+        @JvmStatic
+        @JvmOverloads
+        fun attachThroughProxy(spy: UiControllerSpy = instance) {
+            val eventsInjectorReflected = EventsInjectorReflected(getUiController())
+
+            val eventsInjectionStrategy = eventsInjectorReflected.eventsInjectionStrategy!!
+            val eventsInjectionStrategyProxy = SpyingInvocationHandler.newInstance(eventsInjectionStrategy, spy)
+
+            eventsInjectorReflected.eventsInjectionStrategy = eventsInjectionStrategyProxy
+        }
+    }
+}
+
+private class EventsInjectorReflected constructor(uiController: UiController?) {
+    private /*EventInjector*/ val eventInjector: Any? = Reflect.on(uiController).get("eventInjector")
+
+    var eventsInjectionStrategy: Any? /*EventInjectionStrategy*/
+        get() = Reflect.on(eventInjector).get<Any>("injectionStrategy")
+        set(value) {
+            Reflect.on(eventInjector)["injectionStrategy"] = value
+        }
+}

detox/android/detox/src/main/java/com/wix/detox/espresso/action/DetoxMultiTap.kt
@@ -3,7 +3,12 @@ package com.wix.detox.espresso.action
 import android.view.MotionEvent
 import androidx.test.espresso.UiController
 import androidx.test.espresso.action.Tapper
+import com.wix.detox.common.DetoxErrors.DetoxIllegalStateException
+import com.wix.detox.common.collect.PairsIterator
+import com.wix.detox.common.proxy.CallInfo
+import com.wix.detox.espresso.UiControllerSpy
 import com.wix.detox.espresso.common.DetoxViewConfigurations.getDoubleTapMinTime
+import com.wix.detox.espresso.common.DetoxViewConfigurations.getLongTapMinTime
 import com.wix.detox.espresso.common.DetoxViewConfigurations.getPostTapCoolDownTime
 import com.wix.detox.espresso.common.TapEvents
 
@@ -27,8 +32,10 @@ open class DetoxMultiTap
     @JvmOverloads constructor(
             private val times: Int,
             private val interTapsDelayMs: Long = getDoubleTapMinTime(),
-            private val coolDownTime: Long = getPostTapCoolDownTime(),
-            private val tapEvents: TapEvents = TapEvents())
+            private val coolDownTimeMs: Long = getPostTapCoolDownTime(),
+            private val longTapMinTimeMs: Long = getLongTapMinTime(),
+            private val tapEvents: TapEvents = TapEvents(),
+            private val uiControllerCallSpy: UiControllerSpy = UiControllerSpy.instance)
     : Tapper {
 
     override fun sendTap(uiController: UiController?, coordinates: FloatArray?, precision: FloatArray?)
@@ -43,10 +50,12 @@ open class DetoxMultiTap
         try {
             eventSequence = generateEventSequences(coordinates, precision)
 
-            if (!uiController.injectMotionEventSequence(eventSequence)) {
+            if (!injectEvents(uiController, eventSequence)) {
                 return Tapper.Status.FAILURE
             }
-            uiController.loopMainThreadForAtLeast(coolDownTime)
+            verifyInjectionPeriods()
+
+            uiController.loopMainThreadForAtLeast(coolDownTimeMs)
             return Tapper.Status.SUCCESS
         } finally {
             eventSequence?.forEach { it.recycle() }
@@ -64,5 +73,37 @@ open class DetoxMultiTap
             downTimestamp = tapEvents.last().eventTime + interTapsDelayMs
         }
         return eventSequence
+    }
+
+    private fun injectEvents(uiController: UiController, eventSequence: List<MotionEvent>): Boolean {
+        try {
+            uiControllerCallSpy.start()
+            if (!uiController.injectMotionEventSequence(eventSequence)) {
+                return false
+            }
+        } finally {
+            uiControllerCallSpy.stop()
+        }
+        return true
+    }
+
+    /**
+     * Note: This renders the class non-extensible so as to not being able to handle other types of taps -- for example, a
+     * long-tap created by tapEvents.
+     * If extensibility is ever needed, this can, be solved by refactoring tapEvents onto a tap injection class
+     * that both creates tap events and validates the right constraints over them.
+     */
+    private fun verifyInjectionPeriods() {
+        val rawIterator = uiControllerCallSpy.eventInjectionsIterator()
+        PairsIterator(rawIterator).forEach {
+            verifyTapEventTimes(it.first!!, it.second!!)
+        }
+    }
+
+    private fun verifyTapEventTimes(upEvent: CallInfo, downEvent: CallInfo) {
+        val delta: Long = (upEvent - downEvent)!!
+        if (delta >= longTapMinTimeMs) {
+            throw DetoxIllegalStateException("Tap handled too slowly, and turned into a long-tap!")
+        }
     }
 }

commit c9e42e360e00089ce587c28693473e46d2cb1fa6

Fix regression caused by 9dd9635f

detox/android/detox/src/main/java/com/wix/detox/espresso/UiAutomatorHelper.java
@@ -28,6 +28,7 @@ public class UiAutomatorHelper {
     private static final String LOG_TAG = "detox";
 
     private static final String METHOD_LOOP_UNTIL_IDLE = "loopMainThreadUntilIdle";
+    private static final String METHOD_LOOP_AT_LEAST = "loopMainThreadForAtLeast";
 
     /**
      * This triggers a full Espresso sync. It's intended use is to sync UIAutomator calls.
@@ -61,7 +62,7 @@ public class UiAutomatorHelper {
             @Override
             public void run() {
                 try {
-                    Reflect.on(UiControllerUtils.getUiController()).call(METHOD_LOOP_UNTIL_IDLE, millis);
+                    Reflect.on(UiControllerUtils.getUiController()).call(METHOD_LOOP_AT_LEAST, millis);
                 } catch (ReflectException e) {
                     Log.e(LOG_TAG, "Failed to sync Espresso manually.", e.getCause());
                 }