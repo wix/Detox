f12d4fe8f2a93fba10f253f3c3c27bfdb095746c
Rotem Mizrachi-Meidan <rotemm@wix.com>
2020-11-03 10:19:31 +0200

    trying to update lerna through scripts

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -2,11 +2,12 @@
 
 source $(dirname "$0")/logger.sh
 
+npm install -g lerna@3.22.1 >/dev/null 2>&1
+
 if [ ! -z ${REACT_NATIVE_VERSION} ]; then
   node scripts/change_react_native_version.js "detox/test" ${REACT_NATIVE_VERSION}
 fi
 
 run_f "lerna bootstrap"
-
 run_f "lerna run build"
 run_f "lerna run test"
