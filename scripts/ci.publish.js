c018d853359ab2eece2375d13f6aafeb0a9d466e
Rotem Mizrachi-Meidan <rotemm@wix.com>
2020-11-12 20:41:46 +0200

    force release when asked nicely :)

--- scripts/ci.publish.js
+++ scripts/ci.publish.js
@@ -35,7 +35,7 @@ function publishToNpm(npmTag) {
     log('SKIP NPM is set: Lerna-publishing without publishing to NPM');
   }
   const preid = npmTag === 'latest'? '': `--preid=${npmTag}`;
-  exec.execSync(`lerna publish ${versionType} --yes --dist-tag ${npmTag} ${preid} ${dryRun ? '--no-push': ''}  ${(dryRun || skipNpm) ? '--skip-npm' : ''} -m "Publish %v [ci skip]" --tag-version-prefix=''`);
+  exec.execSync(`lerna publish ${versionType} --yes --dist-tag ${npmTag} ${preid} ${dryRun ? '--no-push': ''}  ${(dryRun || skipNpm) ? '--skip-npm' : ''} -m "Publish %v [ci skip]" --tag-version-prefix='' --force-publish=detox`);
 }
 
 module.exports = publishNewVersion;
