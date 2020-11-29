68e780b17c662329c2f086237559efb603d6a797
Leo Natan <leo.natan@outlook.com>
2020-11-29 14:56:17 +0200

    Create new modern framework build script

--- detox/scripts/build_framework.ios.sh
+++ detox/scripts/build_framework.ios.sh
@@ -29,11 +29,21 @@ function extractFramework () {
 
 function buildFramework () {
   detoxSourcePath="${1}"
-  echo "Building Detox.framework from ${detoxSourcePath}..."
+  echo "Building Detox.framework from ${detoxSourcePath} into ${detoxFrameworkDirPath}"
   mkdir -p "${detoxFrameworkDirPath}"
-	logPath="${detoxFrameworkDirPath}"/detox_ios.log
-	echo -n "" > "${logPath}"
-  "${detoxRootPath}"/scripts/build_universal_framework.sh "${detoxSourcePath}"/Detox.xcodeproj "${detoxFrameworkDirPath}" &> "${logPath}" || {
+  logPath="${detoxFrameworkDirPath}"/detox_ios.log
+  echo -n "" > "${logPath}"
+  
+  XCODEVERSION=$(xcodebuild -version | grep -oEi "([0-9]*(\.[0-9]*)+)")
+  if [ "${XCODEVERSION}" == "`echo -e "${XCODEVERSION}\n12.0" | sort --version-sort -r | head -n1`" ]; then
+    echo "Xcode 12 and above; using modern script for building the framework to support Apple Silicon"
+    FRAMEWORK_SCRIPT="build_universal_framework_modern.sh"
+  else
+    echo "Xcode 11 and below; using legacy script for building"
+    FRAMEWORK_SCRIPT="build_universal_framework.sh"
+  fi
+  
+  "${detoxRootPath}/scripts/${FRAMEWORK_SCRIPT}" "${detoxSourcePath}"/Detox.xcodeproj "${detoxFrameworkDirPath}" &> "${logPath}" || {
 		echo -e "#################################\nError building Detox.framework:\n----------------------------------\n"
 		cat "${logPath}"
 		echo "#################################"
