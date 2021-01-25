b9972d4a0f61d7646dc359c88fc52c89572f3b99
Alon Haiut <alonhai@wix.com>
2021-01-25 13:07:49 +0200

    e2e for drag and drop

--- detox/test/e2e/28.drag-and-drop.test.js
+++ detox/test/e2e/28.drag-and-drop.test.js
@@ -4,7 +4,20 @@ describe('Drag And Drop', () => {
     await element(by.text('Drag And Drop')).tap();
   });
 
-  it('should have drag and drop screen', async () => {
-    await expect(element(by.text('Drag And Drop Screen'))).toBeVisible();
+  it('should drag the first cell and drop at the seven cell position', async () => {
+    await assertCellsOrder([1, 2 ,3 ,4 ,5 ,6 ,7 ,8 ,9, 10])
+    // await element(by.id('cellId_0')).longPressAndDrag(2000, 0.5, 0.5, element(by.id('cellId_6')), 0.5, 0.5, 'fast', 2000);
+    // await assertCellsOrder([2 ,3 ,4 ,5 ,6 ,7 ,1, 8 ,9, 10])
   });
+
+  async function assertCellsOrder(order) {
+    let text = `Cells order: `
+    for (let i = 0; i < order.length; i++) {
+      if (i > 0) {
+        text += ', '
+      }
+      text += `${order[i]}`
+    }
+    await expect(element(by.id('cellsOrderId'))).toHaveText(text);
+  }
 });
