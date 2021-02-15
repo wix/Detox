69adea700278d15c61640c5932d1f0494ba62a49
Yaroslav Serhieiev <yaroslavs@wix.com>
2021-02-15 19:37:08 +0200

    fix: unit test

--- detox/src/utils/customConsoleLogger.test.js
+++ detox/src/utils/customConsoleLogger.test.js
@@ -17,10 +17,11 @@ describe('customConsoleLogger.overrideConsoleMethods(console, bunyanLogger)', ()
     };
 
     bunyanLogger = {
-      debug: jest.fn(),
+      error: jest.fn(),
       warn: jest.fn(),
       info: jest.fn(),
-      error: jest.fn(),
+      debug: jest.fn(),
+      trace: jest.fn(),
     };
   });
 
