8b7a03d35f722361b55df725da23ad60d6b1adb6
Yaroslav Serhieiev <yaroslavs@wix.com>
2021-04-03 11:13:25 +0300

    ci: temporarily disable Genycloud

--- scripts/ci.android.sh
+++ scripts/ci.android.sh
@@ -22,10 +22,10 @@ pushd detox/test
 run_f "npm run build:android"
 cp ../coverage/lcov.info ../../coverage/unit.lcov
 
-run_f "npm run e2e:android-ci-genycloud"
+run_f "npm run e2e:android-ci-google"
 cp coverage/lcov.info ../../coverage/e2e-android-ci.lcov
 
-run_f "npm run e2e:android-ci-google -- e2e/01* e2e/02* e2e/03*"
+# run_f "npm run e2e:android-ci-google -- e2e/01* e2e/02* e2e/03*"
 
 run_f "scripts/ci_unhappy.sh android"
 
