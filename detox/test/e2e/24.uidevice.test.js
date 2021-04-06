a19907909f283550f453fbaff5b2b114a8620cb2
Amit Davidi <amit.d4vidi@gmail.com>
2021-04-06 11:13:26 +0300

    Fix uncovered sync issue with uiDevice e2e

--- detox/test/e2e/24.uidevice.test.js
+++ detox/test/e2e/24.uidevice.test.js
@@ -1,25 +1,27 @@
-  describe(':android: UIDevice', () => {
-    beforeEach(async () => {
-      await device.reloadReactNative();
-    });
+const { forEachSeries } = require('p-iteration');
 
-    it(`device.getUiDevice() + getDisplayHeight() + getDisplayWidth() + click()`, async () => {
-      await element(by.text('Device')).tap();
-      const uiDevice = device.getUiDevice();
-      const height = await uiDevice.getDisplayHeight();
-      const width = await uiDevice.getDisplayWidth();
-      await uiDevice.click(width / 2, height / 2);
-      await expect(element(by.text('Tap works'))).toBeVisible();
-    });
+describe(':android: UIDevice', () => {
+  beforeEach(async () => {
+    await device.reloadReactNative();
+  });
 
-    it(`should type in an element using pressKeyCode()`, async () => {
-      const text = "a1b2c3";
-      const textAsCodes = [29, 8, 30, 9, 31, 10];
-      const uiDevice = device.getUiDevice();
+  it(`device.getUiDevice() + getDisplayHeight() + getDisplayWidth() + click()`, async () => {
+    await element(by.text('Device')).tap();
+    const uiDevice = device.getUiDevice();
+    const height = await uiDevice.getDisplayHeight();
+    const width = await uiDevice.getDisplayWidth();
+    await uiDevice.click(width / 2, height / 2);
+    await expect(element(by.text('Tap works'))).toBeVisible();
+  });
 
-      await element(by.text('Actions')).tap();
-      await element(by.id('UniqueId937')).tap();
-      await textAsCodes.forEach((keyCode) => uiDevice.pressKeyCode(keyCode));
-      await expect(element(by.text(text))).toBeVisible();
-    });
+  it(`should type in an element using pressKeyCode()`, async () => {
+    const text = "a1b2c3";
+    const textAsCodes = [29, 8, 30, 9, 31, 10];
+    const uiDevice = device.getUiDevice();
+
+    await element(by.text('Actions')).tap();
+    await element(by.id('UniqueId937')).tap();
+    await forEachSeries(textAsCodes, (keyCode) => uiDevice.pressKeyCode(keyCode));
+    await expect(element(by.text(text))).toBeVisible();
+  });
 });
