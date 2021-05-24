e1f17b8f76bf8fb2c01c78c3ea16ba68b11d0bf6
Alon Haiut <alonhai@wix.com>
2021-05-24 18:16:38 +0300

    Screenshots e2e running on iOS as well

--- detox/test/e2e/26.element-screenshots.test.js
+++ detox/test/e2e/26.element-screenshots.test.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const { expectToThrow } = require('./utils/custom-expects');
 
-describe(':android: Element screenshots', () => {
+describe('Element screenshots', () => {
 
   beforeEach(async () => {
     await device.reloadReactNative();
@@ -9,14 +9,14 @@ describe(':android: Element screenshots', () => {
   });
 
   it('should take a screenshot of a vertically-clipped element', async () => {
-    const screenshotAssetPath = './e2e/assets/elementScreenshot.android.vert.png';
+    const screenshotAssetPath = `./e2e/assets/elementScreenshot.${device.getPlatform()}.vert.png`;
 
     const bitmapPath = await element(by.id('fancyElement')).takeScreenshot();
     expectBitmapsToBeEqual(bitmapPath, screenshotAssetPath);
   });
 
   it('should take a screenshot of a horizontally-clipped element', async () => {
-    const screenshotAssetPath = './e2e/assets/elementScreenshot.android.horiz.png';
+    const screenshotAssetPath = `./e2e/assets/elementScreenshot.${device.getPlatform()}.horiz.png`;
 
     await element(by.id('switchOrientation')).tap();
 
@@ -24,7 +24,7 @@ describe(':android: Element screenshots', () => {
     expectBitmapsToBeEqual(bitmapPath, screenshotAssetPath);
   });
 
-  it('should fail to take a screenshot of an off-screen element', async () => {
+  it(':android: should fail to take a screenshot of an off-screen element', async () => {
     await expectToThrow(
       () => element(by.id('offscreenElement')).takeScreenshot(),
       `Cannot take screenshot of a view that's out of screen's bounds`,
