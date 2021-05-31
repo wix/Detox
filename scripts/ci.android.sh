d36aca23c64b17143009b60947f9ffa930d9da85
Amit Davidi <amit.d4vidi@gmail.com>
2021-05-31 19:07:16 +0300

    Comment out genycloud, temporarily

--- scripts/ci.android.sh
+++ scripts/ci.android.sh
@@ -26,8 +26,9 @@ pushd detox/test
 run_f "npm run build:android"
 cp ../coverage/lcov.info ../../coverage/unit.lcov
 
-run_f "npm run e2e:android-ci-genycloud"
-cp coverage/lcov.info ../../coverage/e2e-android-ci.lcov
+#DO NOT MERGE
+#run_f "npm run e2e:android-ci-genycloud"
+#cp coverage/lcov.info ../../coverage/e2e-android-ci.lcov
 
 run_f "npm run e2e:android-ci-google -- e2e/01* e2e/02* e2e/03*"
 
