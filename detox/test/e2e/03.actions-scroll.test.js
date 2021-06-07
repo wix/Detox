a1a6ae3af17155f73777eabe632b142f11c38aea
Alon Haiut <alonhai@wix.com>
2021-06-07 14:03:59 +0300

    fixed scrolling test

--- detox/test/e2e/03.actions-scroll.test.js
+++ detox/test/e2e/03.actions-scroll.test.js
@@ -49,7 +49,7 @@ describe('Actions - Scroll', () => {
   it('should scroll from a custom start-position ratio', async () => {
     await expect(element(by.text('Text12'))).not.toBeVisible();
     await element(by.id('toggleScrollOverlays')).tap();
-    await element(by.id('ScrollView161')).scroll(310, 'down', 0.8, 0.6);
+    await element(by.id('ScrollView161')).scroll(550, 'down', 0.8, 0.6);
     await element(by.id('toggleScrollOverlays')).tap();
     await expect(element(by.text('Text12'))).toBeVisible();
 
