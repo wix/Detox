af9b5a78f99b7a601405d0adb4edd17f7f42d676
Alon Haiut <alonhai@wix.com>
2021-07-25 17:32:47 +0300

    re-added the script

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,4 +1,8 @@
 #!/bin/bash -e
 
+mkdir ~/.npm-global
+npm config set prefix '~/.npm-global'
+export PATH=~/.npm-global/bin:$PATH
+
 npm install -g lerna@3.22.1
 npm install -g react-native-cli >/dev/null 2>&1
