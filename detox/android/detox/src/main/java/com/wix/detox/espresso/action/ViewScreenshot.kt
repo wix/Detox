fc04f0fa27d80fe02ac0dea60858fed535dadb59
d4vidi <amit.d4vidi@gmail.com>
2021-08-01 13:58:05 +0300

    Android: Remove code associated with SDK<21 (Detox's min) (#2927)

--- detox/android/detox/src/main/java/com/wix/detox/espresso/action/ViewScreenshot.kt
+++ detox/android/detox/src/main/java/com/wix/detox/espresso/action/ViewScreenshot.kt
@@ -4,13 +4,12 @@ import android.graphics.Bitmap
 import android.graphics.Canvas
 import android.util.Base64
 import android.view.View
-import com.wix.detox.common.SDKSupports
 import java.io.ByteArrayOutputStream
 
 class ScreenshotResult(private val bitmap: Bitmap) {
     fun asBitmap() = bitmap
     fun asRawBytes(): ByteArray {
-        val outStream = ByteArrayOutputStream(if (SDKSupports.API_19_KITKAT) bitmap.allocationByteCount else bitmap.byteCount)
+        val outStream = ByteArrayOutputStream(bitmap.allocationByteCount)
         bitmap.compress(Bitmap.CompressFormat.PNG, 100, outStream)
         return outStream.toByteArray()
     }
