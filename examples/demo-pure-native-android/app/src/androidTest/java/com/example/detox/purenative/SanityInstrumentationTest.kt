db40bc02a4fedf43e17b480c0b8a830b2c515c0c
d4vidi <amit.d4vidi@gmail.com>
2021-08-09 17:31:36 +0300

    POC: Revert unnecessary pure-native proj changes

--- examples/demo-pure-native-android/app/src/androidTest/java/com/example/detox/purenative/SanityInstrumentationTest.kt
+++ examples/demo-pure-native-android/app/src/androidTest/java/com/example/detox/purenative/SanityInstrumentationTest.kt
@@ -1,17 +1,11 @@
 package com.example.detox.purenative
 
-import android.app.Activity
-import android.app.Instrumentation
-import android.app.Instrumentation.ActivityMonitor
-import android.content.Intent
 import android.view.View
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.rule.ActivityTestRule
-import androidx.test.uiautomator.UiDevice
 import com.wix.detox.actions.DetoxViewActions
 import org.hamcrest.CoreMatchers.not
 import org.hamcrest.Matcher
@@ -30,28 +24,22 @@ class ListDriver {
 
 @RunWith(AndroidJUnit4::class)
 class SanityInstrumentationTest {
-//    @get:Rule
-//    val rule = ActivityTestRule(MainActivity::class.java, true, false)
+    @get:Rule
+    val rule = ActivityTestRule(MainActivity::class.java, true, true)
 
     private val driver = ListDriver()
 
     @Before
     fun beforeEach() {
+        onView(withId(R.id.main_button_actions)).perform(DetoxViewActions.tap())
     }
 
     @Test
     fun testTapping() {
-//        launchActivitySync(MainActivity::class.java)
-
-        onView(withId(R.id.main_button_actions)).perform(DetoxViewActions.tap())
-        onView(withText("Actions Screen")).check(matches(isCompletelyDisplayed()))
-
-        UiDevice.getInstance(InstrumentationRegistry.getInstrumentation()).pressHome()
-        launchActivitySync(MainActivity::class.java)
         onView(withText("Actions Screen")).check(matches(isCompletelyDisplayed()))
     }
 
-//    @Test
+    @Test
     fun testSubtleScrollBy() {
         onView(driver.itemMatcher(1)).check(matches(isCompletelyDisplayed()))
         onView(driver.itemMatcher(5)).check(matches(not(isCompletelyDisplayed())))
@@ -65,25 +53,9 @@ class SanityInstrumentationTest {
         onView(driver.itemMatcher(5)).check(matches(not(isCompletelyDisplayed())))
     }
 
-//    @Test
+    @Test
     fun testScrollBy() {
         onView(driver.listMatcher()).perform(DetoxViewActions.scrollDownBy(driver.itemHeight * 11, 0.1f, 0.95f))
         onView(driver.itemMatcher(14)).check(matches(isCompletelyDisplayed()))
-    }
-
-    companion object {
-        private fun launchActivitySync(activityClass: Class<*>) {
-            val instrumentation = InstrumentationRegistry.getInstrumentation()
-            val appContext = instrumentation.targetContext
-
-            val intent = Intent(appContext, activityClass)
-            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-            appContext.startActivity(intent)
-
-            val activityMonitor = ActivityMonitor(activityClass.name, null, true)
-            instrumentation.addMonitor(activityMonitor)
-            instrumentation.waitForMonitorWithTimeout(activityMonitor, 10000L)
-        }
-
     }
 }
