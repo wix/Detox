wix/Detox

commit bcbbcbe43601fb1075744d6ba23b218b3f15911c

fix(artifacts): save app artifacts before its uninstall (#2958)

detox/src/artifacts/screenshot/SimulatorScreenshotPlugin.js
@@ -1,3 +1,4 @@
+const fs = require('../../utils/fsext');
 const log = require('../../utils/logger').child({ __filename });
 const FileArtifact = require('../templates/artifact/FileArtifact');
 const temporaryPath = require('../utils/temporaryPath');
@@ -36,16 +37,31 @@ class SimulatorScreenshotPlugin extends ScreenshotArtifactPlugin {
     }
   }
 
-  _onInvokeFailure({ params }) {
-    const { visibilityFailingScreenshotsURL, visibilityFailingRectsURL } = params;
+  async onBeforeUninstallApp(event) {
+    const snapshots = [
+      ...Object.values(this.snapshots.fromTest),
+      ...Object.values(this.snapshots.fromSession)
+    ];
 
-    this._registerSnapshot('visibilityFailingScreenshots/', new FileArtifact({
-      temporaryPath: visibilityFailingScreenshotsURL
-    }));
+    await Promise.all(snapshots.map(s => s && s.relocate()));
+    await super.onBeforeUninstallApp(event);
+  }
 
-    this._registerSnapshot('visibilityFailingRects/', new FileArtifact({
-      temporaryPath: visibilityFailingRectsURL
-    }));
+  async _onInvokeFailure({ params }) {
+    const artifacts = {
+      visibilityFailingScreenshots: params.visibilityFailingScreenshotsURL,
+      visibilityFailingRects: params.visibilityFailingRectsURL,
+    };
+
+    for (const [key, artifactPath] of Object.entries(artifacts)) {
+      if (!artifactPath || await fs.isDirEmpty(artifactPath)) {
+        continue;
+      }
+
+      this._registerSnapshot(`${key}/`, new FileArtifact({
+        temporaryPath: artifactPath,
+      }));
+    }
   }
 
   createTestArtifact() {

detox/src/artifacts/uiHierarchy/IosUIHierarchyPlugin.js
@@ -1,6 +1,6 @@
 const _ = require('lodash');
 
-const DetoxRuntimeError = require('../../errors/DetoxRuntimeError');
+const DetoxInternalError = require('../../errors/DetoxInternalError');
 const setUniqueProperty = require('../../utils/setUniqueProperty');
 const FileArtifact = require('../templates/artifact/FileArtifact');
 const ArtifactPlugin = require('../templates/plugin/ArtifactPlugin');
@@ -32,7 +32,7 @@ class IosUIHierarchyPlugin extends ArtifactPlugin {
 
   async onCreateExternalArtifact(e) {
     if (!e.artifact) {
-      throw new DetoxRuntimeError('Internal error: expected Artifact instance in the event');
+      throw new DetoxInternalError('Expected Artifact instance in the event');
     }
 
     this._registerSnapshot(e.name, e.artifact);
@@ -67,6 +67,16 @@ class IosUIHierarchyPlugin extends ArtifactPlugin {
 
     this.context.testSummary = null;
     await this._flushArtifacts();
+  }
+
+  async onBeforeUninstallApp(event) {
+    const artifacts = [
+      ...Object.values(this._artifacts.perTest),
+      ...Object.values(this._artifacts.perSession)
+    ];
+
+    await Promise.all(artifacts.map(s => s && s.relocate()));
+    await super.onBeforeUninstallApp(event);
   }
 
   async onBeforeCleanup() {

detox/src/utils/fsext.js
@@ -2,6 +2,11 @@ const path = require('path');
 
 const fs = require('fs-extra');
 
+async function isDirEmpty(dirPath) {
+  const files = await fs.readdir(dirPath);
+  return files.length === 0;
+}
+
 async function getDirectories (rootPath) {
   let files = await fs.readdir(rootPath);
   let dirs = [];
@@ -15,5 +20,6 @@ async function getDirectories (rootPath) {
 }
 
 module.exports = {
-  getDirectories
+  getDirectories,
+  isDirEmpty,
 };

detox/src/artifacts/uiHierarchy/IosUIHierarchyPlugin.test.js
@@ -61,13 +61,27 @@ describe('IosUIHierarchyPlugin', () => {
       expect(session2.save).toHaveBeenCalledWith('artifacts/ui2.viewhierarchy');
       expect(test1.save).toHaveBeenCalledWith('artifacts/test/ui.viewhierarchy');
       expect(test2.save).toHaveBeenCalledWith('artifacts/test/ui2.viewhierarchy');
-    }); });
+    });
+
+    it('should relocate existing artifacts before the app gets uninstalled', async () => {
+      await plugin.onTestStart(testSummaries.running());
+      onTestFailed(aTestFailedPayload(1));
+      onTestFailed(aTestFailedPayload(2));
+
+      await plugin.onBeforeUninstallApp({ deviceId: 'deviceId', bundleId: 'bundleId' });
+
+      const [test1, test2] = FileArtifact.mock.instances;
+
+      expect(test1.relocate).toHaveBeenCalled();
+      expect(test2.relocate).toHaveBeenCalled();
+    });
+  });
 
   describe('behavior for externally created artifacts', () => {
     it('should validate event.artifact', async () => {
       await expect(plugin.onCreateExternalArtifact({ name: 'invalid' }))
         .rejects
-        .toThrowError(/Internal error/);
+        .toThrowError(/Expected Artifact instance/);
     });
 
     it('should register snapshots inside and outside of running test', async () => {

detox/src/utils/fsext.test.js
@@ -1,0 +1,21 @@
+const path = require('path');
+
+const fs = require('fs-extra');
+const tempfile = require('tempfile');
+
+const fsext = require('./fsext');
+
+test('isDirEmpty', async () => {
+  const tempDir = tempfile();
+  try {
+    await expect(fsext.isDirEmpty(tempDir)).rejects.toThrowError(/ENOENT/);
+
+    await fs.ensureDir(tempDir);
+    await expect(fsext.isDirEmpty(tempDir)).resolves.toBe(true);
+
+    await fs.ensureFile(path.join(tempDir, '1'));
+    await expect(fsext.isDirEmpty(tempDir)).resolves.toBe(false);
+  } finally {
+    await fs.remove(tempDir);
+  }
+});

detox/src/artifacts/templates/artifact/FileArtifact.js
@@ -1,4 +1,7 @@
+const path = require('path');
+
 const fs = require('fs-extra');
+const tempfile = require('tempfile');
 
 const appendFile = require('../../../utils/appendFile');
 
@@ -12,6 +15,16 @@ class FileArtifact extends Artifact {
       this.start();
       this.stop();
     }
+  }
+
+  async relocate() {
+    if (!this.temporaryPath || !(await fs.exists(this.temporaryPath))) {
+      return;
+    }
+
+    const newTemporaryPath = tempfile(path.extname(this.temporaryPath));
+    await FileArtifact.moveTemporaryFile(this.logger, this.temporaryPath, newTemporaryPath);
+    this.temporaryPath = newTemporaryPath;
   }
 
   async doSave(artifactPath, options = {}) {

detox/src/artifacts/templates/artifact/FileArtifact.test.js
@@ -148,6 +148,106 @@ describe('FileArtifact', () => {
     });
   });
 
+  describe('relocate', () => {
+    beforeEach(() => {
+      jest.spyOn(FileArtifact, 'moveTemporaryFile').mockImplementation(_.noop);
+      jest.spyOn(FileArtifact, 'writeFile').mockImplementation(_.noop);
+    });
+
+    describe('if temporary file is passed to constructor', () => {
+      beforeEach(() => {
+        fileArtifact = new FileArtifact({
+          name: 'CustomArtifact',
+          temporaryPath,
+        });
+      });
+
+      describe('and the file exists', () => {
+        beforeEach(async () => {
+          await fs.ensureFile(temporaryPath);
+        });
+
+        describe('when called', () => {
+          beforeEach(async () => {
+            await fileArtifact.relocate();
+          });
+
+          it('should call FileArtifact.moveTemporaryFile', async () => {
+            expect(FileArtifact.moveTemporaryFile).toHaveBeenCalledWith(logger, temporaryPath, expect.stringMatching(/\.artifact$/));
+          });
+        });
+      });
+
+      describe('and the file does not exist', () => {
+        beforeEach(async () => {
+          await fs.remove(temporaryPath);
+        });
+
+        describe('when called', () => {
+          beforeEach(async () => {
+            await fileArtifact.relocate();
+          });
+
+          it('should call FileArtifact.moveTemporaryFile', async () => {
+            expect(FileArtifact.moveTemporaryFile).not.toHaveBeenCalled();
+          });
+        });
+      });
+    });
+
+    describe('if temporary file is created in start()', () => {
+      beforeEach(() => {
+        fileArtifact = new FileArtifact({
+          name: 'CustomArtifact',
+          async start() {
+            await fs.ensureFile(temporaryPath);
+            this.temporaryPath = temporaryPath;
+          }
+        });
+      });
+
+      describe('when relocate() is called without start()', () => {
+        beforeEach(async () => {
+          await fileArtifact.relocate();
+        });
+
+        it('should not call FileArtifact.moveTemporaryFile', async () => {
+          expect(FileArtifact.moveTemporaryFile).not.toHaveBeenCalled();
+        });
+      });
+
+      describe('and start() was called', () => {
+        beforeEach(async () => {
+          await fileArtifact.start();
+          await fileArtifact.relocate();
+        });
+
+        it('should call FileArtifact.moveTemporaryFile', async () => {
+          expect(FileArtifact.moveTemporaryFile).toHaveBeenCalledWith(logger, temporaryPath, expect.stringMatching(/\.artifact$/));
+        });
+      });
+    });
+
+    describe('if temporary data is passed to constructor', () => {
+      beforeEach(() => {
+        fileArtifact = new FileArtifact({
+          name: 'CustomArtifact',
+          temporaryData,
+        });
+      });
+
+      describe('when called', () => {
+        beforeEach(async () => {
+          await fileArtifact.relocate();
+        });
+
+        it('should not do anything', async () => {
+          expect(FileArtifact.moveTemporaryFile).not.toHaveBeenCalled();
+        });
+      });
+    });
+  });
+
   describe('static helper methods', () => {
     describe('.moveTemporaryFile', () => {
       describe('if temporary file does not exist', () => {

detox/test/e2e/21.artifacts.test.js
@@ -1,35 +1,42 @@
 const fs = require('fs');
 const path = require('path');
+
 const { PNG } = require('pngjs');
+
+const {
+  assertArtifactExists,
+  assertDirExists,
+  waitUntilArtifactsManagerIsIdle,
+} = require('./utils/artifactUtils');
 
 describe('Artifacts', () => {
   describe('screenshots', () => {
     beforeAll(async () => {
       await device.sendToHome();
-      await device.takeScreenshot('Artifacts/before all').then(printDimensions);
-      await device.launchApp({newInstance: true});
+      await device.takeScreenshot('Artifacts/before all').then(printPNGDimensions);
+      await device.launchApp({ newInstance: true });
     });
 
     beforeEach(async () => {
       // implicitly taking screenshot - beforeEach.png
       await device.reloadReactNative();
-      await device.takeScreenshot('in main menu').then(printDimensions);
+      await device.takeScreenshot('in main menu').then(printPNGDimensions);
 
       await element(by.text('Actions')).tap();
-      await device.takeScreenshot('Actions').then(printDimensions);
+      await device.takeScreenshot('Actions').then(printPNGDimensions);
     });
 
     it('should take screenshots inside test', async () => {
       await element(by.id('UniqueId819')).tap();
-      await device.takeScreenshot('taps - 1').then(printDimensions);
+      await device.takeScreenshot('taps - 1').then(printPNGDimensions);
 
       await element(by.id('UniqueId819')).tap();
-      await device.takeScreenshot('taps - 2').then(printDimensions);
+      await device.takeScreenshot('taps - 2').then(printPNGDimensions);
     });
 
     afterEach(async () => {
       await element(by.text('Tap Me')).tap();
-      await device.takeScreenshot('tap working').then(printDimensions);
+      await device.takeScreenshot('tap working').then(printPNGDimensions);
 
       await device.reloadReactNative();
       // implicitly taking screenshot - afterEach.png
@@ -37,14 +44,22 @@ describe('Artifacts', () => {
 
     afterAll(async () => {
       await device.sendToHome();
-      await device.takeScreenshot('Artifacts/after all').then(printDimensions);
+      await device.takeScreenshot('Artifacts/after all').then(printPNGDimensions);
       await device.launchApp();
     });
 
-    function printDimensions(pathToScreenshot) {
+    afterAll(async () => {
+      await waitUntilArtifactsManagerIsIdle();
+
+      assertArtifactExists('Artifacts_before all.png');
+      assertArtifactExists('✓ Artifacts screenshots should take screenshots inside test/taps - 1.png');
+      assertArtifactExists('✓ Artifacts screenshots should take screenshots inside test/taps - 2.png');
+    });
+
+    function printPNGDimensions(pathToScreenshot) {
       const buffer = fs.readFileSync(pathToScreenshot);
       const filename = path.basename(pathToScreenshot);
-      const {width, height} = PNG.sync.read(buffer);
+      const { width, height } = PNG.sync.read(buffer);
 
       console.log(`Took a screenshot: ${filename} (${width}x${height})`);
     }
@@ -52,42 +67,59 @@ describe('Artifacts', () => {
 
   describe(':ios: View Hierarchy', () => {
     beforeAll(async () => {
-      await device.launchApp({newInstance: true});
-
-      [
-        await device.captureViewHierarchy(), // = 'capture'
-        await device.captureViewHierarchy('before tests'),
-      ].forEach(assertDirExists);
+      await device.launchApp({ newInstance: true });
+      await device.captureViewHierarchy('before tests').then(assertDirExists);
     });
 
-    it('should capture view hierarchies in test and upon multiple invocation failures', async () => {
-      try {
-        await element(by.id('nonExistentId')).tap();
-        fail('should have failed');
-      } catch (e) {
-        [
-          await device.captureViewHierarchy(), // = 'capture'
-          await device.captureViewHierarchy('named capture'),
-        ].forEach(assertDirExists);
-      }
+    it('should capture anonymous view hierarchies upon manual request', async () => {
+      await device.captureViewHierarchy().then(assertDirExists);
+      await device.captureViewHierarchy().then(assertDirExists);
+    });
 
-      try {
-        await element(by.id('nonExistentId2')).tap();
-        fail('should have failed too');
-      } catch (e) {
-        [
-          await device.captureViewHierarchy(), // = 'capture'
-          await device.captureViewHierarchy('named capture'),
-        ].forEach(assertDirExists);
-      }
+    it('should capture named view hierarchies upon manual request', async () => {
+      await device.captureViewHierarchy('named capture').then(assertDirExists);
+      await device.captureViewHierarchy('named capture').then(assertDirExists);
+    });
 
-      // Artifacts folder should contain the following *.viewhierarchy folders: ui, capture, named capture, ui2, capture2, named capture2
+    it('should capture hierarchy upon multiple invocation failures', async () => {
+      for (let i = 0; i < 2; i++) {
+        try {
+          await element(by.id('nonExistentId')).tap();
+          fail('should have failed');
+        } catch (e) {
+        }
+      }
+    });
+
+    afterAll(async () => {
+      await waitUntilArtifactsManagerIsIdle();
+      assertArtifactExists('before tests.viewhierarchy');
+      assertArtifactExists('✓ Artifacts _ios_ View Hierarchy should capture anonymous view hierarchies upon manual request/capture.viewhierarchy');
+      assertArtifactExists('✓ Artifacts _ios_ View Hierarchy should capture anonymous view hierarchies upon manual request/capture.viewhierarchy');
+      assertArtifactExists('✓ Artifacts _ios_ View Hierarchy should capture named view hierarchies upon manual request/named capture.viewhierarchy');
+      assertArtifactExists('✓ Artifacts _ios_ View Hierarchy should capture named view hierarchies upon manual request/named capture2.viewhierarchy');
+      assertArtifactExists('✓ Artifacts _ios_ View Hierarchy should capture hierarchy upon multiple invocation failures/ui.viewhierarchy');
+      assertArtifactExists('✓ Artifacts _ios_ View Hierarchy should capture hierarchy upon multiple invocation failures/ui2.viewhierarchy');
+    });
+
+    describe('edge uninstall case', () => {
+      it('should capture hierarchy regardless', async () => {
+          try {
+            await element(by.id('nonExistentId')).tap();
+            fail('should have failed');
+          } catch (e) {
+            await device.uninstallApp();
+          }
+      });
+
+      afterAll(async () => {
+        await waitUntilArtifactsManagerIsIdle();
+        assertArtifactExists('✓ Artifacts _ios_ View Hierarchy edge uninstall case should capture hierarchy regardless/ui3.viewhierarchy');
+      });
+
+      afterAll(async () => {
+        await device.installApp();
+      });
     });
   });
-
-  function assertDirExists(dirPath) {
-    if (!fs.statSync(dirPath).isDirectory()) {
-      throw new Error('Expected to find a directory at path: ' + dirPath);
-    }
-  }
 });

commit e07dccb8aeeddfb5f33a93c44a8cf5a8ecdac49b

fix: concurrency issues caused by #2958

detox/src/artifacts/ArtifactsManager.js
@@ -49,6 +49,8 @@ class ArtifactsManager {
             return this._executeIdleCallbackRequest(nextCallbackRequest);
           }
         });
+
+        return this._idlePromise;
       },
     };
 

detox/src/artifacts/screenshot/SimulatorScreenshotPlugin.js
@@ -38,23 +38,26 @@ class SimulatorScreenshotPlugin extends ScreenshotArtifactPlugin {
   }
 
   async onBeforeUninstallApp(event) {
-    const snapshots = [
-      ...Object.values(this.snapshots.fromTest),
-      ...Object.values(this.snapshots.fromSession)
-    ];
+    await this.api.requestIdleCallback(async () => {
+      const snapshots = [
+        ...Object.values(this.snapshots.fromTest),
+        ...Object.values(this.snapshots.fromSession)
+      ];
 
-    await Promise.all(snapshots.map(s => s && s.relocate()));
+      await Promise.all(snapshots.map(s => s && s.relocate()));
+    });
+
     await super.onBeforeUninstallApp(event);
   }
 
-  async _onInvokeFailure({ params }) {
+  _onInvokeFailure({ params }) {
     const artifacts = {
       visibilityFailingScreenshots: params.visibilityFailingScreenshotsURL,
       visibilityFailingRects: params.visibilityFailingRectsURL,
     };
 
     for (const [key, artifactPath] of Object.entries(artifacts)) {
-      if (!artifactPath || await fs.isDirEmpty(artifactPath)) {
+      if (!artifactPath || fs.isDirEmptySync(artifactPath)) {
         continue;
       }
 

detox/src/artifacts/uiHierarchy/IosUIHierarchyPlugin.js
@@ -70,12 +70,15 @@ class IosUIHierarchyPlugin extends ArtifactPlugin {
   }
 
   async onBeforeUninstallApp(event) {
-    const artifacts = [
-      ...Object.values(this._artifacts.perTest),
-      ...Object.values(this._artifacts.perSession)
-    ];
+    await this.api.requestIdleCallback(async () => {
+      const artifacts = [
+        ...Object.values(this._artifacts.perTest),
+        ...Object.values(this._artifacts.perSession)
+      ];
 
-    await Promise.all(artifacts.map(s => s && s.relocate()));
+      await Promise.all(artifacts.map(s => s && s.relocate()));
+    });
+
     await super.onBeforeUninstallApp(event);
   }
 

detox/src/utils/fsext.js
@@ -2,8 +2,8 @@ const path = require('path');
 
 const fs = require('fs-extra');
 
-async function isDirEmpty(dirPath) {
-  const files = await fs.readdir(dirPath);
+function isDirEmptySync(dirPath) {
+  const files = fs.readdirSync(dirPath);
   return files.length === 0;
 }
 
@@ -21,5 +21,5 @@ async function getDirectories (rootPath) {
 
 module.exports = {
   getDirectories,
-  isDirEmpty,
+  isDirEmptySync,
 };

detox/src/artifacts/uiHierarchy/IosUIHierarchyPlugin.test.js
@@ -28,6 +28,9 @@ describe('IosUIHierarchyPlugin', () => {
           return 'artifacts/' + name;
         }
       }),
+      requestIdleCallback: jest.fn(async (callback) => {
+        await callback();
+      }),
       userConfig: {
         enabled: true,
         keepOnlyFailedTestsArtifacts: false,

detox/src/utils/fsext.test.js
@@ -5,16 +5,16 @@ const tempfile = require('tempfile');
 
 const fsext = require('./fsext');
 
-test('isDirEmpty', async () => {
+test('isDirEmptySync', async () => {
   const tempDir = tempfile();
   try {
-    await expect(fsext.isDirEmpty(tempDir)).rejects.toThrowError(/ENOENT/);
+    expect(() => fsext.isDirEmptySync(tempDir)).toThrowError(/ENOENT/);
 
     await fs.ensureDir(tempDir);
-    await expect(fsext.isDirEmpty(tempDir)).resolves.toBe(true);
+    expect(fsext.isDirEmptySync(tempDir)).toBe(true);
 
     await fs.ensureFile(path.join(tempDir, '1'));
-    await expect(fsext.isDirEmpty(tempDir)).resolves.toBe(false);
+    expect(fsext.isDirEmptySync(tempDir)).toBe(false);
   } finally {
     await fs.remove(tempDir);
   }