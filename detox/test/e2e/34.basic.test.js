6db653c8207bd665fada945c567d24977bc53cf6
Alon Haiut <alonhai@wix.com>
2021-10-13 15:54:35 +0300

    Entire sequence is now working

--- detox/test/e2e/34.basic.test.js
+++ detox/test/e2e/34.basic.test.js
@@ -6,12 +6,24 @@ describe('Basic', () => {
 
   it('Basic launch action', async () => {
     await device.launchXCTestApp("com.wix.alon.FirstApp");
-    await device.switchTargetApp("bundle.id.try");
   });
 
-  // it('Basic tap action', async () => {
-  //   await device.launchXCTestApp("com.wix.alon.FirstApp");
-  //   await element(by.id('Sanity')).tap();
-  // });
+  it('Basic tap action', async () => {
+    await device.launchXCTestApp("com.wix.alon.FirstApp");
+    await element(by.id('btnSomeScreen')).tap();
+  });
 
+  it('terminate app', async () => {
+    await device.launchXCTestApp("com.wix.alon.FirstApp");
+    await device.terminateXCTestApp("com.wix.alon.FirstApp");
+    await device.launchXCTestApp("com.wix.alon.TryXCUITestsApp");
+    await device.terminateXCTestApp("com.wix.alon.TryXCUITestsApp");
+  });
+
+  it('Moving between apps and interact with them', async () => {
+    await device.launchXCTestApp("com.wix.alon.FirstApp");
+    await element(by.id('btnGoToCoolApp')).tap();
+    await device.switchTargetApp("com.wix.alon.TryXCUITestsApp");
+    await element(by.id('btnFirstScreen')).tap();
+  });
 });
