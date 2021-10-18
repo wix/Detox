6087d62aa603537a3722db3c70615318347fdf7a
Yaroslav Serhieiev <noomorph@gmail.com>
2021-10-18 11:44:38 +0300

    wip: proof of concept

--- detox/test/e2e/config.js
+++ detox/test/e2e/config.js
@@ -11,7 +11,7 @@ module.exports = {
     : ['<rootDir>/runners/jest/streamlineReporter', '<rootDir>/test/node_modules/jest-junit'],
   'verbose': true,
   'bail': false,
-  'maxWorkers': 1,
+  'maxWorkers': Number(process.env.DEMO_MAX_WORKERS || '1'),
   'collectCoverageFrom': [
     'src/**/*.js',
     '!**/__test/**',
