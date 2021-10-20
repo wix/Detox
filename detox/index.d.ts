62fd542fe2ded6207b77b1693e707c38998cf560
Miron Pawlik <mironiasty@users.noreply.github.com>
2021-10-20 12:59:18 +0200

    Add `deprecated` information to TypeScript types (#2974)

--- detox/index.d.ts
+++ detox/index.d.ts
@@ -897,6 +897,7 @@ declare global {
             /**
              * Expect the view to not be visible.
              * @example await expect(element(by.id('UniqueId205'))).toBeNotVisible();
+             * @deprecated Use `.not.toBeVisible()` instead.
              */
             toBeNotVisible(): R;
 
@@ -909,6 +910,7 @@ declare global {
             /**
              * Expect the view to not exist in the UI hierarchy.
              * @example await expect(element(by.id('RandomJunk959'))).toNotExist();
+             * @deprecated Use `.not.toExist()` instead.
              */
             toNotExist(): R;
 
@@ -921,6 +923,7 @@ declare global {
             /**
              * Expect the view not to be focused.
              * @example await expect(element(by.id('passwordInput'))).toBeNotFocused();
+             * @deprecated Use `.not.toBeFocused()` instead.
              */
             toBeNotFocused(): R;
 
@@ -993,9 +996,11 @@ declare global {
         interface NativeElementActions {
             /**
              * Simulate tap on an element
+             * @param point relative coordinates to the matched element (the element size could changes on different devices or even when changing the device font size)
              * @example await element(by.id('tappable')).tap();
+             * @example await element(by.id('tappable')).tap({ x:5, y:10 });
              */
-            tap(): Promise<void>;
+            tap(point?: { x: number; y: number }): Promise<void>;
 
             /**
              * Simulate long press on an element
@@ -1021,6 +1026,7 @@ declare global {
              * Simulate tap at a specific point on an element.
              * Note: The point coordinates are relative to the matched element and the element size could changes on different devices or even when changing the device font size.
              * @example await element(by.id('tappable')).tapAtPoint({ x:5, y:10 });
+             * @deprecated Use `.tap()` instead.
              */
             tapAtPoint(point: { x: number; y: number }): Promise<void>;
 
@@ -1135,7 +1141,7 @@ declare global {
              * @example
              * await expect(element(by.id('PinchableScrollView'))).toBeVisible();
              * await element(by.id('PinchableScrollView')).pinchWithAngle('outward', 'slow', 0);
-             * @deprecated Should use pinch instead
+             * @deprecated Use `.pinch()` instead.
              */
             pinchWithAngle(direction: PinchDirection, speed: Speed, angle: number): Promise<void>;
 
