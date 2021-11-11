2d3ae013358191e7d4573a62a116ec030780c83d
Yaroslav Serhieiev <yaroslavs@wix.com>
2021-11-11 20:47:59 +0200

    ci: disable postinstall for rapid tests

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -3,7 +3,7 @@
 const cp = require('child_process');
 const path = require('path');
 
-if (process.platform === 'darwin') {
+if (process.platform === 'darwin' && !process.env.DETOX_DISABLE_POSTINSTALL) {
   cp.execSync('pod install', { 
     cwd: path.join(process.cwd(), 'detox/test/ios'),
     stdio: 'inherit'
