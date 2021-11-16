490d8adc20658a8c654ffc3791e2a48ff5a86b2b
Asaf Korem <55082339+asafkorem@users.noreply.github.com>
2021-11-16 12:51:31 +0200

    Remove redundant label fields.

--- website/src/pages/index.js
+++ website/src/pages/index.js
@@ -31,9 +31,7 @@ function HomepageHeader() {
 export default function Home() {
   const {siteConfig} = useDocusaurusContext();
   return (
-    <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+    <Layout>
       <HomepageHeader />
       <main>
         <HomepageFeatures />
