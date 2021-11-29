ded65ae6e0d6bde855adad7723a4e20b7bc90da4
Yaroslav Serhieiev <yaroslavs@wix.com>
2021-11-29 14:47:11 +0200

    ci: bump

--- scripts/install.sh
+++ scripts/install.sh
@@ -4,5 +4,8 @@ mkdir ~/.npm-global
 npm config set prefix '~/.npm-global'
 export PATH=~/.npm-global/bin:$PATH
 
+echo "Node version:"
+node --version
+
 npm install -g lerna@3.22.1
 npm install -g react-native-cli >/dev/null 2>&1
