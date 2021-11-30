880c9e384f733afaec950a733a1a4bd7ffec06c3
Asaf Korem <55082339+asafkorem@users.noreply.github.com>
2021-11-30 15:25:17 +0200

    fix(16.location.test): update and add more tests.
    Fix misleading test (`Should accept a location`), where `permissions`
    should not be passed to `relaunchApp`, because Android ignores
    it and uses the manifest.
    Also, refactor and rename tests and write more missing tests.

--- detox/test/e2e/16.location.test.js
+++ detox/test/e2e/16.location.test.js
@@ -1,23 +1,57 @@
-describe('location', () => {
-  const lat = -80.125;
-  const lon = 66.5;
+const { expectToThrow } = require('./utils/custom-expects');
 
-  // Skipped on Android because there is no Android permissions support yet
-  it(':ios: Location should be unavailable', async () => {
+// Skipped on Android because there is no Android permissions support yet
+describe(':ios: location request', () => {
+  it('should show nothing in the app (permission request)', async () => {
+    await device.relaunchApp({ permissions: { location: 'unset' } });
+    await getLocation();
+
+    await expect(element(by.id('error'))).not.toBeVisible();
+    await expectToThrow(() => waitFor(element(by.id(`latitude`))).toBeVisible().withTimeout(5500));
+  });
+
+  it('should err when location permission was set to `never`', async () => {
     await device.relaunchApp({ permissions: { location: 'never' } });
-    await element(by.text('Location')).tap();
-    await element(by.id('getLocationButton')).tap();
+    await getLocation();
+
     await expect(element(by.id('error'))).toBeVisible();
   });
 
-  it('Should accept a location', async () => {
+  it('should allow to get and set location when location permission is set to `always`', async () => {
     await device.relaunchApp({ permissions: { location: 'always' } });
-    await device.setLocation(lat, lon);
-    await element(by.text('Location')).tap();
-    await element(by.id('getLocationButton')).tap();
-    await waitFor(element(by.text(`Latitude: ${lat}`))).toBeVisible().withTimeout(5500);
+    await expectLocationToBeAvailable();
+  });
 
-    await expect(element(by.text(`Latitude: ${lat}`))).toBeVisible();
-    await expect(element(by.text(`Longitude: ${lon}`))).toBeVisible();
+  it('should allow to get and set location when location permission is set to `inuse`', async () => {
+    await device.relaunchApp({ permissions: { location: 'inuse' } });
+    await expectLocationToBeAvailable();
   });
 });
+
+describe(':android: location permission granted from manifest', () => {
+  beforeEach(async () => {
+    await device.relaunchApp();
+  });
+
+  it('should allow to get and set location', async () => {
+    await expectLocationToBeAvailable();
+  });
+});
+
+// Helpers
+
+async function getLocation() {
+  await element(by.text('Location')).tap();
+  await element(by.id('getLocationButton')).tap();
+}
+
+async function expectLocationToBeAvailable() {
+  const lat = -80.125;
+  const lon = 66.5;
+
+  await device.setLocation(lat, lon);
+  await getLocation();
+
+  await waitFor(element(by.id(`latitude`))).toHaveText(`Latitude: ${lat}`).withTimeout(5500);
+  await expect(element(by.id(`longitude`))).toHaveText(`Longitude: ${lon}`);
+}
