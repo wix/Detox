a4a7fc7a59bcda433cb6a1eb04a8b79981e8183e
jonathanmos <jonathanmo@wix.com>
2022-02-07 11:44:47 +0200

    mock crypto-js md5

--- detox/src/devices/common/drivers/android/tools/CryptoUtils.test.js
+++ detox/src/devices/common/drivers/android/tools/CryptoUtils.test.js
@@ -1,13 +1,15 @@
-// const mockMd5 = jest.fn();
-// jest.mock('crypto-js', () => ({
-//   md5: () => mockMd5(),
-// }));
-
-const { getMd5 } = require('./CryptoUtils');
-
 describe('CryptoUtils', () => {
+  let mockMd5;
+  let uut;
+
+  beforeEach(() => {
+    jest.mock('crypto-js/md5', () => jest.fn());
+    mockMd5 = require('crypto-js/md5');
+    uut = require('./CryptoUtils');
+  });
+
   it('should call cryto-js md5 function', async () => {
-    await getMd5('test');
-    // expect(mockMd5).toHaveBeenCalledTimes(1);
+    await uut.getMd5('test');
+    expect(mockMd5).toHaveBeenCalledTimes(1);
   });
 });
