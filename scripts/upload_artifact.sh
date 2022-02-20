31bd80e4f4cb27d93d1e6e60050f3d76d1484078
Igor Gnatyuk <igorgn@wix.com>
2022-02-20 16:02:01 +0200

    comment out aws s3

--- scripts/upload_artifact.sh
+++ scripts/upload_artifact.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-export AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY
-export AWS_SECRET_ACCESS_KEY=$AWS_ACCESS_SECRET
+# export AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY
+# export AWS_SECRET_ACCESS_KEY=$AWS_ACCESS_SECRET
 
 DATE=`date '+%Y-%m-%d_%H-%M-%S'`
 
@@ -19,15 +19,15 @@ upload_artifacts() {
     echo "Packing artifacts... (${CONTEXT})"
     tar cvzf "${ARTIFACTS_NAME}" ${ARTIFACTS_DIR}
 
-    if [ $CI ]; then
-        echo "Upload: Jenkins will upload using built-in plugin"
-    else
-        echo "Uploading artifacts..."
-        aws s3 cp "${ARTIFACTS_NAME}" s3://detox-artifacts/ --region=us-east-1
-    fi
+    # if [ $CI ]; then
+        # echo "Upload: Jenkins will upload using built-in plugin"
+    # else
+        # echo "Uploading artifacts..."
+        # aws s3 cp "${ARTIFACTS_NAME}" s3://detox-artifacts/ --region=us-east-1
+    # fi
 
-    echo "The artifacts archive is available for download at:"
-    echo "https://detox-artifacts.s3.amazonaws.com/${ARTIFACTS_NAME}"
+    # echo "The artifacts archive is available for download at:"
+    # echo "https://detox-artifacts.s3.amazonaws.com/${ARTIFACTS_NAME}"
   fi
 }
 
