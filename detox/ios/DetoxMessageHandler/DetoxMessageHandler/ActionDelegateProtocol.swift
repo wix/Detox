fc671253e013410c58efb6b6893064b0bf375dc0
Asaf Korem <asaf.korem@gmail.com>
2022-02-27 13:07:16 +0200

    iOS(ActionDelegateProtocol): initial commit.

---
+++ detox/ios/DetoxMessageHandler/DetoxMessageHandler/ActionDelegateProtocol.swift
@@ -0,0 +1,18 @@
+//
+//  ActionDelegateProtocol.swift (DetoxMessageHandler)
+//  Created by Asaf Korem (Wix.com) on 2022.
+//
+
+import Foundation
+
+/// Protocol for classes that can be used as a delegate of `MessageHandler`, in which handles
+/// actions on a specified element.
+public protocol ActionDelegateProtocol {
+  /// Performs the provided `action` on the specified `element`.
+  func act(action: Action, on element: AnyHashable) throws
+
+  /// Get the attributes from a list of `elements`. Returns a dictionary in case of one element
+  /// only, or list of dictionaries, each represents the state attributes (specifically, the
+  /// available accessibility information) of the element.
+  func getAttributes(from elements: [AnyHashable]) throws -> AnyCodable
+}
