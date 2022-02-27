015aa4e68fddd99b816c595061436aa0ce6f607c
Asaf Korem <asaf.korem@gmail.com>
2022-02-27 13:08:19 +0200

    iOS(ElementMatcherProtocol): initial commit.

---
+++ detox/ios/DetoxMessageHandler/DetoxMessageHandler/ElementMatcherProtocol.swift
@@ -0,0 +1,13 @@
+//
+//  ElementMatcher.swift (DetoxMessageHandler)
+//  Created by Asaf Korem (Wix.com) on 2022.
+//
+
+import Foundation
+
+/// Protocol for classes that can be used as a matcher for `MessageHandler`, in which handles
+/// elements matching based on given `pattern`.
+public protocol ElementMatcherProtocol {
+  /// Returns elements that matches the given `pattern`.
+  func match(to pattern: ElementPattern) throws -> [AnyHashable]
+}
