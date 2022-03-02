d6f208bf3cbf8af23417aa694bc94e72e1066075
Asaf Korem <asaf.korem@gmail.com>
2022-03-02 13:38:21 +0200

    iOS demo projects script: add pod install.
    Add `pod install` & move detox-instruments reinstallation code.

--- scripts/demo-projects.ios.sh
+++ scripts/demo-projects.ios.sh
@@ -2,13 +2,11 @@
 
 source $(dirname "$0")/demo-projects.sh
 
-#This solves a bug in brew
-HOMEBREW_NO_AUTO_UPDATE=1 brew untap wix/brew
-HOMEBREW_NO_AUTO_UPDATE=1 brew tap wix/brew
-HOMEBREW_NO_AUTO_UPDATE=1 brew cask reinstall detox-instruments
-
-#This must be built first as all other demo apps use this binary.
+# This must be built first as all other demo apps use this binary.
 pushd examples/demo-react-native
+pushd ios
+run_f "pod install"
+popd
 run_f "detox build -c ios.sim.release"
 run_f "detox test -c ios.sim.release"
 DETOX_EXPOSE_GLOBALS=0 run_f "detox test -c ios.sim.release"
@@ -18,6 +16,10 @@ pushd examples/demo-react-native-jest
 run_f "npm run test:ios-release-ci"
 DETOX_EXPOSE_GLOBALS=0 run_f "npm run test:ios-release-ci"
 popd
+
+# This solves a bug in brew
+brew tap wix/brew
+brew cask reinstall detox-instruments
 
 pushd examples/demo-react-native-detox-instruments
 run_f "detox test -c ios.sim.release --record-performance all"
