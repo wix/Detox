cc72320048ff2c7d986cb85d72ec733119d9a24b
Igor Gnatyuk <igorgn@wix.com>
2022-03-06 16:30:50 +0200

    cp

--- scripts/ci.common.js
+++ scripts/ci.common.js
@@ -64,7 +64,7 @@ function releaseNpmTag() {
 function getPackagesFromPreviousBuilds() {
   cp.execSync(`buildkite-agent artifact download "**/Detox*.tbz" . --build ${process.env.BUILDKITE_BUILD_ID}`).toString();
   cp.execSync(`buildkite-agent artifact download "**/ARCHIVE*.tgz" . --build ${process.env.BUILDKITE_BUILD_ID}`).toString();
-  cp.execSync(`find . -name "*.t[bg]z -exec cp {} . \;`);
+  cp.execSync(`find . -name "*.t[bg]z -exec cp {} . \\;`);
 }
 
 module.exports = {
