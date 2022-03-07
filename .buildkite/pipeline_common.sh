bed19387824ce6e5a8117b8fde124080f1566db9
Asaf Korem <asaf.korem@gmail.com>
2022-03-07 16:11:41 +0200

    Revert "Revert "Revert "Revert "Buildkite Migration (#3222)""""
    This reverts commit 2aa56f237d98935ccd1ff1b1779cc54599c88290.

---
+++ .buildkite/pipeline_common.sh
@@ -0,0 +1,9 @@
+#!/bin/bash -e
+
+echo "steps:"
+
+cat .buildkite/jobs/pipeline.ios_rn_64.yml
+cat .buildkite/jobs/pipeline.ios_rn_66.yml
+cat .buildkite/jobs/pipeline.android_rn_64.yml
+cat .buildkite/jobs/pipeline.android_rn_66.yml
+cat .buildkite/jobs/pipeline.android_demo_app_rn_64.yml
