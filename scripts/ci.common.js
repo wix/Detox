12441e201ae3b98a7318a1f0063b90ec44e8a2d0
Igor Gnatyuk <igorgn@wix.com>
2022-03-08 16:51:44 +0200

    artifacts in detox folder

--- scripts/ci.common.js
+++ scripts/ci.common.js
@@ -64,8 +64,8 @@ function releaseNpmTag() {
 }
 
 function getPackagesFromPreviousBuilds() {
-  cp.execSync(`buildkite-agent artifact download "**/Detox*.tbz" . --build ${process.env.BUILDKITE_BUILD_ID}`).toString();
-  cp.execSync(`buildkite-agent artifact download "**/ARCHIVE*.tgz" . --build ${process.env.BUILDKITE_BUILD_ID}`).toString();
+  cp.execSync(`buildkite-agent artifact download "**/Detox*.tbz" detox/ --build ${process.env.BUILDKITE_BUILD_ID}`).toString();
+  cp.execSync(`buildkite-agent artifact download "**/ARCHIVE*.tgz" detox/ --build ${process.env.BUILDKITE_BUILD_ID}`).toString();
   cp.execSync(`find . -name "*.t[bg]z" -exec cp {} . \\;`);
 }
 
