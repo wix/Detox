6e22c7e87a519fc45229c12f365762c8f95532c1
Asaf Korem <asaf.korem@gmail.com>
2022-03-14 15:05:31 +0200

    fix: buildkite demo-project-ios job script and execution command.

--- scripts/demo-projects.ios.sh
+++ scripts/demo-projects.ios.sh
@@ -2,17 +2,24 @@
 
 source $(dirname "$0")/demo-projects.sh
 
+pushd detox
+run_f "npm run build:android"
+popd
+
+
 # This must be built first as all other demo apps use this binary.
 pushd examples/demo-react-native
 pushd ios
 run_f "pod install"
 popd
-run_f "detox build -c ios.sim.release"
-run_f "detox test -c ios.sim.release"
-DETOX_EXPOSE_GLOBALS=0 run_f "detox test -c ios.sim.release"
+run_f "npm run build:ios-release"
 popd
 
 pushd examples/demo-react-native-jest
 run_f "npm run test:ios-release-ci"
-DETOX_EXPOSE_GLOBALS=0 run_f "npm run test:ios-release-ci"
 popd
+
+pushd examples/demo-react-native
+run_f "npm run test:ios-release-ci"
+popd
+
