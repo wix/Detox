d1eb3ba2e2bafb56f43de585dfab95e5b8d66d38
Igor Gnatyuk <igorgn@wix.com>
2022-04-04 13:10:11 +0300

    check if PR is from external contributor

--- .buildkite/pipeline_common.sh
+++ .buildkite/pipeline_common.sh
@@ -2,6 +2,13 @@
 
 echo "steps:"
 
+ORIGINAL_REPO=$(echo $BUILDKITE_REPO | sed 's/.*github.com\///')
+echo $ORIGINAL_REPO
+PR_REPO=$(echo $BUILDKITE_PULL_REQUEST_REPO | sed 's/.*github.com\///')
+echo $PR_REPO
+
+[[ $ORIGINAL_REPO != $PR_REPO ]] && echo '  - block: ":rocket: Release!"'
+
 cat .buildkite/jobs/pipeline.ios_rn_64.yml
 cat .buildkite/jobs/pipeline.ios_rn_67.yml
 cat .buildkite/jobs/pipeline.android_rn_64.yml
