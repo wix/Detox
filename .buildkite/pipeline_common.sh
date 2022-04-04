11bcdce640e21c94fed27cf8223a7529fbe12256
Igor Gnatyuk <igorgn@wix.com>
2022-04-04 21:26:12 +0300

    block1

--- .buildkite/pipeline_common.sh
+++ .buildkite/pipeline_common.sh
@@ -16,4 +16,3 @@ cat .buildkite/jobs/pipeline.ios_rn_64.yml
 # cat .buildkite/jobs/pipeline.android_rn_67.yml
 # cat .buildkite/jobs/pipeline.android_demo_app_rn_67.yml
 # cat .buildkite/jobs/pipeline.ios_demo_app_rn_67.yml
-
