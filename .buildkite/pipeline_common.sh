60ce05f9f408bd2b113db3f9d8969aa70303f537
Igor Gnatyuk <igorgn@wix.com>
2022-04-04 13:54:56 +0300

    block PRs from external contributors

--- .buildkite/pipeline_common.sh
+++ .buildkite/pipeline_common.sh
@@ -2,9 +2,17 @@
 
 echo "steps:"
 
+ORIGINAL_REPO=$(echo $BUILDKITE_REPO | sed 's/.*github.com\///')
+echo $ORIGINAL_REPO
+PR_REPO=$(echo $BUILDKITE_PULL_REQUEST_REPO | sed 's/.*github.com\///')
+echo $PR_REPO
+
+[[ $ORIGINAL_REPO != $PR_REPO ]] && echo '  - block: ":rocket: Release!"'
+
+
 cat .buildkite/jobs/pipeline.ios_rn_64.yml
-cat .buildkite/jobs/pipeline.ios_rn_67.yml
-cat .buildkite/jobs/pipeline.android_rn_64.yml
-cat .buildkite/jobs/pipeline.android_rn_67.yml
-cat .buildkite/jobs/pipeline.android_demo_app_rn_67.yml
-cat .buildkite/jobs/pipeline.ios_demo_app_rn_67.yml
+# cat .buildkite/jobs/pipeline.ios_rn_67.yml
+# cat .buildkite/jobs/pipeline.android_rn_64.yml
+# cat .buildkite/jobs/pipeline.android_rn_67.yml
+# cat .buildkite/jobs/pipeline.android_demo_app_rn_67.yml
+# cat .buildkite/jobs/pipeline.ios_demo_app_rn_67.yml
