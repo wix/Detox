210c5948eee42817ec11834cc89e1aeef3cd9fac
Igor Gnatyuk <igorgn@wix.com>
2022-04-04 14:24:00 +0300

    block

--- .buildkite/pipeline_common.sh
+++ .buildkite/pipeline_common.sh
@@ -1,13 +1,13 @@
 #!/bin/bash -e
 
 ORIGINAL_REPO=$(echo "$BUILDKITE_REPO" | sed 's/.*github.com\///')
-echo $ORIGINAL_REPO
+PR_REPO=$(echo $BUILDKITE_PULL_REQUEST_REPO | sed 's/.*github.com\///')
+# echo $ORIGINAL_REPO
 echo "steps:"
 
-# PR_REPO=$(echo $BUILDKITE_PULL_REQUEST_REPO | sed 's/.*github.com\///')
 # echo $PR_REPO
 
-# [[ $ORIGINAL_REPO != $PR_REPO ]] && echo '  - block: ":rocket: Release!"'
+[[ $ORIGINAL_REPO != $PR_REPO ]] && echo '  - block: ":rocket: Release!"'
 
 
 cat .buildkite/jobs/pipeline.ios_rn_64.yml
