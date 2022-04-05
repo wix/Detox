5c72f9a08e01df95db883044c0f9ce68957ce43d
d4vidi <amit.d4vidi@gmail.com>
2022-04-05 17:34:01 +0300

    Make Android unit tests run locally

--- detox/android/detox/src/testFull/java/com/wix/detox/espresso/registry/IRStatusInquirerTest.kt
+++ detox/android/detox/src/testFull/java/com/wix/detox/espresso/registry/IRStatusInquirerTest.kt
@@ -14,9 +14,13 @@ import org.mockito.kotlin.mock
 import org.mockito.kotlin.whenever
 import org.robolectric.Robolectric
 import org.robolectric.RobolectricTestRunner
+import org.robolectric.annotation.LooperMode
 import java.util.concurrent.Executors
 
 @RunWith(RobolectricTestRunner::class)
+// Fixes: Hangs in UIThread.postFirstSync inside IRStatusInquirer.getAllBusyResources when upgrading robolectric 4.3.x -> 4.4
+// See http://robolectric.org/blog/2019/06/04/paused-looper/ (coming from https://github.com/robolectric/robolectric/releases/tag/robolectric-4.4)
+@LooperMode(LooperMode.Mode.LEGACY)
 class IRStatusInquirerTest {
 
     lateinit var registry: IdlingResourceRegistry
