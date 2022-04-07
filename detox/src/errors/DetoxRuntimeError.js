360d8fee3c9909477242546b5ee7b257ebb739d7
Yaroslav Serhieiev <yaroslavs@wix.com>
2022-04-07 13:36:07 +0300

    feat(android): boot read-only emulators by default

--- detox/src/errors/DetoxRuntimeError.js
+++ detox/src/errors/DetoxRuntimeError.js
@@ -4,22 +4,22 @@ const DetoxError = require('./DetoxError');
 
 /**
  * @typedef DetoxRuntimeErrorOptions
- * @property message { String }
+ * @property [message] { String }
  * @property [hint] { String }
- * @property [debugInfo] { String }
+ * @property [debugInfo] { * }
  * @property [noStack] { Boolean }
  * @property [inspectOptions] { Object }
  */
 
 class DetoxRuntimeError extends DetoxError {
   /**
-   * @param options { DetoxRuntimeErrorOptions }
+   * @param [options] { String | DetoxRuntimeErrorOptions }
    */
   constructor(options) {
     super(formatOptions(options));
     this.name = 'DetoxRuntimeError';
 
-    if (options && options.noStack) {
+    if (options && typeof options !== 'string' && options.noStack) {
       delete this.stack;
     }
   }
