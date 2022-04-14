1afa174d300a1ac28c60f9af27a0fc0a2dff45de
Asaf Korem <asaf.korem@gmail.com>
2022-04-14 11:07:20 +0300

    add comment

--- detox/ios/DetoxTester/DetoxTester/DTXTestCase.h
+++ detox/ios/DetoxTester/DetoxTester/DTXTestCase.h
@@ -5,6 +5,7 @@
 
 #import <XCTest/XCTest.h>
 
+/// Class for test cases for testing DetoxTester's internal components (integration tests).
+/// Invokes nothing when running from Detox Server.
 @interface DTXTestCase : XCTestCase
-
 @end
