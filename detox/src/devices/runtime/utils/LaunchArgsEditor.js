a490877e5cb7468dc0c62b0b2ddbd4e6f2d8e8ca
Yaroslav Serhieiev <yaroslavs@wix.com>
2022-04-27 19:34:11 +0300

    feat: drop device.appLaunchArgs [options] argument
    BREAKING: use device.appLaunchArgs.shared for permanent operations

--- detox/src/devices/runtime/utils/LaunchArgsEditor.js
+++ detox/src/devices/runtime/utils/LaunchArgsEditor.js
@@ -1,13 +1,6 @@
 const _ = require('lodash');
 
-const log = require('../../../utils/logger');
-
 const ScopedLaunchArgsEditor = require('./ScopedLaunchArgsEditor');
-
-/**
- * @typedef {Object} LaunchArgsEditorOptions
- * @property {boolean} [permanent=false] - Indicates whether the operation should affect the permanent app launch args.
- */
 
 class LaunchArgsEditor {
   constructor() {
@@ -19,66 +12,18 @@ class LaunchArgsEditor {
     return this._shared;
   }
 
-  /**
-   * @param {LaunchArgsEditorOptions} [options] - deprecated
-   */
-  modify(launchArgs, options) {
-    this._assertNoDeprecatedOptions('modify', options);
-
-    if (!_.isEmpty(launchArgs)) {
-      if (options && options.permanent) {
-        this._shared.modify(launchArgs);
-      } else {
-        this._local.modify(launchArgs);
-      }
-    }
-
+  modify(launchArgs) {
+    this._local.modify(launchArgs);
     return this;
   }
 
-  /**
-   * @param {LaunchArgsEditorOptions} [options] - deprecated
-   */
-  reset(options) {
-    this._assertNoDeprecatedOptions('reset', options);
+  reset() {
     this._local.reset();
-
-    if (options && options.permanent) {
-      this._shared.reset();
-    }
-
     return this;
   }
 
-  /**
-   * @param {LaunchArgsEditorOptions} [options] - deprecated
-   */
-  get(options) {
-    this._assertNoDeprecatedOptions('get', options);
-    const permanent = options && options.permanent;
-
-    if (permanent === true) {
-      return this._shared.get();
-    }
-
-    if (permanent === false) {
-      return this._local.get();
-    }
-
+  get() {
     return _.merge(this._shared.get(), this._local.get());
-  }
-
-  _assertNoDeprecatedOptions(methodName, options) {
-    if (!options) {
-      return;
-    }
-
-    const target = options.permanent ? 'appLaunchArgs.shared' : 'appLaunchArgs';
-    log.warn({ event: 'DEPRECATION' }, [
-      `Detected an attempt to use device.launchArgs.${methodName}(...) with a deprecated 'options' parameter.`,
-      `Please use device.${target}.${methodName}(...) instead.`,
-      `For more details, see the reference at: https://wix.github.io/Detox/docs/api/device-object-api#deviceapplaunchargs\n`,
-    ].join('\n'));
   }
 }
 
