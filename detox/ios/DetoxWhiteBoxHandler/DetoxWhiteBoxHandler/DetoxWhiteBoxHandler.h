514006f8e04327e797f348ebcf8c1c69090493a6
Asaf Korem <asaf.korem@gmail.com>
2022-05-08 17:13:23 +0300

    DetoxWhiteBoxHandler: initial commit.

---
+++ detox/ios/DetoxWhiteBoxHandler/DetoxWhiteBoxHandler/DetoxWhiteBoxHandler.h
@@ -0,0 +1,14 @@
+//
+//  DetoxWhiteBoxHandler.h (DetoxWhiteBoxHandler)
+//  Created by Asaf Korem (Wix.com) on 2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DetoxWhiteBoxHandler.
+FOUNDATION_EXPORT double DetoxWhiteBoxHandlerVersionNumber;
+
+//! Project version string for DetoxWhiteBoxHandler.
+FOUNDATION_EXPORT const unsigned char DetoxWhiteBoxHandlerVersionString[];
+
+// DetoxWhiteBoxHandler should not expose any public headers. This file should remain as-is.
