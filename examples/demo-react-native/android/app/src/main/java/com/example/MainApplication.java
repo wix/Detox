2ada1e05ee672385840bd02c714b669b2a4bed61
Asaf Korem <asaf.korem@gmail.com>
2022-06-12 15:33:35 +0300

    React Native 0.68.2 compliance

--- examples/demo-react-native/android/app/src/main/java/com/example/MainApplication.java
+++ examples/demo-react-native/android/app/src/main/java/com/example/MainApplication.java
@@ -6,7 +6,9 @@ import com.facebook.react.ReactApplication;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
+import com.facebook.react.config.ReactFeatureFlags;
 import com.facebook.soloader.SoLoader;
+import com.rndiffapp.newarchitecture.MainApplicationReactNativeHost;
 import com.reactnativecommunity.asyncstorage.AsyncStoragePackage;
 
 import java.util.Arrays;
@@ -28,14 +30,24 @@ public class MainApplication extends Application implements ReactApplication {
         }
     };
 
+    private final ReactNativeHost mNewArchitectureNativeHost = new MainApplicationReactNativeHost(this);
+
     @Override
     public ReactNativeHost getReactNativeHost() {
+        if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
+            return mNewArchitectureNativeHost;
+        }
+
         return mReactNativeHost;
     }
 
     @Override
     public void onCreate() {
         super.onCreate();
+
+        // If you opted-in for the New Architecture, we enable the TurboModule system
+        ReactFeatureFlags.useTurboModules = BuildConfig.IS_NEW_ARCHITECTURE_ENABLED;
+
         SoLoader.init(this, /* native exopackage */ false);
     }
 
