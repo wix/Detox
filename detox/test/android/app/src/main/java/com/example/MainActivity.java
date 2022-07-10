f20d154b99a2ac9227869bcf6e77b54944149259
d4vidi <amit.d4vidi@gmail.com>
2022-07-10 15:39:41 +0300

    (Android) Remove everything new-RN-arch-related

--- detox/test/android/app/src/main/java/com/example/MainActivity.java
+++ detox/test/android/app/src/main/java/com/example/MainActivity.java
@@ -4,9 +4,13 @@ import android.content.Intent;
 
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
-import com.facebook.react.ReactRootView;
 
 public class MainActivity extends ReactActivity {
+    @Override
+    public void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        setIntent(intent);
+    }
 
     /**
      * Returns the name of the main component registered from JavaScript.
@@ -22,7 +26,7 @@ public class MainActivity extends ReactActivity {
     * you can specify the rendered you wish to use (Fabric or the older renderer).
     */
     @Override
-        protected ReactActivityDelegate createReactActivityDelegate() {
+    protected ReactActivityDelegate createReactActivityDelegate() {
         return new MainActivityDelegate(this, getMainComponentName());
     }
 
@@ -30,19 +34,5 @@ public class MainActivity extends ReactActivity {
         public MainActivityDelegate(ReactActivity activity, String mainComponentName) {
             super(activity, mainComponentName);
         }
-
-        @Override
-        protected ReactRootView createRootView() {
-            ReactRootView reactRootView = new ReactRootView(getContext());
-            // If you opted-in for the New Architecture, we enable the Fabric Renderer.
-            reactRootView.setIsFabric(BuildConfig.IS_NEW_ARCHITECTURE_ENABLED);
-            return reactRootView;
-        }
-    }
-
-    @Override
-    public void onNewIntent(Intent intent) {
-        super.onNewIntent(intent);
-        setIntent(intent);
     }
 }
--- detox/test/android/app/src/main/java/com/example/MainApplication.java
+++ detox/test/android/app/src/main/java/com/example/MainApplication.java
@@ -5,14 +5,10 @@ import android.webkit.WebView;
 
 import com.facebook.react.ReactApplication;
 import com.facebook.react.ReactNativeHost;
-import com.facebook.react.config.ReactFeatureFlags;
 import com.facebook.soloader.SoLoader;
 
 public class MainApplication extends Application implements ReactApplication {
-    private final ReactNativeHost mReactNativeHost = (
-            BuildConfig.IS_NEW_ARCHITECTURE_ENABLED ?
-                new DetoxRNHostNewArch(this) :
-                new DetoxRNHost(this));
+    private final ReactNativeHost mReactNativeHost = new DetoxRNHost(this);
 
     @Override
     public ReactNativeHost getReactNativeHost() {
@@ -22,9 +18,6 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     public void onCreate() {
         super.onCreate();
-
-        // If you opted-in for the New Architecture, we enable the TurboModule system
-        ReactFeatureFlags.useTurboModules = BuildConfig.IS_NEW_ARCHITECTURE_ENABLED;
 
         SoLoader.init(this, /* native exopackage */ false);
         WebView.setWebContentsDebuggingEnabled(true);
