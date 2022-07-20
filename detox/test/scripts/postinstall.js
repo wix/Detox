e21acdbf9a248ca99fa27c69d7151b57ba7f6ea3
d4vidi <amit.d4vidi@gmail.com>
2022-07-20 11:23:38 +0300

    (Android) Backwards compatibility - RN 64

--- detox/test/scripts/postinstall.js
+++ detox/test/scripts/postinstall.js
@@ -58,10 +58,33 @@ function cleanFindNodeScriptFileForRn64IOS() {
   }
 }
 
+function overrideGradleWrapperVersion() {
+  const gradleWrapperOld =
+    '#!!! Patched by post-install script !!!\n' +
+    '#!!! Do not commit !!!\n' +
+    'distributionBase=GRADLE_USER_HOME\n' +
+    'distributionPath=wrapper/dists\n' +
+    'zipStoreBase=GRADLE_USER_HOME\n' +
+    'zipStorePath=wrapper/dists\n' +
+    'distributionUrl=https\\://services.gradle.org/distributions/gradle-6.9-all.zip\n' +
+    '#!!! Do not commit !!!\n';
+
+  const GRADLE_WRAPPER_PROPS_PATH = path.join('android', 'gradle', 'wrapper', 'gradle-wrapper.properties');
+
+  console.log('  Patching gradle-wrapper.properties file back to gradle v6.9..');
+  try {
+    fs.writeFileSync(GRADLE_WRAPPER_PROPS_PATH, gradleWrapperOld);
+  } catch (e) {
+    console.warn('  Couldn\'t path the gradle-wrapper.properties file', e);
+  }
+}
+
 function run() {
   console.log('Running Detox test-app post-install script...');
 
-  if (semver.minor(rnVersion) === 64) {
+  const version = semver.minor(rnVersion);
+
+  if (version === 64) {
     console.log('  Detected RN version .64! Applying necessary patches...');
     overrideReactAndroidGradleForRn64Android();
     cleanFindNodeScriptFileForRn64IOS();
@@ -70,6 +93,11 @@ function run() {
   if (semver.minor(rnVersion) === 68) {
     console.log('  Detected RN version .68! Applying necessary patches...');
     overrideReactAndroidGradleForRn68Android();
+  }
+
+  if (semver.minor(rnVersion) < 68) {
+    console.log('  Version is lower than 68 - Applying dedicated patches...');
+    overrideGradleWrapperVersion();
   }
 
   console.log('Detox test-app post-install script completed!');
