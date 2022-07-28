145d1815559e32565233cb31bb619d2c06913f2f
d4vidi <amit.d4vidi@gmail.com>
2022-07-28 12:14:58 +0300

    ci: Introduce an additional job for RN 66 compat on Android

--- .buildkite/pipeline_common.sh
+++ .buildkite/pipeline_common.sh
@@ -7,4 +7,5 @@ cat .buildkite/jobs/pipeline.ios_rn_68.yml
 cat .buildkite/jobs/pipeline.android_rn_64.yml
 cat .buildkite/jobs/pipeline.android_rn_68.yml
 cat .buildkite/jobs/pipeline.android_demo_app_rn_68.yml
+cat .buildkite/jobs/pipeline.android_demo_app_rn_66.yml
 cat .buildkite/jobs/pipeline.ios_demo_app_rn_68.yml
