016b3e46efae4e00fe4c91e1452dbaae3528f41a
jonathanmos <48201295+jonathanmos@users.noreply.github.com>
2022-08-31 17:34:15 +0300

    Leave only rn 66 and 69 pipelines

--- .buildkite/pipeline_common.sh
+++ .buildkite/pipeline_common.sh
@@ -2,14 +2,11 @@
 
 echo "steps:"
 
-cat .buildkite/jobs/pipeline.ios_rn_64.yml
-cat .buildkite/jobs/pipeline.ios_rn_68.yml
+cat .buildkite/jobs/pipeline.ios_rn_66.yml
 cat .buildkite/jobs/pipeline.ios_rn_69.yml
-cat .buildkite/jobs/pipeline.android_rn_64.yml
-cat .buildkite/jobs/pipeline.android_rn_68.yml
+cat .buildkite/jobs/pipeline.android_rn_66.yml
 cat .buildkite/jobs/pipeline.android_rn_69.yml
 cat .buildkite/jobs/pipeline.android_demo_app_rn_66.yml
-cat .buildkite/jobs/pipeline.android_demo_app_rn_68.yml
 cat .buildkite/jobs/pipeline.android_demo_app_rn_69.yml
-cat .buildkite/jobs/pipeline.ios_demo_app_rn_68.yml
+cat .buildkite/jobs/pipeline.ios_demo_app_rn_66.yml
 cat .buildkite/jobs/pipeline.ios_demo_app_rn_69.yml
