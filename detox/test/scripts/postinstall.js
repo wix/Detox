4930e0f98feb0f8d85d4d28ad546e24180e52e95
jonathanmos <48201295+jonathanmos@users.noreply.github.com>
2022-09-01 14:45:02 +0300

    Remove rn64 postinstall support

--- detox/test/scripts/postinstall.js
+++ detox/test/scripts/postinstall.js
@@ -7,30 +7,6 @@ const rnVersion = function() {
   return rnPackageJson.version;
 }();
 
-/**
- * In RN .64, it seems that react-native-codegen - a proprietary code generation plugin, whose
- * native code is only available inside react-native's monorepo, has been applied.
- * This patch disables it, and that works - as long as RN is used as an .aar dep and
- * not built from source. I can't account for the latter; It is likely that with .64 it may not
- * be possible to do anymore. If ever needed, one approach to try out is to somehow build the
- * plugin ourselves through the react-native monorepo itself (onto a .jar), and then add it as a
- * direct plugin dependency into our settings.gradle (https://docs.gradle.org/current/userguide/plugins.html#sec:custom_plugin_repositories).
- */
-function overrideReactAndroidGradleForRn64Android() {
-  const REACT_ANDROID_PATH = path.join('node_modules', 'react-native', 'ReactAndroid');
-  const REACT_ANDROID_GRADLE_SCRIPT_PATH = path.join(REACT_ANDROID_PATH, 'build.gradle');
-  const REACT_ANDROID_GRADLE_BAK_SCRIPT_PATH = path.join(REACT_ANDROID_PATH, 'build.gradle.bak');
-  const PATCH_SCRIPT_PATH = path.join('scripts', 'ReactAndroid_rn64_build.gradle');
-
-  console.log('  Overriding ReactAndroid\'s build.gradle...');
-  try {
-    fs.renameSync(REACT_ANDROID_GRADLE_SCRIPT_PATH, REACT_ANDROID_GRADLE_BAK_SCRIPT_PATH);
-  } catch (e) {
-    console.warn('  Couldn\'t create a backup to original script (skipping)', e);
-  }
-  fs.copySync(PATCH_SCRIPT_PATH, REACT_ANDROID_GRADLE_SCRIPT_PATH);
-}
-
 function overrideReactAndroidGradleForRn66Android() {
   const REACT_ANDROID_PATH = path.join('node_modules', 'react-native', 'ReactAndroid');
   const REACT_ANDROID_GRADLE_SCRIPT_PATH = path.join(REACT_ANDROID_PATH, 'build.gradle');
@@ -76,18 +52,6 @@ function overrideReactAndroidGradleForRn67Android() {
   fs.copySync(PATCH_SCRIPT_PATH, REACT_ANDROID_GRADLE_SCRIPT_PATH);
 }
 
-function cleanFindNodeScriptFileForRn64IOS() {
-  const REACT_SCRIPTS_PATH = path.join('node_modules', 'react-native', 'scripts');
-  const REACT_FIND_NODE_SCRIPT_PATH = path.join(REACT_SCRIPTS_PATH, 'find-node.sh');
-
-  console.log('  Clean content of find-node.sh file..');
-  try {
-    fs.writeFileSync(REACT_FIND_NODE_SCRIPT_PATH, '');
-  } catch (e) {
-    console.warn('  Couldn\'t clean content find-node.sh file', e);
-  }
-}
-
 function overrideGradleWrapperVersion() {
   const gradleWrapperOld =
     '#!!! Patched by post-install script !!!\n' +
@@ -114,30 +78,22 @@ function run() {
 
   const version = semver.minor(rnVersion);
 
-  if (version === 64) {
-    console.log('  Detected RN version .64! Applying necessary patches...');
-    overrideReactAndroidGradleForRn64Android();
-    cleanFindNodeScriptFileForRn64IOS();
+  if (version === 66) {
+    console.log('  Detected RN version .66! Applying necessary patches...');
+    overrideReactAndroidGradleForRn66Android();
   }
 
-  if (semver.minor(rnVersion) === 68) {
+  if (version === 67) {
+    console.log('  Detected RN version .67! Applying necessary patches...');
+    overrideReactAndroidGradleForRn67Android();
+  }
+
+  if (version === 68) {
     console.log('  Detected RN version .68! Applying necessary patches...');
     overrideReactAndroidGradleForRn68Android();
   }
 
-  if (semver.minor(rnVersion) === 66) {
-    console.log('  Detected RN version .66! Applying necessary patches...');
-    overrideReactAndroidGradleForRn66Android();
-    cleanFindNodeScriptFileForRn64IOS();
-  }
-
-  if (semver.minor(rnVersion) === 67) {
-    console.log('  Detected RN version .67! Applying necessary patches...');
-    overrideReactAndroidGradleForRn67Android();
-    cleanFindNodeScriptFileForRn64IOS();
-  }
-
-  if (semver.minor(rnVersion) < 68) {
+  if (version < 68) {
     console.log('  Version is lower than 68 - Applying dedicated patches...');
     overrideGradleWrapperVersion();
   }
