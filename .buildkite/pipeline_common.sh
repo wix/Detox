bbc24a74ea5429fe7a7edbfca0e785173e849457
d4vidi <amit.d4vidi@gmail.com>
2022-09-06 17:19:20 +0300

    Remove .66 demo-app CI jobs

--- .buildkite/pipeline_common.sh
+++ .buildkite/pipeline_common.sh
@@ -6,7 +6,5 @@ cat .buildkite/jobs/pipeline.ios_rn_66.yml
 cat .buildkite/jobs/pipeline.ios_rn_69.yml
 cat .buildkite/jobs/pipeline.android_rn_66.yml
 cat .buildkite/jobs/pipeline.android_rn_69.yml
-cat .buildkite/jobs/pipeline.android_demo_app_rn_66.yml
 cat .buildkite/jobs/pipeline.android_demo_app_rn_69.yml
-cat .buildkite/jobs/pipeline.ios_demo_app_rn_66.yml
 cat .buildkite/jobs/pipeline.ios_demo_app_rn_69.yml
