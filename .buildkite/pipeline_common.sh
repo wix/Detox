957edb7faa43ea82338b332105d4573afd2f2248
Asaf Korem <asaf.korem@gmail.com>
2022-10-02 14:45:49 +0300

    ci(pipeline steps): add Demo app iOS + RN.66 job.

--- .buildkite/pipeline_common.sh
+++ .buildkite/pipeline_common.sh
@@ -8,4 +8,5 @@ cat .buildkite/jobs/pipeline.android_rn_66.yml
 cat .buildkite/jobs/pipeline.android_rn_69.yml
 cat .buildkite/jobs/pipeline.android_demo_app_rn_66.yml
 cat .buildkite/jobs/pipeline.android_demo_app_rn_69.yml
+cat .buildkite/jobs/pipeline.ios_demo_app_rn_66.yml
 cat .buildkite/jobs/pipeline.ios_demo_app_rn_69.yml
