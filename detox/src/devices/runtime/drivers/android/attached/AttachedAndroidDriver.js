7966f55eb67976e8518cb749b55eb67bd564d7da
d4vidi <amit.d4vidi@gmail.com>
2022-10-19 11:37:15 +0300

    Align Android attached-device driver

--- detox/src/devices/runtime/drivers/android/attached/AttachedAndroidDriver.js
+++ detox/src/devices/runtime/drivers/android/attached/AttachedAndroidDriver.js
@@ -1,10 +1,15 @@
-const AndroidDriver = require('../AndroidDriver');
+// TODO (multiapps)
 
-class AttachedAndroidDriver extends AndroidDriver {
+const { AndroidDeviceDriver, AndroidAppDriver } = require('../AndroidDrivers');
+
+class AttachedAndroidDriver extends AndroidDeviceDriver {
   /** @override */
   get deviceName() {
     return `AttachedDevice:${this.adbName}`;
   }
 }
 
-module.exports = AttachedAndroidDriver;
+module.exports = {
+  AttachedAndroidDriver,
+
+};
