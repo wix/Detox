fe8cdef44d94e82e2652e7311aeb1266d33a531b
Asaf Korem <asaf.korem@gmail.com>
2022-10-26 20:24:09 +0300

    fix(demo-rn-bloat-bundle-test): fix bundle path.

--- scripts/demo-rn-bloat-bundle-test.sh
+++ scripts/demo-rn-bloat-bundle-test.sh
@@ -1,9 +1,11 @@
-#!/bin/bash -e
+#!/bin/bash -ex
 # This script is used to run the demo project' tests with a bloated JS bundle.
 # See the original issue for the motivation behind this: https://github.com/wix/Detox/issues/3507
 
 OS_PLATFORM=$1
-BUNDLE_PATH=./app.js
+WORKING_DIR=$(pwd)
+BUNDLE_FILE=app.js
+BUNDLE_PATH=$WORKING_DIR/$BUNDLE_FILE
 
 echo "Bloating the demo project bundle for $OS_PLATFORM"
 
@@ -11,7 +13,7 @@ echo "Bloating the demo project bundle for $OS_PLATFORM"
 cp $BUNDLE_PATH $BUNDLE_PATH.original
 
 # Bloat the bundle:
-run_f "npm run bloat-bundle -- $BUNDLE_PATH"
+run_f "npm run bloat-bundle -- ./$BUNDLE_FILE"
 
 # Run the tests:
 run_f "npm run build:$OS_PLATFORM-release"
