16be8f737525a9764b63aac4e24a23310595594a
Asaf Korem <asaf.korem@gmail.com>
2022-10-26 20:26:56 +0300

    fix(demo-rn-bloat-bundle-test): execute npm directly.

--- scripts/demo-rn-bloat-bundle-test.sh
+++ scripts/demo-rn-bloat-bundle-test.sh
@@ -13,11 +13,11 @@ echo "Bloating the demo project bundle for $OS_PLATFORM"
 cp $BUNDLE_PATH $BUNDLE_PATH.original
 
 # Bloat the bundle:
-run_f "npm run bloat-bundle -- ./$BUNDLE_FILE"
+npm run bloat-bundle -- ./$BUNDLE_FILE
 
 # Run the tests:
-run_f "npm run build:$OS_PLATFORM-release"
-run_f "npm run test:$OS_PLATFORM-release-ci"
+npm run build:$OS_PLATFORM-release
+npm run test:$OS_PLATFORM-release-ci
 
 # Reverse the bloating:
 mv $BUNDLE_PATH.original $BUNDLE_PATH
