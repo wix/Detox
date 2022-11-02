aa8045c5b7c3cdc0c536277e7d2a4d494ce99f42
Asaf Korem <asaf.korem@gmail.com>
2022-11-02 16:14:02 +0200

    add missing case

--- detox/ios/DetoxTester/DetoxTester/Tester/Executor/Executor+captureViewHierarchy.swift
+++ detox/ios/DetoxTester/DetoxTester/Tester/Executor/Executor+captureViewHierarchy.swift
@@ -36,7 +36,7 @@ extension Executor {
         execLog("capture view hierarchy failed, reason: \(reason)", type: .error)
         fatalError("capture view hierarchy failed, reason: \(reason)")
 
-      case .boolean, .string, .strings,  .status:
+      case .boolean, .string, .strings, .status, .elementsAttributes(_):
         fatalError("Should never get here")
     }
   }
