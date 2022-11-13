7a1a09e8a244ba0c98ded0f1dce70dfe44f1ad3f
Asaf Korem <asaf.korem@gmail.com>
2022-11-13 08:32:26 +0200

    website(showcase): shuffle apps order.
    Until we'll decide on a better order (for example by downloads
     or rating), this will shuffle up the list of applications on every load
     instead of having a fixed arbitrary order.

--- website/src/components/Showcase/Showcase.js
+++ website/src/components/Showcase/Showcase.js
@@ -5,8 +5,19 @@ import cardList from '@site/showcase.json';
 import styles from './Showcase.module.css';
 
 function Showcase() {
-  const cards = cardList.map((props) => <Card key={props.title} {...props} />);
+  const cards = _makeShuffledCardList().map(_makeCard)
+  return _makeCardsContainer(cards);
+}
 
+const _makeShuffledCardList = () => {
+  return cardList.sort(() => 0.5 - Math.random());
+}
+
+const _makeCard = (props) => {
+  return <Card key={props.title} {...props} />;
+}
+
+const _makeCardsContainer = (cards) => {
   return (
     <section className={clsx('container', styles.container)}>
       <ul className={clsx('col', 'col--12', styles.list)}>{cards}</ul>
