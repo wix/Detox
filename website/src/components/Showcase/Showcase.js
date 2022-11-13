c90f0205b3a90e66d7243ede7b6539ec476822b0
Asaf Korem <asaf.korem@gmail.com>
2022-11-13 14:08:13 +0200

    website: add loading spinner.

--- website/src/components/Showcase/Showcase.js
+++ website/src/components/Showcase/Showcase.js
@@ -4,15 +4,21 @@ import Card from './Card';
 import cardList from '@site/showcase.json';
 import styles from './Showcase.module.css';
 import _ from 'lodash';
+import { TailSpin } from 'react-loader-spinner';
 
 function Showcase() {
   const [cards, setCards] = useState([]);
+  const [toRender, setToRender] = useState([]);
 
   useEffect(() => {
     setCards(_makeShuffledCardList(cardList).map(_makeCard));
   }, []);
 
-  return _makeCardsContainer(cards);
+  useEffect(() => {
+    setToRender(cards.length > 0 ? _makeCardsContainer(cards) : _makeLoadingSpinner());
+  }, [cards]);
+
+  return toRender;
 }
 
 const _makeShuffledCardList = (cardList) => {
@@ -23,10 +29,19 @@ const _makeShuffledCardList = (cardList) => {
 const _makeCard = (props) => <Card key={props.title} {...props} />;
 
 const _makeCardsContainer = (cards) =>
-  (
-    <section className={clsx('container', styles.container)}>
-      <ul className={clsx('col', 'col--12', styles.list)}>{cards}</ul>
-    </section>
-  );
+  <section className={clsx('container', styles.container)}>
+    <ul className={clsx('col', 'col--12', styles.list)}>{cards}</ul>
+  </section>;
+
+const _makeLoadingSpinner = () =>
+  <TailSpin
+    height="60"
+    width="60"
+    color="#dddddd"
+    ariaLabel="loading"
+    radius="2"
+    wrapperClass={styles.spinner}
+    visible={true}
+  />;
 
 export default Showcase;
