e507cabe80e0c1a4f713ce3ecefef55c73f7d2dd
Asaf Korem <asaf.korem@gmail.com>
2022-11-13 11:43:24 +0200

    website(showcase): shuffle apps order.

--- website/src/components/Showcase/Showcase.js
+++ website/src/components/Showcase/Showcase.js
@@ -1,17 +1,32 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import clsx from 'clsx';
 import Card from './Card';
 import cardList from '@site/showcase.json';
 import styles from './Showcase.module.css';
+import _ from 'lodash';
 
 function Showcase() {
-  const cards = cardList.map((props) => <Card key={props.title} {...props} />);
+  const [cards, setCards] = useState([]);
 
-  return (
+  useEffect(() => {
+    setCards(_makeShuffledCardList(cardList).map(_makeCard));
+  }, []);
+
+  return _makeCardsContainer(cards);
+}
+
+const _makeShuffledCardList = (cardList) => {
+  const partition = _.partition(cardList, 'shouldStayOnTop');
+  return partition[0].concat(_.shuffle(partition[1]));
+}
+
+const _makeCard = (props) => <Card key={props.title} {...props} />;
+
+const _makeCardsContainer = (cards) =>
+  (
     <section className={clsx('container', styles.container)}>
       <ul className={clsx('col', 'col--12', styles.list)}>{cards}</ul>
     </section>
   );
-}
 
 export default Showcase;
