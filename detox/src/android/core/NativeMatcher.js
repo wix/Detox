562cb59f53794f8d3d96422e98da0fb3aaa5a3aa
Yaroslav Serhieiev <yaroslavs@wix.com>
2022-12-13 17:33:45 +0200

    fix: immutable matchers

--- detox/src/android/core/NativeMatcher.js
+++ detox/src/android/core/NativeMatcher.js
@@ -2,29 +2,33 @@ const invoke = require('../../invoke');
 const DetoxMatcherApi = require('../espressoapi/DetoxMatcher');
 
 class NativeMatcher {
+  constructor(call) {
+    this._call = call || null;
+  }
+
   withAncestor(matcher) {
-    this._call = invoke.callDirectly(DetoxMatcherApi.matcherWithAncestor(this, matcher));
-    return this;
+    const call = invoke.callDirectly(DetoxMatcherApi.matcherWithAncestor(this, matcher));
+    return new NativeMatcher(call);
   }
 
   withDescendant(matcher) {
-    this._call = invoke.callDirectly(DetoxMatcherApi.matcherWithDescendant(this, matcher));
-    return this;
+    const call = invoke.callDirectly(DetoxMatcherApi.matcherWithDescendant(this, matcher));
+    return new NativeMatcher(call);
   }
 
   and(matcher) {
-    this._call = invoke.callDirectly(DetoxMatcherApi.matcherForAnd(this, matcher));
-    return this;
+    const call = invoke.callDirectly(DetoxMatcherApi.matcherForAnd(this, matcher));
+    return new NativeMatcher(call);
   }
 
   or(matcher) {
-    this._call = invoke.callDirectly(DetoxMatcherApi.matcherForOr(this, matcher));
-    return this;
+    const call = invoke.callDirectly(DetoxMatcherApi.matcherForOr(this, matcher));
+    return new NativeMatcher(call);
   }
 
   get not() {
-    this._call = invoke.callDirectly(DetoxMatcherApi.matcherForNot(this));
-    return this;
+    const call = invoke.callDirectly(DetoxMatcherApi.matcherForNot(this));
+    return new NativeMatcher(call);
   }
 
   _avoidProblematicReactNativeElements() {
