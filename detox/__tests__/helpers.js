f3c9b7a1d599216cce7e859d5adad97823773323
Yaroslav Serhieiev <yaroslavs@wix.com>
2022-12-23 14:06:47 +0200

    refactor(tests): reusable cli mock (#3780)

--- detox/__tests__/helpers.js
+++ detox/__tests__/helpers.js
@@ -1,6 +1,8 @@
 const path = require('path');
 
+const fs = require('fs-extra');
 const _ = require('lodash');
+const tempfile = require('tempfile');
 const yargs = require('yargs');
 
 function callCli(modulePath, cmd) {
@@ -27,14 +29,58 @@ function callCli(modulePath, cmd) {
         'camel-case-expansion': false,
         'dot-notation': false,
         'duplicate-arguments-array': false,
+        'populate--': true,
       })
       .command(spiedModule)
+      .wrap(null)
       .exitProcess(false)
       .fail((msg, err) => reject(err || msg))
       .parse(cmd, (err) => err && reject(err));
   });
 }
 
+function buildMockCommand(options = {}) {
+  if (!options.stdout) {
+    options.stdout = tempfile('.txt');
+  }
+
+  return {
+    get cmd() {
+      const env = [
+        options.exitCode ? `CLI_EXIT_CODE=${options.exitCode}` : undefined,
+        options.sleep ? `CLI_SLEEP=${options.sleep}` : undefined,
+        `CLI_TEST_STDOUT=${options.stdout}`,
+      ].filter(Boolean).join(' ');
+
+      return `cross-env ${env} node ${path.join(__dirname, '../local-cli/__mocks__/executable')}`;
+    },
+
+    options,
+
+    _calls: undefined,
+
+    get calls() {
+      if (this._calls === undefined) {
+        if (fs.existsSync(options.stdout)) {
+          this._calls = fs.readFileSync(options.stdout, 'utf8')
+            .trim()
+            .split('\n')
+            .map(c => JSON.parse(c));
+        } else {
+          this._calls = [];
+        }
+      }
+
+      return this._calls;
+    },
+
+    async clean() {
+      await fs.remove(options.stdout);
+    },
+  };
+}
+
+exports.buildMockCommand = buildMockCommand;
 exports.callCli = callCli;
 exports.latestInstanceOf = (clazz) => _.last(clazz.mock.instances);
 exports.lastCallTo = (mocked) => _.last(mocked.mock.calls);
