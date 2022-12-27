c31720f7b0b8bb74590711f1d5733ff8cd9413ca
Yaroslav Serhieiev <yaroslavs@wix.com>
2022-12-27 17:36:34 +0200

    test: stricter integration tests

--- detox/test/integration/utils/simplistic-runner.js
+++ detox/test/integration/utils/simplistic-runner.js
@@ -3,8 +3,8 @@ const detox = require('detox/internals');
 async function main() {
   try {
     await detox.init();
-    await test1().catch(e => console.error(e));
-    await test2().catch(e => console.error(e));
+    await test1();
+    await test2();
   } finally {
     await detox.cleanup();
   }
