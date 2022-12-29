eaad43dc5ca384ae4edc8a38367e27011484fd27
Maurice Döpke <mauricedoepkeoutlook.de>
2022-12-29 14:50:20 +0100

    use button to open android datepicker instead of implicit click on text

--- detox/test/src/Screens/DatePickerScreen.js
+++ detox/test/src/Screens/DatePickerScreen.js
@@ -1,6 +1,6 @@
 import moment from 'moment';
 import React, { Component } from 'react';
-import { Text, View, StyleSheet, DatePickerIOS, TouchableOpacity, Platform } from 'react-native';
+import { Text, View, StyleSheet, DatePickerIOS, Platform, Button } from 'react-native';
 import { DateTimePickerAndroid } from "@react-native-community/datetimepicker";
 
 const isAndroid = Platform.OS === 'android';
@@ -38,16 +38,6 @@ export default class DatePickerScreen extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <TouchableOpacity testID='openAndroidDatePicker' onPress={() => {
-            if (!isAndroid) return;
-
-            DateTimePickerAndroid.open({
-              testID: "datePicker44",
-              value: this.state.chosenDate,
-              onChange: (_, newDate) => this.setDate(newDate),
-              mode: "date",
-            });
-        }} >
         <Text style={styles.dateText} testID="utcDateLabel">
           {"Date (UTC): " + this.getDateUTC()}
         </Text>
@@ -57,8 +47,20 @@ export default class DatePickerScreen extends Component {
         <Text style={styles.dateText} testID='localTimeLabel'>
           {"Time: " + this.getTimeLocal()}
         </Text>
-        </TouchableOpacity>
-        {isIos && <DatePickerIOS testID="datePicker" style={styles.datePicker} date={this.state.chosenDate} onDateChange={this.setDate} />}
+        { isAndroid && <View style={styles.openDatePickerButtonContainer}>
+            <Button 
+              title="Open datepicker"
+              testID='openAndroidDatePicker'
+              onPress={() => {
+                DateTimePickerAndroid.open({
+                  value: this.state.chosenDate,
+                  onChange: (_, newDate) => this.setDate(newDate),
+                  mode: "date",
+                });
+              }}
+            />
+        </View>}
+        {isIos && <DatePickerIOS testID="datePicker" style={styles.datePickerIos} date={this.state.chosenDate} onDateChange={this.setDate} />}
       </View>
     );
   }
@@ -71,9 +73,12 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center'
   },
-  datePicker: {
+  datePickerIos: {
     width:'100%',
     height:200
+  },
+  openDatePickerButtonContainer: {
+    paddingTop: 20,
   },
   dateText: {
     textAlign:'center'
