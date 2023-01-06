f62e6cd4b7b5ad7fd316dd0c6735502ccdc30608
Yaroslav Serhieiev <yaroslavs@wix.com>
2023-01-06 19:45:37 +0200

    test(e2e): fix padding in the app

--- detox/test/src/Screens/DatePickerScreen.js
+++ detox/test/src/Screens/DatePickerScreen.js
@@ -104,6 +104,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     paddingTop: 20,
+    paddingBottom: 50,
     flexDirection: 'column',
     alignItems: 'center',
     justifyContent: 'flex-start',
