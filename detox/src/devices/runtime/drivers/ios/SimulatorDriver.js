7e30b090fec968cc9d525a3ee4e35d13d3b7aef7
Asaf Korem <asaf.korem@gmail.com>
2023-01-26 12:23:57 +0200

    Avoid printing error if undefined

--- detox/src/devices/runtime/drivers/ios/SimulatorDriver.js
+++ detox/src/devices/runtime/drivers/ios/SimulatorDriver.js
@@ -129,7 +129,9 @@ class SimulatorDriver extends IosDriver {
         log.info(`Tests execution finished:\n ${result.stdout.toString()}`);
       })
       .catch(function (err) {
-        log.error(`Error occurred:\n ${err.stderr}`);
+        if (err !== undefined) {
+          log.error(`Error occurred:\n ${err.stderr}`);
+        }
       });
 
     log.info(`Allowing network permissions`);
