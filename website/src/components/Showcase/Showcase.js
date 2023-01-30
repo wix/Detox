0ece5aa11f09ddc1a9bbc76f1e22d04ed98e018e
Jason <jasonba@wix.com>
2023-01-30 20:07:50 +0200

    Implement ?shuffle=disabled logic in the Showcase component

--- website/src/components/Showcase/Showcase.js
+++ website/src/components/Showcase/Showcase.js
@@ -6,9 +6,9 @@ import styles from './Showcase.module.css';
 import _ from 'lodash';
 import { TailSpin } from 'react-loader-spinner';
 
-function Showcase() {
+const Showcase = () => {
   const [cards, setCards] = useState([]);
-  const [toRender, setToRender] = useState([]);
+  const [toRender, setToRender] = useState([<TailSpin />]);
 
   useEffect(() => {
     setCards(_makeShuffledCardList(cardList).map(_makeCard));
@@ -19,29 +19,25 @@ function Showcase() {
   }, [cards]);
 
   return toRender;
-}
+};
 
 const _makeShuffledCardList = (cardList) => {
   const partition = _.partition(cardList, 'shouldStayOnTop');
-  return partition[0].concat(_.shuffle(partition[1]));
-}
+  const shouldShuffle = new URLSearchParams(window.location.search).get('shuffle');
+  const toConcat = shouldShuffle !== 'disabled' ? _.shuffle(partition[1]) : partition[1];
+  return partition[0].concat(toConcat);
+};
 
 const _makeCard = (props) => <Card key={props.title} {...props} />;
 
-const _makeCardsContainer = (cards) =>
+const _makeCardsContainer = (cards) => (
   <section className={clsx('container', styles.container)}>
     <ul className={clsx('col', 'col--12', styles.list)}>{cards}</ul>
-  </section>;
+  </section>
+);
 
-const _makeLoadingSpinner = () =>
-  <TailSpin
-    height="60"
-    width="60"
-    color="#dddddd"
-    ariaLabel="loading"
-    radius="2"
-    wrapperClass={styles.spinner}
-    visible={true}
-  />;
+const _makeLoadingSpinner = () => (
+  <TailSpin height="60" width="60" color="#dddddd" ariaLabel="loading" radius="2" wrapperClass={styles.spinner} visible={true} />
+);
 
 export default Showcase;
