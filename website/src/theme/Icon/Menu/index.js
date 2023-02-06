cb01cb6344ef7ef1abe436d728117f545bc4e9c7
Artem Vasylenko <irbiskronos@gmail.com>
2023-02-06 19:32:11 +0200

    docs(website): mobile styling for NavBar

---
+++ website/src/theme/Icon/Menu/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+export default function IconMenu({ width = 30, height = 30, className, ...restProps }) {
+  return (
+    <svg className={className} width={width} height={height} viewBox="0 0 30 30" aria-hidden="true" {...restProps}>
+      <path stroke="currentColor" strokeLinecap="round" strokeMiterlimit="10" strokeWidth="2" d="M4 7h22M4 15h22M4 23h22" />
+    </svg>
+  );
+}
