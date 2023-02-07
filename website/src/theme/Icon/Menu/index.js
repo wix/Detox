dbecade8089378c22732b7771af634d82a06ade2
Artem Vasylenko <irbiskronos@gmail.com>
2023-02-07 20:49:35 +0200

    In the process of fixing a hamburger

--- website/src/theme/Icon/Menu/index.js
+++ website/src/theme/Icon/Menu/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
-export default function IconMenu({ width = 30, height = 30, className, ...restProps }) {
+export default function IconMenu({ width = 19, height = 12, className, ...restProps }) {
   return (
-    <svg className={className} width={width} height={height} viewBox="0 0 30 30" aria-hidden="true" {...restProps}>
-      <path stroke="currentColor" strokeLinecap="round" strokeMiterlimit="10" strokeWidth="2" d="M4 7h22M4 15h22M4 23h22" />
+    <svg className={className} width={width} height={height} viewBox="0 0 19 12" fill="currentColor" aria-hidden="true" {...restProps}>
+      <rect x="0.5" y="5.5" width="18" height="1" rx="0.5" stroke="currentColor" stroke-linecap="round" />
+      <rect x="0.5" y="10.5" width="18" height="1" rx="0.5" stroke="currentColor" stroke-linecap="round" />
+      <rect x="0.5" y="0.5" width="18" height="1" rx="0.5" stroke="currentColor" stroke-linecap="round" />
     </svg>
   );
 }
