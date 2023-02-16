a24cea3607b4f4547d85d6ade2e056679899c30c
d4vidi <amit.d4vidi@gmail.com>
2023-02-16 22:14:24 +0200

    React Native 70 compliance - Detox demo projects (iOS)

--- .buildkite/pipeline_common.sh
+++ .buildkite/pipeline_common.sh
@@ -9,5 +9,5 @@ cat .buildkite/jobs/pipeline.android_rn_70.yml
 cat .buildkite/jobs/pipeline.android_demo_app_rn_68.yml
 cat .buildkite/jobs/pipeline.android_demo_app_rn_70.yml
 cat .buildkite/jobs/pipeline.ios_demo_app_rn_68.yml
-cat .buildkite/jobs/pipeline.ios_demo_app_rn_69.yml
+cat .buildkite/jobs/pipeline.ios_demo_app_rn_70.yml
 cat .buildkite/pipeline.post_processing.yml
