52ef95252ecab53dd9155c8e5d399354bf769ba8
Yaroslav Serhieiev <yaroslavs@wix.com>
2023-02-20 18:47:52 +0200

    fix(jest): focused test with afterAll hook (#3863)

--- detox/test/integration/e2e/passing-skipped.test.js
+++ detox/test/integration/e2e/passing-skipped.test.js
@@ -1,6 +1,10 @@
-describe('Skipped', () => {
+describe('Suite with skipped tests', () => {
   it.skip('Skipped test', async () => {
     // Checking that skipped tests are also traced
+  });
+
+  it('Regular test', async () => {
+    // should be traced as usual
   });
 
   it.todo('Check that todo tests are also traced');
