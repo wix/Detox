010738d1c2f6f983eacd9f02e59b99e6c45892b4
d4vidi <amit.d4vidi@gmail.com>
2023-02-28 15:27:42 +0200

    wip

--- detox/test/e2e/01.sanity.test.js
+++ detox/test/e2e/01.sanity.test.js
@@ -7,7 +7,7 @@ describe('Sanity', () => {
   it('should have welcome screen', async () => {
     await expect(element(by.text('Welcome'))).toBeVisible();
     await expect(element(by.text('Say Hello'))).toBeVisible();
-    await expect(element(by.text('Say World'))).toBeVisible();
+    // await expect(element(by.text('Say World'))).toBeVisible();
   });
 
   it('should show hello screen after tap', async () => {
