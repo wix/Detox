1b3f4ff6bfafc2c3beaf482640c03dc54d622385
Asaf Korem <asaf.korem@gmail.com>
2023-03-12 15:11:58 +0200

    try new permission mechanism

--- detox/src/devices/runtime/drivers/ios/XCUITestUtils.js
+++ detox/src/devices/runtime/drivers/ios/XCUITestUtils.js
@@ -79,8 +79,58 @@ async function _runLaunchCommand(
   await _allowNetworkPermissionsXCUITest(callback);
 }
 
+async function getXcodeAppPath() {
+  const { stdout: xcodeSelectStdout } = await exec('xcode-select -p');
+  const xcodePath = xcodeSelectStdout.trim();
+  log.debug(`[XCUITest] Xcode path: ${xcodePath}`);
+
+  const xcodeAppPath = xcodePath.substring(0, xcodePath.lastIndexOf('.app') + 4);
+  log.debug(`[XCUITest] Xcode app path: ${xcodeAppPath}`);
+
+  return xcodeAppPath;
+}
+
+async function isXcodeDefinedInFirewall(xcodeAppPath) {
+  const { stdout } = await exec('/usr/libexec/ApplicationFirewall/socketfilterfw --listapps');
+  const result = stdout.includes(xcodeAppPath);
+  log.debug(`[XCUITest] Xcode is defined in the Firewall app: ${result}`);
+  return result;
+}
+
 async function _allowNetworkPermissionsXCUITest(callback) {
   log.debug(`[XCUITest] Allowing network permissions`);
+
+  try {
+    const xcodeAppPath = await getXcodeAppPath();
+
+    // check if Xcode is already defined in the firewall apps (if not, add it):
+    if (!(await isXcodeDefinedInFirewall(xcodeAppPath))) {
+      log.debug(`[XCUITest] Xcode is not defined in the Firewall app, adding it`);
+
+      const socketfilterfwPath = '/usr/libexec/ApplicationFirewall/socketfilterfw';
+
+      log.debug(`[XCUITest] Adding Xcode to the Firewall app`);
+      const addXcodeResult = await exec(socketfilterfwPath +' --add ' + xcodeAppPath +
+        '/Contents/MacOS/Xcode');
+      log.debug(`[XCUITest] Adding Xcode result: ${addXcodeResult.stdout}`);
+
+      log.debug(`[XCUITest] Adding Simulator to the Firewall app`);
+      const addSimulatorResult = await exec(socketfilterfwPath +' --add ' + xcodeAppPath +
+        '/Contents/Developer/Applications/Simulator.app/Contents/MacOS/Simulator');
+      log.debug(`[XCUITest] Adding Simulator result: ${addSimulatorResult.stdout}`);
+    }
+
+    if (await isXcodeDefinedInFirewall(xcodeAppPath)) {
+      log.debug(`[XCUITest] Xcode was successfully added to the Firewall app`);
+      callback();
+      return;
+    } else {
+      log.error(
+        `[XCUITest] Failed to add Xcode to the Firewall app using socketfilterfw, trying to add it using AppleScript`);
+    }
+  } catch (e) {
+    log.error(`[XCUITest] Failed to add Xcode to the Firewall apps:\n\t${e}`);
+  }
 
   osascript.executeFile(
     `${__dirname}/allowNetworkPermissionsXCUITest.scpt`,
