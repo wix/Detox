55a1a2bc24669ae667051e9cfc68c87e971409ca
Jason Bariamis <jasonba@wix.com>
2023-04-20 15:11:46 +0300

    Start updating social icons on homepage subheader replace with figma images. Use PAT and add function inside HomepageSubHeader.js file to get max 5000 calls and the stargazers count to display on the page using the API

--- website/src/components/Homepage/SubHeader/HomepageSubHeader.js
+++ website/src/components/Homepage/SubHeader/HomepageSubHeader.js
@@ -1,7 +1,32 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styles from './HomepageSubHeader.module.scss';
 
+const PAT = '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';
+
+const getGithubStarsCount = async () => {
+  const url = 'https://api.github.com/repos/wix/Detox';
+  const response = await fetch(url, {
+    headers: {
+      Authorization: `token ${PAT}`
+    }
+  });
+  const repoData = await response.json();
+  const starsCount = repoData.stargazers_count;
+  return starsCount;
+};
+
 const HomepageSubHeader = () => {
+  const [githubStarsCount, setGithubStarsCount] = useState(0);
+
+  useEffect(() => {
+    async function fetchData() {
+      const starsCount = await getGithubStarsCount();
+      setGithubStarsCount(starsCount);
+    }
+
+    fetchData();
+  }, []);
+
   return (
     <div className={styles.subHeader}>
       <div className="container">
@@ -23,16 +48,10 @@ const HomepageSubHeader = () => {
                 title="Follow Detox on Twitter"
                 width="200"
                 height="37"></iframe>
-              <iframe
-                className={styles.githubStar}
-                src="https://ghbtns.com/github-btn.html?user=wix&repo=detox&type=star&count=true&size=large"
-                frameBorder="0"
-                scrolling="0"
-                width="200"
-                height="37"
-                title="GitHub"
-              />
             </div>
+            <div className={styles.githubLogo}></div>
+            <div className={styles.ghTriangle}></div>
+            <div className={styles.githubStar}>{githubStarsCount}</div>
           </div>
         </div>
       </div>
