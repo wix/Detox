48b62508928b2f6fd8342f660fbe6d670f4c19eb
Asaf Korem <asaf.korem@gmail.com>
2023-05-14 12:19:31 +0300

    remove redundant pipelines, run on agent 14

--- .buildkite/pipeline_common.sh
+++ .buildkite/pipeline_common.sh
@@ -4,10 +4,3 @@ echo "steps:"
 
 cat .buildkite/jobs/pipeline.ios_rn_68.yml
 cat .buildkite/jobs/pipeline.ios_rn_70.yml
-cat .buildkite/jobs/pipeline.android_rn_68.yml
-cat .buildkite/jobs/pipeline.android_rn_70.yml
-cat .buildkite/jobs/pipeline.android_demo_app_rn_68.yml
-cat .buildkite/jobs/pipeline.android_demo_app_rn_70.yml
-cat .buildkite/jobs/pipeline.ios_demo_app_rn_68.yml
-cat .buildkite/jobs/pipeline.ios_demo_app_rn_70.yml
-cat .buildkite/pipeline.post_processing.yml
