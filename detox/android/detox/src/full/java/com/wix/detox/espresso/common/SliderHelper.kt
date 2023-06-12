3d91504a1d7f5ae078a8434eceb7550eedcfcf5b
Asaf Korem <asaf.korem@gmail.com>
2023-06-12 16:43:19 +0300

    feat: add support for RN 0.71. (WIP)

--- detox/android/detox/src/full/java/com/wix/detox/espresso/common/SliderHelper.kt
+++ detox/android/detox/src/full/java/com/wix/detox/espresso/common/SliderHelper.kt
@@ -4,12 +4,10 @@ import android.view.View
 import androidx.appcompat.widget.AppCompatSeekBar
 import com.facebook.react.bridge.JavaOnlyMap
 import com.facebook.react.uimanager.ReactStylesDiffMap
-import com.facebook.react.views.slider.ReactSlider
 import com.wix.detox.common.DetoxErrors.DetoxIllegalStateException
 import com.wix.detox.espresso.action.common.ReflectUtils
 import org.joor.Reflect
 
-private const val CLASS_REACT_SLIDER_LEGACY = "com.facebook.react.views.slider.ReactSlider"
 private const val CLASS_REACT_SLIDER_COMMUNITY = "com.reactnativecommunity.slider.ReactSlider"
 
 abstract class SliderHelper(protected val slider: AppCompatSeekBar) {
@@ -46,26 +44,10 @@ abstract class SliderHelper(protected val slider: AppCompatSeekBar) {
                 ?: throw DetoxIllegalStateException("Cannot handle this type of a seek-bar view (Class ${view.javaClass.canonicalName}). " +
                         "Only React Native sliders are currently supported.")
 
-        fun maybeCreate(view: View): SliderHelper? =
-            when {
-                ReflectUtils.isAssignableFrom(view, CLASS_REACT_SLIDER_LEGACY)
-                   -> LegacySliderHelper(view as ReactSlider)
-                ReflectUtils.isAssignableFrom(view, CLASS_REACT_SLIDER_COMMUNITY)
-                    -> CommunitySliderHelper(view as AppCompatSeekBar)
-                else
-                   -> null
-            }
+        fun maybeCreate(view: View): SliderHelper? = CommunitySliderHelper(view as AppCompatSeekBar)
     }
 }
 
-private class LegacySliderHelper(slider: AppCompatSeekBar): SliderHelper(slider) {
-    override fun setProgressJS(valueJS: Double) {
-        val reactSliderManager = com.facebook.react.views.slider.ReactSliderManager()
-        reactSliderManager.updateProperties(slider as ReactSlider, buildStyles("value", valueJS))
-    }
-
-    private fun buildStyles(vararg keysAndValues: Any) = ReactStylesDiffMap(JavaOnlyMap.of(*keysAndValues))
-}
 
 private class CommunitySliderHelper(slider: AppCompatSeekBar): SliderHelper(slider) {
     override fun setProgressJS(valueJS: Double) {
