c94b624dfe6737f9c7c5c616b318df53449bcb43
Asaf Korem <asaf.korem@gmail.com>
2023-07-16 13:58:38 +0300

    add phases

--- .buildkite/pipeline_common.sh
+++ .buildkite/pipeline_common.sh
@@ -9,4 +9,6 @@ cat .buildkite/jobs/pipeline.android_rn_70.yml
 cat .buildkite/jobs/pipeline.android_demo_app_rn_71.yml
 cat .buildkite/jobs/pipeline.ios_demo_app_rn_71.yml
 cat .buildkite/jobs/pipeline.ios_demo_app_rn_70.yml
+cat .buildkite/jobs/pipeline.ios.unit-tests.yml
+cat .buildkite/jobs/pipeline.ios.integration-tests.yml
 cat .buildkite/pipeline.post_processing.yml
