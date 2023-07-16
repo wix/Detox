b09f648b42f3e398ccc1ff2e5276aa878bb4e916
Asaf Korem <asaf.korem@gmail.com>
2023-07-16 14:41:59 +0300

    temp remove all phases (except for ios)

--- .buildkite/pipeline_common.sh
+++ .buildkite/pipeline_common.sh
@@ -3,12 +3,12 @@
 echo "steps:"
 
 cat .buildkite/jobs/pipeline.ios_rn_71.yml
-cat .buildkite/jobs/pipeline.ios_rn_70.yml
-cat .buildkite/jobs/pipeline.android_rn_71.yml
-cat .buildkite/jobs/pipeline.android_rn_70.yml
-cat .buildkite/jobs/pipeline.android_demo_app_rn_71.yml
-cat .buildkite/jobs/pipeline.ios_demo_app_rn_71.yml
-cat .buildkite/jobs/pipeline.ios_demo_app_rn_70.yml
+#cat .buildkite/jobs/pipeline.ios_rn_70.yml
+#cat .buildkite/jobs/pipeline.android_rn_71.yml
+#cat .buildkite/jobs/pipeline.android_rn_70.yml
+#cat .buildkite/jobs/pipeline.android_demo_app_rn_71.yml
+#cat .buildkite/jobs/pipeline.ios_demo_app_rn_71.yml
+#cat .buildkite/jobs/pipeline.ios_demo_app_rn_70.yml
 cat .buildkite/jobs/pipeline.ios.unit-tests.yml
 cat .buildkite/jobs/pipeline.ios.integration-tests.yml
 cat .buildkite/pipeline.post_processing.yml
