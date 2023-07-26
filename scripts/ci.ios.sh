39532a37774e7710693c5bdfaf993d8e26f34e5c
Yaroslav Serhieiev <yaroslavs@wix.com>
2023-07-26 13:54:18 +0300

    ci: disable unhappy tests

--- scripts/ci.ios.sh
+++ scripts/ci.ios.sh
@@ -11,9 +11,6 @@ pushd detox/test
 
 run_f "npm run build:ios"
 run_f "npm run e2e:ios"
-cp coverage/lcov.info ../../coverage/e2e-ios-ci.lcov
-
-run_f "scripts/ci_unhappy.sh ios"
 
 # run_f "npm run verify-artifacts:ios"
 popd
