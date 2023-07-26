baafabada226647e3e5bfc243798488147c473ee
Yaroslav Serhieiev <yaroslavs@wix.com>
2023-07-26 13:23:25 +0300

    ci: disable demo projects

--- scripts/demo-projects.ios.sh
+++ scripts/demo-projects.ios.sh
@@ -1,22 +1,3 @@
 #!/bin/bash -e
 
-UPLOAD_ARTIFACT="$(pwd)/scripts/upload_artifact.sh"
-trap "$UPLOAD_ARTIFACT" EXIT
-
-SCRIPTS_PATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
-
-source $SCRIPTS_PATH/demo-projects.sh
-
-# This must be built first as all other demo apps use this binary.
-pushd examples/demo-react-native
-  pushd ios
-    run_f "pod install"
-  popd
-
-  run_f "npm run build:ios-debug"
-  run_f "npm run test:ios-debug"
-
-  # Run tests with bloated JS bundle:
-  source $SCRIPTS_PATH/demo-rn-bloat-bundle-test.sh ios
-popd
-
+echo "No iOS demo projects yet."
