225c02d407d41302aea2562f6c9b725573e509e3
Asaf Korem <asaf.korem@gmail.com>
2023-07-30 08:21:10 +0300

    wip

--- detox/ios/DetoxTester/DetoxTester/Matchers/ElementMatcher.swift
+++ detox/ios/DetoxTester/DetoxTester/Matchers/ElementMatcher.swift
@@ -39,14 +39,60 @@ class ElementMatcher: ElementMatcherProtocol {
     return result
   }
 
-  func matchWebViews(to pattern: DetoxInvokeHandler.ElementPattern?) throws -> [AnyHashable] {
-    fatalError("unsupported yet")
+  func matchWebViews(to pattern: ElementPattern?) throws -> [AnyHashable] {
+    matcherLog("called to match web-views with pattern: " +
+               "\(pattern != nil ? String(describing: pattern) : "default web-view")")
+
+    let query: XCUIElementQuery = app.newQuery().webViews
+    let result: [XCUIElement]
+
+    if let pattern = pattern {
+      result = try query.matching(
+        pattern: pattern,
+        whiteBoxMessageHandler: whiteBoxMessageHandler,
+        app: app
+      ).run()
+    } else {
+      result = [query.firstMatch]
+    }
+
+    if result.isEmpty {
+      matcherLog("found zero elements", type: .error)
+    } else {
+      matcherLog(
+        "matched elements: " +
+        "\(result.map {"(\($0.identifier), size: \($0.frame.size), origin: \($0.frame.origin))"})"
+      )
+    }
+
+    return result
   }
 
   func matchWebViewElements(
     on webView: AnyHashable,
-    to pattern: DetoxInvokeHandler.WebElementPattern
+    to pattern: WebElementPattern
   ) throws -> [AnyHashable] {
-    fatalError("unsupported yet")
+    matcherLog("called to match web-view elements with pattern: \(String(describing: pattern))")
+
+    guard let webView = webView as? XCUIElement else {
+      fatalError("`webView` must be an XCUIElement")
+    }
+
+    let query = try app.newQuery().webMatching(
+      pattern: pattern,
+      webView: webView
+    )
+    let result = query.run()
+
+    if result.isEmpty {
+      matcherLog("found zero elements", type: .error)
+    } else {
+      matcherLog(
+        "matched elements: " +
+        "\(result.map {"(\($0.identifier), size: \($0.frame.size), origin: \($0.frame.origin))"})"
+      )
+    }
+
+    return result
   }
 }
