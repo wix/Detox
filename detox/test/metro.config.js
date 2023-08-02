c284c5a4e46d25d9879ee1aef9350c50acf2bfac
d4vidi <amit.d4vidi@gmail.com>
2023-08-02 18:52:01 +0300

    Blind upgrade to rn 72 based on upgrade-helper

--- detox/test/metro.config.js
+++ detox/test/metro.config.js
@@ -1,3 +1,4 @@
+// TODO is this still relevant???
 let createBlacklist;
 try {
   // RN .64
@@ -10,7 +11,21 @@ try {
   }
 }
 
+
+const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
+
+/**
+ * Metro configuration
+ * https://facebook.github.io/metro/docs/configuration
+ *
+ * @type {import('metro-config').MetroConfig}
+ */
+const config = {};
+const baseConfig = mergeConfig(getDefaultConfig(__dirname), config);
+
+
 module.exports = {
+  ...baseConfig,
   resolver: {
     blacklistRE: createBlacklist([/detox\/node_modules\/react-native\/.*/]),
   },
