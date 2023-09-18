f37a4e072f88403fbffe0d53c8d7fdeeb05c5c1b
kirillzyusko <zyusko.kirik@gmail.com>
2023-09-18 10:51:50 +0300

    refactor: changes after review

--- detox/index.d.ts
+++ detox/index.d.ts
@@ -624,6 +624,10 @@ declare global {
             get(): object;
         }
 
+        type DigitWithoutZero = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9;
+        type Digit = 0 | DigitWithoutZero;
+        type BatteryLevel = `${Digit}` | `${DigitWithoutZero}${Digit}` | "100";
+
         interface Device {
             /**
              * Holds the environment-unique ID of the device, namely, the adb ID on Android (e.g. emulator-5554) and the Mac-global simulator UDID on iOS -
@@ -808,6 +812,8 @@ declare global {
              *   // If specified must be one of 'notSupported', 'searching', 'failed', or 'active'.
              *   cellularBars: "3",
              *   // If specified must be 0-4.
+             *   operatorName: "A1",
+             *   // Set the cellular operator/carrier name. Use '' for the empty string.
              *   batteryState: "charging",
              *   // If specified must be one of 'charging', 'charged', or 'discharging'.
              *   batteryLevel: "50",
@@ -816,17 +822,14 @@ declare global {
              */
             setStatusBar(config: {
               time?: string,
-              dataNetwork?: "wifi" | '3g'| '4g'| 'lte'| 'lte-a' | "lte+",
+              dataNetwork?: "hide" | "wifi" | "3g" | "4g" | "lte" | "lte-a" | "lte+" | "5g" | "5g+" | "5g-uwb" | "5g-uc",
               wifiMode?: "searching" |"failed" | "active",
               wifiBars?: "0" | "1" | "2" | "3",
               cellularMode?: "notSupported" | "searching" | "failed" | "active",
               cellularBars?: "0" | "1" | "2" | "3" | "4",
+              operatorName?: string;
               batteryState?: "charging" | "charged" | "discharging",
-              /**
-                * In order to generate this type run: 
-                * Array(101).fill(0).map((_, i) => `"${i}"`).join(" | ")
-              */
-              batteryLevel?:  "0" | "1" | "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9" | "10" | "11" | "12" | "13" | "14" | "15" | "16" | "17" | "18" | "19" | "20" | "21" | "22" | "23" | "24" | "25" | "26" | "27" | "28" | "29" | "30" | "31" | "32" | "33" | "34" | "35" | "36" | "37" | "38" | "39" | "40" | "41" | "42" | "43" | "44" | "45" | "46" | "47" | "48" | "49" | "50" | "51" | "52" | "53" | "54" | "55" | "56" | "57" | "58" | "59" | "60" | "61" | "62" | "63" | "64" | "65" | "66" | "67" | "68" | "69" | "70" | "71" | "72" | "73" | "74" | "75" | "76" | "77" | "78" | "79" | "80" | "81" | "82" | "83" | "84" | "85" | "86" | "87" | "88" | "89" | "90" | "91" | "92" | "93" | "94" | "95" | "96" | "97" | "98" | "99" | "100",
+              batteryLevel?: BatteryLevel,
             }): Promise<void>;
 
             /**
