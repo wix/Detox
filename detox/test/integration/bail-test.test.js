316f798c19a18fe4bd48db2c9fb913b52cbf7280
Yaroslav Serhieiev <yaroslavs@wix.com>
2023-09-25 19:44:10 +0300

    test: add more logs

--- detox/test/integration/bail-test.test.js
+++ detox/test/integration/bail-test.test.js
@@ -6,6 +6,7 @@ describe('jest --bail tests', () => {
     `detox test -c stub --config integration/e2e/config.js --bail --maxWorkers 2 --retries 1 flaky passing-simple`,
     `cross-env DETOX_CONFIGURATION=stub jest --config integration/e2e/config.js --bail --runInBand passing-simple`,
   ])('should pass: %s', async (cmd) => {
+    console.log(`Running: ${cmd}`);
     const handle = execa.commandSync(cmd, { stdio: 'inherit', shell: true });
     expect(handle.exitCode).toBe(0);
   });
@@ -14,6 +15,7 @@ describe('jest --bail tests', () => {
     `detox test -c stub --config integration/e2e/config.js --bail --runInBand flaky passing-simple`,
     `cross-env DETOX_CONFIGURATION=stub jest --config integration/e2e/config.js --maxWorkers 4 flaky passing`,
   ])('should fail: %s', async (cmd) => {
+    console.log(`Running: ${cmd}`);
     const handle = _.attempt(() => execa.commandSync(cmd, { stdio: 'inherit', shell: true }));
     expect(handle.exitCode).not.toBe(0);
   });
