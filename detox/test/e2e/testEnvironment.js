e7de7554d1c5c1b024167f9fac0e322018232d18
Asaf Korem <asaf.korem@gmail.com>
2023-10-02 13:00:28 +0300

    to revert

--- detox/test/e2e/testEnvironment.js
+++ detox/test/e2e/testEnvironment.js
@@ -1,7 +1,16 @@
+require('wtfnode').init();
+
 const { DetoxCircusEnvironment } = require('detox/runners/jest');
 const { worker } = require('detox/internals')
 
 class CustomDetoxEnvironment extends DetoxCircusEnvironment {
+  async teardown() {
+    setInterval(() => {
+      require('wtfnode').dump();
+    }, 5000).unref();
+    await super.teardown();
+  }
+
   async setup() {
     await super.setup();
 
