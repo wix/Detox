7ea3728cfdc10e6865c93313ecd5a8253645d9ec
Artem Vasylenko <irbiskronos@gmail.com>
2023-10-13 17:00:27 +0300

    sub-header fix

--- website/src/components/Homepage/SubHeader/HomepageSubHeader.js
+++ website/src/components/Homepage/SubHeader/HomepageSubHeader.js
@@ -11,7 +11,11 @@ const HomepageSubHeader = () => {
       </div>
       <div className={styles.socialNetworks}>
         <a className={styles.discordButton} href="https://discord.gg/CkD5QKheF5" target="_blank"></a>
-        <a className={styles.twitterButton} href="https://x.com/detoxe2e?s=20" target="_blank"></a>
+        <a className={styles.twitterButton} href="https://x.com/detoxe2e?s=20" target="_blank">
+          <span className={styles.twitterLogo}></span>
+          <span className={styles.followText}>Follow </span>
+          <span className={styles.followLink}>@detoxe2e</span>
+        </a>
         <span className={styles.arrow}></span>
         <a className={styles.twitterCounter} href="https://x.com/detoxe2e?s=20" target="_blank">
           <span>486</span>
