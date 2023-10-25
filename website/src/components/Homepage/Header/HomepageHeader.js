fe817257ebede863e9d336d7625a35fd46cbe923
Artem Vasylenko <103461364+IrbisKronos@users.noreply.github.com>
2023-10-25 20:17:06 +0300

    docs(website): home page hero banner styling (#4227)

--- website/src/components/Homepage/Header/HomepageHeader.js
+++ website/src/components/Homepage/Header/HomepageHeader.js
@@ -2,25 +2,32 @@ import React from 'react';
 import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import styles from './HomepageHeader.module.scss';
+import clsx from 'clsx';
 
 const HomepageHeader = () => {
   const { siteConfig } = useDocusaurusContext();
 
   return (
     <header className={styles.heroBanner}>
-      <div className="container">
-        <div className="row">
-          <div className="col">
-            <h1 className={styles.heroBanner.h1}>{siteConfig.title}</h1>
-            <h2 className={styles.heroBanner.h2}>{siteConfig.tagline}</h2>
-            <div className={styles.buttons}>
-              <Link className="button button--secondary button--lg" href="/docs/introduction/getting-started" target="_blank">
-                Getting Started <strong>with Detox</strong>
-              </Link>
+      <div className={styles.contentContainer}>
+        <div className="container">
+          <div className="row">
+            <div className="col">
+              <h1 className={styles.title}>{siteConfig.title}</h1>
+              <h2 className={styles.tagline}>{siteConfig.tagline}</h2>
+              <div className={styles.buttons}>
+                <Link
+                  className={clsx('button button--secondary button--lg', styles.linkText)}
+                  href="/docs/introduction/getting-started"
+                  target="_blank">
+                  Getting Started <strong>with Detox</strong>
+                </Link>
+              </div>
             </div>
           </div>
         </div>
       </div>
+      <div className={styles.backgroundContainer}></div>
     </header>
   );
 };
