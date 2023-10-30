90138e73d32807ac3b32ec2e55c3899f9281b4b5
Gregor Gilchrist <gregorgilchrist@gmail.com>
2023-10-30 17:03:31 +0100

    adding which lib and changing node check

--- detox-doctor/src/rules/global-environmental-rules/global-node-installation/index.ts
+++ detox-doctor/src/rules/global-environmental-rules/global-node-installation/index.ts
@@ -1,20 +1,23 @@
-import { execSync } from 'child_process';
+import which from 'which';
+
 import type { ActionResult, Rule } from '../../../types';
 import { FAILURE, SUCCESS } from '../../../constants';
 import { flattenResult } from '../../../utils';
 
 export class GlobalNodeInstallationRule implements Rule {
-  readonly id = 'DETOX-00?';
+  readonly id = 'DETOX-001';
   readonly alias = 'global-node-installation';
   readonly description = `Checks if Node is installed globally and which version is installed.`;
 
   async check(): Promise<ActionResult> {
-    const nodeVersion = execSync('node -v').toString().replace('\n', '');
-    const nodeLocation = execSync('which node').toString().replace('\n', '');
+    const nodeVersion = process.version;
+    const nodeLocation = await which('node');
+    console.log('LOGGING NODE version', nodeVersion);
+    console.log('LOGGING WHICH NODE', nodeLocation);
 
     return flattenResult({
       status: nodeVersion && nodeLocation ? SUCCESS : FAILURE,
-      successMessage: `Node version ${nodeVersion} globally installed at ${nodeLocation}`,
+      successMessage: `Node ${nodeVersion} globally installed at ${nodeLocation}`,
       failureMessage: [
         `Node not globally installed`,
         `Ensure it is installed via the Node website, a tool such as Homebrew, or an version manager like NVM`,
