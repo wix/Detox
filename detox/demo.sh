7cd274d243ba3536ada79508e734f5a81b12db65
Yaroslav Serhieiev <yaroslavs@wix.com>
2023-11-01 16:07:59 +0200

    debug process hierarchy

--- detox/demo.sh
+++ detox/demo.sh
@@ -1,5 +1,17 @@
 #!/usr/bin/env bash
 
+function print_process_hierarchy {
+  local pid=$1
+  local indent=$2
+  while [ -n "$pid" ] && [ "$pid" -ne 0 ]; do
+    local process_info=$(ps -o pid,ppid,args -p $pid | sed -n '2p')
+    local parent_pid=$(echo $process_info | awk '{print $2}')
+    echo "$indent$process_info"
+    pid=$parent_pid
+    indent="  $indent"
+  done
+}
+
 function handler {
   echo -e "\nDemo stopped with code $1 and signal $2\n"
   exit 0
@@ -10,6 +22,7 @@ trap 'handler $? SIGTERM' SIGTERM
 trap 'handler $? EXIT' EXIT
 
 echo "Demo started $(date)"
+print_process_hierarchy $$
 
 while true; do
   echo "Still running $(date)"
