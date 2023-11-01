8e7af5e57aceed397e5e85a6ef66872fa04bf31b
Artem Vasylenko <103461364+IrbisKronos@users.noreply.github.com>
2023-11-01 19:15:33 +0200

    docs(website): home page styling (#4246)

--- website/src/components/Homepage/Features/HomepageFeatures.js
+++ website/src/components/Homepage/Features/HomepageFeatures.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import styles from './HomepageFeatures.module.scss';
+import clsx from 'clsx';
 import FeatureList from './FeatureList';
 import Features from './Features';
 
 const HomepageFeatures = () => {
   return (
     <section>
-      <div className="container">
+      <div className={clsx('container', styles.benefitsHeader)}>
         <div className="row">
           <div className="col">
-            <h1 className={styles.benefitsHeader}>Detox benefits</h1>
+            <h1 className={styles.benefitsTitle}>Detox benefits</h1>
           </div>
         </div>
         <div className="row">
