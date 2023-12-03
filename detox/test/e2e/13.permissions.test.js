f4a265f045a74d810f9f933dfa6c04f661346103
Asaf Korem <asaf.korem@gmail.com>
2023-12-03 15:44:58 +0200

    test(e2e): refactor permissions tests.

--- detox/test/e2e/13.permissions.test.js
+++ detox/test/e2e/13.permissions.test.js
@@ -1,14 +1,33 @@
+const CALENDAR_AUTHORIZATION_STATUS = 'calendar_authorization_status';
+const NOTIFICATION_AUTHORIZATION_STATUS = 'notification_authorization_status';
+
 describe(':ios: Permissions', () => {
+  describe('Calendar', () => {
+    const calendarAuthorizationStatus = element(by.id(CALENDAR_AUTHORIZATION_STATUS));
 
-  it('Permissions is granted', async () => {
-    await device.launchApp({permissions: {calendar: 'YES'}, newInstance: true});
-    await element(by.text('Permissions')).tap();
-    await expect(element(by.text('granted'))).toBeVisible();
-  });
+    it('should show default permissions', async () => {
+      await device.launchApp({newInstance: true});
+      await element(by.text('Permissions')).tap();
+      await expect(calendarAuthorizationStatus).toHaveText('denied');
+    });
 
-  it('Permissions denied', async () => {
-    await device.launchApp({permissions: {calendar: 'NO'}, newInstance: true});
-    await element(by.text('Permissions')).tap();
-    await expect(element(by.text('denied'))).toBeVisible();
+    it('should show default permissions when unset', async () => {
+      await device.launchApp({permissions: {calendar: 'unset'}, newInstance: true});
+      await element(by.text('Permissions')).tap();
+      await expect(calendarAuthorizationStatus).toHaveText('denied');
+    });
+
+    it('should grant permission', async () => {
+      await device.launchApp({permissions: {calendar: 'YES'}, newInstance: true});
+      await element(by.text('Permissions')).tap();
+      await expect(calendarAuthorizationStatus).toHaveText('granted');
+    });
+
+    it('should deny permissions', async () => {
+      await device.launchApp({permissions: {calendar: 'NO'}, newInstance: true});
+      await element(by.text('Permissions')).tap();
+      await expect(calendarAuthorizationStatus).toHaveText('denied');
+    });
   });
 });
+
--- detox/test/src/Screens/Permissions.js
+++ detox/test/src/Screens/Permissions.js
@@ -7,27 +7,39 @@ import {
 
 const CalendarManager = NativeModules.CalendarManager;
 
+const CALENDAR_AUTH_STATUS_TEST_ID = 'calendar_authorization_status';
+
 export default class Permissions extends Component {
 
   constructor(props) {
     super(props);
-    this.state = {status: "nothing yet"};
+    this.state = {
+      calendarAuthStatus: undefined,
+    };
   }
 
-  async getAuthorizationStatus() {
-    const status = await CalendarManager.getAuthorizationStatus();
+  async getCalendarAuthStatus() {
+    const calendarAuthStatus = await CalendarManager.getAuthorizationStatus();
+
     this.setState({
-      status: status
+      calendarAuthStatus: calendarAuthStatus
     });
   }
 
   render() {
-    const {status} = this.state;
-
+    // Table of permissions (name and value)
     return (
-      <View onLayout={this.getAuthorizationStatus.bind(this)}
+      <View onLayout={this.getCalendarAuthStatus.bind(this)}
             style={{flex: 1, paddingTop: 20, justifyContent: 'center', alignItems: 'center'}}>
-        <Text>{status}</Text>
+        <View style={{flexDirection: 'row'}}>
+          <Text style={{fontSize: 20, marginBottom: 20}}>Permissions</Text>
+        </View>
+
+        <View style={{flexDirection: 'row'}}>
+          <Text style={{marginBottom: 20}}>Calendar: </Text>
+          <Text testID={CALENDAR_AUTH_STATUS_TEST_ID}
+                style={{marginBottom: 20}}>{this.state.calendarAuthStatus}</Text>
+        </View>
       </View>
     );
   }
