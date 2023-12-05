62a5f78554d267d518e8d872f43a9955e7bd4b2e
Asaf Korem <asaf.korem@gmail.com>
2023-12-05 12:27:14 +0200

    test(e2e): add more permissions tests.

--- detox/test/e2e/13.permissions.test.js
+++ detox/test/e2e/13.permissions.test.js
@@ -1,33 +1,197 @@
 const { RESULTS, PERMISSIONS } = require('react-native-permissions');
 
-const CALENDAR_AUTHORIZATION_STATUS = 'calendar_authorization_status';
+const REGULAR_PERMISSIONS_TO_CHECK = [
+  'userTracking',
+  'calendar',
+  'camera',
+  'contacts',
+  'microphone',
+  'reminders',
+  'siri',
+  'speech',
+  // 'bluetooth_peripheral', 'storekit', // Unsupported in Detox
+  // 'faceid','medialibrary', 'motion', // Unavailable, though *supported* in Detox
+];
+
+const LOCATION_ALWAYS = 'location_always';
+const LOCATION_WHEN_IN_USE = 'location_when_in_use';
+const LOCATION_LATITUDE = 'location_latitude';
+const LOCATION_LONGITUDE = 'location_longitude';
+const LOCATION_ERROR = 'location_error';
+
+const PHOTO_LIBRARY = 'photo_library';
+const PHOTO_LIBRARY_ADD_ONLY = 'photo_library_add_only';
 
 describe(':ios: Permissions', () => {
-  describe('Calendar', () => {
-    const calendarAuthorizationStatus = element(by.id(PERMISSIONS.IOS.CALENDARS));
+  REGULAR_PERMISSIONS_TO_CHECK.forEach((name) => {
+    describe(name, () => {
+      const authorizationStatus = element(by.id(name));
+
+      it('should find element with test-id: ' + name, async () => {
+        await device.launchApp({newInstance: true});
+        await element(by.text('Permissions')).tap();
+        await expect(authorizationStatus).toBeVisible();
+      });
+
+      it('should show default permissions when undefined', async () => {
+        await device.launchApp({newInstance: true});
+        await element(by.text('Permissions')).tap();
+        await expect(authorizationStatus).toHaveText(RESULTS.DENIED);
+      });
+
+      it('should show default permissions when defined to `unset`', async () => {
+        const permissions = {[name]: 'unset'};
+        await device.launchApp({permissions, newInstance: true});
+        await element(by.text('Permissions')).tap();
+        await expect(authorizationStatus).toHaveText(RESULTS.DENIED);
+      });
+
+      it('should grant permission', async () => {
+        const permissions = {[name]: 'YES'};
+        await device.launchApp({permissions, newInstance: true});
+        await element(by.text('Permissions')).tap();
+        await expect(authorizationStatus).toHaveText('granted');
+      });
+
+      it('should block permissions', async () => {
+        const permissions = {[name]: 'NO'};
+        await device.launchApp({permissions, newInstance: true});
+        await element(by.text('Permissions')).tap();
+        await expect(authorizationStatus).toHaveText(RESULTS.BLOCKED);
+      });
+    });
+  });
+
+  describe("location", () => {
+    const locationAlways = element(by.id(LOCATION_ALWAYS));
+    const locationInuse = element(by.id(LOCATION_WHEN_IN_USE));
+    const locationLatitude = element(by.id(LOCATION_LATITUDE));
+    const locationLongitude = element(by.id(LOCATION_LONGITUDE));
+    const locationError = element(by.id(LOCATION_ERROR));
+
+    it('should find status elements', async () => {
+      await device.launchApp({newInstance: true});
+      await element(by.text('Permissions')).tap();
+
+      await expect(locationAlways).toBeVisible();
+      await expect(locationInuse).toBeVisible();
+      await expect(locationLatitude).toBeVisible();
+      await expect(locationLongitude).toBeVisible();
+      await expect(locationError).toBeVisible();
+    });
 
     it('should show default permissions when undefined', async () => {
       await device.launchApp({newInstance: true});
       await element(by.text('Permissions')).tap();
-      await expect(calendarAuthorizationStatus).toHaveText(RESULTS.DENIED);
+
+      await expect(locationAlways).toHaveText(RESULTS.DENIED);
+      await expect(locationInuse).toHaveText(RESULTS.DENIED);
     });
 
     it('should show default permissions when defined to `unset`', async () => {
-      await device.launchApp({permissions: {calendar: 'unset'}, newInstance: true});
+      const permissions = {location: 'unset'};
+      await device.launchApp({permissions, newInstance: true});
       await element(by.text('Permissions')).tap();
-      await expect(calendarAuthorizationStatus).toHaveText(RESULTS.DENIED);
+
+      await expect(locationAlways).toHaveText(RESULTS.DENIED);
+      await expect(locationInuse).toHaveText(RESULTS.DENIED);
     });
 
-    it('should grant permission', async () => {
-      await device.launchApp({permissions: {calendar: 'YES'}, newInstance: true});
+    it('should grant permission `inuse`', async () => {
+      const permissions = {location: 'inuse'};
+      await device.launchApp({permissions, newInstance: true});
       await element(by.text('Permissions')).tap();
-      await expect(calendarAuthorizationStatus).toHaveText(RESULTS.GRANTED);
+
+      await expect(locationAlways).toHaveText(RESULTS.DENIED);
+      await expect(locationInuse).toHaveText(RESULTS.GRANTED);
+    });
+
+    it('should grant permission `always`', async () => {
+      const permissions = {location: 'always'};
+      await device.launchApp({permissions, newInstance: true});
+      await element(by.text('Permissions')).tap();
+
+      await expect(locationAlways).toHaveText(RESULTS.GRANTED);
+      await expect(locationInuse).toHaveText(RESULTS.GRANTED);
+    });
+
+    it('should set location when granted', async () => {
+      const permissions = {location: 'always'};
+      await device.launchApp({permissions, newInstance: true});
+      await element(by.text('Permissions')).tap();
+
+      await device.setLocation(37.785834, -122.406417);
+
+      await waitFor(locationError).toHaveText('none').withTimeout(3000);
+
+      await expect(locationLatitude).toHaveText('37.785834');
+      await expect(locationLongitude).toHaveText('-122.406417');
     });
 
     it('should block permissions', async () => {
-      await device.launchApp({permissions: {calendar: 'NO'}, newInstance: true});
+      const permissions = {location: 'never'};
+      await device.launchApp({permissions, newInstance: true});
       await element(by.text('Permissions')).tap();
-      await expect(calendarAuthorizationStatus).toHaveText(RESULTS.BLOCKED);
+
+      await expect(locationAlways).toHaveText(RESULTS.BLOCKED);
+      await expect(locationInuse).toHaveText(RESULTS.BLOCKED);
+    });
+  });
+
+  describe("photos", () => {
+    const photoLibrary = element(by.id(PHOTO_LIBRARY));
+    const photoLibraryAddOnly = element(by.id(PHOTO_LIBRARY_ADD_ONLY));
+
+    it('should find status elements', async () => {
+      await device.launchApp({newInstance: true});
+      await element(by.text('Permissions')).tap();
+
+      await expect(photoLibrary).toBeVisible();
+      await expect(photoLibraryAddOnly).toBeVisible();
+    });
+
+    it('should show default permissions when undefined', async () => {
+      await device.launchApp({newInstance: true});
+      await element(by.text('Permissions')).tap();
+
+      await expect(photoLibrary).toHaveText(RESULTS.DENIED);
+      await expect(photoLibraryAddOnly).toHaveText(RESULTS.DENIED);
+    });
+
+    it('should show default permissions when defined to `unset`', async () => {
+      const permissions = {photos: 'unset'};
+      await device.launchApp({permissions, newInstance: true});
+      await element(by.text('Permissions')).tap();
+
+      await expect(photoLibrary).toHaveText(RESULTS.DENIED);
+      await expect(photoLibraryAddOnly).toHaveText(RESULTS.DENIED);
+    });
+
+    it('should grant permission `limited`', async () => {
+      const permissions = {photos: 'limited'};
+      await device.launchApp({permissions, newInstance: true});
+      await element(by.text('Permissions')).tap();
+
+      await expect(photoLibrary).toHaveText(RESULTS.DENIED);
+      await expect(photoLibraryAddOnly).toHaveText(RESULTS.GRANTED);
+    });
+
+    it('should grant permission', async () => {
+      const permissions = {photos: 'YES'};
+      await device.launchApp({permissions, newInstance: true});
+      await element(by.text('Permissions')).tap();
+
+      await expect(photoLibrary).toHaveText(RESULTS.GRANTED);
+      await expect(photoLibraryAddOnly).toHaveText(RESULTS.GRANTED);
+    });
+
+    it('should block permissions', async () => {
+      const permissions = {photos: 'NO'};
+      await device.launchApp({permissions, newInstance: true});
+      await element(by.text('Permissions')).tap();
+
+      await expect(photoLibrary).toHaveText(RESULTS.BLOCKED);
+      await expect(photoLibraryAddOnly).toHaveText(RESULTS.BLOCKED);
     });
   });
 });
--- detox/test/src/Screens/Permissions.js
+++ detox/test/src/Screens/Permissions.js
@@ -3,40 +3,141 @@ import {
   View,
   Text,
 } from 'react-native';
+import Geolocation from '@react-native-community/geolocation';
 
-import {checkMultiple, PERMISSIONS} from 'react-native-permissions';
+import {checkMultiple, request, PERMISSIONS, RESULTS} from 'react-native-permissions';
+
+const PERMISSIONS_TO_CHECK = [
+  { name: 'userTracking', key: PERMISSIONS.IOS.APP_TRACKING_TRANSPARENCY },
+  { name: 'bluetooth_peripheral', key: PERMISSIONS.IOS.BLUETOOTH_PERIPHERAL },
+  { name: 'calendar', key: PERMISSIONS.IOS.CALENDARS },
+  { name: 'camera', key: PERMISSIONS.IOS.CAMERA },
+  { name: 'contacts', key: PERMISSIONS.IOS.CONTACTS },
+  { name: 'faceid', key: PERMISSIONS.IOS.FACE_ID },
+  { name: 'location_always', key: PERMISSIONS.IOS.LOCATION_ALWAYS },
+  { name: 'location_when_in_use', key: PERMISSIONS.IOS.LOCATION_WHEN_IN_USE },
+  { name: 'medialibrary', key: PERMISSIONS.IOS.MEDIA_LIBRARY },
+  { name: 'microphone', key: PERMISSIONS.IOS.MICROPHONE },
+  { name: 'motion', key: PERMISSIONS.IOS.MOTION },
+  { name: 'photo_library', key: PERMISSIONS.IOS.PHOTO_LIBRARY },
+  { name: 'photo_library_add_only', key: PERMISSIONS.IOS.PHOTO_LIBRARY_ADD_ONLY },
+  { name: 'reminders', key: PERMISSIONS.IOS.REMINDERS },
+  { name: 'siri', key: PERMISSIONS.IOS.SIRI },
+  { name: 'speech', key: PERMISSIONS.IOS.SPEECH_RECOGNITION },
+  { name: 'storekit', key: PERMISSIONS.IOS.STOREKIT },
+];
 
 export default class Permissions extends Component {
-
   constructor(props) {
     super(props);
+
     this.state = {
-      calendarAuthStatus: undefined,
+      statuses: {},
+      location: {
+        latitude: "undefined",
+        longitude: "undefined",
+        error: 'undefined',
+      },
     };
   }
 
-  async getCalendarAuthStatus() {
-    const statuses = await checkMultiple([PERMISSIONS.IOS.CALENDARS]);
-    const calendarAuthStatus = statuses[PERMISSIONS.IOS.CALENDARS];
+  onLocationReceived = (position) => {
+    this.setState({
+      latitude: position.coords.latitude,
+      longitude: position.coords.longitude,
+      error: 'none',
+    });
+  }
+
+  onLocationError = (error) => {
+    this.setState({
+      latitude: "undefined",
+      longitude: "undefined",
+      error: `error: ${error.message}, code: ${error.code}`,
+    });
+  }
+
+  requestLocation = async () => {
+    const options = {
+      enableHighAccuracy: true,
+      timeout: 5000,
+      maximumAge: 0,
+    };
+
+    Geolocation.watchPosition(this.onLocationReceived, this.onLocationError, options);
+  }
+
+  async getStatuses() {
+    const toCheck = PERMISSIONS_TO_CHECK.map((permission) => permission.key);
+    const statuses = await checkMultiple(toCheck);
 
     this.setState({
-      calendarAuthStatus: calendarAuthStatus
+      statuses: statuses,
+    });
+
+    if (statuses[PERMISSIONS.IOS.LOCATION_WHEN_IN_USE] === RESULTS.GRANTED) {
+      await this.requestLocation();
+    }
+  }
+
+  async requestPermission(permissionKey) {
+    const status = await request(permissionKey);
+
+    this.setState({
+      statuses: {
+        ...this.state.statuses,
+        [permissionKey]: status,
+      },
     });
   }
 
   render() {
     // Table of permissions (name and value)
     return (
-      <View onLayout={this.getCalendarAuthStatus.bind(this)}
+      <View onLayout={this.getStatuses.bind(this)}
             style={{flex: 1, paddingTop: 20, justifyContent: 'center', alignItems: 'center'}}>
         <View style={{flexDirection: 'row'}}>
           <Text style={{fontSize: 20, marginBottom: 20}}>Permissions</Text>
         </View>
+        {PERMISSIONS_TO_CHECK.map(({name, key}) => {
+          const status = this.state.statuses[key];
+          const statusColor = status === RESULTS.GRANTED ? 'green' : status === RESULTS.BLOCKED ? 'red' : 'black';
+
+          return (
+            <View style={{flexDirection: 'row'}}>
+              <Text style={{marginBottom: 10, fontSize: 14, marginRight: 20}}>{name}</Text>
+              <Text style={{marginBottom: 10, fontSize: 14, marginRight: 20, color: statusColor}} testID={name}>
+                {status}
+              </Text>
+              <Text
+                style={{marginBottom: 10, fontSize: 14, color: 'blue', textDecorationLine: 'underline'}}
+                onPress={() => this.requestPermission(key)}>
+                ask
+              </Text>
+            </View>
+          );
+        })}
 
         <View style={{flexDirection: 'row'}}>
-          <Text style={{marginBottom: 20}}>Calendar: </Text>
-          <Text testID={PERMISSIONS.IOS.CALENDARS}
-                style={{marginBottom: 20}}>{this.state.calendarAuthStatus}</Text>
+          <Text style={{fontSize: 20, marginTop: 30, marginBottom: 20}}>Location Coordinates</Text>
+        </View>
+        <View style={{flexDirection: 'row'}}>
+          <Text style={{marginBottom: 10, fontSize: 14, marginRight: 20}}>Latitude</Text>
+          <Text style={{marginBottom: 10, fontSize: 14, marginRight: 20}} testID="location_latitude">
+            {this.state.location.latitude}
+          </Text>
+        </View>
+        <View style={{flexDirection: 'row'}}>
+          <Text style={{marginBottom: 10, fontSize: 14, marginRight: 20}}>Longitude</Text>
+          <Text style={{marginBottom: 10, fontSize: 14, marginRight: 20}} testID="location_longitude">
+            {this.state.location.longitude}
+          </Text>
+        </View>
+        <View style={{flexDirection: 'row'}}>
+          <Text style={{marginBottom: 10, fontSize: 14, marginRight: 20}}>Error</Text>
+          <Text style={{marginBottom: 10, fontSize: 14, marginRight: 20}} testID="location_error">
+            {this.state.location.error}
+          </Text>
         </View>
       </View>
     );
