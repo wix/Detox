a630b87fd750fa68367f001976b863a4db08c801
d4vidi <amit.d4vidi@gmail.com>
2023-12-10 16:45:05 +0200

    Re: Supervised android launch (#4293)

--- detox/android/detox/src/full/java/com/wix/detox/adapters/server/DetoxServerInfo.kt
+++ detox/android/detox/src/full/java/com/wix/detox/adapters/server/DetoxServerInfo.kt
@@ -1,7 +1,9 @@
 package com.wix.detox.adapters.server
 
+import android.util.Log
 import androidx.test.platform.app.InstrumentationRegistry
 import com.wix.detox.LaunchArgs
+import com.wix.detox.common.DetoxLog
 
 private const val DEFAULT_URL = "ws://localhost:8099"
 
@@ -9,7 +11,7 @@ class DetoxServerInfo internal constructor(launchArgs: LaunchArgs = LaunchArgs()
     val serverUrl: String = launchArgs.detoxServerUrl ?: DEFAULT_URL
     val sessionId: String = launchArgs.detoxSessionId ?: InstrumentationRegistry.getInstrumentation().targetContext.applicationInfo.packageName
 
-    override fun toString(): String {
-        return "url=$serverUrl, sessionId=$sessionId"
+    init {
+        Log.i(DetoxLog.LOG_TAG, "Detox server connection details: url=$serverUrl, sessionId=$sessionId")
     }
 }
