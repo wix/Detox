16ef1b5284e1842689672906ca3db756b76f8110
d4vidi <amit.d4vidi@gmail.com>
2023-12-18 12:40:40 +0200

    Revert "Re: Supervised android launch (#4293)"
    This reverts commit a630b87fd750fa68367f001976b863a4db08c801.

--- detox/android/detox/src/full/java/com/wix/detox/adapters/server/DetoxServerInfo.kt
+++ detox/android/detox/src/full/java/com/wix/detox/adapters/server/DetoxServerInfo.kt
@@ -1,9 +1,7 @@
 package com.wix.detox.adapters.server
 
-import android.util.Log
 import androidx.test.platform.app.InstrumentationRegistry
 import com.wix.detox.LaunchArgs
-import com.wix.detox.common.DetoxLog
 
 private const val DEFAULT_URL = "ws://localhost:8099"
 
@@ -11,7 +9,7 @@ class DetoxServerInfo internal constructor(launchArgs: LaunchArgs = LaunchArgs()
     val serverUrl: String = launchArgs.detoxServerUrl ?: DEFAULT_URL
     val sessionId: String = launchArgs.detoxSessionId ?: InstrumentationRegistry.getInstrumentation().targetContext.applicationInfo.packageName
 
-    init {
-        Log.i(DetoxLog.LOG_TAG, "Detox server connection details: url=$serverUrl, sessionId=$sessionId")
+    override fun toString(): String {
+        return "url=$serverUrl, sessionId=$sessionId"
     }
 }
