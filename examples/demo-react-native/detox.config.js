6bd60c5a5d8212d46812ff469017cfbabfcc45da
d4vidi <amit.d4vidi@gmail.com>
2023-12-27 16:53:16 +0200

    Revert "Propose debug test APK all-around"
    This reverts commit 6a5f870dd9c072411e510233829e7bbc7760a2d0.

--- examples/demo-react-native/detox.config.js
+++ examples/demo-react-native/detox.config.js
@@ -37,8 +37,7 @@ module.exports = {
     "android.release": {
       "type": "android.apk",
       "binaryPath": "android/app/build/outputs/apk/release/app-release.apk",
-      "testBinaryPath": "android/app/build/outputs/apk/androidTest/debug/app-debug-androidTest.apk",
-      "build": "cd android ; ./gradlew assembleRelease assembleAndroidTest -DtestBuildType=debug ; cd -",
+      "build": "cd android ; ./gradlew assembleRelease assembleAndroidTest -DtestBuildType=release ; cd -",
     }
   },
   devices: {
