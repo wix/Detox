90c6756e90d3700b0525aa701f6f2498296b1be2
Asaf Korem <asaf.korem@gmail.com>
2024-01-03 11:48:04 +0200

    test(e2e): enable media-library test.
    react-native-permissions enabled this feature in https://github.com/zoontek/react-native-permissions/releases/tag/4.0.2.

--- detox/test/e2e/13.permissions.test.js
+++ detox/test/e2e/13.permissions.test.js
@@ -9,7 +9,7 @@ const BASIC_PERMISSIONS_TO_CHECK = [
   'reminders',
   'siri',
   'speech',
-  // 'medialibrary', // todo: Temporary disabled, see https://github.com/zoontek/react-native-permissions/pull/831
+  'medialibrary'
 ];
 
 const LOCATION_ALWAYS = 'location_always';
