eae1cb684db2a5414ba68e911dfd22f230eab74e
Mobile CI <mre@wix.com>
2024-01-08 10:15:34 +0200

    Revert "Merge pull request #4319 from wix/fix/revert-patch"
    This reverts commit e293d15c996dfce3a0b292b440bc0c6f8140eeed, reversing
    changes made to bab6a565856ff3d59f1b63de05ea2b16cc00444d.

--- detox/test/scripts/postinstall.js
+++ detox/test/scripts/postinstall.js
@@ -1,16 +1,42 @@
+const fs = require('fs-extra');
+const path = require('path');
 const cp = require('child_process');
+
+const patchBoostPodspec = () => {
+  const log = message => console.log(`[POST-INSTALL] ${message}`);
+  const boostPodspecPath = `${process.cwd()}/node_modules/react-native/third-party-podspecs/boost.podspec`;
+  const originalUrl = 'https://boostorg.jfrog.io/artifactory/main/release/1.76.0/source/boost_1_76_0.tar.bz2';
+  const patchedUrl = 'https://sourceforge.net/projects/boost/files/boost/1.76.0/boost_1_76_0.tar.bz2';
+
+  if (!fs.existsSync(boostPodspecPath)) {
+    log('boost.podspec does not exist, skipping patch...');
+    return;
+  }
+
+  let boostPodspec = fs.readFileSync(boostPodspecPath, 'utf8');
+
+  if (!boostPodspec.includes(originalUrl)) {
+    log('boost.podspec is already patched or the URL is different, skipping patch...');
+    return;
+  }
+
+  log('Applying boost.podspec patch...');
+  boostPodspec = boostPodspec.replace(originalUrl, patchedUrl);
+  fs.writeFileSync(boostPodspecPath, boostPodspec, 'utf8');
+};
 
 function podInstallIfRequired() {
   if (process.platform === 'darwin' && !process.env.DETOX_DISABLE_POD_INSTALL) {
-    console.log('[POST-INSTALL] Running test-app pod install...');
+    console.log('[POST-INSTALL] Running pod install...');
+    patchBoostPodspec();
 
     cp.execSync('pod install', {
       cwd: `${process.cwd()}/ios`,
       stdio: 'inherit'
     });
-
-    console.log('[POST-INSTALL] test-app pod install completed')
   }
 }
 
+console.log('[POST-INSTALL] Running Detox\'s test-app post-install script...');
 podInstallIfRequired();
+console.log('[POST-INSTALL] Completed!');
