1a6243940951aa707b416de69bf0b8fc9d53fd32
Asaf Korem <asaf.korem@gmail.com>
2024-01-08 16:10:37 +0200

    test: update testing device.
    Due to Xcode 15 upgrade in CI.

--- detox/test/e2e/28.drag-and-drop.test.js
+++ detox/test/e2e/28.drag-and-drop.test.js
@@ -3,7 +3,7 @@ describe(':ios: Drag And Drop', () => {
     await device.reloadReactNative();
     await element(by.text('Drag And Drop')).tap();
   });
-  
+
   afterEach(async () => {
     await element(by.id('closeButton')).tap();
   });
@@ -14,18 +14,20 @@ describe(':ios: Drag And Drop', () => {
     await assertCellText(2, '10');
   });
 
-  it('should drag the second cell and drop before the ten cell position', async () => {
-    await assertCellText(9, '9');
-    await element(by.id('cell2')).longPressAndDrag(1000, 0.9, NaN, element(by.id('cell10')), 0.9, 0.01, 'slow', 0);
+  it('should drag the second cell and drop on the ten cell position', async () => {
+    await assertCellText(2, '2');
     await assertCellText(10, '10');
-    //Because we used 0.001 as the drop Y point, the `cell2` actually landed at cell9, not cell10.
-    await assertCellText(9, '2');
+
+    await element(by.id('cell2')).longPressAndDrag(1000, 0.9, NaN, element(by.id('cell10')), 0.9, 0.01, 'slow', 0);
+
+    await assertCellText(2, '3');
+    await assertCellText(10, '2');
   });
-  
+
   async function assertCellText(idx, value) {
     const attribs = await element(by.id('cellTextLabel')).getAttributes();
     const cellStrings = attribs.elements.map(x => x.text);
-    
+
     if(cellStrings[idx - 1] !== value) {
       throw new Error("Failed!");
     }
--- detox/test/e2e/detox.config.js
+++ detox/test/e2e/detox.config.js
@@ -99,7 +99,7 @@ const config = {
       type: 'ios.simulator',
       headless: Boolean(process.env.CI),
       device: {
-        type: 'iPhone 14 Pro Max',
+        type: 'iPhone 15 Pro Max',
       },
     },
 
