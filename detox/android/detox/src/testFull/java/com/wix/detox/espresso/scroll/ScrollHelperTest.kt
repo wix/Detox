374bd349e1c27a285b404b852e98eed6e34a2d05
Georgy Steshin <georgys@wix.com>
2024-01-09 16:19:56 +0200

    Added more tests

--- detox/android/detox/src/testFull/java/com/wix/detox/espresso/scroll/ScrollHelperTest.kt
+++ detox/android/detox/src/testFull/java/com/wix/detox/espresso/scroll/ScrollHelperTest.kt
@@ -7,8 +7,10 @@ import android.view.WindowInsets
 import androidx.test.espresso.UiController
 import androidx.test.platform.app.InstrumentationRegistry
 import com.wix.detox.action.common.MOTION_DIR_DOWN
+import com.wix.detox.action.common.MOTION_DIR_LEFT
+import com.wix.detox.action.common.MOTION_DIR_RIGHT
+import com.wix.detox.action.common.MOTION_DIR_UP
 import com.wix.detox.espresso.DeviceDisplay
-import com.wix.detox.espresso.scroll.ScrollHelper.getViewSafeScrollableRangePix
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.mockito.kotlin.any
@@ -21,8 +23,12 @@ import org.robolectric.annotation.Config
 import kotlin.test.assertEquals
 
 private const val INSETS_SIZE = 100
+private const val SCROLL_RANGE_SAFE_PERCENT = 0.9f // ScrollHelper.SCROLL_RANGE_SAFE_PERCENT
 
-@Config(qualifiers = "xxxhdpi", sdk = [33])
+@Config(
+    qualifiers = "xxxhdpi", // 1280x1880
+    sdk = [33]
+)
 @RunWith(RobolectricTestRunner::class)
 class ScrollHelperTest {
 
@@ -36,7 +42,7 @@ class ScrollHelperTest {
     private val viewMock = mockViewWithGestureNavigation(displayWidth, displayHeight)
 
     @Test
-    fun `should take gesture navigation into account when scrolling down`() {
+    fun `should scrolling down by 200 when gesture navigation enabled`() {
         val amountInDp = 200.0
         val amountInPx = amountInDp * DeviceDisplay.getDensity()
 
@@ -50,14 +56,80 @@ class ScrollHelperTest {
     }
 
     @Test
+    fun `should scrolling down by 200 when gesture navigation disabled`() {
+        val amountInDp = 200.0
+        val amountInPx = amountInDp * DeviceDisplay.getDensity()
+
+        val viewMock = mockViewWithoutGestureNavigation(displayWidth, displayHeight)
+        ScrollHelper.perform(uiControllerMock, viewMock, MOTION_DIR_DOWN, amountInDp, null, null)
+
+        val upEvent = getUpEvent()
+        // Verify that the scroll started at the center of the view
+        assertEquals(displayWidth / 2.0, upEvent.x.toDouble(), 0.0)
+        // Verify that the scroll ended at the center of the view minus the requested amount
+        assertEquals(displayHeight - amountInPx - touchSlopPx - safetyMarginPx, upEvent.y.toDouble(), 0.0)
+    }
+
+    @Test
     fun `should scroll down to edge on full screen view when gesture navigation enabled`() {
         ScrollHelper.performOnce(uiControllerMock, viewMock, MOTION_DIR_DOWN)
         val upEvent = getUpEvent()
-        val amountInPx = getViewSafeScrollableRangePix(viewMock, MOTION_DIR_DOWN).toFloat()
+        val amountInPx = displayHeight * SCROLL_RANGE_SAFE_PERCENT
+
+        // Calculate where the scroll should end
+        val targetY = displayHeight - amountInPx -
+            touchSlopPx -
+            safetyMarginPx -
+            INSETS_SIZE
 
         assertEquals(displayWidth / 2.0, upEvent.x.toDouble(), 0.0)
-        assertEquals(displayHeight - amountInPx - touchSlopPx - safetyMarginPx - INSETS_SIZE, upEvent.y, 0.0f)
+        assertEquals(targetY, upEvent.y, 0.0f)
+    }
 
+    @Test
+    fun `should scroll left to edge on full screen view when gesture navigation enabled`() {
+        ScrollHelper.performOnce(uiControllerMock, viewMock, MOTION_DIR_LEFT)
+        val upEvent = getUpEvent()
+        val amountInPx = displayWidth * SCROLL_RANGE_SAFE_PERCENT
+
+        // Calculate where the scroll should end
+        val targetX = amountInPx +
+            touchSlopPx +
+            INSETS_SIZE
+
+        assertEquals(targetX, upEvent.x, 0.0f)
+        assertEquals(displayHeight / 2.0, upEvent.y.toDouble(), 0.0)
+    }
+
+    @Test
+    fun `should scroll up to edge on full screen view when gesture navigation enabled`() {
+        ScrollHelper.performOnce(uiControllerMock, viewMock, MOTION_DIR_UP)
+        val upEvent = getUpEvent()
+        val amountInPx = displayHeight * SCROLL_RANGE_SAFE_PERCENT
+
+        // Calculate where the scroll should end
+        val targetY = amountInPx +
+            touchSlopPx +
+            INSETS_SIZE
+
+        assertEquals(displayWidth / 2.0, upEvent.x.toDouble(), 0.0)
+        assertEquals(targetY, upEvent.y, 0.0f)
+    }
+
+    @Test
+    fun `should scroll right to edge on full screen view when gesture navigation enabled`() {
+        ScrollHelper.performOnce(uiControllerMock, viewMock, MOTION_DIR_RIGHT)
+        val upEvent = getUpEvent()
+        val amountInPx = displayWidth * SCROLL_RANGE_SAFE_PERCENT
+
+        // Calculate where the scroll should end
+        val targetX = displayWidth - amountInPx -
+            touchSlopPx -
+            safetyMarginPx -
+            INSETS_SIZE
+
+        assertEquals(targetX, upEvent.x, 0.0f)
+        assertEquals(displayHeight / 2.0, upEvent.y.toDouble(), 0.0)
     }
 
     /**
@@ -73,6 +145,17 @@ class ScrollHelperTest {
         return listOfCapturedEvents.last()
     }
 
+    private fun mockViewWithoutGestureNavigation(displayWidth: Int, displayHeight: Int): View {
+        // This is how we disable gesture navigation
+        val windowInsets = mock<WindowInsets>() {
+            whenever(it.systemGestureInsets).thenReturn(
+                Insets.of(0, 0, 0, 0)
+            )
+        }
+
+        return mockView(displayWidth, displayHeight, windowInsets)
+    }
+
     /**
      * Mock a view with gesture navigation enabled
      */
@@ -84,10 +167,19 @@ class ScrollHelperTest {
             )
         }
 
+        return mockView(displayWidth, displayHeight, windowInsets)
+    }
+
+    private fun mockView(
+        displayWidth: Int,
+        displayHeight: Int,
+        windowInsets: WindowInsets
+    ): View {
         val view = mock<View>() {
             whenever(it.width).thenReturn(displayWidth)
             whenever(it.height).thenReturn(displayHeight)
             whenever(it.canScrollVertically(any())).thenReturn(true) // We allow endless scroll
+            whenever(it.canScrollHorizontally(any())).thenReturn(true) // We allow endless scroll
             whenever(it.context).thenReturn(InstrumentationRegistry.getInstrumentation().targetContext)
             whenever(it.rootWindowInsets).thenReturn(windowInsets)
         }
