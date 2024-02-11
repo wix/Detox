3417c257fc415f3dc2a6ab79296028077b1e6a36
Georgy Steshin <georgys@wix.com>
2024-02-11 21:07:46 +0200

    Fixed broken build

--- detox/scripts/updateGradle.js
+++ detox/scripts/updateGradle.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 
-const rnMinor = require('../test/e2e/utils/rn-consts/rn-consts').rnVersion.minor;
+const rnMinor = require('../src/utils/rn-consts/rn-consts').rnVersion.minor;
 
 function getGradleVersionByRNVersion() {
   switch (rnMinor) {
