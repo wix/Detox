9ae6c7349178e8c9db21f2fb279944aec50deeb6
Georgy Steshin <georgys@wix.com>
2024-02-19 16:42:22 +0200

    wip

--- detox/test/e2e/28.drag-and-drop.test.js
+++ detox/test/e2e/28.drag-and-drop.test.js
@@ -1,35 +1,28 @@
-describe(':ios: Drag And Drop', () => {
+describe('Drag And Drop', () => {
   beforeEach(async () => {
     await device.reloadReactNative();
     await element(by.text('Drag And Drop')).tap();
   });
 
   afterEach(async () => {
-    await element(by.id('closeButton')).tap();
+    //await element(by.id('closeButton')).tap();
   });
 
-  it('should drag the ten cell and drop "on" the second cell position', async () => {
-    await assertCellText(2, '2');
-    await element(by.id('cell10')).longPressAndDrag(1000, 0.9, NaN, element(by.id('cell2')), 0.9, NaN, 'fast', 0);
-    await assertCellText(2, '10');
+
+  it('should drag pan to left of the title', async () => {
+    console.log('attributes: ', await element(by.id('draggable')).getAttributes());
+    await element(by.id('draggable')).longPressAndDrag(
+      1000,
+      0.5,
+      0.5,
+      element(by.id('DragAndDropTitle')),
+      0,
+      0,
+      'fast',
+      0);
+
+
+    await new Promise(r => setTimeout(r, 2000));
   });
 
-  it('should drag the second cell and drop on the ten cell position', async () => {
-    await assertCellText(2, '2');
-    await assertCellText(10, '10');
-
-    await element(by.id('cell2')).longPressAndDrag(1000, 0.9, NaN, element(by.id('cell10')), 0.9, 0.01, 'slow', 0);
-
-    await assertCellText(2, '3');
-    await assertCellText(10, '2');
-  });
-
-  async function assertCellText(idx, value) {
-    const attribs = await element(by.id('cellTextLabel')).getAttributes();
-    const cellStrings = attribs.elements.map(x => x.text);
-
-    if(cellStrings[idx - 1] !== value) {
-      throw new Error("Failed!");
-    }
-  }
 });
--- detox/test/src/Screens/DragNDropScreen.tsx
+++ detox/test/src/Screens/DragNDropScreen.tsx
@@ -1,12 +1,48 @@
-import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import React, { Component, useRef } from 'react';
+import { Animated, PanResponder, SafeAreaView, StatusBar, Text, View } from 'react-native';
+
+
+const DragNDropComponent = () => {
+    const pan = useRef(new Animated.ValueXY()).current;
+
+    const panResponder = useRef(
+        PanResponder.create({
+            onMoveShouldSetPanResponder: () => true,
+            onPanResponderMove: Animated.event([null, { dx: pan.x, dy: pan.y }], { useNativeDriver: false }),
+            onPanResponderRelease: () => {
+                pan.extractOffset();
+            }
+        })
+    ).current;
+
+
+    return (
+        <Animated.View
+            style={{
+                transform: [{ translateX: pan.x }, { translateY: pan.y }]
+            }}
+            {...panResponder.panHandlers}>
+            <View testID="draggable" style={{ height: 30, width: 30, backgroundColor: 'blue', borderRadius: 16 }} />
+        </Animated.View>
+    );
+};
 
 export default class DragNDropScreen extends Component {
     render() {
         return (
-            <View style={{ flex: 1, paddingTop: 20, justifyContent: 'center', alignItems: 'center' }}>
-                <Text>DragNDropScreen</Text>
-            </View>
+            <SafeAreaView style={{flex: 1}}>
+                <View style={{
+                    flex: 1,
+                    marginTop: 32,
+                    paddingHorizontal: 24
+                }}>
+                    <Text testID="DragAndDropTitle">DragNDropScreen</Text>
+
+                    <View style={{ flex: 1, justifyContent: 'flex-end', backgroundColor: 'orange' }}>
+                        <DragNDropComponent />
+                    </View>
+                </View>
+            </SafeAreaView>
         );
     }
 }
