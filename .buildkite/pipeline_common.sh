575a90b8b006fcdc41968a0c92768c8b928eb1a1
Georgy Steshin <georgys@wix.com>
2024-02-20 20:10:40 +0200

    Fixed build

--- .buildkite/pipeline_common.sh
+++ .buildkite/pipeline_common.sh
@@ -9,5 +9,4 @@ cat .buildkite/jobs/pipeline.ios_demo_app_rn_71.yml
 cat .buildkite/jobs/pipeline.android_rn_73.yml
 cat .buildkite/jobs/pipeline.android_rn_71.yml
 cat .buildkite/jobs/pipeline.android_demo_app_rn_73.yml
-cat .buildkite/jobs/pipeline.android_demo_app_rn_71.yml
 cat .buildkite/pipeline.post_processing.yml
