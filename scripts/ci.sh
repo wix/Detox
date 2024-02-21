3088f738976078e724375361ade4eff92b0ddb35
Georgy Steshin <georgys@wix.com>
2024-02-21 11:11:37 +0200

    Update all react native versions

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -3,11 +3,7 @@
 source $(dirname "$0")/logger.sh
 source $(dirname "$0")/install.sh
 
-if [ ! -z ${REACT_NATIVE_VERSION} ]; then
-  node scripts/change_react_native_version.js "examples/demo-react-native" ${REACT_NATIVE_VERSION} "dependencies"
-  node scripts/change_react_native_version.js "detox/test" ${REACT_NATIVE_VERSION} "dependencies"
-  node scripts/change_react_native_version.js "detox" ${REACT_NATIVE_VERSION} "devDependencies"
-fi
+source $(dirname "$0")/change_all_react_native_versions.sh
 
 run_f "lerna bootstrap --no-ci"
 run_f "lerna run build"
