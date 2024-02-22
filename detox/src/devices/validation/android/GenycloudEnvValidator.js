609d21cd62876b660f9fb8862bfb90610b77d074
d4vidi <amit.d4vidi@gmail.com>
2024-02-22 15:37:47 +0200

    genycloud: Omit preliminary login check

--- detox/src/devices/validation/android/GenycloudEnvValidator.js
+++ detox/src/devices/validation/android/GenycloudEnvValidator.js
@@ -12,15 +12,13 @@ class GenycloudEnvValidator extends EnvironmentValidatorBase {
    * @param authService { GenyAuthService }
    * @param exec { GenyCloudExec }
    */
-  constructor({ authService, exec }) {
+  constructor({ exec }) {
     super();
-    this._authService = authService;
     this._exec = exec;
   }
 
   async validate() {
     await this._validateGmsaasVersion();
-    await this._validateGmsaasAuth();
   }
 
   async _validateGmsaasVersion() {
@@ -29,15 +27,6 @@ class GenycloudEnvValidator extends EnvironmentValidatorBase {
       throw new DetoxRuntimeError({
         message: `Your Genymotion-Cloud executable (found in ${environment.getGmsaasPath()}) is too old! (version ${version})`,
         hint: `Detox requires version 1.6.0, or newer. To use 'android.genycloud' type devices, you must upgrade it, first.`,
-      });
-    }
-  }
-
-  async _validateGmsaasAuth() {
-    if (!await this._authService.getLoginEmail()) {
-      throw new DetoxRuntimeError({
-        message: `Cannot run tests using 'android.genycloud' type devices, because Genymotion was not logged-in to!`,
-        hint: `Log-in to Genymotion-cloud by running this command (and following instructions):\n${environment.getGmsaasPath()} auth login --help`,
       });
     }
   }
