0721463c30399671d6e3b64f74b82bec87d44b72
Asaf Korem <asaf.korem@gmail.com>
2024-03-02 17:43:59 +0200

    iOS(WebJSCodeBuilder): initial commit.

---
+++ detox/ios/Detox/Invocation/WebJSCodeBuilder.swift
@@ -0,0 +1,35 @@
+//
+//  WebJSCodeBuilder.swift (Detox)
+//  Created by Asaf Korem (Wix.com) on 2024.
+//
+
+import Foundation
+
+class WebJSCodeBuilder {
+	private var predicate: WebPredicate?
+	private var expectation: WebExpectationType?
+	private var params: [String]?
+	private var modifiers: [WebModifier]?
+
+	func with(predicate: WebPredicate) -> WebJSCodeBuilder {
+		self.predicate = predicate
+
+		return self
+	}
+
+	func with(
+		expectation: WebExpectationType,
+		params: [String]?,
+		modifiers: [WebModifier]?
+	) -> WebJSCodeBuilder {
+		self.expectation = expectation
+		self.params = params
+		self.modifiers = modifiers
+
+		return self
+	}
+
+	func build() -> String {
+		return "document.body.style.backgroundColor = `red`; return true;"
+	}
+}
