7da57a5de0206ff2832119e36febaf5a0147019c
Asaf Korem <asaf.korem@gmail.com>
2024-03-11 09:13:23 +0200

    test(e2e): use SSIM score to compare snapshots.

--- detox/test/e2e/utils/snapshot.js
+++ detox/test/e2e/utils/snapshot.js
@@ -1,29 +1,55 @@
 const fs = require('fs-extra');
+const { ssim } = require('ssim.js');
+const { PNG } = require('pngjs');
 
-const expectElementSnapshotToMatch = async function (element, snapshotName) {
-    const bitmapPath = await element.takeScreenshot(snapshotName);
+// Threshold for SSIM comparison, if two images have SSIM score less then it, th
+const SSIM_SCORE_THRESHOLD = 0.997;
+
+async function expectElementSnapshotToMatch (elementOrDevice, snapshotName) {
+    const bitmapPath = await elementOrDevice.takeScreenshot(snapshotName);
     const expectedBitmapPath = `./e2e/assets/${snapshotName}.${device.getPlatform()}.png`;
+
     if (await fs.pathExists(expectedBitmapPath) === false || process.env.UPDATE_SNAPSHOTS === 'true') {
         await fs.copy(bitmapPath, expectedBitmapPath, {overwrite: true});
+    } else {
+        await expectSSIMToBeClose(bitmapPath, expectedBitmapPath);
     }
+};
 
-    expectBitmapsToBeEqual(bitmapPath, expectedBitmapPath);
-}
-
-const expectDeviceSnapshotToMatch = async function (snapshotName) {
+async function expectDeviceSnapshotToMatch (snapshotName) {
     // Set status bar to consistent state for snapshot. Currently, doesn't work on iOS 17.
     await device.setStatusBar({time: '2024-03-08T09:41:00-07:00'});
+
     await expectElementSnapshotToMatch(device, snapshotName);
+};
+
+async function expectSSIMToBeClose (bitmapPath, expectedBitmapPath) {
+    const image = loadImage(bitmapPath);
+    const expectedImage = loadImage(expectedBitmapPath);
+
+    const { mssim, performance } = ssim(image, expectedImage);
+
+    if (mssim < SSIM_SCORE_THRESHOLD) {
+        throw new Error(
+            `Expected bitmaps at '${bitmapPath}' and '${expectedBitmapPath}' to have an SSIM score ` +
+            `of at least ${SSIM_SCORE_THRESHOLD}, but got ${mssim}. This means the snapshots are different ` +
+            `(comparison took ${performance}ms)`
+        )
+    }
 }
 
-function expectBitmapsToBeEqual(bitmapPath, expectedBitmapPath) {
-    const bitmapBuffer = fs.readFileSync(bitmapPath);
-    const expectedBitmapBuffer = fs.readFileSync(expectedBitmapPath);
+function loadImage (path) {
+    const imageBuffer = fs.readFileSync(path);
+    const image = PNG.sync.read(imageBuffer);
+    return convertToSSIMFormat(image);
+}
 
-    if (!bitmapBuffer.equals(expectedBitmapBuffer)) {
-        throw new Error(
-            `Expected bitmap at ${bitmapPath} to be equal to ${expectedBitmapPath}, but it is different!`);
-    }
+function convertToSSIMFormat (image) {
+    return {
+        data: new Uint8Array(image.data),
+        width: image.width,
+        height: image.height,
+    };
 }
 
 module.exports = {
