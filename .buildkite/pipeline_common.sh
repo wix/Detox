1221e15a85dd0090a7e4feb6f31e070bfcb292af
Igor Gnatyuk <igorgn@wix.com>
2024-04-09 16:16:55 +0300

    java_home 17

--- .buildkite/pipeline_common.sh
+++ .buildkite/pipeline_common.sh
@@ -2,10 +2,10 @@
 
 echo "steps:"
 
-cat .buildkite/jobs/pipeline.ios_rn_73.yml
-cat .buildkite/jobs/pipeline.ios_rn_71.yml
-cat .buildkite/jobs/pipeline.ios_demo_app_rn_73.yml
-cat .buildkite/jobs/pipeline.ios_demo_app_rn_71.yml
+# cat .buildkite/jobs/pipeline.ios_rn_73.yml
+# cat .buildkite/jobs/pipeline.ios_rn_71.yml
+# cat .buildkite/jobs/pipeline.ios_demo_app_rn_73.yml
+# cat .buildkite/jobs/pipeline.ios_demo_app_rn_71.yml
 cat .buildkite/jobs/pipeline.android_rn_73.yml
 cat .buildkite/jobs/pipeline.android_rn_71.yml
 cat .buildkite/jobs/pipeline.android_demo_app_rn_73.yml
