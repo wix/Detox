21c335ef3827496d3996922eaeefe1f8764cc365
Rolando-Barbella <rolandobarbella@gmail.com>
2024-04-15 15:09:52 +0700

    Simplify solution added

--- detox/android/detox/src/full/java/com/wix/detox/reactnative/ReactMarkersLogger.kt
+++ detox/android/detox/src/full/java/com/wix/detox/reactnative/ReactMarkersLogger.kt
@@ -7,44 +7,40 @@ import com.facebook.react.bridge.ReactMarkerConstants.*
 
 object ReactMarkersLogger : ReactMarker.MarkerListener {
 
-    private var isLoggerEnabled = false
-    
     fun attach() {
         if (ReactNativeInfo.rnVersion().minor >= 71) {
-            isLoggerEnabled = true
-        } else {
             ReactMarker.addListener(this)
         }
     }
 
     override fun logMarker(marker: ReactMarkerConstants, p1: String?, p2: Int) {
-        if (isLoggerEnabled) {
-            when (marker) {
-                DOWNLOAD_START,
-                DOWNLOAD_END,
-                BUILD_REACT_INSTANCE_MANAGER_START,
-                BUILD_REACT_INSTANCE_MANAGER_END,
-                REACT_BRIDGE_LOADING_START,
-                REACT_BRIDGE_LOADING_END,
-                REACT_BRIDGELESS_LOADING_START,
-                REACT_BRIDGELESS_LOADING_END,
-                CREATE_MODULE_START,
-                CREATE_MODULE_END,
-                NATIVE_MODULE_SETUP_START,
-                NATIVE_MODULE_SETUP_END,
-                PRE_RUN_JS_BUNDLE_START,
-                RUN_JS_BUNDLE_START,
-                RUN_JS_BUNDLE_END,
-                CONTENT_APPEARED,
-                CREATE_CATALYST_INSTANCE_START,
-                CREATE_CATALYST_INSTANCE_END,
-                DESTROY_CATALYST_INSTANCE_START,
-                DESTROY_CATALYST_INSTANCE_END,
-                CREATE_REACT_CONTEXT_START,
-                CREATE_REACT_CONTEXT_END,
-                PROCESS_PACKAGES_START,
-                PROCESS_PACKAGES_END -> Log.d("Detox.RNMarker", "$marker ($p1)")
-            }
+        when {
+            marker == DOWNLOAD_START ||
+            marker == DOWNLOAD_END ||
+            marker == BUILD_REACT_INSTANCE_MANAGER_START ||
+            marker == BUILD_REACT_INSTANCE_MANAGER_END ||
+            marker == REACT_BRIDGE_LOADING_START ||
+            marker == REACT_BRIDGE_LOADING_END ||
+            marker == REACT_BRIDGELESS_LOADING_START ||
+            marker == REACT_BRIDGELESS_LOADING_END ||
+            marker == CREATE_MODULE_START ||
+            marker == CREATE_MODULE_END ||
+            marker == NATIVE_MODULE_SETUP_START ||
+            marker == NATIVE_MODULE_SETUP_END ||
+            marker == PRE_RUN_JS_BUNDLE_START ||
+            marker == RUN_JS_BUNDLE_START ||
+            marker == RUN_JS_BUNDLE_END ||
+            marker == CONTENT_APPEARED ||
+            marker == CREATE_CATALYST_INSTANCE_START ||
+            marker == CREATE_CATALYST_INSTANCE_END ||
+            marker == DESTROY_CATALYST_INSTANCE_START ||
+            marker == DESTROY_CATALYST_INSTANCE_END ||
+            marker == CREATE_REACT_CONTEXT_START ||
+            marker == CREATE_REACT_CONTEXT_END ||
+            marker == PROCESS_PACKAGES_START ||
+            marker == PROCESS_PACKAGES_END ||
+                false ->
+                Log.d("Detox.RNMarker", "$marker ($p1)")
         }
     }
 }
