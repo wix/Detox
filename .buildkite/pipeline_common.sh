2d072fcfa150b46fca493546bf466c88e76d77d2
Igor Gnatyuk <igorgn@wix.com>
2024-04-15 11:20:12 +0300

    java 17 global

--- .buildkite/pipeline_common.sh
+++ .buildkite/pipeline_common.sh
@@ -1,11 +1,13 @@
 #!/bin/bash -e
 
+echo "env:"
+echo "  JAVA_HOME: /opt/openjdk/openlogic-openjdk-17.0.9+9-mac-x64/jdk-17.0.9.jdk/Contents/Home"
 echo "steps:"
 
-cat .buildkite/jobs/pipeline.ios_rn_73.yml
-cat .buildkite/jobs/pipeline.ios_rn_71.yml
-cat .buildkite/jobs/pipeline.ios_demo_app_rn_73.yml
-cat .buildkite/jobs/pipeline.ios_demo_app_rn_71.yml
+# cat .buildkite/jobs/pipeline.ios_rn_73.yml
+# cat .buildkite/jobs/pipeline.ios_rn_71.yml
+# cat .buildkite/jobs/pipeline.ios_demo_app_rn_73.yml
+# cat .buildkite/jobs/pipeline.ios_demo_app_rn_71.yml
 cat .buildkite/jobs/pipeline.android_rn_73.yml
 cat .buildkite/jobs/pipeline.android_rn_71.yml
 cat .buildkite/jobs/pipeline.android_demo_app_rn_73.yml
