009a99eebf905e2ebe028fb7b562930cef889eb7
Asaf Korem <asaf.korem@gmail.com>
2024-04-24 10:05:06 +0300

    test(e2e): make element accessible by adding top-padding.
    Since title is being hidden by the "Active" / "Busy" mark.

--- detox/test/src/app.js
+++ detox/test/src/app.js
@@ -64,7 +64,7 @@ export default class example extends Component {
 
   renderMainMenu() {
     return (
-      <View style={{flex: 1, paddingTop: 10, justifyContent: 'center', alignItems: 'center'}}>
+      <View style={{flex: 1, paddingTop: 30, justifyContent: 'center', alignItems: 'center'}}>
         <Text style={{fontSize: 18, marginBottom: 10}}>
           Choose a test
         </Text>
