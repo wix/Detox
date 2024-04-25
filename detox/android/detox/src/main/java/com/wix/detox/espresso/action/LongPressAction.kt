292ea496ad7230a36e8d24bac4d9e5d72f87d489
Asaf Korem <asaf.korem@gmail.com>
2024-04-25 16:14:19 +0300

    fix(android): fix LongPressAction.

--- detox/android/detox/src/main/java/com/wix/detox/espresso/action/LongPressAction.kt
+++ detox/android/detox/src/main/java/com/wix/detox/espresso/action/LongPressAction.kt
@@ -2,6 +2,8 @@ package com.wix.detox.espresso.action
 
 import android.graphics.Point
 import android.view.View
+import android.os.SystemClock
+import android.view.MotionEvent
 import androidx.test.espresso.UiController
 import androidx.test.espresso.ViewAction
 import androidx.test.espresso.matcher.ViewMatchers
@@ -10,8 +12,8 @@ import kotlin.math.ceil
 
 class LongPressAction(
     private val duration: Int,
-    private val x: Int?,
-    private val y: Int?
+    private val x: Int,
+    private val y: Int
 ) : ViewAction {
 
     override fun getDescription(): String {
@@ -19,24 +21,54 @@ class LongPressAction(
     }
 
     override fun getConstraints(): Matcher<View> {
-        return ViewMatchers.isAssignableFrom(View::class.java)
+        return allOf(isAssignableFrom(View.class), isDisplayed());
     }
 
     override fun perform(uiController: UiController, view: View) {
-        var tapPoint: Point? = null
+        val sourceViewPoint = IntArray(2)
+        view.getLocationOnScreen(sourceViewPoint)
 
-        if (x != null && y != null) {
-            val sourceViewPoint = IntArray(2)
-            view.getLocationOnScreen(sourceViewPoint)
-            tapPoint = Point(sourceViewPoint[0] + x, sourceViewPoint[1] + y)
+        val tapPoint = Point(sourceViewPoint[0] + x, sourceViewPoint[1] + y)
+
+        val downTime = SystemClock.uptimeMillis()
+        val eventList = mutableListOf<MotionEvent>()
+
+        eventList.add(
+            MotionEvent.obtain(
+                downTime,
+                downTime,
+                MotionEvent.ACTION_DOWN,
+                x,
+                y,
+                0
+            )
+        )
+
+        if (duration > 0) {
+            eventList.add(
+                MotionEvent.obtain(
+                    downTime,
+                    downTime + duration,
+                    MotionEvent.ACTION_MOVE,
+                    x,
+                    y,
+                    0
+                )
+            )
         }
 
-        if (tapPoint != null) {
-            uiController.injectMotionEventDown(tapPoint)
-            uiController.loopMainThreadForAtLeast(duration.toLong())
-            uiController.injectMotionEventUp(tapPoint)
-        } else {
-            uiController.longClick(view)
-        }
+        eventList.add(
+            MotionEvent.obtain(
+                downTime,
+                downTime + duration,
+                MotionEvent.ACTION_UP,
+                x,
+                y,
+                0
+            )
+        )
+
+       uiController.injectMotionEventSequence(eventList)
+       eventList.forEach { it.recycle() }
     }
 }
