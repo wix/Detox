52cefa9432288676256b4b3e5dc8463bf1e14d93
Asaf Korem <asaf.korem@gmail.com>
2024-05-13 14:18:06 +0300

    chore(ios): remove dead code.

--- detox/ios/DetoxXCUITestRunner/DetoxXCUITestRunner/Extensions/XCUIApplication+Extensions.swift
+++ detox/ios/DetoxXCUITestRunner/DetoxXCUITestRunner/Extensions/XCUIApplication+Extensions.swift
@@ -10,8 +10,4 @@ extension XCUIApplication {
   static var springboard: XCUIApplication {
     return XCUIApplication(bundleIdentifier: "com.apple.springboard")
   }
-
-  static var safari: XCUIApplication {
-    return XCUIApplication(bundleIdentifier: "com.apple.mobilesafari")
-  }
 }
