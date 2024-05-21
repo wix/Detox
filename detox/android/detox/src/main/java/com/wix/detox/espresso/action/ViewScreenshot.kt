499290b510cd69c9a3990c37b581359d843ecc38
Maurice D??pke <mauricedoepke@outlook.de>
2024-05-21 16:33:40 +0200

    manually render texture views

--- detox/android/detox/src/main/java/com/wix/detox/espresso/action/ViewScreenshot.kt
+++ detox/android/detox/src/main/java/com/wix/detox/espresso/action/ViewScreenshot.kt
@@ -3,7 +3,9 @@ package com.wix.detox.espresso.action
 import android.graphics.Bitmap
 import android.graphics.Canvas
 import android.util.Base64
+import android.view.TextureView
 import android.view.View
+import android.view.ViewGroup
 import java.io.ByteArrayOutputStream
 
 class ScreenshotResult(private val bitmap: Bitmap) {
@@ -18,9 +20,36 @@ class ScreenshotResult(private val bitmap: Bitmap) {
 }
 
 class ViewScreenshot() {
+    /**
+     * Texture views do not support to drar themselves. To still capture them for a screenshot
+     * we have to traverse and render them manually. Some libraries like react-native-skia use them.
+     */
+    fun drawTextureViews(view: View, canvas: Canvas) {
+        if (view is TextureView) {
+            val viewBitmap = Bitmap.createBitmap(view.width, view.height, Bitmap.Config.ARGB_8888)
+            view.getBitmap(viewBitmap)
+            canvas.drawBitmap(viewBitmap, 0f, 0f, null)
+        } else if (view is ViewGroup) {
+            for (i in 0..(view.getChildCount() - 1)) {
+                val childContainerPos = view.getChildDrawingOrder(i)
+                val childView = view.getChildAt(childContainerPos)
+
+                val left = childView.left.toFloat()
+                val top = childView.top.toFloat()
+
+                canvas.translate(left, top);
+                this.drawTextureViews(childView, canvas)
+                canvas.translate(-left, -top);
+            }
+        }
+    }
+
     fun takeOf(view: View): ScreenshotResult {
         val bitmap = Bitmap.createBitmap(view.width, view.height, Bitmap.Config.ARGB_8888)
-        view.draw(Canvas(bitmap))
+        val canvas = Canvas(bitmap)
+
+        view.draw(canvas)
+        this.drawTextureViews(view, canvas)
 
         return ScreenshotResult(bitmap)
     }
