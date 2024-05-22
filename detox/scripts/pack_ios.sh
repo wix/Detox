bad0ea0b655c98fb70b1378daa6e00508de68b42
Asaf Korem <asaf.korem@gmail.com>
2024-05-22 15:24:04 +0300

    ci(fix): fix iOS version pack script.

--- detox/scripts/pack_ios.sh
+++ detox/scripts/pack_ios.sh
@@ -8,25 +8,25 @@ rm -rf build_temp
 find ./ios -name Build -type d -exec rm -rf {} \;
 
 # Package sources
-pushd . &> /dev/null
 cd ios
 tar --exclude-from=.tbzignore -cjf ../Detox-ios-src.tbz .
-popd &> /dev/null
+cd ..
+
+echo "Packaging iOS sources and prebuilt frameworks"
+
+# Create temp build directory
+mkdir build_temp
 
 # Package prebuilt framework
-mkdir build_temp
-scripts/build_framework.ios.sh "ios/Detox.xcodeproj" "build_temp" &> build_temp/detox_ios.log
-pushd . &> /dev/null
-cd build_temp
-tar --exclude-from=../ios/.tbzignore -cjf ../Detox-ios-framework.tbz .
-popd &> /dev/null
+scripts/build_framework.ios.sh "ios/Detox.xcodeproj" "build_temp"
 
-# Package prebuilt xcuitest runner
-scripts/build_xcuitest.ios.sh "ios/DetoxXCUITestRunner/DetoxXCUITestRunner.xcworkspace" "build_temp" &> build_temp/detox_ios_xcuitest.log
-pushd . &> /dev/null
+# Package prebuilt XCUITest runner
+scripts/build_xcuitest.ios.sh "ios/DetoxXCUITestRunner/DetoxXCUITestRunner.xcodeproj" "build_temp"
+
 cd build_temp
 tar --exclude-from=../ios/.tbzignore -cjf ../Detox-ios-xcuitest.tbz .
-popd &> /dev/null
+tar --exclude-from=../ios/.tbzignore -cjf ../Detox-ios-framework.tbz .
+cd ..
 
 # Cleanup
 rm -fr build_temp
