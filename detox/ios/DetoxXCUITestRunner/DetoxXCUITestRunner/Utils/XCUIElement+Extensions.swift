230b06007ea8743699aa0a86c9d01d1c9c0d5252
Asaf Korem <asaf.korem@gmail.com>
2024-06-10 07:18:19 +0300

    feat: implement XCUITest typing actions.

---
+++ detox/ios/DetoxXCUITestRunner/DetoxXCUITestRunner/Utils/XCUIElement+Extensions.swift
@@ -0,0 +1,32 @@
+//
+//  XCUIElement+Keyboard.swift (DetoxXCUITestRunner)
+//  Created by Asaf Korem (Wix.com) on 2024.
+//
+
+import Foundation
+import XCTest
+
+extension XCUIElement {
+  public func replaceText(_ text: String) {
+    clearText()
+    typeTextOnEnd(text)
+  }
+
+  public func typeTextOnEnd(_ text: String) {
+    let end = coordinate(withNormalizedOffset: CGVector(dx: 1, dy: 0))
+    end.tap()
+
+    typeText(text)
+  }
+
+  public func clearText() {
+    tap()
+
+    // Select all text by tapping and holding, then dragging to select all
+    let start = coordinate(withNormalizedOffset: CGVector(dx: 0, dy: 0))
+    let end = coordinate(withNormalizedOffset: CGVector(dx: 1, dy: 0))
+    start.press(forDuration: 1.5, thenDragTo: end)
+
+    typeText(XCUIKeyboardKey.delete.rawValue)
+  }
+}
