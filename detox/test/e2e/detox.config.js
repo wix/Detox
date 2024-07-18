64a750d685c88b814a14b9fa9e961f40a547f729
Georgy Steshin <georgys@wix.com>
2024-07-18 17:22:10 +0300

    Changed detached

--- detox/test/e2e/detox.config.js
+++ detox/test/e2e/detox.config.js
@@ -16,7 +16,7 @@ const config = {
       forceExit: process.env.CI ? true : undefined,
       _: ['e2e/'],
     },
-    detached: !!process.env.CI,
+    detached: false,
     retries: process.env.CI ? 1 : undefined,
     jest: {
       setupTimeout: +`${process.env.DETOX_JEST_SETUP_TIMEOUT || 300000}`,
