e935f10a742b18b3512708901028ed3cdbd8f09a
Asaf Korem <asaf.korem@gmail.com>
2024-09-02 17:28:19 +0300

    feat(copilot): introduce `CopilotFacade` type.

--- detox-copilot/src/index.ts
+++ detox-copilot/src/index.ts
@@ -1,22 +1,29 @@
 import { Copilot } from "@/Copilot";
 
-export const copilot = {
+export type CopilotFacade = {
+    init: (config: Config) => void;
+    reset: () => void;
+    act: (action: string) => Promise<any>;
+    assert: (assertion: string) => Promise<any>;
+};
+
+export const copilot: CopilotFacade = {
     init: (config: Config) => {
         Copilot.init(config);
     },
     reset: () => {
         Copilot.getInstance().reset();
     },
-    act: async (action: string) => {
-        return await Copilot.getInstance().perform({
+    act: (action: string) => {
+        return Copilot.getInstance().perform({
             type: 'action',
             value: action
         });
     },
-    assert: async (assertion: string) => {
-        return await Copilot.getInstance().perform({
+    assert: (assertion: string) => {
+        return Copilot.getInstance().perform({
             type: 'assertion',
             value: assertion
         });
     }
-}
+};
