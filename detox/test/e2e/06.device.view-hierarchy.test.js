3345ec1875a3f5dddd7072d10799bcde4fd73bd4
Asaf Korem <asaf.korem@gmail.com>
2024-09-22 00:26:27 +0300

    test(e2e): update view-hierarchy snapshots.

--- detox/test/e2e/06.device.view-hierarchy.test.js
+++ detox/test/e2e/06.device.view-hierarchy.test.js
@@ -7,15 +7,16 @@ describe('generate view hierarchy', () => {
 
   beforeEach(async () => {
     await device.reloadReactNative();
-    await element(by.text('Actions')).tap();
   });
 
   it('generateViewHierarchyXml() - should generate a valid view hierarchy XML without injected test-ids', async () => {
+    await element(by.text('Actions')).tap();
     const hierarchy = await device.generateViewHierarchyXml();
     await expectViewHierarchySnapshotToMatch(hierarchy, `view-hierarchy-without-test-id-injection`);
   });
 
   it('generateViewHierarchyXml(true) - should generate a valid view hierarchy XML with injected test-ids', async () => {
+    await element(by.text('Actions')).tap();
     const hierarchy = await device.generateViewHierarchyXml(true);
     await expectViewHierarchySnapshotToMatch(hierarchy, `view-hierarchy-with-test-id-injection`);
   });
