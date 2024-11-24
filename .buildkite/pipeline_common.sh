8d5f7062a3f96cf1e236bf2e081a07c73b1351f9
Igor Gnatyuk <igorgn@wix.com>
2024-11-24 16:33:27 +0200

    debug

--- .buildkite/pipeline_common.sh
+++ .buildkite/pipeline_common.sh
@@ -1,7 +1,8 @@
 #!/bin/bash -e
 
 echo "steps:"
-
+echo "agents:"
+echo "  queue: 'mac-m2-mobile-debug'"
 cat .buildkite/jobs/pipeline.ios_rn_73.yml
 cat .buildkite/jobs/pipeline.ios_rn_71.yml
 cat .buildkite/jobs/pipeline.ios_demo_app_rn_73.yml
