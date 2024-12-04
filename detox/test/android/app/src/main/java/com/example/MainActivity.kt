140ba1bd9487d2a9ccbcf470a4426f2a9a9be496
Georgy Steshin <gosha212@gmail.com>
2024-12-04 14:07:21 +0200

    Update detox/test/android/app/src/main/java/com/example/MainActivity.kt
    Co-authored-by: d4vidi <amit.d4vidi@gmail.com>

--- detox/test/android/app/src/main/java/com/example/MainActivity.kt
+++ detox/test/android/app/src/main/java/com/example/MainActivity.kt
@@ -16,9 +16,7 @@ open class MainActivity : ReactActivity() {
      * Returns the name of the main component registered from JavaScript.
      * This is used to schedule rendering of the component.
      */
-    override fun getMainComponentName(): String {
-        return "example"
-    }
+    override fun getMainComponentName() = "example"
 
     /**
      * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
