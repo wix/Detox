f89f72ab1b10c6d85cf6df24e7f8cdf87a8d3b45
Georgy Steshin <georgys@wix.com>
2024-12-05 14:12:03 +0200

    Fixed build for react native demo app

--- examples/demo-react-native/android/app/src/main/java/com/example/MainApplication.java
+++ examples/demo-react-native/android/app/src/main/java/com/example/MainApplication.java
@@ -2,19 +2,24 @@ package com.example;
 
 import android.app.Application;
 
+import androidx.annotation.Nullable;
+
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
+import com.facebook.react.defaults.DefaultReactNativeHost;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
 import com.reactnativecommunity.asyncstorage.AsyncStoragePackage;
 
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
+import com.facebook.react.soloader.OpenSourceMergedSoMapping;
 
 public class MainApplication extends Application implements ReactApplication {
-    private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
+    private final ReactNativeHost mReactNativeHost = new DefaultReactNativeHost(this) {
         @Override
         public boolean getUseDeveloperSupport() {
             return BuildConfig.DEBUG;
@@ -23,6 +28,22 @@ public class MainApplication extends Application implements ReactApplication {
         @Override
         protected List<ReactPackage> getPackages() {
             return new PackageList(this).getPackages();
+        }
+
+        @Override
+        protected String getJSMainModuleName() {
+            return "index";
+        }
+
+        @Override
+        protected boolean isNewArchEnabled() {
+            return BuildConfig.IS_NEW_ARCHITECTURE_ENABLED;
+        }
+
+        @Nullable
+        @Override
+        protected Boolean isHermesEnabled() {
+            return BuildConfig.IS_HERMES_ENABLED;
         }
     };
 
@@ -34,7 +55,11 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     public void onCreate() {
         super.onCreate();
-        SoLoader.init(this, /* native exopackage */ false);
+        try {
+            SoLoader.init(this, OpenSourceMergedSoMapping.INSTANCE);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
 }
