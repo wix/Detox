bc3d50b6f720210a6cbe356a09d70ff78ae59402
Georgy Steshin <georgys@wix.com>
2024-12-10 12:06:55 +0200

    Removed old build kite jobs

--- .buildkite/pipeline_common.sh
+++ .buildkite/pipeline_common.sh
@@ -6,8 +6,6 @@ cat .buildkite/jobs/pipeline.ios_rn_76.yml
 cat .buildkite/jobs/pipeline.ios_rn_75.yml
 cat .buildkite/jobs/pipeline.ios_rn_73.yml
 cat .buildkite/jobs/pipeline.ios_demo_app_rn_76.yml
-cat .buildkite/jobs/pipeline.ios_demo_app_rn_75.yml
-cat .buildkite/jobs/pipeline.ios_demo_app_rn_73.yml
 cat .buildkite/jobs/pipeline.android_rn_76.yml
 cat .buildkite/jobs/pipeline.android_rn_75.yml
 cat .buildkite/jobs/pipeline.android_rn_73.yml
