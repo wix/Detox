488c203ea7dada24b3df6bc677bcfb097ced4b99
Asaf Korem <asaf.korem@gmail.com>
2024-12-17 00:34:41 +0200

    feat(ios): support new-arch's `RCTParagraphComponentView`.

--- detox/ios/Detox/Invocation/Predicate.swift
+++ detox/ios/Detox/Invocation/Predicate.swift
@@ -66,7 +66,9 @@ class Predicate : CustomStringConvertible, CustomDebugStringConvertible {
           return ValuePredicate(kind: kind, modifiers: modifiers, value: label, requiresAccessibilityElement: true, isRegex: isRegex)
         } else {
           //Will crash if RN app and neither class exists
-          let RCTTextViewClass : AnyClass = NSClassFromString("RCTText") ?? NSClassFromString("RCTTextView")!
+          let RCTTextViewClass : AnyClass =
+            NSClassFromString("RCTParagraphComponentView") ??
+            NSClassFromString("RCTText") ?? NSClassFromString("RCTTextView")!
 
           let descendantPredicate = DescendantPredicate(predicate: AndCompoundPredicate(predicates: [
             try KindOfPredicate(kind: Kind.type, modifiers: [], className: NSStringFromClass(RCTTextViewClass)),
@@ -89,7 +91,9 @@ class Predicate : CustomStringConvertible, CustomDebugStringConvertible {
 
         if ReactNativeSupport.isReactNativeApp == true {
           //Will crash if RN app and neither class exists
-          let RCTTextViewClass : AnyClass = NSClassFromString("RCTText") ?? NSClassFromString("RCTTextView")!
+          let RCTTextViewClass : AnyClass =
+            NSClassFromString("RCTParagraphComponentView") ??
+            NSClassFromString("RCTText") ?? NSClassFromString("RCTTextView")!
           orPredicates.append(try KindOfPredicate(kind: Kind.type, modifiers: [], className: NSStringFromClass(RCTTextViewClass)))
         }
 
--- detox/ios/Detox/Utilities/NSObject+DontCrash.m
+++ detox/ios/Detox/Utilities/NSObject+DontCrash.m
@@ -7,36 +7,81 @@
 
 #import "NSObject+DontCrash.h"
 
+@interface NSObject (DontCrashPrivate)
++ (Class)dtx_classForName:(NSString *)className;
+- (nullable NSString *)dtx_extractTextFromRCTComponent;
+@end
+
 @implementation NSObject (DontCrash)
 
-- (id)_dtx_text
-{
-	if([self respondsToSelector:@selector(text)])
-	{
-		return [(UITextView*)self text];
-	}
-	
-	static Class RCTTextView;
-	static dispatch_once_t onceToken;
-	dispatch_once(&onceToken, ^{
-		RCTTextView = NSClassFromString(@"RCTTextView");
-	});
-	if(RCTTextView != nil && [self isKindOfClass:RCTTextView])
-	{
-		return [(NSTextStorage*)[self valueForKey:@"textStorage"] string];
-	}
-	
-	return nil;
+#pragma mark - Public Methods
+
+- (id)_dtx_text {
+    if ([self respondsToSelector:@selector(text)]) {
+        return [(UITextView *)self text];
+    }
+
+    NSString *reactText = [self dtx_extractTextFromRCTComponent];
+    if (reactText) {
+        return reactText;
+    }
+
+    return nil;
 }
 
-- (id)_dtx_placeholder
-{
-	if([self respondsToSelector:@selector(placeholder)])
-	{
-		return [(UITextField*)self placeholder];
-	}
-	
-	return nil;
+- (id)_dtx_placeholder {
+    if ([self respondsToSelector:@selector(placeholder)]) {
+        return [(UITextField *)self placeholder];
+    }
+    return nil;
+}
+
+@end
+
+@implementation NSObject (DontCrashPrivate)
+
+#pragma mark - Private Methods
+
++ (Class)dtx_classForName:(NSString *)className {
+    static NSMutableDictionary<NSString *, Class> *classCache;
+    static dispatch_once_t onceToken;
+
+    dispatch_once(&onceToken, ^{
+        classCache = [NSMutableDictionary dictionary];
+    });
+
+    Class cachedClass = classCache[className];
+    if (!cachedClass) {
+        cachedClass = NSClassFromString(className);
+        if (cachedClass) {
+            classCache[className] = cachedClass;
+        }
+    }
+
+    return cachedClass;
+}
+
+- (nullable NSString *)dtx_extractTextFromRCTComponent {
+    static Class RCTTextViewClass;
+    static Class RCTParagraphComponentViewClass;
+    static dispatch_once_t onceToken;
+
+    dispatch_once(&onceToken, ^{
+        RCTTextViewClass = [self.class dtx_classForName:@"RCTTextView"];
+        RCTParagraphComponentViewClass = [self.class dtx_classForName:@"RCTParagraphComponentView"];
+    });
+
+    if (RCTTextViewClass && [self isKindOfClass:RCTTextViewClass]) {
+        NSTextStorage *textStorage = [self valueForKey:@"textStorage"];
+        return [textStorage string];
+    }
+
+    if (RCTParagraphComponentViewClass && [self isKindOfClass:RCTParagraphComponentViewClass]) {
+        NSAttributedString *attributedText = [self valueForKey:@"attributedText"];
+        return [attributedText string];
+    }
+
+    return nil;
 }
 
 @end
