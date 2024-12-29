af2a963ef15459ed31ccb7fbf218abd970bab698
Georgy Steshin <georgys@wix.com>
2024-12-29 09:58:41 +0200

    Fixed timers idling resources reflection

--- detox/android/detox/src/full/java/com/wix/detox/reactnative/idlingresources/timers/TimersIdlingResource.kt
+++ detox/android/detox/src/full/java/com/wix/detox/reactnative/idlingresources/timers/TimersIdlingResource.kt
@@ -7,7 +7,7 @@ import com.facebook.react.bridge.ReactContext
 import com.facebook.react.modules.core.TimingModule
 import com.wix.detox.reactnative.idlingresources.DetoxIdlingResource
 
-const val BUSY_WINDOW_THRESHOLD: kotlin.Long = 1500L
+const val BUSY_WINDOW_THRESHOLD: Long = 1500L
 
 class TimersIdlingResource @JvmOverloads constructor(
     reactContext: ReactContext,
@@ -43,6 +43,6 @@ class TimersIdlingResource @JvmOverloads constructor(
     }
 
     companion object {
-        internal const val LOG_TAG = "TimersIdlingResource"
+        private const val LOG_TAG = "TimersIdlingResource"
     }
 }
