2b4df68ed3b1a1cb28f54a1bb0377fe0bbe42ca0
d4vidi <amit.d4vidi@gmail.com>
2024-12-29 19:23:19 +0200

    Starting using 'gmsaas doctor' command (#4676)

--- detox/src/devices/validation/android/GenycloudEnvValidator.js
+++ detox/src/devices/validation/android/GenycloudEnvValidator.js
@@ -6,6 +6,7 @@ const environment = require('../../../utils/environment');
 const EnvironmentValidatorBase = require('../EnvironmentValidatorBase');
 
 const MIN_GMSAAS_VERSION = '1.6.0';
+const MIN_GMSAAS_VERSION_WITH_DOCTOR = '1.11.0';
 
 class GenycloudEnvValidator extends EnvironmentValidatorBase {
   /**
@@ -18,15 +19,31 @@ class GenycloudEnvValidator extends EnvironmentValidatorBase {
   }
 
   async validate() {
-    await this._validateGmsaasVersion();
+    const { version } = await this._exec.getVersion();
+
+    await this._validateGmsaasVersion(version);
+    await this._validateGmsaasDoctorCheck(version);
   }
 
-  async _validateGmsaasVersion() {
-    const { version } = await this._exec.getVersion();
+  async _validateGmsaasVersion(version) {
     if (semver.lt(version, MIN_GMSAAS_VERSION)) {
       throw new DetoxRuntimeError({
         message: `Your Genymotion-Cloud executable (found in ${environment.getGmsaasPath()}) is too old! (version ${version})`,
         hint: `Detox requires version 1.6.0, or newer. To use 'android.genycloud' type devices, you must upgrade it, first.`,
+      });
+    }
+  }
+
+  async _validateGmsaasDoctorCheck(version) {
+    if (semver.lt(version, MIN_GMSAAS_VERSION_WITH_DOCTOR)) {
+      return;
+    }
+
+    try {
+      await this._exec.doctor();
+    } catch (e) {
+      throw new DetoxRuntimeError({
+        message: e.message,
       });
     }
   }
