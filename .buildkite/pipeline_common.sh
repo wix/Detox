30b0dacdf9bc21fae19d2fbbfe0e04f1c8faa69c
Asaf Korem <asaf.korem@gmail.com>
2025-01-06 15:50:46 +0200

    ci: remove common & redundant steps from pipelines.

--- .buildkite/pipeline_common.sh
+++ .buildkite/pipeline_common.sh
@@ -4,10 +4,8 @@ echo "steps:"
 
 cat .buildkite/jobs/pipeline.ios_rn_76_new_arch.yml
 cat .buildkite/jobs/pipeline.ios_rn_76.yml
-cat .buildkite/jobs/pipeline.ios_rn_75.yml
 cat .buildkite/jobs/pipeline.ios_rn_73.yml
 cat .buildkite/jobs/pipeline.ios_demo_app_rn_76_new_arch.yml
-cat .buildkite/jobs/pipeline.ios_demo_app_rn_76.yml
 cat .buildkite/jobs/pipeline.android_rn_76.yml
 cat .buildkite/jobs/pipeline.android_rn_75.yml
 cat .buildkite/jobs/pipeline.android_rn_73.yml
