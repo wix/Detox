ec91497f8d4f14d8fa79042445f7da3b32e4a394
Asaf Korem <asaf.korem@gmail.com>
2025-01-14 23:54:28 +0200

    test(ios): fix overlay messages layout

--- detox/test/ios/AppDelegate.m
+++ detox/test/ios/AppDelegate.m
@@ -13,81 +13,81 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-  self.moduleName = @"example";
-  self.initialProps = @{};
+    self.moduleName = @"example";
+    self.initialProps = @{};
 
-  BOOL result = [super application:application didFinishLaunchingWithOptions:launchOptions];
+    BOOL result = [super application:application didFinishLaunchingWithOptions:launchOptions];
 
-  [self setupNotifications];
-  [self setupScreenManager];
-  [self setupApplicationStateObservers];
-  [UIViewController swizzleMotionEnded];
+    [self setupNotifications];
+    [self setupScreenManager];
+    [self setupApplicationStateObservers];
+    [UIViewController swizzleMotionEnded];
 
-  return result;
+    return result;
 }
 
 - (NSURL *)sourceURLForBridge:(RCTBridge *)bridge
 {
-  return [self bundleURL];
+    return [self bundleURL];
 }
 
 - (NSURL *)bundleURL
 {
 #if DEBUG
-  return [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index"];
+    return [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index"];
 #else
-  return [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
+    return [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
 #endif
 }
 
 #pragma mark - Setup Methods
 
 - (void)setupNotifications {
-  UNUserNotificationCenter.currentNotificationCenter.delegate = self;
+    UNUserNotificationCenter.currentNotificationCenter.delegate = self;
 
-  [[NSNotificationCenter defaultCenter] addObserver:self
-                                         selector:@selector(handleChangeScreen:)
-                                             name:@"ChangeScreen"
-                                           object:nil];
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(handleChangeScreen:)
+                                                 name:@"ChangeScreen"
+                                               object:nil];
 }
 
 - (void)setupScreenManager {
-  self.screenManager = [[NativeScreenManager alloc] initWithWindow:self.window];
+    self.screenManager = [[NativeScreenManager alloc] initWithWindow:self.window];
 }
 
 - (void)setupApplicationStateObservers {
-  [[NSNotificationCenter defaultCenter] addObserver:self
-                                         selector:@selector(applicationDidBecomeActive:)
-                                             name:UIApplicationDidBecomeActiveNotification
-                                           object:nil];
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(applicationDidBecomeActive:)
+                                                 name:UIApplicationDidBecomeActiveNotification
+                                               object:nil];
 
-  [[NSNotificationCenter defaultCenter] addObserver:self
-                                         selector:@selector(applicationWillResignActive:)
-                                             name:UIApplicationWillResignActiveNotification
-                                           object:nil];
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(applicationWillResignActive:)
+                                                 name:UIApplicationWillResignActiveNotification
+                                               object:nil];
 
-  [[NSNotificationCenter defaultCenter] addObserver:self
-                                         selector:@selector(applicationDidEnterBackground:)
-                                             name:UIApplicationDidEnterBackgroundNotification
-                                           object:nil];
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(applicationDidEnterBackground:)
+                                                 name:UIApplicationDidEnterBackgroundNotification
+                                               object:nil];
 }
 
 #pragma mark - Notification Handlers
 
 - (void)handleChangeScreen:(NSNotification *)notification {
-  [self.screenManager handle:notification];
+    [self.screenManager handle:notification];
 }
 
 - (void)applicationDidBecomeActive:(NSNotification *)notification {
-  [self showOverlayMessageWithMessage:@"Active"];
+    [self showOverlayMessageWithMessage:@"Active"];
 }
 
 - (void)applicationWillResignActive:(NSNotification *)notification {
-  [self showOverlayMessageWithMessage:@"Inactive"];
+    [self showOverlayMessageWithMessage:@"Inactive"];
 }
 
 - (void)applicationDidEnterBackground:(NSNotification *)notification {
-  [self showOverlayMessageWithMessage:@"Background"];
+    [self showOverlayMessageWithMessage:@"Background"];
 }
 
 #pragma mark - UNUserNotificationCenterDelegate
@@ -95,17 +95,18 @@
 - (void)userNotificationCenter:(UNUserNotificationCenter *)center
        willPresentNotification:(UNNotification *)notification
          withCompletionHandler:(void (^)(UNNotificationPresentationOptions))completionHandler {
-  completionHandler(UNNotificationPresentationOptionList | UNNotificationPresentationOptionBanner |
-                   UNNotificationPresentationOptionBadge | UNNotificationPresentationOptionSound);
+    [self showOverlayMessageWithMessage:notification.request.content.title];
 
-  [self showOverlayMessageWithMessage:notification.request.content.title];
+    completionHandler(UNNotificationPresentationOptionList | UNNotificationPresentationOptionBanner |
+                      UNNotificationPresentationOptionBadge | UNNotificationPresentationOptionSound);
 }
 
 - (void)userNotificationCenter:(UNUserNotificationCenter *)center
 didReceiveNotificationResponse:(UNNotificationResponse *)response
          withCompletionHandler:(void(^)(void))completionHandler {
-  [self showOverlayMessageWithMessage:response.notification.request.content.title];
-  completionHandler();
+    [self showOverlayMessageWithMessage:response.notification.request.content.title];
+
+    completionHandler();
 }
 
 #pragma mark - Deep Linking
@@ -113,12 +114,12 @@ didReceiveNotificationResponse:(UNNotificationResponse *)response
 - (BOOL)application:(UIApplication *)app
             openURL:(NSURL *)url
             options:(NSDictionary<UIApplicationOpenURLOptionsKey,id> *)options {
-  return [RCTLinkingManager application:app openURL:url options:options];
+    return [RCTLinkingManager application:app openURL:url options:options];
 }
 
 - (BOOL)application:(UIApplication *)application
-    continueUserActivity:(NSUserActivity *)userActivity
-    restorationHandler:(void (^)(NSArray<id<UIUserActivityRestoring>> * _Nullable))restorationHandler {
+continueUserActivity:(NSUserActivity *)userActivity
+ restorationHandler:(void (^)(NSArray<id<UIUserActivityRestoring>> * _Nullable))restorationHandler {
     if ([userActivity.activityType isEqualToString:CSSearchableItemActionType]) {
         NSString *identifier = userActivity.userInfo[CSSearchableItemActivityIdentifier];
         NSURL *url = identifier ? [NSURL URLWithString:identifier] : nil;
