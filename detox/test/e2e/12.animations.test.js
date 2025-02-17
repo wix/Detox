c5be558c6f788f2b1eb8bb7a49f913bd1b16d8ce
Asaf Korem <asaf.korem@gmail.com>
2025-02-17 12:02:52 +0200

    test(ios): enable native-animation test.

--- detox/test/e2e/12.animations.test.js
+++ detox/test/e2e/12.animations.test.js
@@ -51,8 +51,8 @@ describe('React-Native Animations', () => {
       await expect(element(by.id('UniqueId_AnimationsScreen_afterAnimationText'))).not.toExist();
     });
 
-    // todo: investigate test failure on new-arch.
-    it(`:ios: @legacy should wait during delays shorter than 1.5s`, async () => {
+    // todo: JS-animation is failing on new architecture. See https://github.com/wix/Detox/issues/4706
+    it(`:ios: ${driver === 'JS' ? '@legacy' : ''} should wait during delays shorter than 1.5s`, async () => {
       await _startTest(driver, { delay: 500 });
       await expect(element(by.id('UniqueId_AnimationsScreen_afterAnimationText'))).toExist();
     });
