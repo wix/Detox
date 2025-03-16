0bfe99682f550752e6e54faef62a2cd1a34900d1
Asaf Korem <asaf.korem@gmail.com>
2025-03-16 07:48:37 +0200

    build: make pilot an optional dependency.
    typo
    
    remove usused types
    
    add copilot peer dep
    
    pilot facade

--- detox/src/pilot/DetoxPilot.js
+++ detox/src/pilot/DetoxPilot.js
@@ -1,25 +1,36 @@
-const { Pilot } = require('@wix-pilot/core');
-const { DetoxFrameworkDriver }  = require('@wix-pilot/detox');
-
 const detox = require('../..');
 
 /** @type {Detox.PilotFacade} */
 class DetoxPilot {
   init(promptHandler) {
-     this.pilot = new Pilot({
+    const { Pilot } = this.requireOrThrow('@wix-pilot/core', 'Pilot');
+    const { DetoxFrameworkDriver } = this.requireOrThrow('@wix-pilot/detox', 'DetoxFrameworkDriver');
+
+    this.pilot = new Pilot({
       frameworkDriver: new DetoxFrameworkDriver(detox),
       promptHandler: promptHandler
     });
   }
 
-  start(){
+  requireOrThrow(packageName, exportName) {
+    try {
+      return require(packageName);
+    } catch (error) {
+      throw new Error(
+        `Failed to load ${exportName} from ${packageName}. ` +
+        `Please install ${packageName} as it's a peer dependency: npm install ${packageName}`
+      );
+    }
+  }
+
+  start() {
     if (!this.isInitialized()) {
       throw new Error('DetoxPilot is not initialized');
     }
     return this.pilot.start();
   }
 
-  perform(...steps){
+  perform(...steps) {
     if (!this.isInitialized()) {
       throw new Error('DetoxPilot is not initialized');
     }
@@ -27,21 +38,27 @@ class DetoxPilot {
   }
 
   autopilot(goal) {
+    if (!this.isInitialized()) {
+      throw new Error('DetoxPilot is not initialized');
+    }
     return this.pilot.autopilot(goal);
   }
 
   extendAPICatalog(categories, context) {
+    if (!this.isInitialized()) {
+      throw new Error('DetoxPilot is not initialized');
+    }
     return this.pilot.extendAPICatalog(categories, context);
   }
 
-  end(shouldSaveInCache){
-    if(!this.isInitialized()){
+  end(shouldSaveInCache) {
+    if (!this.isInitialized()) {
       throw new Error('DetoxPilot is not initialized');
     }
     return this.pilot.end(shouldSaveInCache);
   }
 
-  isInitialized(){
+  isInitialized() {
     return !!this.pilot;
   }
 }
