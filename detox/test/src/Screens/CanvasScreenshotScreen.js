40f6744fb8571accfa2ed0f3737a8092e2308a2f
Maurice D??pke <mauricedoepke@outlook.de>
2025-04-10 00:48:31 +0800

    use Current Element canvas as template

--- detox/test/src/Screens/CanvasScreenshotScreen.js
+++ detox/test/src/Screens/CanvasScreenshotScreen.js
@@ -1,6 +1,6 @@
-import { Canvas, Rect } from "@shopify/react-native-skia";
 import React, { Component } from 'react';
 import { View, Dimensions, TouchableHighlight } from 'react-native';
+import { Canvas, Rect } from '@shopify/react-native-skia';
 
 const screenWidth = Dimensions.get('window').width;
 const screenHeight = Dimensions.get('window').height;
@@ -8,26 +8,26 @@ const screenHeight = Dimensions.get('window').height;
 class ArtisticRectangle extends Component {
   static defaultProps = {
     borderSizeV: 12,
-    borderSizeH: 12,
-  }
+    borderSizeH: 12
+  };
 
+  // there are two skia canvas on purpose to see if the screenshot mechanism 
+  // can handle multiple on different levels
   render() {
     const paddingHorizontal = this.props.borderSizeH;
     const paddingVertical = this.props.borderSizeV;
     return (
-      <View testID={this.props.testID}>
-        <View style={{paddingHorizontal, paddingVertical, backgroundColor: 'cyan'}}>
-          <View style={{paddingHorizontal, paddingVertical, backgroundColor: 'magenta'}}>
-            <View style={{paddingHorizontal, paddingVertical, backgroundColor: 'yellow'}}>              
-              <Canvas
-                style={{ paddingHorizontal, paddingVertical }}
-                onLayout={(e) => {
-                  //setSize({ width: e.nativeEvent.layout.width, height: e.nativeEvent.layout.height });
-                }}>
+      <View testID={this.props.testID} accessible={true}>
+        <View style={{ paddingHorizontal, paddingVertical, backgroundColor: 'cyan' }}>
+          <View style={{ paddingHorizontal, paddingVertical, backgroundColor: 'magenta' }}>
+            <View style={{ paddingHorizontal, paddingVertical, backgroundColor: 'yellow' }}>
+              <Canvas style={{ paddingHorizontal, paddingVertical }}>
                 <Rect x={0} y={0} width={10} height={10} color={'black'} />
               </Canvas>
-
             </View>
+            <Canvas style={{ paddingHorizontal, paddingVertical }}>
+              <Rect x={10} y={10} width={10} height={10} color={'black'} />
+            </Canvas>
           </View>
         </View>
       </View>
@@ -37,13 +37,13 @@ class ArtisticRectangle extends Component {
 
 export default class CanvasScreenshotScreen extends Component {
   static orientations = {
-    'vertical': {
+    vertical: {
       borderSizeH: 12,
-      borderSizeV: screenHeight * 0.9 / 8,
+      borderSizeV: (screenHeight * 0.9) / 8
     },
-    'horizontal': {
-      borderSizeH: screenWidth * 0.9 / 8,
-      borderSizeV: 12,
+    horizontal: {
+      borderSizeH: (screenWidth * 0.9) / 8,
+      borderSizeV: 12
     }
   };
 
@@ -52,7 +52,7 @@ export default class CanvasScreenshotScreen extends Component {
     this.switchOrientation = this.switchOrientation.bind(this);
 
     this.state = {
-      orientation: 'vertical',
+      orientation: 'vertical'
     };
   }
 
@@ -60,9 +60,9 @@ export default class CanvasScreenshotScreen extends Component {
     const { borderSizeH, borderSizeV } = CanvasScreenshotScreen.orientations[this.state.orientation];
 
     return (
-      <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-        <TouchableHighlight testID='switchOrientation' onPress={this.switchOrientation}>
-          <ArtisticRectangle testID='fancyElement' borderSizeH={borderSizeH} borderSizeV={borderSizeV} />
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <TouchableHighlight testID="switchOrientation" onPress={this.switchOrientation}>
+          <ArtisticRectangle testID="fancyElement" borderSizeH={borderSizeH} borderSizeV={borderSizeV} />
         </TouchableHighlight>
       </View>
     );
@@ -70,7 +70,7 @@ export default class CanvasScreenshotScreen extends Component {
 
   switchOrientation() {
     this.setState({
-      orientation: this.state.orientation === 'vertical' ? 'horizontal' : 'vertical',
+      orientation: this.state.orientation === 'vertical' ? 'horizontal' : 'vertical'
     });
   }
 }
