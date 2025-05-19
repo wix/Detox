687fedec231be01623c66f740982ce1acb4fbfc7
Georgy Steshin <georgys@wix.com>
2025-05-19 13:50:36 +0300

    Fixed edge to edge on api 35

--- detox/test/android/app/src/main/java/com/example/MainActivity.kt
+++ detox/test/android/app/src/main/java/com/example/MainActivity.kt
@@ -1,12 +1,55 @@
 package com.example
 
 import android.content.Intent
+import android.os.Build
+import android.os.Bundle
+import android.view.WindowInsets
+import androidx.activity.enableEdgeToEdge
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
 import com.facebook.react.defaults.DefaultReactActivityDelegate
 
 open class MainActivity : ReactActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setupEdgeToEdgeDisplay()
+    }
+
+    /**
+     * Sets up edge-to-edge display for the activity.
+     * Adds padding to the root view to accommodate system bars.
+     */
+    private fun setupEdgeToEdgeDisplay() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            // Enable edge-to-edge for Android 11 (API 30) and above
+            enableEdgeToEdge()
+
+            // Set up window insets controller
+            val windowInsetsController = WindowCompat.getInsetsController(window, window.decorView)
+            windowInsetsController.isAppearanceLightStatusBars = true
+            windowInsetsController.isAppearanceLightNavigationBars = true
+
+            // Handle insets
+            window.decorView.setOnApplyWindowInsetsListener { view, windowInsets ->
+                val insets = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars())
+
+                // Apply padding to the root view
+                view.setPadding(
+                    view.paddingLeft,
+                    insets.top,
+                    view.paddingRight,
+                    insets.bottom
+                )
+
+                WindowInsets.CONSUMED
+            }
+        }
+    }
+
     override fun onNewIntent(intent: Intent) {
         super.onNewIntent(intent)
         setIntent(intent)
@@ -24,4 +67,6 @@ open class MainActivity : ReactActivity() {
      */
     override fun createReactActivityDelegate(): ReactActivityDelegate =
         DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
+
+
 }
