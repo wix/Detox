228629e033318c554bf322a138c922801db5d564
Georgy Steshin <georgys@wix.com>
2025-06-25 14:04:54 +0300

    Updated jobs to run 77 old arch

--- .buildkite/pipeline_common.sh
+++ .buildkite/pipeline_common.sh
@@ -5,7 +5,7 @@ echo "steps:"
 cat .buildkite/jobs/pipeline.android_demo_app_rn_78.yml
 cat .buildkite/jobs/pipeline.android_rn_73.yml
 cat .buildkite/jobs/pipeline.android_rn_77.yml
-cat .buildkite/jobs/pipeline.android_rn_78_old_arch.yml
+cat .buildkite/jobs/pipeline.android_rn_77_old_arch.yml
 cat .buildkite/jobs/pipeline.android_rn_78.yml
 cat .buildkite/jobs/pipeline.ios_demo_app_rn_77.yml
 cat .buildkite/jobs/pipeline.ios_demo_app_rn_78.yml
