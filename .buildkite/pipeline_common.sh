56b807e2f1d8f66904cad1ad60e9638ae68daec8
Georgy Steshin <gosha212@gmail.com>
2025-06-30 12:03:34 +0300

    Upgrade to RN 0.78 (#4779)

--- .buildkite/pipeline_common.sh
+++ .buildkite/pipeline_common.sh
@@ -2,14 +2,15 @@
 
 echo "steps:"
 
-cat .buildkite/jobs/pipeline.android_demo_app_rn_77.yml
+cat .buildkite/jobs/pipeline.android_demo_app_rn_78.yml
 cat .buildkite/jobs/pipeline.android_rn_73.yml
-cat .buildkite/jobs/pipeline.android_rn_76.yml
 cat .buildkite/jobs/pipeline.android_rn_77.yml
-cat .buildkite/jobs/pipeline.android_rn_77_old_arch.yml
+cat .buildkite/jobs/pipeline.android_rn_78_old_arch.yml
+cat .buildkite/jobs/pipeline.android_rn_78.yml
 cat .buildkite/jobs/pipeline.ios_demo_app_rn_77.yml
+cat .buildkite/jobs/pipeline.ios_demo_app_rn_78.yml
 cat .buildkite/jobs/pipeline.ios_rn_73.yml
-cat .buildkite/jobs/pipeline.ios_rn_76.yml
 cat .buildkite/jobs/pipeline.ios_rn_77.yml
 cat .buildkite/jobs/pipeline.ios_rn_77_old_arch.yml
+cat .buildkite/jobs/pipeline.ios_rn_78.yml
 cat .buildkite/pipeline.post_processing.yml
