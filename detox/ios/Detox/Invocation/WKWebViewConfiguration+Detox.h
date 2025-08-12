3041c1513ce6a208f816440bef426f1270b5551e
Mark de Vocht <markd@wix.com>
2025-08-12 22:38:02 +0300

    Detox iOS18 compatibility - #4749

--- detox/ios/Detox/Invocation/WKWebViewConfiguration+Detox.h
+++ detox/ios/Detox/Invocation/WKWebViewConfiguration+Detox.h
@@ -9,6 +9,14 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface WKWebViewConfiguration (Detox)
 
+- (BOOL)shouldDisableWebKitSecurity;
+
+@end
+
+@interface WKWebView (DetoxSecurity)
+
+- (instancetype)dtx_initWithFrame:(CGRect)frame configuration:(WKWebViewConfiguration *)configuration;
+
 @end
 
 NS_ASSUME_NONNULL_END
