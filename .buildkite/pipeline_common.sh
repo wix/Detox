97f14fce61699f52350334521c9599dac820bf66
d4vidi <amit.d4vidi@gmail.com>
2025-09-02 15:58:29 +0300

    CI: Use Genymotion ARM64 only for latest RN build job (#4825)

--- .buildkite/pipeline_common.sh
+++ .buildkite/pipeline_common.sh
@@ -5,7 +5,6 @@ echo "steps:"
 cat .buildkite/jobs/pipeline.android_demo_app_rn_79.yml
 cat .buildkite/jobs/pipeline.android_rn_73.yml
 cat .buildkite/jobs/pipeline.android_rn_77.yml
-cat .buildkite/jobs/pipeline.android_rn_78_old_arch.yml
 cat .buildkite/jobs/pipeline.android_rn_78.yml
 cat .buildkite/jobs/pipeline.android_rn_79.yml
 cat .buildkite/jobs/pipeline.ios_demo_app_rn_79.yml
