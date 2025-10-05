aac2dcc555f0bc8c8808adf7f84b451e0db152b7
Georgy Steshin <georgys@wix.com>
2025-10-05 13:31:06 +0300

    Removed old build kite configuraitons

--- .buildkite/pipeline_common.sh
+++ .buildkite/pipeline_common.sh
@@ -2,17 +2,10 @@
 
 echo "steps:"
 
-cat .buildkite/jobs/pipeline.android_demo_app_rn_79.yml
 cat .buildkite/jobs/pipeline.android_demo_app_rn_80.yml
 cat .buildkite/jobs/pipeline.android_rn_77.yml
-cat .buildkite/jobs/pipeline.android_rn_78.yml
-cat .buildkite/jobs/pipeline.android_rn_79.yml
 cat .buildkite/jobs/pipeline.android_rn_80.yml
-cat .buildkite/jobs/pipeline.ios_demo_app_rn_79.yml
 cat .buildkite/jobs/pipeline.ios_demo_app_rn_80.yml
 cat .buildkite/jobs/pipeline.ios_rn_77.yml
-cat .buildkite/jobs/pipeline.ios_rn_77_old_arch.yml
-cat .buildkite/jobs/pipeline.ios_rn_78.yml
-cat .buildkite/jobs/pipeline.ios_rn_79.yml
 cat .buildkite/jobs/pipeline.ios_rn_80.yml
 cat .buildkite/pipeline.post_processing.yml
