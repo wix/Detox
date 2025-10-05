9e1b0ad49d37723500d4b19c7bacb1b6b970650b
Georgy Steshin <georgys@wix.com>
2025-10-05 11:41:16 +0300

    Removed rn 73 configuration

--- .buildkite/pipeline_common.sh
+++ .buildkite/pipeline_common.sh
@@ -4,14 +4,12 @@ echo "steps:"
 
 cat .buildkite/jobs/pipeline.android_demo_app_rn_79.yml
 cat .buildkite/jobs/pipeline.android_demo_app_rn_80.yml
-cat .buildkite/jobs/pipeline.android_rn_73.yml
 cat .buildkite/jobs/pipeline.android_rn_77.yml
 cat .buildkite/jobs/pipeline.android_rn_78.yml
 cat .buildkite/jobs/pipeline.android_rn_79.yml
 cat .buildkite/jobs/pipeline.android_rn_80.yml
 cat .buildkite/jobs/pipeline.ios_demo_app_rn_79.yml
 cat .buildkite/jobs/pipeline.ios_demo_app_rn_80.yml
-cat .buildkite/jobs/pipeline.ios_rn_73.yml
 cat .buildkite/jobs/pipeline.ios_rn_77.yml
 cat .buildkite/jobs/pipeline.ios_rn_77_old_arch.yml
 cat .buildkite/jobs/pipeline.ios_rn_78.yml
