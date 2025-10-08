d3e9ce553769c5e48bed397e268367c148e3a833
Georgy Steshin <georgys@wix.com>
2025-10-08 21:07:25 +0300

    Upgraded to RN 81

--- examples/demo-react-native/android/app/src/main/java/com/example/MainApplication.kt
+++ examples/demo-react-native/android/app/src/main/java/com/example/MainApplication.kt
@@ -11,6 +11,7 @@ import com.facebook.react.defaults.DefaultReactNativeHost
 import com.facebook.react.soloader.OpenSourceMergedSoMapping
 import com.facebook.soloader.SoLoader
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.load
+import com.facebook.react.ReactNativeApplicationEntryPoint.loadReactNative
 
 
 class MainApplication : Application(), ReactApplication {
@@ -40,10 +41,6 @@ class MainApplication : Application(), ReactApplication {
 
     override fun onCreate() {
         super.onCreate()
-        SoLoader.init(this, OpenSourceMergedSoMapping)
-        if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
-            // If you opted-in for the New Architecture, we load the native entry point for this app.
-            load()
-        }
+        loadReactNative(this)
     }
 }
