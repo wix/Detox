7ac62c4d766490c4a540a84f7c9b291c8c9f82d8
Georgy Steshin <gosha212@gmail.com>
2025-10-16 18:16:45 +0300

    Update .buildkite/pipeline_common.sh
    Co-authored-by: d4vidi <amit.d4vidi@gmail.com>

--- .buildkite/pipeline_common.sh
+++ .buildkite/pipeline_common.sh
@@ -2,7 +2,6 @@
 
 echo "steps:"
 
-cat .buildkite/jobs/pipeline.android_demo_app_rn_80.yml
 cat .buildkite/jobs/pipeline.android_demo_app_rn_81.yml
 cat .buildkite/jobs/pipeline.android_rn_77.yml
 cat .buildkite/jobs/pipeline.android_rn_80.yml
