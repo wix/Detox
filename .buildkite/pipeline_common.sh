b3f7a134eddd5977eefa4d2da3b8334ce16cdd55
Georgy Steshin <georgys@wix.com>
2025-10-16 10:51:47 +0300

    Implement RN 82

--- .buildkite/pipeline_common.sh
+++ .buildkite/pipeline_common.sh
@@ -2,12 +2,12 @@
 
 echo "steps:"
 
-cat .buildkite/jobs/pipeline.android_demo_app_rn_81.yml
+cat .buildkite/jobs/pipeline.android_demo_app_rn_82.yml
 cat .buildkite/jobs/pipeline.android_rn_77.yml
-cat .buildkite/jobs/pipeline.android_rn_80.yml
 cat .buildkite/jobs/pipeline.android_rn_81.yml
-cat .buildkite/jobs/pipeline.ios_demo_app_rn_81.yml
+cat .buildkite/jobs/pipeline.android_rn_82.yml
+cat .buildkite/jobs/pipeline.ios_demo_app_rn_82.yml
 cat .buildkite/jobs/pipeline.ios_rn_77.yml
-cat .buildkite/jobs/pipeline.ios_rn_80.yml
 cat .buildkite/jobs/pipeline.ios_rn_81.yml
+cat .buildkite/jobs/pipeline.ios_rn_82.yml
 cat .buildkite/pipeline.post_processing.yml
--- detox/test/android/app/src/main/java/com/example/MainApplication.kt
+++ detox/test/android/app/src/main/java/com/example/MainApplication.kt
@@ -3,26 +3,31 @@ package com.example
 import android.app.Application
 import android.webkit.WebView
 import com.example.utils.DetoxSoLoader
+import com.facebook.react.PackageList
 import com.facebook.react.ReactApplication
 import com.facebook.react.ReactHost
+import com.facebook.react.ReactNativeApplicationEntryPoint.loadReactNative
 import com.facebook.react.ReactNativeHost
-import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.load
 import com.facebook.react.defaults.DefaultReactHost.getDefaultReactHost
 
 class MainApplication : Application(), ReactApplication {
-    override val reactNativeHost: ReactNativeHost = DetoxRNHost(this)
+
+    @Deprecated("The old architecture is no longer supported. Keep it for BW compatibility.")
+    override val reactNativeHost: ReactNativeHost
+        get() = throw IllegalStateException(
+            "The old architecture is no longer supported"
+        )
 
     override val reactHost: ReactHost
-        get() = getDefaultReactHost(applicationContext, reactNativeHost)
+        get() = getDefaultReactHost(context = applicationContext, packageList = PackageList(this).packages.apply {
+            add(NativeModulePackage())
+        })
 
     override fun onCreate() {
         super.onCreate()
         DetoxSoLoader.init(this)
 
         WebView.setWebContentsDebuggingEnabled(true)
-        if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
-            // If you opted-in for the New Architecture, we load the native entry point for this app.
-            load()
-        }
+        loadReactNative(this)
     }
 }
