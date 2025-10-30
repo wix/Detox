33bd26210a622cb0f39ffdb66a04951e0e85d818
Georgy Steshin <georgys@wix.com>
2025-10-30 16:26:26 +0200

    Fixed animations idling issue on android

--- detox/android/detox/src/full/java/com/wix/detox/reactnative/idlingresources/animations/AnimatedModuleIdlingResource.kt
+++ detox/android/detox/src/full/java/com/wix/detox/reactnative/idlingresources/animations/AnimatedModuleIdlingResource.kt
@@ -9,11 +9,7 @@ import com.facebook.react.animated.NativeAnimatedNodesManager
 import com.facebook.react.bridge.ReactContext
 import com.wix.detox.common.DetoxErrors
 import com.wix.detox.common.DetoxLog.Companion.LOG_TAG
-import com.wix.detox.common.KotlinReflectUtils
-import com.wix.detox.reactnative.ReactNativeInfo
 import com.wix.detox.reactnative.idlingresources.DetoxIdlingResource
-import kotlin.reflect.full.memberProperties
-import kotlin.reflect.jvm.isAccessible
 
 class AnimatedModuleIdlingResource(private val reactContext: ReactContext) : DetoxIdlingResource(),
     Choreographer.FrameCallback {
@@ -31,8 +27,7 @@ class AnimatedModuleIdlingResource(private val reactContext: ReactContext) : Det
             return false
         }
 
-        if (animatedModule.hasQueuedAnimations() ||
-            animatedModule.hasActiveAnimations()) {
+        if (animatedModule.hasActiveAnimations()) {
             Choreographer.getInstance().postFrameCallback(this)
             return false
         }
@@ -56,7 +51,7 @@ class AnimatedModuleIdlingResource(private val reactContext: ReactContext) : Det
     }
 
     private fun getAnimatedModule(): AnimatedModuleFacade? =
-         if (animatedModule != null) {
+        if (animatedModule != null) {
             animatedModule
         } else {
             reactContext.getNativeModule(NativeAnimatedModule::class.java)?.let {
@@ -68,50 +63,11 @@ class AnimatedModuleIdlingResource(private val reactContext: ReactContext) : Det
 }
 
 private class AnimatedModuleFacade(private val animatedModule: NativeAnimatedModule) {
-    private val operationsQueue: OperationsQueueReflected
-    private val preOperationsQueue: OperationsQueueReflected
-    private val nodesManager: NativeAnimatedNodesManager
-
-    init {
-        val operationsQueueName = if (ReactNativeInfo.rnVersion().minor > 79) "operations" else "mOperations"
-        val preOperationsQueueName = if (ReactNativeInfo.rnVersion().minor > 79) "preOperations" else "mPreOperations"
-
-
-        operationsQueue = (NativeAnimatedModule::class.memberProperties.find { it.name == operationsQueueName } ?:
-            throw DetoxErrors.DetoxIllegalStateException("$operationsQueueName property cannot be accessed")).let {
-
-                it.isAccessible = true
-                OperationsQueueReflected(it.get(animatedModule) as Any)
-            }
-
-        preOperationsQueue = (NativeAnimatedModule::class.memberProperties.find { it.name == preOperationsQueueName } ?:
-            throw DetoxErrors.DetoxIllegalStateException("$preOperationsQueueName property cannot be accessed")).let {
-
-                it.isAccessible = true
-                OperationsQueueReflected(it.get(animatedModule) as Any)
-            }
-
-        nodesManager = animatedModule.nodesManager ?:
-            throw DetoxErrors.DetoxIllegalStateException("AnimatedModule exists but nodesManager is null")
-    }
-
-    @UiThread
-    fun hasQueuedAnimations(): Boolean =
-        !preOperationsQueue.isEmpty() ||
-        !operationsQueue.isEmpty()
+    private val nodesManager: NativeAnimatedNodesManager = animatedModule.nodesManager
+        ?: throw DetoxErrors.DetoxIllegalStateException("AnimatedModule exists but nodesManager is null")
 
     @UiThread
     fun hasActiveAnimations(): Boolean {
         return nodesManager.hasActiveAnimations()
-    }
-}
-
-class OperationsQueueReflected(private val operationsQueue: Any) {
-    fun isEmpty(): Boolean {
-        KotlinReflectUtils.getPropertyValueWithCustomGetter<Boolean>(operationsQueue, "isEmpty")?.let {
-            return it
-        }
-
-        throw DetoxErrors.DetoxIllegalStateException("isEmpty method/property cannot be reached")
     }
 }
