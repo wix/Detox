6584a769886161b8fcf221e1a29a91e24e30633f
d4vidi <amit.d4vidi@gmail.com>
2025-11-11 11:43:16 +0200

    Extract system UI cfg onto a class

--- detox/src/devices/allocation/drivers/android/AndroidAllocDriver.js
+++ detox/src/devices/allocation/drivers/android/AndroidAllocDriver.js
@@ -3,47 +3,10 @@
  * @typedef {import('../../../common/drivers/DeviceCookie').DeviceCookie} DeviceCookie
  */
 
-const _ = require('lodash');
-
 const log = require('../../../../utils/logger').child({ __filename });
 const sleep = require('../../../../utils/sleep');
 
-/**
- * @type Detox.DetoxSystemUIConfig
- */
-const minimalSystemUIConfig = {
-  keyboard: 'hide',
-  touches: 'show',
-  navigationMode: '3-button',
-  statusBar: {
-    notifications: 'hide',
-    wifiSignal: 'strong',
-    cellSignal: 'none',
-    batteryLevel: 'full',
-    charging: true,
-    clock: '1337',
-  },
-};
-
-const batteryPercent = {
-  full: 100,
-  half: 50,
-  low: 20,
-};
-
-const navigationModesDictionary = {
-  '3-button': 'com.android.internal.systemui.navbar.threebutton',
-//      '2-button': 'com.android.internal.systemui.navbar.twobutton',
-  'gesture': 'com.android.internal.systemui.navbar.gestural',
-};
-
-/**
- *
- * @param {any | undefined | null} value
- * @param {(value: any) => any | undefined} fn
- * @returns {any | undefined}
- */
-const nullishOrMap = (value, fn) => (value === undefined || value === null) ? undefined : fn(value);
+const SystemUIDemoMode = require('./SystemUICfgHelper');
 
 /**
  * @abstract {AllocationDriverBase}
@@ -60,150 +23,43 @@ class AndroidAllocDriver {
   }
 
   /**
-   * @param {DeviceCookie} deviceCookie
+   * @param {DeviceCookie & { adbName: string }} deviceCookie
    * @param {{ deviceConfig: Detox.DetoxSharedAndroidDriverConfig }} configs
    * @returns {Promise<DeviceCookie>}
    */
   async postAllocate(deviceCookie, configs) {
     const { deviceConfig } = configs;
 
-    if (!deviceConfig.systemUI) {
-      return;
+    if (deviceConfig.systemUI) {
+      await this._setupSystemUI(deviceCookie, deviceConfig);
     }
-
-    /** @type Detox.DetoxSystemUIConfig */
-    let systemUIConfig;
-    if (deviceConfig.systemUI === 'minimal') {
-      systemUIConfig = minimalSystemUIConfig;
-    } else if (_.isObject(deviceConfig.systemUI) && deviceConfig.systemUI.extends === 'minimal') {
-      systemUIConfig = _.merge({}, minimalSystemUIConfig, deviceConfig.systemUI);
-    } else {
-      systemUIConfig = deviceConfig.systemUI;
-    }
-    await this._initDemoMode(deviceCookie, systemUIConfig);
-
     return deviceCookie;
   }
 
   /**
-   * @param deviceCookie
-   * @param {Detox.DetoxSystemUIConfig} systemUIConfig
+   * @param {DeviceCookie & { adbName: string }} deviceCookie
+   * @private
    */
-  async _initDemoMode(deviceCookie, systemUIConfig) {
+  async _setupSystemUI(deviceCookie, deviceConfig) {
     const { adbName } = deviceCookie;
-    const adb = {
+    const adbWrapper = {
       shell: async (cmd) => {
         await this._adb.shell(deviceCookie.adbName, cmd);
         await sleep(200);
       },
     };
 
+    const systemUIDemoMode = new SystemUIDemoMode({ adb: adbWrapper });
+    const systemUIConfig = systemUIDemoMode.resolveConfig(deviceConfig.systemUI);
+
     log.debug(`Running keyboard behavior setup for ${adbName}`);
-    await this._setupKeyboardBehavior(adb, systemUIConfig);
+    await systemUIDemoMode.setupKeyboardBehavior(systemUIConfig);
 
     log.debug(`Running system UI setup for ${adbName}`);
-    await this._setupPointerIndicators(adb, systemUIConfig);
-    await this._setupNavigationMode(adb, systemUIConfig);
-    await this._setupStatusBar(adb, systemUIConfig);
+    await systemUIDemoMode.setupPointerIndicators(systemUIConfig);
+    await systemUIDemoMode.setupNavigationMode(systemUIConfig);
+    await systemUIDemoMode.setupStatusBar(systemUIConfig);
     log.debug(`Finished system UI setup for ${adbName}`);
-  }
-
-  /**
-   * @param {object} adb
-   * @param {Detox.DetoxSystemUIConfig} systemUIConfig
-   */
-  async _setupKeyboardBehavior(adb, systemUIConfig) {
-    const showKbd = nullishOrMap(systemUIConfig.keyboard, (keyboard) => Number(keyboard === 'show'));
-
-    if (showKbd !== undefined) {
-      await adb.shell(`settings put Secure show_ime_with_hard_keyboard ${showKbd}`);
-    }
-  }
-
-  /**
-   * @param {object} adb
-   * @param {Detox.DetoxSystemUIConfig} systemUIConfig
-   */
-  async _setupPointerIndicators(adb, systemUIConfig) {
-    const showTouches = nullishOrMap(systemUIConfig.touches, (touches) => Number(touches === 'show'));
-    if (showTouches !== undefined) {
-      await adb.shell(`settings put system show_touches ${showTouches}`);
-    }
-
-    const showPointerLocationBar = nullishOrMap(systemUIConfig.pointerLocationBar, (pointerLocationBar) => Number(pointerLocationBar === 'show'));
-    if (showPointerLocationBar !== undefined) {
-      await adb.shell(`settings put system pointer_location 1`);
-    }
-  }
-
-  /**
-   * @param {object} adb
-   * @param {Detox.DetoxSystemUIConfig} systemUIConfig
-   */
-  async _setupNavigationMode(adb, systemUIConfig) {
-    const navigationMode = nullishOrMap(systemUIConfig.navigationMode, (navigationMode) => navigationModesDictionary[navigationMode]);
-    if (navigationMode !== undefined) {
-      await adb.shell(`cmd overlay enable ${navigationMode}`);
-    }
-  }
-
-  /**
-   * Ref: https://android.googlesource.com/platform/frameworks/base/+/master/packages/SystemUI/docs/demo_mode.md
-   *
-   * @param {object} adb
-   * @param {Detox.DetoxSystemUIConfig} systemUIConfig
-   */
-  async _setupStatusBar(adb, systemUIConfig) {
-    const { statusBar: statusBarConfig } = systemUIConfig;
-    if (_.isUndefined(statusBarConfig)) {
-      return;
-    }
-
-    // Enable, then get out (= reset status-bar) and back into demo mode
-    await adb.shell('settings put global sysui_demo_allowed 1');
-    await adb.shell('am broadcast -a com.android.systemui.demo -e command exit');
-    await adb.shell('am broadcast -a com.android.systemui.demo -e command enter');
-
-    // Force status bar content
-    const notificationsVisible = nullishOrMap(statusBarConfig.notifications, (notifications) => Number(notifications === 'show'));
-    if (notificationsVisible !== undefined) {
-      await adb.shell(`am broadcast -a com.android.systemui.demo -e command notifications -e visible ${notificationsVisible}`);
-    }
-
-    const wifiSignal = nullishOrMap(statusBarConfig.wifiSignal, (wifiSignal) => wifiSignal);
-    if (wifiSignal !== undefined) {
-      await adb.shell('am broadcast -a com.android.systemui.demo -e command network -e wifi hide');
-      if (wifiSignal !== 'none') {
-        const wifiLevel = wifiSignal === 'strong' ? 4 : 2;
-        await adb.shell(`am broadcast -a com.android.systemui.demo -e command network -e wifi show -e level ${wifiLevel} -e fully true`);
-      }
-    }
-
-    const cellSignal = nullishOrMap(statusBarConfig.cellSignal, (cellSignal) => cellSignal);
-    if (cellSignal !== undefined) {
-      await adb.shell('am broadcast -a com.android.systemui.demo -e command network -e mobile hide');
-      if (cellSignal !== 'none') {
-        const cellLevel = cellSignal === 'strong' ? 4 : 2;
-        await adb.shell(`am broadcast -a com.android.systemui.demo -e command network -e mobile show -e level ${cellLevel} -e fully true -e datatype none`);
-      }
-    }
-
-    const clock = nullishOrMap(statusBarConfig.clock, (clock) => clock);
-    if (clock !== undefined) {
-      await adb.shell(`am broadcast -a com.android.systemui.demo -e command clock -e hhmm ${clock}`);
-    }
-
-    // Best to keep this last due to a "charging" indicator animation which can
-    // break UI changes made by consequent commands
-    const batteryLevel = nullishOrMap(statusBarConfig.batteryLevel, (level) => batteryPercent[level]);
-    const charging = nullishOrMap(statusBarConfig.charging, (isCharging) => String(isCharging));
-    if (batteryLevel !== undefined || charging !== undefined) {
-      const command = 'am broadcast -a com.android.systemui.demo -e command battery' +
-        (batteryLevel !== undefined ? ` -e level ${batteryLevel}` : '') +
-        (charging !== undefined ? ` -e plugged ${charging}` : '');
-      await adb.shell(command);
-      await sleep(1500); // Wait for the charging animation to finish
-    }
   }
 }
 
---
+++ detox/src/devices/allocation/drivers/android/SystemUICfgHelper.js
@@ -0,0 +1,172 @@
+/**
+ * @typedef {import('../../../common/drivers/DeviceCookie').DeviceCookie} DeviceCookie
+ */
+
+const _ = require('lodash');
+
+const sleep = require('../../../../utils/sleep');
+
+/**
+ * @type Detox.DetoxSystemUIConfig
+ */
+const minimalConfigPreset = {
+  keyboard: 'hide',
+  touches: 'show',
+  navigationMode: '3-button',
+  statusBar: {
+    notifications: 'hide',
+    wifiSignal: 'strong',
+    cellSignal: 'none',
+    batteryLevel: 'full',
+    charging: true,
+    clock: '1337',
+  },
+};
+
+const batteryPrecents = {
+  full: 100,
+  half: 50,
+  low: 20,
+};
+
+const navigationModes = {
+//  '2-button': 'com.android.internal.systemui.navbar.twobutton',
+  '3-button': 'com.android.internal.systemui.navbar.threebutton',
+  'gesture': 'com.android.internal.systemui.navbar.gestural',
+};
+
+/**
+ * Maps a nullish value (null or undefined) to undefined, otherwise applies the function.
+ *
+ * @param {any | undefined | null} value
+ * @param {(value: any) => any | undefined} fn
+ * @returns {any | undefined}
+ */
+const nullishOrMap = (value, fn) => (value === undefined || value === null) ? undefined : fn(value);
+
+/**
+ * Helper class for initializing Android System UI demo mode.
+ */
+class SystemUICfgHelper {
+  /**
+   * @param {object} options
+   * @param {object} options.adb An ADB shell wrapper with a shell method
+   */
+  constructor({ adb }) {
+    this._adb = adb;
+  }
+
+  /**
+   * Resolves the system UI configuration, handling presets and extends.
+   *
+   * @param {Detox.DetoxSystemUI} systemUI
+   * @returns {Detox.DetoxSystemUIConfig}
+   */
+  resolveConfig(systemUI) {
+    if (systemUI === 'minimal') {
+      return minimalConfigPreset;
+    }
+
+    if (_.isObject(systemUI) && systemUI.extends === 'minimal') {
+      return _.merge({}, minimalConfigPreset, systemUI);
+    }
+
+    return systemUI;
+  }
+
+  /**
+   * @param {Detox.DetoxSystemUIConfig} systemUIConfig
+   */
+  async setupKeyboardBehavior(systemUIConfig) {
+    const showKbd = nullishOrMap(systemUIConfig.keyboard, (keyboard) => Number(keyboard === 'show'));
+
+    if (showKbd !== undefined) {
+      await this._adb.shell(`settings put Secure show_ime_with_hard_keyboard ${showKbd}`);
+    }
+  }
+
+  /**
+   * @param {Detox.DetoxSystemUIConfig} systemUIConfig
+   */
+  async setupPointerIndicators(systemUIConfig) {
+    const showTouches = nullishOrMap(systemUIConfig.touches, (touches) => Number(touches === 'show'));
+    if (showTouches !== undefined) {
+      await this._adb.shell(`settings put system show_touches ${showTouches}`);
+    }
+
+    const showPointerLocationBar = nullishOrMap(systemUIConfig.pointerLocationBar, (pointerLocationBar) => Number(pointerLocationBar === 'show'));
+    if (showPointerLocationBar !== undefined) {
+      await this._adb.shell(`settings put system pointer_location 1`);
+    }
+  }
+
+  /**
+   * @param {Detox.DetoxSystemUIConfig} systemUIConfig
+   */
+  async setupNavigationMode(systemUIConfig) {
+    const navigationMode = nullishOrMap(systemUIConfig.navigationMode, (navigationMode) => navigationModes[navigationMode]);
+    if (navigationMode !== undefined) {
+      await this._adb.shell(`cmd overlay enable ${navigationMode}`);
+    }
+  }
+
+  /**
+   * Ref: https://android.googlesource.com/platform/frameworks/base/+/master/packages/SystemUI/docs/demo_mode.md
+   *
+   * @param {Detox.DetoxSystemUIConfig} systemUIConfig
+   */
+  async setupStatusBar(systemUIConfig) {
+    const { statusBar: statusBarConfig } = systemUIConfig;
+    if (_.isUndefined(statusBarConfig)) {
+      return;
+    }
+
+    // Enable, then get out (= reset status-bar) and back into demo mode
+    await this._adb.shell('settings put global sysui_demo_allowed 1');
+    await this._adb.shell('am broadcast -a com.android.systemui.demo -e command exit');
+    await this._adb.shell('am broadcast -a com.android.systemui.demo -e command enter');
+
+    // Force status bar content
+    const notificationsVisible = nullishOrMap(statusBarConfig.notifications, (notifications) => Number(notifications === 'show'));
+    if (notificationsVisible !== undefined) {
+      await this._adb.shell(`am broadcast -a com.android.systemui.demo -e command notifications -e visible ${notificationsVisible}`);
+    }
+
+    const wifiSignal = nullishOrMap(statusBarConfig.wifiSignal, (wifiSignal) => wifiSignal);
+    if (wifiSignal !== undefined) {
+      await this._adb.shell('am broadcast -a com.android.systemui.demo -e command network -e wifi hide');
+      if (wifiSignal !== 'none') {
+        const wifiLevel = wifiSignal === 'strong' ? 4 : 2;
+        await this._adb.shell(`am broadcast -a com.android.systemui.demo -e command network -e wifi show -e level ${wifiLevel} -e fully true`);
+      }
+    }
+
+    const cellSignal = nullishOrMap(statusBarConfig.cellSignal, (cellSignal) => cellSignal);
+    if (cellSignal !== undefined) {
+      await this._adb.shell('am broadcast -a com.android.systemui.demo -e command network -e mobile hide');
+      if (cellSignal !== 'none') {
+        const cellLevel = cellSignal === 'strong' ? 4 : 2;
+        await this._adb.shell(`am broadcast -a com.android.systemui.demo -e command network -e mobile show -e level ${cellLevel} -e fully true -e datatype none`);
+      }
+    }
+
+    const clock = nullishOrMap(statusBarConfig.clock, (clock) => clock);
+    if (clock !== undefined) {
+      await this._adb.shell(`am broadcast -a com.android.systemui.demo -e command clock -e hhmm ${clock}`);
+    }
+
+    // Best to keep this last due to a "charging" indicator animation which can
+    // break UI changes made by consequent commands
+    const batteryLevel = nullishOrMap(statusBarConfig.batteryLevel, (level) => batteryPrecents[level]);
+    const charging = nullishOrMap(statusBarConfig.charging, (isCharging) => String(isCharging));
+    if (batteryLevel !== undefined || charging !== undefined) {
+      const command = 'am broadcast -a com.android.systemui.demo -e command battery' +
+        (batteryLevel !== undefined ? ` -e level ${batteryLevel}` : '') +
+        (charging !== undefined ? ` -e plugged ${charging}` : '');
+      await this._adb.shell(command);
+      await sleep(1500); // Wait for the charging animation to finish
+    }
+  }
+}
+
+module.exports = SystemUICfgHelper;
