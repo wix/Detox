93543bb5c50779503dac79bdb9406dee5f806a75
Mark de Vocht <markd@wix.com>
2025-12-29 14:30:07 +0200

    cleanup

--- detox/test/e2e/utils/deviceInfo.js
+++ detox/test/e2e/utils/deviceInfo.js
@@ -14,16 +14,12 @@ async function getIOSMajorVersion() {
   }
 
   try {
-    // Get device info from Detox internals
     const { device: deviceHandle } = require('detox');
-    
-    // Check if we're on iOS first
     if (deviceHandle && deviceHandle.getPlatform && deviceHandle.getPlatform() !== 'ios') {
       cachedIOSVersion = null;
       return null;
     }
-    
-    // Try to get the device cookie which contains OS version info
+
     if (deviceHandle && deviceHandle._device && deviceHandle._device.deviceConfig) {
       const osVersion = deviceHandle._device.deviceConfig.device?.os;
       if (osVersion) {
@@ -33,11 +29,9 @@ async function getIOSMajorVersion() {
       }
     }
   } catch (e) {
-    // Fallback: If we can't determine version, assume iOS 18+ behavior
     console.warn('Could not determine iOS version, assuming iOS 18+');
   }
 
-  // Default to 18 (safer assumption for modern iOS)
   cachedIOSVersion = 18;
   return cachedIOSVersion;
 }
@@ -48,12 +42,9 @@ async function getIOSMajorVersion() {
  */
 async function isIOS18OrHigher() {
   const majorVersion = await getIOSMajorVersion();
-  
-  // If not iOS (null), return false
   if (majorVersion === null) {
     return false;
   }
-  
   return majorVersion >= 18;
 }
 
