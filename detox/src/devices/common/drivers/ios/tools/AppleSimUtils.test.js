c41a7f390e717eb1d791a4647258db1e2bcbf319
Yaroslav Serhieiev <yaroslavs@wix.com>
2026-03-21 12:33:37 +0200

    feat(ios): ignore --arch flag on simulators running iOS < 26
    The --arch option for simctl launch is only meaningful on iOS 26+
    Universal runtimes. Silently strip it for older simulators so that
    app configs can set arch broadly without breaking pre-iOS-26 runs.

--- detox/src/devices/common/drivers/ios/tools/AppleSimUtils.test.js
+++ detox/src/devices/common/drivers/ios/tools/AppleSimUtils.test.js
@@ -27,11 +27,25 @@ describe('AppleSimUtils', () => {
       });
     });
 
-    it('should build --arch=x86_64 in simctl command when arch is set', async () => {
+    function givenArchArgumentAccessible(accessible) {
+      jest.spyOn(uut, '_isArchArgumentAccessible').mockResolvedValue(accessible);
+    }
+
+    it('should pass --arch=x86_64 when arch is set and iOS >= 26', async () => {
+      givenArchArgumentAccessible(true);
       await uut.launch(udid, bundleId, {}, '', 'x86_64');
 
       const launchCmd = childProcess.execWithRetriesAndLogs.mock.calls[0][0];
       expect(launchCmd).toContain(`simctl launch --arch=x86_64 ${udid} ${bundleId}`);
+    });
+
+    it('should strip --arch when arch is set but iOS < 26', async () => {
+      givenArchArgumentAccessible(false);
+      await uut.launch(udid, bundleId, {}, '', 'x86_64');
+
+      const launchCmd = childProcess.execWithRetriesAndLogs.mock.calls[0][0];
+      expect(launchCmd).toContain(`simctl launch ${udid} ${bundleId}`);
+      expect(launchCmd).not.toContain('--arch');
     });
 
     it('should omit --arch when arch is undefined', async () => {
@@ -40,6 +54,13 @@ describe('AppleSimUtils', () => {
       const launchCmd = childProcess.execWithRetriesAndLogs.mock.calls[0][0];
       expect(launchCmd).toContain(`simctl launch ${udid} ${bundleId}`);
       expect(launchCmd).not.toContain('--arch');
+    });
+
+    it('should not query device version when arch is undefined', async () => {
+      const spy = jest.spyOn(uut, '_isArchArgumentAccessible');
+      await uut.launch(udid, bundleId, {}, '');
+
+      expect(spy).not.toHaveBeenCalled();
     });
   });
 });
