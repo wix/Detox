cecba9fa345642312857b3432ac600260022a255
Mark de Vocht <markd@wix.com>
2026-03-25 11:13:53 +0200

    yarn change

--- scripts/change_all_react_native_versions.sh
+++ scripts/change_all_react_native_versions.sh
@@ -5,5 +5,5 @@ if [ ! -z ${REACT_NATIVE_VERSION} ]; then
   node scripts/change_react_native_version.js "detox/test" ${REACT_NATIVE_VERSION} "dependencies"
   node scripts/change_react_native_version.js "detox" ${REACT_NATIVE_VERSION} "devDependencies"
 
-  yarn install
+  YARN_ENABLE_IMMUTABLE_INSTALLS=false yarn install
 fi
