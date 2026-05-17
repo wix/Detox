f7beabd75a3828822915b683e0a9ec9b8da56f39
Omri SirComp <omribz156@gmail.com>
2026-05-17 23:39:52 +0300

    fix(sync): accept null one-time event objects

--- detox/src/client/actions/formatters/sync-resources/OneTimeEventsFormatter.js
+++ detox/src/client/actions/formatters/sync-resources/OneTimeEventsFormatter.js
@@ -3,6 +3,6 @@ const { makeResourceTitle } = require('./utils');
 module.exports = function(properties) {
   const objectName = properties.object;
   return makeResourceTitle(
-    `The event "${properties.event}" is taking place${(objectName === undefined) ? `.` : ` with object: "${objectName}".`}`
+    `The event "${properties.event}" is taking place${(objectName == null) ? `.` : ` with object: "${objectName}".`}`
   );
 };
